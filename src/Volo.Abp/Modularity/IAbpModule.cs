0b704b8b3560bb9dd2925abc20596cb71c96a3a0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-12-04 02:07:38 +0300

    Added initial solution.

---
+++ src/Volo.Abp/Modularity/IAbpModule.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Volo.Abp.Modularity
+{
+    public interface IAbpModule
+    {
+        void ConfigureServices(IServiceCollection services);
+    }
+}
