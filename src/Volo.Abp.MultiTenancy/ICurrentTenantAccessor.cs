ad680601eb23a75b254e69e6f4cfefe791585ce8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-12-07 23:26:39 +0300

    Upgraded project.json files and added current tenant accessor.

---
+++ src/Volo.Abp.MultiTenancy/ICurrentTenantAccessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Volo.Abp.MultiTenancy
+{
+    public interface ICurrentTenantAccessor
+    {
+        Guid Id { get; }
+
+        string Name { get; }
+    }
+
+    public class CurrentTenantAccessor : ICurrentTenantAccessor
+    {
+        public Guid Id => GetTenantId();
+
+        public string Name { get; }
+
+        private readonly IEnumerable<ICurrentTenantResolver> _currentTenantResolvers;
+
+        public CurrentTenantAccessor(IEnumerable<ICurrentTenantResolver> currentTenantResolvers)
+        {
+            _currentTenantResolvers = currentTenantResolvers;
+        }
+
+        public virtual Guid GetTenantId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICurrentTenantResolver
+    {
+        void Resolve(ICurrentTenantResolveContext context);
+    }
+
+    public interface ICurrentTenantResolveContext
+    {
+
+    }
+}
---
+++ src/Volo.Abp.MultiTenancy/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Volo.Abp.MultiTenancy")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58fa9f8f-216d-4c93-8929-d40d22b11ca7")]
