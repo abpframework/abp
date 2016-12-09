b9a82ff6a9c69be427911f633bdfe253742940ff
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-12-09 14:20:09 +0300

    Created Volo.Abp.AspNetCore.TestBase package.

---
+++ src/Volo.Abp.AspNetCore.TestBase/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Volo.Abp.AspNetCore.TestBase")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ddec5d74-212f-41bd-974c-4b4e88e574e1")]
