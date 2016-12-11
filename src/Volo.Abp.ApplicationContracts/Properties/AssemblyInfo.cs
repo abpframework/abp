1dbf92a3bcb5a9f40d5e845e64813d0f4e2d066c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-12-11 17:07:48 +0300

    Created Volo.Abp.ApplicationContracts package.

---
+++ src/Volo.Abp.ApplicationContracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Volo.Abp.ApplicationContracts")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69c72c98-b864-43f7-88b8-19536c7c9b43")]
