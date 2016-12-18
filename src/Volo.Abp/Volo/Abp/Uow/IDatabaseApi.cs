50a8b463e90b211999c15189050da5cda99dba52
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-12-19 00:07:47 +0300

    Move IDatabaseApi into it's own file.

---
+++ src/Volo.Abp/Volo/Abp/Uow/IDatabaseApi.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Volo.Abp.Uow
+{
+    public interface IDatabaseApi
+    {
+        Task SaveChangesAsync();
+
+        Task CommitAsync();
+    }
+}
--- src/Volo.Abp/Volo/Abp/Uow/IUnitOfWorkManager.cs
+++ src/Volo.Abp/Volo/Abp/Uow/IUnitOfWorkManager.cs
@@ -1,18 +1,9 @@
-﻿using System.Threading.Tasks;
-
-namespace Volo.Abp.Uow
+﻿namespace Volo.Abp.Uow
 {
     public interface IUnitOfWorkManager
     {
         IUnitOfWork Current { get; }
 
         IUnitOfWork Begin();
-    }
-
-    public interface IDatabaseApi
-    {
-        Task SaveChangesAsync();
-
-        Task CommitAsync();
     }
 }
