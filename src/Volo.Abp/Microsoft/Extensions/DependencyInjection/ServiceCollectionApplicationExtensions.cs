54098a79d0681e1865ec233da3f84706bb6482ad
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-07-26 20:20:46 +0300

    Remove unnecessary overloads.

--- src/Volo.Abp/Microsoft/Extensions/DependencyInjection/ServiceCollectionApplicationExtensions.cs
+++ src/Volo.Abp/Microsoft/Extensions/DependencyInjection/ServiceCollectionApplicationExtensions.cs
@@ -8,15 +8,8 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class ServiceCollectionApplicationExtensions
     {
         public static IAbpApplicationWithExternalServiceProvider AddApplication<TStartupModule>(
-            [NotNull] this IServiceCollection services)
-            where TStartupModule : IAbpModule
-        {
-            return AbpApplicationFactory.Create<TStartupModule>(services);
-        }
-
-        public static IAbpApplicationWithExternalServiceProvider AddApplication<TStartupModule>(
             [NotNull] this IServiceCollection services, 
-            [CanBeNull] Action<AbpApplicationCreationOptions> optionsAction)
+            [CanBeNull] Action<AbpApplicationCreationOptions> optionsAction = null)
             where TStartupModule : IAbpModule
         {
             return AbpApplicationFactory.Create<TStartupModule>(services, optionsAction);
@@ -24,15 +17,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static IAbpApplicationWithExternalServiceProvider AddApplication(
             [NotNull] this IServiceCollection services,
-            [NotNull] Type startupModuleType)
-        {
-            return AbpApplicationFactory.Create(startupModuleType, services);
-        }
-
-        public static IAbpApplicationWithExternalServiceProvider AddApplication(
-            [NotNull] this IServiceCollection services,
             [NotNull] Type startupModuleType,
-            [CanBeNull] Action<AbpApplicationCreationOptions> optionsAction)
+            [CanBeNull] Action<AbpApplicationCreationOptions> optionsAction = null)
         {
             return AbpApplicationFactory.Create(startupModuleType, services, optionsAction);
         }
