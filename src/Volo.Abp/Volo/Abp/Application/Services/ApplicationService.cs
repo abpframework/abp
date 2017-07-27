08c52d55cfea46828112abfa0c412c00d1ba36f8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-07-27 21:49:07 +0300

    Add UnitOfWorkManager to ApplicationService.

--- src/Volo.Abp/Volo/Abp/Application/Services/ApplicationService.cs
+++ src/Volo.Abp/Volo/Abp/Application/Services/ApplicationService.cs
@@ -1,9 +1,14 @@
 using Volo.Abp.ObjectMapping;
+using Volo.Abp.Uow;
 
 namespace Volo.Abp.Application.Services
 {
     public abstract class ApplicationService : IApplicationService
     {
+        public IUnitOfWorkManager UnitOfWorkManager { get; set; }
+        
         public IObjectMapper ObjectMapper { get; set; }
+
+        protected IUnitOfWork CurrentUnitOfWork => UnitOfWorkManager.Current;
     }
 }
