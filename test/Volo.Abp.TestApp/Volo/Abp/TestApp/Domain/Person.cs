073f97ee0ff0c72d94fe2a2f7819cc2cf461654a
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-08-29 14:00:56 +0300

    Created new test projects.

---
+++ test/Volo.Abp.TestApp/Volo/Abp/TestApp/Domain/Person.cs
@@ -0,0 +1,15 @@
+namespace App
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ test/Volo.Abp.TestApp/Volo/Abp/TestApp/Domain/PersonAppService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Volo.Abp.Application.Services;
+
+namespace App
+{
+    public class PersonAppService : ApplicationService
+    {
+        public List<Person> GetAll()
+        {
+            return new List<Person>
+            {
+                new Person("John", 33),
+                new Person("Dougles", 42),
+            };
+        }
+    }
+}
