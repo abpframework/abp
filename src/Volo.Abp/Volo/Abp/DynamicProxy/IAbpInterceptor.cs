02f2c8b42c2732a50672985df4cdd1e4052a1bd4
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-08-29 10:30:18 +0300

    Remove old todo.

--- src/Volo.Abp/Volo/Abp/DynamicProxy/IAbpInterceptor.cs
+++ src/Volo.Abp/Volo/Abp/DynamicProxy/IAbpInterceptor.cs
@@ -2,7 +2,6 @@
 
 namespace Volo.Abp.DynamicProxy
 {
-	//TODO: Create also Intercept() for sync method interception! Create AbpInterceptor for simpler usage!
 	public interface IAbpInterceptor
     {
         void Intercept(IAbpMethodInvocation invocation);
