bd55cc97bc8924d59575ed6d7b898bbaf24671b1
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-09-13 21:32:02 +0300

    Added todo

---
+++ src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/Controllers/ApiDefinitionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.Http.Modeling;
+
+namespace Volo.Abp.AspNetCore.Mvc.Controllers
+{
+    public class ApiDefinitionController : AbpController
+    {
+        private readonly IApiDescriptionModelProvider _modelProvider;
+
+        public ApiDefinitionController(IApiDescriptionModelProvider modelProvider)
+        {
+            _modelProvider = modelProvider;
+        }
+
+        [HttpGet]
+        [Route("api/abp/api-description")]
+        public ApplicationApiDescriptionModel Get()
+        {
+            //TODO: It can not create methods if there are overloads of same action in a controller!
+
+            return _modelProvider.CreateModel();
+        }
+    }
+}
