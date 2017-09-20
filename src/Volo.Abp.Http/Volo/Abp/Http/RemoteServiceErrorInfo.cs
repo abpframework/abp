2b90ba164b2abf8ec733d427e5ffef255f9faee8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-09-20 16:12:28 +0300

    Add Remote service error response DTOs. #90.

---
+++ src/Volo.Abp.Http/Volo/Abp/Http/RemoteServiceErrorInfo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Volo.Abp.Http
+{
+    /// <summary>
+    /// Used to store information about an error.
+    /// </summary>
+    [Serializable]
+    public class RemoteServiceErrorInfo
+    {
+        /// <summary>
+        /// Error code.
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// Error message.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Error details.
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Validation errors if exists.
+        /// </summary>
+        public RemoteServiceValidationErrorInfo[] ValidationErrors { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RemoteServiceErrorInfo"/>.
+        /// </summary>
+        public RemoteServiceErrorInfo()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RemoteServiceErrorInfo"/>.
+        /// </summary>
+        /// <param name="message">Error message</param>
+        public RemoteServiceErrorInfo(string message)
+        {
+            Message = message;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RemoteServiceErrorInfo"/>.
+        /// </summary>
+        /// <param name="code">Error code</param>
+        public RemoteServiceErrorInfo(int code)
+        {
+            Code = code;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RemoteServiceErrorInfo"/>.
+        /// </summary>
+        /// <param name="code">Error code</param>
+        /// <param name="message">Error message</param>
+        public RemoteServiceErrorInfo(int code, string message)
+            : this(message)
+        {
+            Code = code;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RemoteServiceErrorInfo"/>.
+        /// </summary>
+        /// <param name="message">Error message</param>
+        /// <param name="details">Error details</param>
+        public RemoteServiceErrorInfo(string message, string details)
+            : this(message)
+        {
+            Details = details;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RemoteServiceErrorInfo"/>.
+        /// </summary>
+        /// <param name="code">Error code</param>
+        /// <param name="message">Error message</param>
+        /// <param name="details">Error details</param>
+        public RemoteServiceErrorInfo(int code, string message, string details)
+            : this(message, details)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ src/Volo.Abp.Http/Volo/Abp/Http/RemoteServiceErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Volo.Abp.Http
+{
+    public abstract class RemoteServiceErrorResponse
+    {
+        public RemoteServiceErrorInfo Error { get; set; }
+
+        /// <summary>
+        /// A special signature of ABP.
+        /// </summary>
+        public bool __abp { get; } = true;
+    }
+}
