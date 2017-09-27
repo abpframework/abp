383a71f6f239a19a80d0f019fb4ecc3dd5e77cff
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-09-27 13:43:04 +0300

    Added ps script to create nuget packages.

---
+++ nupkg/pack.ps1
@@ -0,0 +1,32 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$slnPath = Join-Path $packFolder "../"
+$srcPath = Join-Path $slnPath "src"
+
+# List of projects
+$projects = (
+    "Volo.Abp"
+)
+
+# Rebuild solution
+Set-Location $slnPath
+& dotnet restore
+
+# Copy all nuget packages to the pack folder
+foreach($project in $projects) {
+    
+    $projectFolder = Join-Path $srcPath $project
+
+    # Create nuget pack
+    Set-Location $projectFolder
+    Remove-Item -Recurse (Join-Path $projectFolder "bin/Release")
+    & dotnet msbuild /t:pack /p:Configuration=Release /p:SourceLinkCreate=true
+
+    # Copy nuget package
+    $projectPackPath = Join-Path $projectFolder ("/bin/Release/" + $project + ".*.nupkg")
+    Move-Item $projectPackPath $packFolder
+
+}
+
+# Go back to the pack folder
+Set-Location $packFolder
