beb9abb61816f3fb35da000e7d67e9a303a7061f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-10-04 11:24:45 +0300

    Fix FixtureController

--- src/Volo.Abp.Identity.HttpApi/Volo/Abp/Identity/FixtureController.cs
+++ src/Volo.Abp.Identity.HttpApi/Volo/Abp/Identity/FixtureController.cs
@@ -4,10 +4,10 @@ using Volo.Abp.AspNetCore.Mvc;
 
 namespace Volo.Abp.Identity
 {
-    //TODO: This is just a test controller and will be removed lster
-    [Route("api/identity/fixture")]
-    [ApiVersion("2.0", Deprecated = true)]
+    //TODO: This is just a test controller and will be removed later
     [ApiVersion("3.0")]
+    [ApiVersion("2.0", Deprecated = true)]
+    [Route("api/v{api-version:apiVersion}/identity/fixture")]
     public class FixtureController : AbpController, IRemoteService
     {
         [HttpGet]
@@ -16,11 +16,11 @@ namespace Volo.Abp.Identity
             return 42;
         }
 
-        [HttpGet, MapToApiVersion("3.0")]
-        public int Get3()
-        {
-            return 42;
-        }
+        //[HttpGet, MapToApiVersion("3.0")]
+        //public int Get3()
+        //{
+        //    return 42;
+        //}
 
         [HttpPost]
         public int Post()
