1be4e003509df33bd7a54b72adec0a10b678b317
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-10-08 19:19:46 +0300

    Added console application sample.

---
+++ samples/BasicConsoleApplication/AbpConsoleDemo/HelloWorldService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.DependencyInjection;
+
+namespace AbpConsoleDemo
+{
+    public class HelloWorldService : ITransientDependency
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ samples/BasicConsoleApplication/AbpConsoleDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+
+namespace AbpConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var application = AbpApplicationFactory.Create<AppModule>())
+            {
+                application.Initialize();
+
+                var helloWorldService = application.ServiceProvider.GetService<HelloWorldService>();
+                helloWorldService.SayHello();
+
+                Console.WriteLine("Press ENTER to stop application...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
