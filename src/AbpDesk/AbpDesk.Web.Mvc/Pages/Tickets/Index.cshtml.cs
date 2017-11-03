0e7dd6628dbd34e9ddf52079538c1179aed87dac
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-11-03 10:31:37 +0300

    Made Ticket index razor page.

---
+++ src/AbpDesk/AbpDesk.Web.Mvc/Pages/Tickets/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AbpDesk.Tickets;
+using AbpDesk.Tickets.Dtos;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbpDesk.Web.Mvc.Pages.Tickets
+{
+    public class IndexModel : PageModel
+    {
+        public IReadOnlyList<TicketDto> Tickets { get; set; }
+        
+        private readonly ITicketAppService _ticketAppService;
+
+        public IndexModel(ITicketAppService ticketAppService)
+        {
+            _ticketAppService = ticketAppService;
+        }
+
+        public async Task OnGetAsync(GetAllTicketsInput input)
+        {
+            var result = await _ticketAppService.GetAll(input);
+            Tickets = result.Items;
+        }
+    }
+}
