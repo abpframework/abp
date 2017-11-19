7e8f7a526696fb36f88aa2ef0ba22dfe34f55d33
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-11-19 22:57:13 +0300

    Removed accidently added code for DependencyAttribute

--- src/Volo.Abp/Volo/Abp/DependencyInjection/DependencyAttribute.cs
+++ src/Volo.Abp/Volo/Abp/DependencyInjection/DependencyAttribute.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
-using Volo.Abp.Application.Services;
-using Volo.Abp.Modularity;
 
 namespace Volo.Abp.DependencyInjection
 {
@@ -23,47 +19,6 @@ namespace Volo.Abp.DependencyInjection
         public DependencyAttribute(ServiceLifetime lifetime)
         {
             Lifetime = lifetime;
-        }
-    }
-
-    [Dependency(ReplaceServices = true)]
-    [ExposeServices(typeof(ITaxCalculator))]
-    public class TaxCalculator : ITaxCalculator, ITransientDependency
-    {
-
-    }
-
-    public interface ITaxCalculator
-    {
-    }
-
-    public class BlogModule : AbpModule
-    {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            //Add single type
-            services.AddType(typeof(TaxCalculator));
-
-            //Add multiple types in once call
-            services.AddTypes(typeof(TaxCalculator), typeof(MyOtherService));
-
-            //Add single type generic shortcut
-            services.AddType<TaxCalculator>();
-        }
-    }
-
-    public class MyService : ITransientDependency
-    {
-        public ILogger<MyService> Logger { get; set; }
-
-        public MyService()
-        {
-            Logger = NullLogger<MyService>.Instance;
-        }
-
-        public void DoSomething()
-        {
-            //...use Logger to write logs...
         }
     }
 }
