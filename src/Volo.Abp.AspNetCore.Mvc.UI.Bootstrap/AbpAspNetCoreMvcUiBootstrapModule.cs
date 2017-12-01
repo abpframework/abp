5cfe852fcb3ab251ef365056694c1eda5cca8437
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-12-01 14:35:31 +0300

    Move vue.js to embedded resource.

--- src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/AbpAspNetCoreMvcUiBootstrapModule.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/AbpAspNetCoreMvcUiBootstrapModule.cs
@@ -27,12 +27,11 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap
 
                 options.ScriptBundles.Add("GlobalScripts", new[]
                 {
-                    //TODO: Split jQuery to it's own nuget package!
-
                     "/libs/jquery/jquery-3.1.1.min.js",
                     "/libs/tether/js/tether.min.js",
                     "/libs/bootstrap/js/bootstrap.min.js",
-                    "/libs/abp/abp-jquery.js?_v" + DateTime.Now.Ticks //TODO: Move this to Volo.Abp.AspNetCore.Mvc.UI.. or to new jQuery package?
+                    "/libs/vue/vue.js",
+                    "/libs/abp/abp-jquery.js?_v" + DateTime.Now.Ticks
                 });
             });
         }
