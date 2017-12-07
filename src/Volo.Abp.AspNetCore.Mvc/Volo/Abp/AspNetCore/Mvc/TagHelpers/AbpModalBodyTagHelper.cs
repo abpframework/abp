2994a17d82149a638a1866db5d059f9f1d2f4f93
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-12-07 17:35:38 +0300

    Created tag heper for modals.

---
+++ src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/TagHelpers/AbpModalBodyTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.TagHelpers
+{
+    public class AbpModalBodyTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.Add("class", "modal-body"); //TODO: Append class if any exists
+        }
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/TagHelpers/AbpModalHeaderTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.TagHelpers
+{
+    public class AbpModalHeaderTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.Add("class", "modal-header"); //TODO: Append class if any exists
+            output.Content.SetHtmlContent(CreateContent());
+        }
+
+        private string CreateContent()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("    <h5 class=\"modal-title\">Modal Title</h5>");
+            sb.AppendLine("    <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">");
+            sb.AppendLine("        <span aria-hidden=\"true\">&times;</span>");
+            sb.AppendLine("    </button>");
+
+            return sb.ToString();
+        }
+    }
+}
