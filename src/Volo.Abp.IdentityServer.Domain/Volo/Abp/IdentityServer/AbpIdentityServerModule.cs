b116a3ff97c191c2903693e35a8c20ac41ebb36d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-01-02 11:05:33 +0300

    Added Volo.Abp.IdentityServer.Domain and Ef Core packages. #185.

---
+++ src/Volo.Abp.IdentityServer.Domain/Volo/Abp/IdentityServer/AbpIdentityServerModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.IdentityServer
+{
+    public class AbpIdentityServerModule : AbpModule
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAssemblyOf<AbpIdentityServerModule>();
+        }
+    }
+}
---
+++ src/Volo.Abp.IdentityServer.EntityFrameworkCore/Volo/Abp/IdentityServer/EntityFrameworkCore/AbpIdentityServerModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.IdentityServer.EntityFrameworkCore
+{
+    public class AbpIdentityServerEntityFrameworkCoreModule : AbpModule
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAssemblyOf<AbpIdentityServerEntityFrameworkCoreModule>();
+        }
+    }
+}
