127ac4f6170e8a4266394aca88179c6a426583f1
Alper Ebicoglu <alperonline@hotmail.com>
2018-01-06 00:51:13 +0300

    Added IdentityResourceStore

---
+++ src/Volo.Abp.IdentityServer.Domain/Volo/Abp/IdentityServer/IdentityResources/IIdentityResourceRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using Volo.Abp.Domain.Repositories;
+using ApiResource = Volo.Abp.IdentityServer.ApiResources.ApiResource;
+
+namespace Volo.Abp.IdentityServer.IdentityResources
+{
+    public interface IIdentityResourceRepository : IRepository<IdentityResource>
+    {
+        Task<List<IdentityResource>> FindIdentityResourcesByScopeAsync(string[] scopeNames);
+
+        Task<List<ApiResource>> FindApiResourcesByScopeAsync(string[] scopeNames);
+
+        Task<ApiResource> FindApiResourceAsync(string name);
+
+        Task<ApiResources.ApiResources> GetAllResourcesAsync();
+    }
+}
---
+++ src/Volo.Abp.IdentityServer.Domain/Volo/Abp/IdentityServer/IdentityResources/IdentityResourceStore.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.ObjectMapping;
+
+namespace Volo.Abp.IdentityServer.IdentityResources
+{
+    public class IdentityResourceStore : IResourceStore, ITransientDependency
+    {
+        private readonly IIdentityResourceRepository _identityResourceRepository;
+        private readonly IObjectMapper _objectMapper;
+
+        public IdentityResourceStore(IIdentityResourceRepository identityResourceRepository, IObjectMapper objectMapper)
+        {
+            _identityResourceRepository = identityResourceRepository;
+            _objectMapper = objectMapper;
+        }
+
+        public virtual async Task<IEnumerable<IdentityServer4.Models.IdentityResource>> FindIdentityResourcesByScopeAsync(IEnumerable<string> scopeNames)
+        {
+            var results = await _identityResourceRepository.FindIdentityResourcesByScopeAsync(scopeNames.ToArray());
+            return _objectMapper.Map<List<IdentityResource>, List<IdentityServer4.Models.IdentityResource>>(results);
+        }
+
+        public virtual async Task<IEnumerable<ApiResource>> FindApiResourcesByScopeAsync(IEnumerable<string> scopeNames)
+        {
+            var results = await _identityResourceRepository.FindApiResourcesByScopeAsync(scopeNames.ToArray());
+            return results?.Select(x => _objectMapper.Map<ApiResources.ApiResource, ApiResource>(x));
+        }
+
+        public virtual async Task<ApiResource> FindApiResourceAsync(string name)
+        {
+            var result = await _identityResourceRepository.FindApiResourceAsync(name);
+            return _objectMapper.Map<ApiResources.ApiResource, ApiResource>(result);
+        }
+
+        public virtual async Task<Resources> GetAllResourcesAsync()
+        {
+            var result = await _identityResourceRepository.GetAllResourcesAsync();
+            return new Resources
+            (
+                result.Resources.Result.Select(y => _objectMapper.Map<IdentityResource, IdentityServer4.Models.IdentityResource>(y)),
+                result.IdentityResources.Result.Select(x => _objectMapper.Map<ApiResources.ApiResource, ApiResource>(x))
+            );
+        }
+    }
+}
