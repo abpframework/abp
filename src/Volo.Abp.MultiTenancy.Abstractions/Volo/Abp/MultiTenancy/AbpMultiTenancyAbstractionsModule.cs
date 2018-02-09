169d019f7db7cd0ce15c5ff7c490462b211cb3cd
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-02-09 13:11:16 +0300

    Created tenant setting value provider.

--- src/Volo.Abp.MultiTenancy.Abstractions/Volo/Abp/MultiTenancy/AbpMultiTenancyAbstractionsModule.cs
+++ src/Volo.Abp.MultiTenancy.Abstractions/Volo/Abp/MultiTenancy/AbpMultiTenancyAbstractionsModule.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Data;
 using Volo.Abp.Modularity;
+using Volo.Abp.Settings;
 
 namespace Volo.Abp.MultiTenancy
 {
     [DependsOn(typeof(AbpDataModule))]
+    [DependsOn(typeof(AbpSettingsModule))]
     public class AbpMultiTenancyAbstractionsModule : AbpModule
     {
         public override void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<SettingOptions>(options =>
+            {
+                options.ValueProviders.Add<TenantSettingValueProvider>();
+            });
+
             services.AddAssemblyOf<AbpMultiTenancyAbstractionsModule>();
         }
     }
