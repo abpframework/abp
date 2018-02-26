abf80fc8974b91f0dd8eaf4f259c9442247b0274
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-02-26 10:16:17 +0300

    Created empty microservice demo project

---
+++ samples/MicroserviceDemo/MicroserviceDemo.Web/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroserviceDemo.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello microservices!");
+            });
+        }
+    }
+}
