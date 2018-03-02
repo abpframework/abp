a53f9796c0cb5d220d6c0d690278bd96c4880bf0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-03-02 11:09:12 +0300

    Created a console client for the microservice demo

--- src/MicroserviceDemo/MicroserviceDemo.Web/MicroservicesDemoWebModule.cs
+++ src/MicroserviceDemo/MicroserviceDemo.Web/MicroservicesDemoWebModule.cs
@@ -75,7 +75,29 @@ namespace MicroserviceDemo.Web
                 }); 
             });
 
-            services.AddAuthentication();
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = "Cookies";
+
+                    options.Authority = "http://localhost:54307";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = "mvc";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code id_token";
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Scope.Add("api1");
+                    options.Scope.Add("offline_access");
+                });
 
             services.Configure<RemoteServiceOptions>(configuration);
 
