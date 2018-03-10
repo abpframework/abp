c86bfbc02566ddd5c96748a9b0f37fde30af93f0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-03-10 21:48:30 +0300

    Minor refactoring.

--- src/Volo.Abp.Data/Volo/Abp/Uow/UnitOfWorkDefaultOptions.cs
+++ src/Volo.Abp.Data/Volo/Abp/Uow/UnitOfWorkDefaultOptions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Data;
-using System.Reflection;
 
 namespace Volo.Abp.Uow
 {
--- src/Volo.Abp.Data/Volo/Abp/Uow/UnitOfWorkInterceptor.cs
+++ src/Volo.Abp.Data/Volo/Abp/Uow/UnitOfWorkInterceptor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Options;
