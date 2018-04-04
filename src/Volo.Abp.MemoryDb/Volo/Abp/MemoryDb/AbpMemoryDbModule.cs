d4c4fa3f7059d43ad094b44c1d5714114588b8cf
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-04-04 14:42:30 +0300

    Added TODO for in-memory db.

--- src/Volo.Abp.MemoryDb/Volo/Abp/MemoryDb/AbpMemoryDbModule.cs
+++ src/Volo.Abp.MemoryDb/Volo/Abp/MemoryDb/AbpMemoryDbModule.cs
@@ -6,6 +6,9 @@ using Volo.Abp.Uow.MemoryDb;
 
 namespace Volo.Abp.MemoryDb
 {
+    /* TODO: Consider to store objects as binary serialized in the memory, which makes unit tests more realistic.
+     */
+
     [DependsOn(typeof(AbpDddDomainModule))]
     public class AbpMemoryDbModule : AbpModule
     {
