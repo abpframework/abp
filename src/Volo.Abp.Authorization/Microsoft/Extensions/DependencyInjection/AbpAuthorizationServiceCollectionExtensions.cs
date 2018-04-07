f9847e0a67920b17c80d4f84e57f62bb9d7afd2b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-04-07 19:17:41 +0300

    Added AddAlwaysAllowAuthorization method and AlwaysAllowAuthorizationService.

--- src/Volo.Abp.Authorization/Microsoft/Extensions/DependencyInjection/AbpAuthorizationServiceCollectionExtensions.cs
+++ src/Volo.Abp.Authorization/Microsoft/Extensions/DependencyInjection/AbpAuthorizationServiceCollectionExtensions.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Volo.Abp.Authorization;
 using Volo.Abp.Authorization.Permissions;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AbpAuthorizationServiceCollectionExtensions
     {
+        //TODO: Remove this and use AddAlwaysAllowAuthorization
         public static IServiceCollection AddAlwaysAllowPermissionChecker(this IServiceCollection services)
         {
+            return services.Replace(ServiceDescriptor.Singleton<IPermissionChecker, AlwaysAllowPermissionChecker>());
+        }
+
+        public static IServiceCollection AddAlwaysAllowAuthorization(this IServiceCollection services)
+        {
+            services.Replace(ServiceDescriptor.Singleton<IAuthorizationService, AlwaysAllowAuthorizationService>());
+            services.Replace(ServiceDescriptor.Singleton<IAbpAuthorizationService, AlwaysAllowAuthorizationService>());
             return services.Replace(ServiceDescriptor.Singleton<IPermissionChecker, AlwaysAllowPermissionChecker>());
         }
     }
