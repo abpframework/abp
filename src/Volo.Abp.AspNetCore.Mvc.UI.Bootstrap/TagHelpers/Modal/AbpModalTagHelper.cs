f7be98a37d3d2d35ed501172e4ac9651158560df
yekalkan <emre.kalkan@volosoft.com>
2018-04-24 14:48:28 +0300

    AbpModalTagHelperService

--- src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Modal/AbpModalTagHelper.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Modal/AbpModalTagHelper.cs
@@ -1,37 +1,11 @@
-﻿using System.Text;
-using Microsoft.AspNetCore.Razor.TagHelpers;
-
-namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Modal
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Modal
 {
-    public class AbpModalTagHelper : TagHelper
+    public class AbpModalTagHelper : AbpTagHelper<AbpModalTagHelper, AbpModalTagHelperService>
     {
-        public override void Process(TagHelperContext context, TagHelperOutput output)
+        public AbpModalTagHelper(AbpModalTagHelperService tagHelperService)
+            : base(tagHelperService)
         {
-            output.TagName = null;
-            output.PreContent.SetHtmlContent(CreatePreContent());
-            output.PostContent.SetHtmlContent(CreatePostContent());
-        }
 
-        private string CreatePreContent()
-        {
-            var sb = new StringBuilder();
-
-            sb.AppendLine("<div class=\"modal fade\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">");
-            sb.AppendLine("    <div class=\"modal-dialog\" role=\"document\">");
-            sb.AppendLine("        <div class=\"modal-content\">");
-
-            return sb.ToString();
-        }
-
-        private string CreatePostContent()
-        {
-            var sb = new StringBuilder();
-
-            sb.AppendLine("        </div>");
-            sb.AppendLine("    </div>");
-            sb.AppendLine("</div>");
-
-            return sb.ToString();
         }
     }
 }
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Modal/AbpModalTagHelperService.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Modal
+{
+    public class AbpModalTagHelperService : AbpTagHelperService<AbpModalTagHelper>
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            output.PreContent.SetHtmlContent(CreatePreContent());
+            output.PostContent.SetHtmlContent(CreatePostContent());
+        }
+
+        private string CreatePreContent()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("<div class=\"modal fade\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">");
+            sb.AppendLine("    <div class=\"modal-dialog\" role=\"document\">");
+            sb.AppendLine("        <div class=\"modal-content\">");
+
+            return sb.ToString();
+        }
+
+        private string CreatePostContent()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("        </div>");
+            sb.AppendLine("    </div>");
+            sb.AppendLine("</div>");
+
+            return sb.ToString();
+        }
+    }
+}
