7197659066fb73b898257d6fe04d26877d1005ac
yekalkan <emre.kalkan@volosoft.com>
2018-04-24 14:45:51 +0300

    AbpModalHeaderTagHelperService

--- src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Modal/AbpModalHeaderTagHelper.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Modal/AbpModalHeaderTagHelper.cs
@@ -1,30 +1,13 @@
-﻿using System.Text;
-using Microsoft.AspNetCore.Razor.TagHelpers;
-using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Microsoft.AspNetCore.Razor.TagHelpers;
-
-namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Modal
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Modal
 {
-    public class AbpModalHeaderTagHelper : TagHelper
+    public class AbpModalHeaderTagHelper : AbpTagHelper<AbpModalHeaderTagHelper, AbpModalHeaderTagHelperService>
     {
         public string Title { get; set; }
-
-        public override void Process(TagHelperContext context, TagHelperOutput output)
+        
+        public AbpModalHeaderTagHelper(AbpModalHeaderTagHelperService tagHelperService)
+            : base(tagHelperService)
         {
-            output.TagName = "div";
-            output.Attributes.AddClass("modal-header");
-            output.Content.SetHtmlContent(CreateContent());
-        }
 
-        private string CreateContent()
-        {
-            var sb = new StringBuilder();
-
-            sb.AppendLine("    <h5 class=\"modal-title\">" + Title + "</h5>");
-            sb.AppendLine("    <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">");
-            sb.AppendLine("        <span aria-hidden=\"true\">&times;</span>");
-            sb.AppendLine("    </button>");
-
-            return sb.ToString();
         }
     }
 }
