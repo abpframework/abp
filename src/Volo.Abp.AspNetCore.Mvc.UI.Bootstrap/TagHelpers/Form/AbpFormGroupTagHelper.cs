15dca510e5b76e813f92e5100a18156a001e35f4
yekalkan <emre.kalkan@volosoft.com>
2018-04-26 13:35:56 +0300

    basic form-group tag helper

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/AbpFormGroupTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form
+{
+    public class AbpFormGroupTagHelper : AbpTagHelper<AbpFormGroupTagHelper, AbpFormGroupTagHelperService>
+    {
+        
+        public bool Checkbox { get; set; }
+
+        public AbpFormGroupTagHelper(AbpFormGroupTagHelperService tagHelperService)
+            : base(tagHelperService)
+        {
+
+        }
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/AbpFormGroupTagHelperService.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form
+{
+    public class AbpFormGroupTagHelperService : AbpTagHelperService<AbpFormGroupTagHelper>
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.AddClass("form-group");
+            if (TagHelper.Checkbox)
+            {
+                 output.Attributes.AddClass("form-check");
+            }
+        }
+    }
+}
