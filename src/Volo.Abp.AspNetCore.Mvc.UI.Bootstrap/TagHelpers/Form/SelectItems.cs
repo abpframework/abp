0e5d126ebf97dc82b39441a625dd5ffc4123bc2f
yekalkan <emre.kalkan@volosoft.com>
2018-04-30 17:42:08 +0300

    Select Items Attribute

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/SelectItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SelectItems: Attribute
+    {
+        public IEnumerable<SelectListItem> Items { get; set; }
+
+        public SelectItems(IEnumerable<SelectListItem> items)
+        {
+            Items = items;
+        }
+    }
+}
