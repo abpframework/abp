296748c2d6f899e25a0df0667712c2896168b4d3
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-01 15:51:30 +0300

    Fix toastr url

--- src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/AbpAspNetCoreMvcUiThemeSharedModule.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/AbpAspNetCoreMvcUiThemeSharedModule.cs
@@ -52,7 +52,7 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared
                     "/libs/abp/aspnetcore.mvc.ui.theme.shared/bootstrap/modal-manager.js",
                     "/libs/abp/aspnetcore.mvc.ui.theme.shared/datatables/datatables-extensions.js",
                     "/libs/abp/aspnetcore.mvc.ui.theme.shared/sweetalert/abp-sweetalert.js",
-                    "/libs/abp/aspnetcore.mvc.ui.theme.shared/sweetalert/abp-toastr.js"
+                    "/libs/abp/aspnetcore.mvc.ui.theme.shared/toastr/abp-toastr.js"
                 });
             });
 
