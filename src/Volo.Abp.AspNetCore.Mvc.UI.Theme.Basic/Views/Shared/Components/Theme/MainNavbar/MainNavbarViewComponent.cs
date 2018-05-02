5fe07987759a04f72677e4101c47e06e59e8d71b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-02 13:23:51 +0300

    Separated menu and navbar components

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/Views/Shared/Components/Theme/MainNavbar/MainNavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Views.Shared.Components.Theme.MainNavbar
+{
+    public class MainNavbarViewComponent : AbpViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Shared/Components/Theme/MainNavbar/Default.cshtml");
+        }
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/Views/Shared/Components/Theme/MainNavbarMenu/AbpMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.UI.Navigation;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Views.Shared.Components.Theme.MainNavbarMenu
+{
+    public class MainNavbarMenuViewComponent : AbpViewComponent
+    {
+        private readonly IMenuManager _menuManager;
+
+        public MainNavbarMenuViewComponent(IMenuManager menuManager)
+        {
+            _menuManager = menuManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var menu = await _menuManager.GetAsync(StandardMenus.Main);
+            return View("~/Views/Shared/Components/Theme/MainNavbarMenu/Default.cshtml", menu);
+        }
+    }
+}
