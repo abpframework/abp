1e85261b079769d874a3cf46ecd8fa14f3df7bfb
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-02 09:28:45 +0300

    Apply AbpValidationAttributeTagHelper for asp-validation-summary too.

--- src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/AbpValidationAttributeTagHelper.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/AbpValidationAttributeTagHelper.cs
@@ -4,6 +4,7 @@ using Volo.Abp.DependencyInjection;
 namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form
 {
     [HtmlTargetElement(Attributes = "asp-validation-for")]
+    [HtmlTargetElement(Attributes = "asp-validation-summary")]
     public class AbpValidationAttributeTagHelper : AbpTagHelper<AbpValidationAttributeTagHelper, AbpValidationAttributeTagHelperService>, ITransientDependency
     {
         public AbpValidationAttributeTagHelper(AbpValidationAttributeTagHelperService tagHelperService)
