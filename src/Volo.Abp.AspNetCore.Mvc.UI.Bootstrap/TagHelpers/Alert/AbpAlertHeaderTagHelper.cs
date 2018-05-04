1080f56794af35ff7b41a7eea5365368492f8d7d
yekalkan <emre.kalkan@volosoft.com>
2018-05-04 15:31:30 +0300

    Abp alert header refactor

--- src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Alert/AbpAlertHeaderTagHelper.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Alert/AbpAlertHeaderTagHelper.cs
@@ -1,8 +1,15 @@
-﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Alert
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Alert
 {
+    [HtmlTargetElement("h1", Attributes = "abp-alert-header", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("h2", Attributes = "abp-alert-header", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("h3", Attributes = "abp-alert-header", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("h4", Attributes = "abp-alert-header", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("h5", Attributes = "abp-alert-header", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement("h6", Attributes = "abp-alert-header", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class AbpAlertHeaderTagHelper : AbpTagHelper<AbpAlertHeaderTagHelper, AbpAlertHeaderTagHelperService>
     {
-        public AbpAlertHeaderSize Size { get; set; } = AbpAlertHeaderSize.Default;
 
         public AbpAlertHeaderTagHelper(AbpAlertHeaderTagHelperService tagHelperService)
             : base(tagHelperService)
--- src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Alert/AbpAlertHeaderTagHelperService.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Alert/AbpAlertHeaderTagHelperService.cs
@@ -7,19 +7,8 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Alert
     {
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.TagName = GetHeaderSize(context, output);
             output.Attributes.AddClass("alert-heading");
-            output.TagMode = TagMode.StartTagAndEndTag;
-        }
-
-        protected virtual string GetHeaderSize(TagHelperContext context, TagHelperOutput output)
-        {
-            if (TagHelper.Size == AbpAlertHeaderSize.Default)
-            {
-                return AbpAlertHeaderSize.h4.ToString();
-            }
-
-            return TagHelper.Size.ToString();
+            output.Attributes.RemoveAll("abp-alert-header");
         }
     }
 }
