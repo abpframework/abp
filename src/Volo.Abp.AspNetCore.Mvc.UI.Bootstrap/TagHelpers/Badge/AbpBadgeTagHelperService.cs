ef487ec33a1d9315c8d81e4708ab4fa909265571
yekalkan <emre.kalkan@volosoft.com>
2018-05-04 16:28:17 +0300

    badge tag helper

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Badge/AbpBadgeTagHelperService.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Badge
+{
+    public class AbpBadgeTagHelperService : AbpTagHelperService<AbpBadgeTagHelper>
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var badgeType = TagHelper.BadgeType != AbpBadgeType._? TagHelper.BadgeType : TagHelper.BadgePillType;
+
+            output.Attributes.AddClass("badge");
+
+            if (TagHelper.BadgePillType != AbpBadgeType._)
+            {
+                output.Attributes.AddClass("badge-pill");
+            }
+
+            if (badgeType != AbpBadgeType.Default && badgeType != AbpBadgeType._)
+            {
+                output.Attributes.AddClass("badge-" + badgeType.ToString().ToLowerInvariant());
+            }
+
+        }
+        
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Badge/AbpBadgeType.cs
@@ -0,0 +1,16 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Badge
+{
+    public enum AbpBadgeType
+    {
+        _,
+        Default,
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark
+    }
+}
