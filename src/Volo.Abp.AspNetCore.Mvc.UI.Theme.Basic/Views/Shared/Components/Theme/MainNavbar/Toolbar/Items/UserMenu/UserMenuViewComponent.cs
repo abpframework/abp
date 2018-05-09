b81652cf4f75daa2fdf9a0909138a60ad642b750
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-09 16:03:12 +0300

    Render user menu using menumanager

--- src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/Views/Shared/Components/Theme/MainNavbar/Toolbar/Items/UserMenu/UserMenuViewComponent.cs
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/Views/Shared/Components/Theme/MainNavbar/Toolbar/Items/UserMenu/UserMenuViewComponent.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.UI.Navigation;
 
 namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Views.Shared.Components.Theme.MainNavbar.Toolbar.Items.UserMenu
 {
     public class UserMenuViewComponent : AbpViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IMenuManager _menuManager;
+
+        public UserMenuViewComponent(IMenuManager menuManager)
         {
-            return View("~/Views/Shared/Components/Theme/MainNavbar/Toolbar/Items/UserMenu/Default.cshtml");
+            _menuManager = menuManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var menu = await _menuManager.GetAsync(StandardMenus.User);
+            return View("~/Views/Shared/Components/Theme/MainNavbar/Toolbar/Items/UserMenu/Default.cshtml", menu);
         }
     }
 }
