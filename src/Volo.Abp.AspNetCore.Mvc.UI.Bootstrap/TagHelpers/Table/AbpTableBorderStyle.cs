92a5ff78a89d68e6b64b8f25ff83e4442948a0f6
yekalkan <emre.kalkan@volosoft.com>
2018-05-14 09:51:13 +0300

    Added Abp Table Tag Helper

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Table/AbpTableBorderStyle.cs
@@ -0,0 +1,9 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Table
+{
+    public enum AbpTableBorderStyle
+    {
+        Default,
+        Bordered,
+        Borderless
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Table/AbpTableHeaderTheme.cs
@@ -0,0 +1,9 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Table
+{
+    public enum AbpTableHeaderTheme
+    {
+        Default,
+        Light,
+        Dark
+    }
+}
