022e8e511321498074d67789b91068fc26348e8f
yekalkan <emre.kalkan@volosoft.com>
2018-05-22 06:46:43 +0300

    Added abp-navbar-toggle tag helper

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Nav/AbpNavbarToggleTagHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Nav
+{
+    public class AbpNavbarToggleTagHelper : AbpTagHelper<AbpNavbarToggleTagHelper, AbpNavbarToggleTagHelperService>
+    {
+        public string Id { get; set; }
+
+        public AbpNavbarToggleTagHelper(AbpNavbarToggleTagHelperService tagHelperService)
+            : base(tagHelperService)
+        {
+
+        }
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Nav/AbpNavbarToggleTagHelperService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Nav
+{
+    public class AbpNavbarToggleTagHelperService : AbpTagHelperService<AbpNavbarToggleTagHelper>
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            SetRandomNameIfNotProvided();
+            output.TagName = "div";
+            output.Attributes.AddClass("collapse");
+            output.Attributes.AddClass("navbar-collapse");
+            output.Attributes.Add("id",TagHelper.Id);
+            SetToggleButton(context,output);
+        }
+
+        protected virtual void SetToggleButton(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreElement.SetHtmlContent(
+                "<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#" +
+                TagHelper.Id + "\" aria-controls=\"" + TagHelper.Id +
+                "\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n    <span class=\"navbar-toggler-icon\"></span>\r\n  </button>");
+        }
+
+        protected virtual void SetRandomNameIfNotProvided()
+        {
+            if (string.IsNullOrWhiteSpace(TagHelper.Id))
+            {
+                TagHelper.Id = Guid.NewGuid().ToString("N");
+            }
+        }
+    }
+}
