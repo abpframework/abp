695cf313c88ee49f455d001f0925fd77375c3db6
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-22 09:19:30 +0300

    Added abp-if tag helper

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/If/AbpIfTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.If
+{
+    [HtmlTargetElement(Attributes = "abp-if")]
+    public class AbpIfTagHelper : AbpTagHelper
+    {
+        [HtmlAttributeName("abp-if")]
+        public bool Condition { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Condition)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
