0903da0dba00fd0e87e4dba7ae685b5efab247b0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-24 08:06:21 +0300

    Added AbpPage

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI/RazorPages/AbpPage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Volo.Abp.Users;
+
+namespace Volo.Abp.AspNetCore.Mvc.RazorPages
+{
+    public abstract class AbpPage : Page
+    {
+        [RazorInject]
+        protected ICurrentUser CurrentUser { get; set; }
+    }
+}
