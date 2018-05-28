419cd5c35256f7b945d44033870cdd1a4dc9314a
yekalkan <emre.kalkan@volosoft.com>
2018-05-28 11:40:28 +0300

    Abp border tag helper

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Border/AbpBorderTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Border
+{
+    [HtmlTargetElement(Attributes = "abp-border")]
+    public class AbpBorderTagHelper : AbpTagHelper<AbpBorderTagHelper, AbpBorderTagHelperService>
+    {
+        public AbpBorderType AbpBorder { get; set; } = AbpBorderType._;
+
+        public AbpBorderTagHelper(AbpBorderTagHelperService tagHelperService)
+            : base(tagHelperService)
+        {
+
+        }
+    }
+}
