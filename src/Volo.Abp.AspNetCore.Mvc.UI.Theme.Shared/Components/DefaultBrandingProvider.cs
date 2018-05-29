29a6eab705257cba212c6bdd479ce84230ec3853
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-05-30 02:15:13 +0300

    Introduce IBrandingProvider

---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/Components/DefaultBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Components
+{
+    public class DefaultBrandingProvider : IBrandingProvider, ITransientDependency
+    {
+        public virtual string AppName => "MyApplication";
+
+        public virtual string LogoUrl => null;
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/Components/IBrandingProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Components
+{
+    public interface IBrandingProvider
+    {
+        string AppName { get; }
+
+        string LogoUrl { get; }
+    }
+}
