384ac80fd08860cc5a8d33cf85bd0af419f2e8d5
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-04 00:39:07 +0300

    Created sweetalert package.

--- npm/packs/aspnetcore.mvc.ui.theme.shared/abp.resourcemapping.js
+++ npm/packs/aspnetcore.mvc.ui.theme.shared/abp.resourcemapping.js
@@ -4,12 +4,10 @@
         "../font-awesome/abp.resourcemapping.js",
         "../jquery-form/abp.resourcemapping.js",
         "../jquery-validation-unobtrusive/abp.resourcemapping.js",
+        "../sweetalert/abp.resourcemapping.js",
         "../datatables.net-bs4/abp.resourcemapping.js"
     ],
     mappings: {
-        //Sweetalert
-        "@node_modules/sweetalert/dist/*.*": "@libs/sweetalert/",
-        
         //Toastr
         "@node_modules/toastr/build/*.*": "@libs/toastr/"
     }
