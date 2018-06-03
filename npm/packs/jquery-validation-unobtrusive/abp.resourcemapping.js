e7e46b85847deb5a9a900bdf875edb454e889c53
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-04 00:30:17 +0300

    Created jquery-validation package.

--- npm/packs/jquery-validation-unobtrusive/abp.resourcemapping.js
+++ npm/packs/jquery-validation-unobtrusive/abp.resourcemapping.js
@@ -1,12 +1,8 @@
 ﻿module.exports = {
     imports: [
-        "../jquery/abp.resourcemapping.js"
+        "../jquery-validation/abp.resourcemapping.js"
     ],
     mappings: {
-        //jQuery-Validation
-        "@node_modules/jquery-validation/dist/jquery.validate.js": "@libs/jquery-validation/",
-        "@node_modules/jquery-validation/dist/localization/*.*": "@libs/jquery-validation/localization/",
-
         //jQuery-Validation-Unobtrusive
         "@node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js": "@libs/jquery-validation-unobtrusive/"
     }
---
+++ npm/packs/jquery-validation/abp.resourcemapping.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+    imports: [
+        "../jquery/abp.resourcemapping.js"
+    ],
+    mappings: {
+        //jQuery-Validation
+        "@node_modules/jquery-validation/dist/jquery.validate.js": "@libs/jquery-validation/",
+        "@node_modules/jquery-validation/dist/localization/*.*": "@libs/jquery-validation/localization/"
+    }
+}
