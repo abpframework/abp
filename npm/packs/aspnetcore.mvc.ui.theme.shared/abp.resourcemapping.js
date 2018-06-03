c62c31bd00e34dcb718dc46e27f5d61068fdd310
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-04 00:21:27 +0300

    Created jquery-validation-unobtrusive package

--- npm/packs/aspnetcore.mvc.ui.theme.shared/abp.resourcemapping.js
+++ npm/packs/aspnetcore.mvc.ui.theme.shared/abp.resourcemapping.js
@@ -1,19 +1,12 @@
 ﻿module.exports = {
     imports: [
-        "../jquery/abp.resourcemapping.js",
         "../bootstrap/abp.resourcemapping.js",
         "../font-awesome/abp.resourcemapping.js",
         "../jquery-form/abp.resourcemapping.js",
+        "../jquery-validation-unobtrusive/abp.resourcemapping.js",
         "../datatables.net-bs4/abp.resourcemapping.js"
     ],
     mappings: {
-        //jQuery-Validation
-        "@node_modules/jquery-validation/dist/jquery.validate.js": "@libs/jquery-validation/",
-        "@node_modules/jquery-validation/dist/localization/*.*": "@libs/jquery-validation/localization/",
-
-        //jQuery-Validation-Unobtrusive
-        "@node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js": "@libs/jquery-validation-unobtrusive/",
-        
         //Sweetalert
         "@node_modules/sweetalert/dist/*.*": "@libs/sweetalert/",
         
---
+++ npm/packs/jquery-validation-unobtrusive/abp.resourcemapping.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    imports: [
+        "../jquery/abp.resourcemapping.js"
+    ],
+    mappings: {
+        //jQuery-Validation
+        "@node_modules/jquery-validation/dist/jquery.validate.js": "@libs/jquery-validation/",
+        "@node_modules/jquery-validation/dist/localization/*.*": "@libs/jquery-validation/localization/",
+
+        //jQuery-Validation-Unobtrusive
+        "@node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js": "@libs/jquery-validation-unobtrusive/"
+    }
+}
