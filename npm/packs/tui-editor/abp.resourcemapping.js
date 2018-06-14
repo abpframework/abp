d2cbfb28bdcc82e14a483ebf4311f11d99c1d294
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-14 16:32:35 +0300

    Added tui-editor standard nmp package.

---
+++ npm/packs/tui-editor/abp.resourcemapping.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+    imports: [
+        "../jquery/abp.resourcemapping.js"
+    ],
+    mappings: {
+        "@node_modules/to-mark/dist/to-mark.min.js": "@libs/to-mark/",
+        "@node_modules/tui-code-snippet/dist/tui-code-snippet.min.js": "@libs/tui-code-snippet/",
+        "@node_modules/squire-rte/squire.js": "@libs/squire-rte/",
+        "@node_modules/tui-editor/dist/*.*": "@libs/tui-editor/"
+    }
+}
