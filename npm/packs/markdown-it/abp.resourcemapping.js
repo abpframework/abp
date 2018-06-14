521f37eb98b32711b37fcd6420532808550f18be
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-14 15:22:31 +0300

    Added markdown-it standard npm package.

---
+++ npm/packs/markdown-it/abp.resourcemapping.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+    imports: [
+        "../core/abp.resourcemapping.js"
+    ],
+    mappings: {
+        "@node_modules/markdown-it/dist/markdown-it.min.js": "@libs/markdown-it/"
+    }
+}
