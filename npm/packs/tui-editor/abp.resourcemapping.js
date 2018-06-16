3755e3ffe82de74b8e488c4e2c470c43d26e79de
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-17 00:42:08 +0300

    Updated npm packages

--- npm/packs/tui-editor/abp.resourcemapping.js
+++ npm/packs/tui-editor/abp.resourcemapping.js
@@ -1,11 +1,14 @@
 ﻿module.exports = {
     imports: [
-        "../jquery/abp.resourcemapping.js"
+        "../jquery/abp.resourcemapping.js",
+        "../codemirror/abp.resourcemapping.js",
+        "../highlight.js/abp.resourcemapping.js",
+        "../markdown-it/abp.resourcemapping.js"
     ],
     mappings: {
         "@node_modules/to-mark/dist/to-mark.min.js": "@libs/to-mark/",
         "@node_modules/tui-code-snippet/dist/tui-code-snippet.min.js": "@libs/tui-code-snippet/",
-        "@node_modules/squire-rte/squire.js": "@libs/squire-rte/",
+        "@node_modules/squire-rte/build/squire.js": "@libs/squire-rte/",
         "@node_modules/tui-editor/dist/*.*": "@libs/tui-editor/"
     }
 }
