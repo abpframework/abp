205eb3a09d525d4ff59f73389e6c996e59a717b7
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-06-21 13:58:30 +0300

    Blogging automapper profile added

---
+++ modules/blogging/src/Volo.Blogging.Application/Volo/Blogging/BloggingApplicationAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Volo.Blogging.Blogs;
+
+namespace Volo.Blogging
+{
+    public class BloggingApplicationAutoMapperProfile : Profile
+    {
+        public BloggingApplicationAutoMapperProfile()
+        {
+            CreateMap<Blog, BlogDto>();
+        }
+    }
+}
