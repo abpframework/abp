0bb97c6a7fbb512e8448bb828b27bedcae140e3a
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-28 16:06:13 +0300

    Use Microsoft.AspNetCore.App package for blogging module.

--- modules/blogging/src/Volo.Blogging.Application.Contracts/Volo/Blogging/BloggingPermissionDefinitionProvider.cs
+++ modules/blogging/src/Volo.Blogging.Application.Contracts/Volo/Blogging/BloggingPermissionDefinitionProvider.cs
@@ -16,12 +16,12 @@ namespace Volo.Blogging
             var blogs = bloggingGroup.AddPermission(BloggingPermissions.Blogs.Default, L("Permission:BlogManagement"));
             blogs.AddChild(BloggingPermissions.Blogs.Update, L("Permission:Edit"));
             blogs.AddChild(BloggingPermissions.Blogs.Delete, L("Permission:Delete"));
-            blogs.AddChild(BloggingPermissions.Blogs.Create, L("Permission:Delete"));
+            blogs.AddChild(BloggingPermissions.Blogs.Create, L("Permission:Create"));
 
             var posts = bloggingGroup.AddPermission(BloggingPermissions.Posts.Default, L("Permission:PostManagement"));
             posts.AddChild(BloggingPermissions.Posts.Update, L("Permission:Edit"));
             posts.AddChild(BloggingPermissions.Posts.Delete, L("Permission:Delete"));
-            posts.AddChild(BloggingPermissions.Posts.Create, L("Permission:Delete"));
+            posts.AddChild(BloggingPermissions.Posts.Create, L("Permission:Create"));
         }
 
         private static LocalizableString L(string name)
