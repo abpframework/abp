be562c4b5e1b243c1eda5209349fbd240c618825
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-06-29 10:52:14 +0300

    Blogging add post url controls

---
+++ modules/blogging/src/Volo.Blogging.Web/wwwroot/pages/blog/posts/edit.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    $('#Post_Title').on("change paste keyup", function() {
+        var title = $('#Post_Title').val();
+
+        if (title.length > 64) {
+            title = title.substring(0, 64);
+        }
+
+        title = title.replace(' ','-');
+
+        $('#Post_Url').val(title);
+    });
+
+})(jQuery);
