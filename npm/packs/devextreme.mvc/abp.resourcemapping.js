efc77697f1a9031aa84f0db43a3ddf80baa38ea1
bbakermmc <bbaker@mmcweb.com>
2018-06-30 11:39:20 -0400

    Created DevExtreme.MVC NPM Pack.

---
+++ npm/packs/devextreme.mvc/abp.resourcemapping.js
@@ -0,0 +1,16 @@
+﻿module.exports = {
+    aliases: {
+        "@node_modules": "./node_modules",
+        "@libs": "./wwwroot/libs"
+    },
+    clean: [
+        "@libs/jszip",
+        "@libs/devextreme",        
+        "@libs/devextreme-aspnet-data"
+    ],
+    mappings: {
+        "@node_modules/jszip/dist": "@libs/jszip",
+        "@node_modules/devextreme": "@libs/devextreme",        
+        "@node_modules/devextreme-aspnet-data/js": "@libs/devextreme-aspnet-data"
+    }
+}
