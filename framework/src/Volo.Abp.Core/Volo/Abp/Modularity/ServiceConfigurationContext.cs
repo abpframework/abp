5325870dc9b26010d92e4915bdfdda9db374f600
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-07-06 11:47:53 +0300

    Added ServiceConfigurationContext

---
+++ framework/src/Volo.Abp.Core/Volo/Abp/Modularity/ServiceConfigurationContext.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Volo.Abp.Modularity
+{
+    public class ServiceConfigurationContext
+    {
+        public IServiceCollection Services { get; }
+
+        public ServiceConfigurationContext([NotNull] IServiceCollection services)
+        {
+            Services = Check.NotNull(services, nameof(services));
+        }
+    }
+}
