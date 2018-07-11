4fe7b3e7e517c525df0f88e29648843c25a99755
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-07-11 11:15:47 +0300

    Added localization resource to audit logging module

---
+++ modules/audit-logging/src/Volo.Abp.AuditLogging.Domain.Shared/Volo/Abp/AuditLogging/Localization/AuditLoggingResource.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Localization;
+
+namespace Volo.Abp.AuditLogging.Localization
+{
+    [LocalizationResourceName("AuditLogging")]
+    public class AuditLoggingResource
+    {
+    }
+}
