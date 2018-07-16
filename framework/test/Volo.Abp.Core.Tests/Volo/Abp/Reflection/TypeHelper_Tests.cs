b3ccf2c11f20fb3351b21bab6363f2f53e629045
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-07-16 16:07:37 +0300

    Added TypeHelper_Tests

---
+++ framework/test/Volo.Abp.Core.Tests/Volo/Abp/Reflection/TypeHelper_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+using Xunit;
+
+namespace Volo.Abp.Reflection
+{
+    public class TypeHelper_Tests
+    {
+        [Fact]
+        public void Should_Generic_Type_From_Nullable()
+        {
+            var nullableType = typeof(Guid?);
+            var guidType = nullableType.GetFirstGenericArgumentIfNullable();
+
+            guidType.ShouldBe(typeof(Guid));
+        }
+    }
+}
