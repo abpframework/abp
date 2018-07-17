0a28ff704b79e83596758a31d44af909eefa2b8a
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-07-17 16:18:56 +0300

    Audit logging paging

--- modules/audit-logging/src/Volo.Abp.AuditLogging.EntityFrameworkCore/Volo/Abp/AuditLogging/EntityFrameworkCore/EfCoreAuditLogRepository.cs
+++ modules/audit-logging/src/Volo.Abp.AuditLogging.EntityFrameworkCore/Volo/Abp/AuditLogging/EntityFrameworkCore/EfCoreAuditLogRepository.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -11,6 +15,31 @@ namespace Volo.Abp.AuditLogging.EntityFrameworkCore
             : base(dbContextProvider)
         {
 
+        }
+
+        public async Task<Paging.PagedResult<AuditLog>> GetListAsync(
+            string sorting = null,
+            int maxResultCount = 50,
+            int skipCount = 0,
+            string filter = null,
+            string httpMethod = null,
+            string url = null,
+            HttpStatusCode? httpStatusCode = null,
+            bool includeDetails = false)
+        {
+            var query = DbSet.AsNoTracking()
+                .IncludeDetails(includeDetails)
+                .WhereIf(httpMethod != null, q => q.HttpMethod == httpMethod)
+                .WhereIf(url != null, q => q.Url == url)
+                .WhereIf(httpStatusCode != null, q => q.HttpStatusCode == (decimal?) httpStatusCode);
+
+            var totalCount = await query.LongCountAsync();
+
+            var auditLogs = await query.OrderBy(sorting)
+                .PageBy(skipCount, maxResultCount)
+                .ToListAsync();
+
+            return new Paging.PagedResult<AuditLog>(totalCount, auditLogs);
         }
 
         public override IQueryable<AuditLog> WithDetails()
--- modules/audit-logging/src/Volo.Abp.AuditLogging.MongoDB/Volo/Abp/AuditLogging/MongoDB/MongoAuditLogRepository.cs
+++ modules/audit-logging/src/Volo.Abp.AuditLogging.MongoDB/Volo/Abp/AuditLogging/MongoDB/MongoAuditLogRepository.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Net;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 using Volo.Abp.Domain.Repositories.MongoDB;
 using Volo.Abp.MongoDB;
 
@@ -10,6 +16,23 @@ namespace Volo.Abp.AuditLogging.MongoDB
             : base(dbContextProvider)
         {
 
+        }
+
+        public async Task<Paging.PagedResult<AuditLog>> GetListAsync(string sorting = null, int maxResultCount = 50, int skipCount = 0, string filter = null,
+            string httpMethod = null, string url = null, HttpStatusCode? httpStatusCode = null, bool includeDetails = false)
+        {
+            var query = GetMongoQueryable()
+                .WhereIf(httpMethod != null, q => q.HttpMethod == httpMethod)
+                .WhereIf(url != null, q => q.Url == url)
+                .WhereIf(httpStatusCode != null && httpStatusCode > 0, q => q.HttpStatusCode == (int?)httpStatusCode);
+
+            var totalCount = await query.As<IMongoQueryable<AuditLog>>().LongCountAsync();
+
+            var questions = await query.OrderBy(sorting).As<IMongoQueryable<AuditLog>>()
+                .PageBy<AuditLog, IMongoQueryable<AuditLog>>(skipCount, maxResultCount)
+                .ToListAsync();
+
+            return new Paging.PagedResult<AuditLog>(totalCount, questions);
         }
     }
 }
