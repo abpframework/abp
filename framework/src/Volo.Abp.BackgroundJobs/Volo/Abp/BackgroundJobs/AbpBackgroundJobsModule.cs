8769aa08ac40a56b9e92becc96c089e003e36faf
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-07-23 13:46:56 +0300

    Added background jobs module as an empty project.

---
+++ framework/src/Volo.Abp.BackgroundJobs/Volo/Abp/BackgroundJobs/AbpBackgroundJobsModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.BackgroundJobs
+{
+    public class AbpBackgroundJobsModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAssemblyOf<AbpBackgroundJobsModule>();
+        }
+    }
+}
