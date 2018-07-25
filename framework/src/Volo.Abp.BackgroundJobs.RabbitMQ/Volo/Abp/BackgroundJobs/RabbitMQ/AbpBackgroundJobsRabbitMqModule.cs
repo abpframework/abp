d33459a7bcfdf1b7e43af9003831c6503d5a6e3a
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-07-25 17:32:48 +0300

    Created Volo.Abp.RabbitMQ package.

---
+++ framework/src/Volo.Abp.BackgroundJobs.RabbitMQ/Volo/Abp/BackgroundJobs/RabbitMQ/AbpBackgroundJobsRabbitMqModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+using Volo.Abp.RabbitMQ;
+
+namespace Volo.Abp.BackgroundJobs.RabbitMQ
+{
+    [DependsOn(
+        typeof(AbpBackgroundJobsAbstractionsModule),
+        typeof(AbpRabbitMqModule)
+    )]
+    public class AbpBackgroundJobsRabbitMqModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAssemblyOf<AbpBackgroundJobsRabbitMqModule>();
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.RabbitMQ/Volo/Abp/RabbitMQ/AbpBackgroundJobsRabbitMqModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.RabbitMQ
+{
+    public class AbpRabbitMqModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAssemblyOf<AbpRabbitMqModule>();
+        }
+    }
+}
