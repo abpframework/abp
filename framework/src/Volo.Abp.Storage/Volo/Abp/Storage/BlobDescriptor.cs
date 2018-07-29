8fd7cc9a30a4285f5bee596d81098d8b1b773b76
hitaspdotnet <mostafa.foruji@gmail.com>
2018-07-29 21:12:44 +0430

    Blob Storage abstraction.

---
+++ framework/src/Volo.Abp.Storage/Volo/Abp/Storage/BlobDescriptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Volo.Abp.Storage
+{
+    public class BlobDescriptor
+    {
+        public string ContentType { get; set; }
+
+        public string ContentMD5 { get; set; }
+
+        public string ContentDisposition { get; set; }
+
+        public string ETag { get; set; }
+
+        public long Length { get; set; }
+
+        public DateTimeOffset? LastModified { get; set; }
+
+        public BlobSecurity Security { get; set; }
+
+        public string Name { get; set; }
+
+        public string Container { get; set; }
+
+        public string Url { get; set; }
+
+        public IDictionary<string, string> Metadata { get; set; } = new Dictionary<string, string>();
+    }
+}
---
+++ framework/src/Volo.Abp.Storage/Volo/Abp/Storage/Enums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Volo.Abp.Storage
+{
+    public enum BlobSecurity
+    {
+        Private,
+        Public
+    }
+
+    [Flags]
+    public enum BlobUrlAccess
+    {
+        None = 0,
+        Read = 1,
+        Write = 2,
+        Delete = 4,
+        All = Read | Write | Delete,
+    }
+
+    public enum StorageErrorCode
+    {
+        None = 0,
+        InvalidCredentials = 1000,
+        GenericException = 1001,
+        InvalidAccess = 1002,
+        BlobInUse = 1003,
+        InvalidName = 1005,
+        ErrorOpeningBlob = 1006,
+        NoCredentialsProvided = 1007,
+        NotFound = 1008
+    }
+}
