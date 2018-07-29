95445fb779ca7867914c9840a2fcbbdb26ea7728
hitaspdotnet <mostafa.foruji@gmail.com>
2018-07-29 21:42:44 +0430

    Azure Blob Storage abstraction.

---
+++ framework/src/Volo.Abp.Storage.Azure/Volo/Abp/Storage/AbpAzureStorageModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.Storage
+{
+    [DependsOn(typeof(AbpStorageModule))]
+    public class AbpAzureStorageModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAssemblyOf<AbpAzureStorageModule>();
+        }
+    }
+}
