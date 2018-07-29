5b2311812cfe2047419a98b9397a24ef08785fb9
hitaspdotnet <mostafa.foruji@gmail.com>
2018-07-29 21:15:52 +0430

    Amazon Blob Storage abstraction.

---
+++ framework/src/Volo.Abp.Storage.Amazon/Volo/Abp/Storage/Amazon/AmazonProviderOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Volo.Abp.Storage.Amazon
+{
+    public class AmazonProviderOptions
+    {
+        public string PublicKey { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string Bucket { get; set; }
+
+        public string ServiceUrl { get; set; }
+
+        public string ServerSideEncryptionMethod { get; set; }
+
+        public string ProfileName { get; set; }
+
+        public TimeSpan? Timeout { get; set; }
+    }
+}
