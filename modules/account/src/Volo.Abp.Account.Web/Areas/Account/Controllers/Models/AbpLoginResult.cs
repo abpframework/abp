44a0282408a711e551fa5c9111e614d03cbf6433
Alper Ebicoglu <alperonline@hotmail.com>
2018-08-11 02:35:34 +0300

    Added LoginController for 3rd party apps to sign in

---
+++ modules/account/src/Volo.Abp.Account.Web/Areas/Account/Controllers/Models/AbpLoginResult.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp.Account.Web.Areas.Account.Controllers.Models;
+
+namespace Volo.Abp.Account.Web.Areas.Account.Controllers
+{
+    public class AbpLoginResult
+    {
+        public AbpLoginResult(LoginResultType result)
+        {
+            Result = result;
+        }
+
+        public string IdentityCookieToken { get; set; }
+
+        public LoginResultType Result { get; }
+
+        public string Description => Result.ToString();
+    }
+}
