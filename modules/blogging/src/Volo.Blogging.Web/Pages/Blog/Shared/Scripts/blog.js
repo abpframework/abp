de281335c0cf4040852de02240794fd4d493d59d
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-09-07 09:44:12 +0300

    Added blog title

--- modules/blogging/src/Volo.Blogging.Web/Pages/Blog/Shared/Scripts/blog.js
+++ modules/blogging/src/Volo.Blogging.Web/Pages/Blog/Shared/Scripts/blog.js
@@ -1,71 +1,67 @@
-function handleArrows() {
-    var herosWidth = $(".hero-articles").width();
-    var arrowsPosition = (herosWidth / 2 - 90);
-    $(".owl-next").css("right", arrowsPosition);
-    $(".owl-prev").css("left", arrowsPosition);
-}
-function handleImages() {
-    if ($(window).width() > 767) { 
-        $(".box-articles .img-container").each(function(){
-            var squareWidth = $(this).width(); 
-            $(this).css("height", squareWidth); 
+(function() {
+    function handleArrows() {
+        var herosWidth = $(".hero-articles").width();
+        var arrowsPosition = (herosWidth / 2 - 90);
+        $(".owl-next").css("right", arrowsPosition);
+        $(".owl-prev").css("left", arrowsPosition);
+    }
+
+    function handleImages() {
+        if ($(window).width() > 767) {
+            $(".box-articles .img-container").each(function () {
+                var squareWidth = $(this).width();
+                $(this).css("height", squareWidth);
+            });
+        }
+        else {
+            $(".box-articles .img-container").css("height", "auto");
+        }
+    }
+
+    function handleOwlCarousel() {
+        $('.hero-section .owl-carousel').owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: false,
+            dots: false,
+            //center: true, 
+            autoplay: true,
+            autoHeight: true,
+            autoplaySpeed: 1000,
+            items: 1
+        });
+        $('.card-article-container .owl-carousel').owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            responsive: {
+                0: {
+                    items: 1
+                }
+            }
         });
     }
-    else {
-        $(".box-articles .img-container").css("height", "auto");
-    }
-}
-function handleOwlCarousel() { 
-    $('.hero-section .owl-carousel').owlCarousel({
-        loop:true,
-        margin: 0,
-        nav: false,
-        dots: false,
-        //center: true, 
-        autoplay: true,
-        autoHeight: true,
-        autoplaySpeed: 1000,  
-        items:1,
-    });
-    $('.card-article-container .owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        responsive:{
-            0: { 
-                items:1
-            }, 
-        }
-    });
-}
- 
-$(document).ready(function(){    
-    setTimeout(function(){ 
-        handleArrows();  
-    }, 500);  
-    handleImages(); 
-    handleOwlCarousel(); 
-    var stickySidebar = new StickySidebar('#sidebar', {
-        topSpacing: 80,
-        bottomSpacing: 0,
-        containerSelector: false,
-        innerWrapperSelector: '.sidebar__inner',
-        resizeSensor: true,
-        stickyClass: 'is-affixed',
-        minWidth: 0
-    }); 
-});
- 
-$(window).resize(function(){  
-    setTimeout(function(){ 
-        handleArrows(); 
-        handleImages(); 
-    }, 500); 
-});
 
-$(".nav-link").on("click", function(){ 
-    $(this).parent().parent();
-});
+    $(function () {
+        handleImages();
+        handleOwlCarousel();
+
+        $(".nav-link").on("click", function () {
+            $(this).parent().parent();
+        });
+
+        $(window).resize(function () {
+            setTimeout(function () {
+                handleArrows();
+                handleImages();
+            }, 500);
+        });
+
+        setTimeout(function () {
+            handleArrows();
+        }, 500);
+    });
+})();
