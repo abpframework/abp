1b3384a8ffa328779e0b8d0df56343aeb09bccf7
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-10-12 10:43:30 +0300

    Add missing comma

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -131,7 +131,7 @@ $projects = (
     "modules/identityserver/src/Volo.Abp.IdentityServer.Domain.Shared",
     "modules/identityserver/src/Volo.Abp.IdentityServer.Domain",
     "modules/identityserver/src/Volo.Abp.IdentityServer.EntityFrameworkCore", 
-    "modules/identityserver/src/Volo.Abp.IdentityServer.MongoDB"
+    "modules/identityserver/src/Volo.Abp.IdentityServer.MongoDB",
 
     # modules/account
     "modules/account/src/Volo.Abp.Account.Web",
