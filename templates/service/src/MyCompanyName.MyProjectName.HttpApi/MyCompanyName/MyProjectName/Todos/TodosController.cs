96ee0b04368bc21fc595554758d1e9a4ca428249
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-10-30 17:47:44 +0300

    #377 Create a sample API.

---
+++ templates/service/src/MyCompanyName.MyProjectName.HttpApi/MyCompanyName/MyProjectName/Todos/TodosController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace MyCompanyName.MyProjectName.Todos
+{
+    [RemoteService]
+    [Area("MyProjectName")]
+    [Route("api/MyProjectName/todos")]
+    public class TodosController : AbpController
+    {
+        private readonly ITodoAppService _todoAppService;
+
+        public TodosController(ITodoAppService todoAppService)
+        {
+            _todoAppService = todoAppService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public Task<PagedResultDto<TodoDto>> GetListAsync()
+        {
+            return _todoAppService.GetListAsync();
+        }
+    }
+}
