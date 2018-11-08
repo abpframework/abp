4de7ca3fa0027f04f3ba1c4b001448a452b28d95
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-11-08 20:59:48 +0300

    Add SmsSenderExtensions and add constructor to the SmsMessage class.

--- framework/src/Volo.Abp.Sms/Volo/Abp/Sms/SmsMessage.cs
+++ framework/src/Volo.Abp.Sms/Volo/Abp/Sms/SmsMessage.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace Volo.Abp.Sms
 {
     public class SmsMessage
     {
-        public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; }
 
-        public string Text { get; set; }
+        public string Text { get; }
 
-        public IDictionary<string, object> Properties { get; set; }
+        public IDictionary<string, object> Properties { get; }
+
+        public SmsMessage([NotNull] string phoneNumber, [NotNull] string text)
+        {
+            PhoneNumber = Check.NotNullOrWhiteSpace(phoneNumber, nameof(phoneNumber));
+            Text = Check.NotNullOrWhiteSpace(text, nameof(text));
+
+            Properties = new Dictionary<string, object>();
+        }
     }
 }
