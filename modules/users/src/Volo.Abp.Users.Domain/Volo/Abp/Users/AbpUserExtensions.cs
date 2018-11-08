4aaecf68ec65cf0a450e931687b918e3738e7b60
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-11-08 14:35:01 +0300

    Fix order

--- modules/users/src/Volo.Abp.Users.Domain/Volo/Abp/Users/AbpUserExtensions.cs
+++ modules/users/src/Volo.Abp.Users.Domain/Volo/Abp/Users/AbpUserExtensions.cs
@@ -5,15 +5,15 @@ namespace Volo.Abp.Users
         public static IUserData ToAbpUserData(this IUser user)
         {
             return new UserData(
-                user.Id,
-                user.UserName,
-                user.Name,
-                user.Surname,
-                user.Email,
-                user.EmailConfirmed,
-                user.PhoneNumber,
-                user.PhoneNumberConfirmed,
-                user.TenantId
+                id: user.Id,
+                userName: user.UserName,
+                email: user.Email,
+                name: user.Name,
+                surname: user.Surname,
+                emailConfirmed: user.EmailConfirmed,
+                phoneNumber: user.PhoneNumber,
+                phoneNumberConfirmed: user.PhoneNumberConfirmed,
+                tenantId: user.TenantId
             );
         }
     }
