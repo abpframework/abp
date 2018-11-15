5250f0a48dcec79ec89927a7455e20892ba1940c
Adem <ademsis@yahoo.com>
2018-11-15 17:17:17 +0300

    postgresql added

---
+++ framework/src/Volo.Abp.EntityFrameworkCore.PostgreSql/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextPostgreSqlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+using Volo.Abp.EntityFrameworkCore.DependencyInjection;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextConfigurationContextPostgreSqlExtensions
+    {
+        public static DbContextOptionsBuilder UsePostgreSql(
+            [NotNull] this AbpDbContextConfigurationContext context,
+            [CanBeNull] Action<NpgsqlDbContextOptionsBuilder> sqlServerOptionsAction = null)
+        {
+            if (context.ExistingConnection != null)
+            {
+                return context.DbContextOptions.UseNpgsql(context.ExistingConnection, sqlServerOptionsAction);
+            }
+            else
+            {
+                return context.DbContextOptions.UseNpgsql(context.ConnectionString, sqlServerOptionsAction);
+            }
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.EntityFrameworkCore.PostgreSql/Volo/Abp/EntityFrameworkCore/AbpDbContextOptionsSqlServerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JetBrains.Annotations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextOptionsSqlServerExtensions
+    {
+        public static void UsePostgreSql(
+            [NotNull] this AbpDbContextOptions options,
+            [CanBeNull] Action<NpgsqlDbContextOptionsBuilder> sqlServerOptionsAction = null)
+        {
+            options.Configure(context =>
+            {
+                context.UsePostgreSql(sqlServerOptionsAction);
+            });
+        }
+
+        public static void UsePostgreSql<TDbContext>(
+            [NotNull] this AbpDbContextOptions options,
+            [CanBeNull] Action<NpgsqlDbContextOptionsBuilder> sqlServerOptionsAction = null)
+            where TDbContext : AbpDbContext<TDbContext>
+        {
+            options.Configure<TDbContext>(context =>
+            {
+                context.UsePostgreSql(sqlServerOptionsAction);
+            });
+        }
+    }
+}
