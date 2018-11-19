762c909027dc7cc1e010e1c7af511d7bac6b34bc
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-11-19 16:10:23 +0300

    Update common.ps1

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -57,7 +57,7 @@ $projects = (
     "framework/src/Volo.Abp.EntityFrameworkCore.MySQL",
     "framework/src/Volo.Abp.EntityFrameworkCore.PostgreSql",
     "framework/src/Volo.Abp.EventBus",
-    "framework/src/Volo.Abp.EventBus.RabbitMQ",
+    "framework/src/Volo.Abp.EventBus.Distributed.RabbitMQ",
     "framework/src/Volo.Abp.Guids",
     "framework/src/Volo.Abp.HangFire",
     "framework/src/Volo.Abp.Http",
