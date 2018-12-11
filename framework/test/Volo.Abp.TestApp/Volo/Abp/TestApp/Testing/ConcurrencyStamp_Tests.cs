0c1aaccebb121b678ce5fe78bfc126b4cc1738d4
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-12-11 17:08:51 +0300

    ConfigureExtraProperties for test aggregate roots.

---
+++ framework/test/Volo.Abp.TestApp/Volo/Abp/TestApp/Testing/ConcurrencyStamp_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+using Volo.Abp.TestApp.Domain;
+using Xunit;
+
+namespace Volo.Abp.TestApp.Testing
+{
+    public class ConcurrencyStamp_Tests<TStartupModule> : TestAppTestBase<TStartupModule>
+        where TStartupModule : IAbpModule
+    {
+        protected readonly ICityRepository CityRepository;
+
+        public ConcurrencyStamp_Tests()
+        {
+            CityRepository = GetRequiredService<ICityRepository>();
+        }
+
+        [Fact]
+        public async Task Should_Not_Allow_To_Update_If_The_Entity_Has_Changed()
+        {
+            var london = await CityRepository.FindByNameAsync("London");
+            london.Name = "London-1";
+            await CityRepository.UpdateAsync(london);
+        }
+    }
+}
