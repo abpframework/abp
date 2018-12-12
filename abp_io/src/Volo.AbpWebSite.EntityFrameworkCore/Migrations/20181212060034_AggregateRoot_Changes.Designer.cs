66ed2f034c7bf91ce0417ffd077f902acad28413
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-12-12 09:01:14 +0300

    Abp web site AggregateRoot changes

--- abp_io/src/Volo.AbpWebSite.EntityFrameworkCore/Migrations/20181212060034_AggregateRoot_Changes.Designer.cs
+++ abp_io/src/Volo.AbpWebSite.EntityFrameworkCore/Migrations/20181212060034_AggregateRoot_Changes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.AbpWebSite.EntityFrameworkCore;
 
 namespace Volo.AbpWebSite.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(AbpWebSiteDbContext))]
-    partial class AbpWebSiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181212060034_AggregateRoot_Changes")]
+    partial class AggregateRoot_Changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
