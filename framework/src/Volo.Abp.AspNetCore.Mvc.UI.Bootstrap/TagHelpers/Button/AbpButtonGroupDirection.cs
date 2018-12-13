f9bf0960ec61662302a0501c35de862ce5d61ab9
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-12-13 14:50:37 +0300

    Added abp-button-group tag helper

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Button/AbpButtonGroupDirection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Button
+{
+    public enum AbpButtonGroupDirection
+    {
+        Horizontal,
+        Vertical
+    }
+}
