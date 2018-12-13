ac3d7c7c16ea0d24fa818e12345d4d68e929980b
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-12-13 14:10:56 +0300

    Resolved #203: Encrypt option to settings

--- framework/src/Volo.Abp.Settings/Volo/Abp/Settings/AbpSettingsModule.cs
+++ framework/src/Volo.Abp.Settings/Volo/Abp/Settings/AbpSettingsModule.cs
@@ -1,10 +1,12 @@
 ﻿using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.Security;
 
 namespace Volo.Abp.Settings
 {
     [DependsOn(
-        typeof(AbpLocalizationAbstractionsModule)
+        typeof(AbpLocalizationAbstractionsModule),
+        typeof(AbpSecurityModule)
         )]
     public class AbpSettingsModule : AbpModule
     {
