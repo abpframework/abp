da066da522cd890365e178b8a439e57c6752d7f0
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-12-14 11:32:10 +0300

    Remove abp-alert-link attribute at the end of process

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Alert/AbpAlertLinkTagHelperService.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Alert/AbpAlertLinkTagHelperService.cs
@@ -8,6 +8,7 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Alert
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.Attributes.AddClass("alert-link");
+            output.Attributes.RemoveAll("abp-alert-link");
         }
     }
 }
