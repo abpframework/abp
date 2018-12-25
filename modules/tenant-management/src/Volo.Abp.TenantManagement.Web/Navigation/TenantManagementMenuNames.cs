0a5a3133e07e8b03d8092bd2ae025315e2e322b5
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2018-12-25 16:33:34 +0300

    Resolved #686 #687

---
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.Web/Navigation/TenantManagementMenuNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Volo.Abp.TenantManagement.Web.Navigation
+{
+    public class TenantManagementMenuNames
+    {
+        public const string GroupName = "TenantManagement";
+
+        public const string Tenants = GroupName + ".Tenants";
+    }
+}
