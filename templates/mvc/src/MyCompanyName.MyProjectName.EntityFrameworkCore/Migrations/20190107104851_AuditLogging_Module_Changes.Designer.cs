95dcc1fc6efe7374cbb06c91ada5766d8cca28b3
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-01-07 13:50:41 +0300

    mvc template migration

--- templates/mvc/src/MyCompanyName.MyProjectName.EntityFrameworkCore/Migrations/20190107104851_AuditLogging_Module_Changes.Designer.cs
+++ templates/mvc/src/MyCompanyName.MyProjectName.EntityFrameworkCore/Migrations/20190107104851_AuditLogging_Module_Changes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCompanyName.MyProjectName.EntityFrameworkCore;
 
 namespace MyCompanyName.MyProjectName.Migrations
 {
     [DbContext(typeof(MyProjectNameDbContext))]
-    partial class MyProjectNameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190107104851_AuditLogging_Module_Changes")]
+    partial class AuditLogging_Module_Changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
