f1ec845038c26671995d070c6c327539d2af152b
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-01-10 11:49:40 +0300

    Resolved #724 Replace AddAlwaysAllowPermissionChecker usage by AddAlwaysAllowAuthorization

--- modules/tenant-management/test/Volo.Abp.TenantManagement.Application.Tests/Volo/Abp/TenantManagement/AbpTenantManagementApplicationTestModule.cs
+++ modules/tenant-management/test/Volo.Abp.TenantManagement.Application.Tests/Volo/Abp/TenantManagement/AbpTenantManagementApplicationTestModule.cs
@@ -11,7 +11,7 @@ namespace Volo.Abp.TenantManagement
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            context.Services.AddAlwaysAllowPermissionChecker();
+            context.Services.AddAlwaysAllowAuthorization();
         }
     }
 }
