a3f0c118976b36998bbe04d72ccddad67a15e4e5
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-01-17 13:55:03 +0300

    Make the AuthServer working.

--- samples/MicroserviceDemo/applications/authserver/AuthServer.Host/AuthServerHostModule.cs
+++ samples/MicroserviceDemo/applications/authserver/AuthServer.Host/AuthServerHostModule.cs
@@ -5,20 +5,29 @@ using Volo.Abp;
 using Volo.Abp.Account.Web;
 using Volo.Abp.AspNetCore.Modularity;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.Autofac;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.SqlServer;
+using Volo.Abp.Identity;
 using Volo.Abp.Identity.AspNetCore;
 using Volo.Abp.Identity.EntityFrameworkCore;
 using Volo.Abp.IdentityServer.EntityFrameworkCore;
+using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+using Volo.Abp.Threading;
 
 namespace AuthServer.Host
 {
     [DependsOn(
         typeof(AbpAutofacModule),
-        typeof(AbpIdentityAspNetCoreModule),
+        typeof(AbpPermissionManagementEntityFrameworkCoreModule),
+        typeof(AbpAuditLoggingEntityFrameworkCoreModule),
+        typeof(AbpSettingManagementEntityFrameworkCoreModule),
         typeof(AbpIdentityEntityFrameworkCoreModule),
+        typeof(AbpIdentityApplicationContractsModule),
         typeof(AbpIdentityServerEntityFrameworkCoreModule),
         typeof(AbpEntityFrameworkCoreSqlServerModule),
         typeof(AbpAccountWebModule),
@@ -33,13 +42,14 @@ namespace AuthServer.Host
                 options.AddDefaultRepositories();
             });
 
-            context.Services.Configure<AbpDbContextOptions>(options =>
+            Configure<AbpDbContextOptions>(options =>
             {
-                //Configures defaults for all EF Core DbContexts in this application
-                options.Configure(opts =>
-                {
-                    opts.UseSqlServer();
-                });
+                options.UseSqlServer();
+            });
+
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Languages.Add(new LanguageInfo("en", "en", "English"));
             });
         }
 
@@ -48,10 +58,21 @@ namespace AuthServer.Host
             var app = context.GetApplicationBuilder();
 
             app.UseVirtualFiles();
-            //app.UseIdentityServer(); //TODO: Enable
+            app.UseIdentityServer(); //TODO: Enable this and disable UseAuthentication
             app.UseAbpRequestLocalization();
             app.UseAuditing();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvcWithDefaultRouteAndArea();
+
+            //TODO: Problem on a clustered environment
+            AsyncHelper.RunSync(async () =>
+            {
+                await context.ServiceProvider
+                    .GetRequiredService<IIdentityDataSeeder>()
+                    .SeedAsync(
+                        "1q2w3E*",
+                        IdentityPermissions.GetAll()
+                    );
+            });
         }
     }
 }
