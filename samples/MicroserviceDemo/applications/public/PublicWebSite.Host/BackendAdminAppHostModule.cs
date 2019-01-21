a56d14a6fa5a4b86373ff88177c08120ca93ff32
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-01-21 11:47:15 +0300

    Added PublicWebSite.Host project

---
+++ samples/MicroserviceDemo/applications/public/PublicWebSite.Host/BackendAdminAppHostModule.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Authentication.OAuth;
+using Volo.Abp.AspNetCore.Modularity;
+using Volo.Abp.AspNetCore.Mvc.Client;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+using Volo.Abp.Autofac;
+using Volo.Abp.Http.Client.IdentityModel;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+
+namespace PublicWebSite.Host
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(AbpAspNetCoreMvcClientModule),
+        typeof(AbpAspNetCoreAuthenticationOAuthModule),
+        typeof(AbpHttpClientIdentityModelModule),
+        typeof(AbpAspNetCoreMvcUiBasicThemeModule)
+        )]
+    public class BackendAdminAppHostModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Languages.Add(new LanguageInfo("en", "en", "English"));
+            });
+
+            context.Services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = "http://localhost:64999";
+                    options.RequireHttpsMetadata = false;
+                    options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+
+                    options.ClientId = "public-website-client";
+                    options.ClientSecret = "1q2w3e*";
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Scope.Add("role");
+                    options.Scope.Add("email");
+                    options.Scope.Add("phone");
+                    //options.Scope.Add("ProductService"); //TODO: Enable once available
+
+                    options.ClaimActions.MapAbpClaimTypes();
+                });
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+
+            app.UseVirtualFiles();
+            app.UseAuthentication();
+            app.UseAbpRequestLocalization();
+            app.UseMvcWithDefaultRouteAndArea();
+        }
+    }
+}
