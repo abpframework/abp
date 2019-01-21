66566aabdd343c1698c2e43873f9096e027e9c70
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-01-21 11:36:43 +0300

    Completed initial BackendAdminApp.Host project.

--- samples/MicroserviceDemo/applications/backend/BackendAdminApp.Host/Startup.cs
+++ samples/MicroserviceDemo/applications/backend/BackendAdminApp.Host/Startup.cs
@@ -1,34 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Volo.Abp;
 
 namespace BackendAdminApp.Host
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication<BackendAdminAppHostModule>(options =>
+            {
+                options.UseAutofac();
+            });
+
+            return services.BuildServiceProviderFromFactory();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.InitializeApplication();
         }
     }
 }
