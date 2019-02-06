fa17332ff32939869782cd82f4d090b9c2e74601
Marcelo Mohr Maciel <marcelo.mohr.maciel@gmail.com>
2019-02-06 21:37:33 -0200

    adicionando PT-BR

--- modules/account/src/Volo.Abp.Account.Web/Pages/Account/Login.cshtml.cs
+++ modules/account/src/Volo.Abp.Account.Web/Pages/Account/Login.cshtml.cs
@@ -14,6 +14,7 @@ using Volo.Abp.Identity;
 using Volo.Abp.Security.Claims;
 using Volo.Abp.Uow;
 using Volo.Abp.Validation;
+using Microsoft.AspNetCore.Mvc.Localization;
 
 namespace Volo.Abp.Account.Web.Pages.Account
 {
