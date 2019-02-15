0775b93e337bb0a98a197d26dad1cfcd88196af5
Atakan Özceviz <atakanozceviz@hotmail.com>
2019-02-15 15:14:23 +0100

    Fixes RedirectUris when running in Docker

--- samples/MicroserviceDemo/databases/restore/entrypoint.sh
+++ samples/MicroserviceDemo/databases/restore/entrypoint.sh
@@ -21,3 +21,11 @@ done
    -Q 'RESTORE DATABASE MsDemo_ProductManagement FROM DISK = "/var/opt/mssql/backup/MsDemo_ProductManagement.bak" WITH REPLACE,
          MOVE "MsDemo_ProductManagement" TO "/var/opt/mssql/data/MsDemo_ProductManagement.mdf", 
          MOVE "MsDemo_ProductManagement_log" TO "/var/opt/mssql/data/MsDemo_ProductManagement_log.ldf"'
+
+/opt/mssql-tools/bin/sqlcmd \
+   -S sqlserver -U SA -P $SA_PASSWORD \
+   -d MsDemo_Identity \
+   -Q 'UPDATE IdentityServerClientRedirectUris SET RedirectUri = "http://localhost:51512/signin-oidc" WHERE ClientId = "00265494-2D70-9615-4BD0-39EB2AF3CD33"
+       UPDATE IdentityServerClientRedirectUris SET RedirectUri = "http://localhost:51513/signin-oidc" WHERE ClientId = "10265494-2D70-9615-4BD0-39EB2AF3CD33"
+       UPDATE IdentityServerClientPostLogoutRedirectUris SET PostLogoutRedirectUri = "http://localhost:51512/signout-callback-oidc" WHERE ClientId = "00265494-2D70-9615-4BD0-39EB2AF3CD33"
+       UPDATE IdentityServerClientPostLogoutRedirectUris SET PostLogoutRedirectUri = "http://localhost:51513/signout-callback-oidc" WHERE ClientId = "10265494-2D70-9615-4BD0-39EB2AF3CD33"'
