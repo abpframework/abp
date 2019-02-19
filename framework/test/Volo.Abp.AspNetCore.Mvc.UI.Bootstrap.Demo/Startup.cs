01ccdcf7c5f0aa80275bae7a59cf898a7c78163a
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-02-19 11:22:05 +0300

    Use Autofac in the Bootstrap.Demo project

--- framework/test/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Demo/Startup.cs
+++ framework/test/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Demo/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
