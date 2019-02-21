853aab9704c5c453727c0a3fc9a3d84359732d3f
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-02-21 16:49:49 +0300

    Remove unused GetListPagedAsync

--- modules/blogging/src/Volo.Blogging.Domain/Volo/Blogging/Blogs/IBlogRepository.cs
+++ modules/blogging/src/Volo.Blogging.Domain/Volo/Blogging/Blogs/IBlogRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
 
@@ -8,8 +7,6 @@ namespace Volo.Blogging.Blogs
     public interface IBlogRepository : IBasicRepository<Blog, Guid>
     {
         Task<Blog> FindByShortNameAsync(string shortName);
-
-        Task<List<Blog>> GetListAsync(string sorting, int maxResultCount, int skipCount);
 
         Task<int> GetTotalCount();
     }
