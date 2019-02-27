79da56dd1329d22c11741cbbddd9e336f2214483
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-02-27 10:48:38 +0300

    Simplify permission value providers.

--- framework/src/Volo.Abp.MultiTenancy.Abstractions/Volo/Abp/MultiTenancy/AbpMultiTenancyAbstractionsModule.cs
+++ framework/src/Volo.Abp.MultiTenancy.Abstractions/Volo/Abp/MultiTenancy/AbpMultiTenancyAbstractionsModule.cs
@@ -1,10 +1,12 @@
-﻿using Volo.Abp.Data;
+﻿using Volo.Abp.Authorization;
+using Volo.Abp.Data;
 using Volo.Abp.Modularity;
 
 namespace Volo.Abp.MultiTenancy
 {
     [DependsOn(
-        typeof(AbpDataModule)
+        typeof(AbpDataModule),
+        typeof(AbpAuthorizationModule)
         )]
     public class AbpMultiTenancyAbstractionsModule : AbpModule //TODO: Rename to AbpMultiTenancyModule?
     {
