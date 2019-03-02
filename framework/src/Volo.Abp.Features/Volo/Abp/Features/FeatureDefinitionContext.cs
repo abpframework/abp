0589bbf38903523b92ca356274c649a89ce81e3b
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-03-02 19:00:16 +0300

    Initial feature system implemented

---
+++ framework/src/Volo.Abp.Features/Volo/Abp/Features/FeatureDefinitionContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Volo.Abp.Features
+{
+    public class FeatureDefinitionContext : IFeatureDefinitionContext
+    {
+        protected Dictionary<string, FeatureDefinition> Features { get; }
+
+        public FeatureDefinitionContext(Dictionary<string, FeatureDefinition> features)
+        {
+            Features = features;
+        }
+
+        public virtual FeatureDefinition GetOrNull(string name)
+        {
+            return Features.GetOrDefault(name);
+        }
+
+        public virtual IReadOnlyList<FeatureDefinition> GetAll()
+        {
+            return Features.Values.ToImmutableList();
+        }
+
+        public virtual void Add(params FeatureDefinition[] definitions)
+        {
+            if (definitions.IsNullOrEmpty())
+            {
+                return;
+            }
+
+            foreach (var definition in definitions)
+            {
+                Features[definition.Name] = definition;
+            }
+        }
+    }
+}
