dda709bcb4274647e254fffad06503d278da0306
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-03-04 11:03:37 +0300

    Auto register setting definition providers.

--- framework/src/Volo.Abp.Settings/Volo/Abp/Settings/AbpSettingsModule.cs
+++ framework/src/Volo.Abp.Settings/Volo/Abp/Settings/AbpSettingsModule.cs
@@ -1,4 +1,7 @@
-﻿using Volo.Abp.Localization;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
 using Volo.Abp.MultiTenancy;
 using Volo.Abp.Security;
@@ -12,6 +15,11 @@ namespace Volo.Abp.Settings
         )]
     public class AbpSettingsModule : AbpModule
     {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            AutoAddDefinitionProviders(context.Services);
+        }
+
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             Configure<SettingOptions>(options =>
@@ -20,6 +28,24 @@ namespace Volo.Abp.Settings
                 options.ValueProviders.Add<GlobalSettingValueProvider>();
                 options.ValueProviders.Add<TenantSettingValueProvider>();
                 options.ValueProviders.Add<UserSettingValueProvider>();
+            });
+        }
+
+        private static void AutoAddDefinitionProviders(IServiceCollection services)
+        {
+            var definitionProviders = new List<Type>();
+
+            services.OnRegistred(context =>
+            {
+                if (typeof(ISettingDefinitionProvider).IsAssignableFrom(context.ImplementationType))
+                {
+                    definitionProviders.Add(context.ImplementationType);
+                }
+            });
+
+            services.Configure<SettingOptions>(options =>
+            {
+                options.DefinitionProviders.AddIfNotContains(definitionProviders);
             });
         }
     }
