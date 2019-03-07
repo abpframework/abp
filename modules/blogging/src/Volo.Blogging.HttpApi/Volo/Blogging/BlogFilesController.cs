590c68d67850b80b366d69a8a24843aa8bf4c570
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-03-07 11:25:20 +0300

    Added BlogFilesController

---
+++ modules/blogging/src/Volo.Blogging.HttpApi/Volo/Blogging/BlogFilesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Blogging.Files;
+
+namespace Volo.Blogging
+{
+    [RemoteService]
+    [Area("blogging")]
+    [Route("api/blogging/files")]
+    public class BlogFilesController : AbpController, IFileAppService
+    {
+        private readonly IFileAppService _fileAppService;
+
+        public BlogFilesController(IFileAppService fileAppService)
+        {
+            _fileAppService = fileAppService;
+        }
+
+        [HttpPost]
+        public Task<FileUploadOutputDto> UploadAsync(FileUploadInputDto input)
+        {
+            return _fileAppService.UploadAsync(input);
+        }
+    }
+}
