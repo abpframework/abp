7e1d53f35aed644a347ffe5319cae1a043c38ef1
Bruno Bertechini <bruno.bertechini@outlook.com>
2019-03-15 21:55:34 -0300

    mvc: added serializable attribute and unit test to check all classes in contracts project

--- framework/src/Volo.Abp.AspNetCore.Mvc.Contracts/Volo/Abp/AspNetCore/Mvc/ApplicationConfigurations/ApplicationFeatureConfigurationDto.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.Contracts/Volo/Abp/AspNetCore/Mvc/ApplicationConfigurations/ApplicationFeatureConfigurationDto.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Volo.Abp.AspNetCore.Mvc.ApplicationConfigurations
 {
+    [Serializable]
     public class ApplicationFeatureConfigurationDto
     {
         public Dictionary<string, string> Values { get; set; }
---
+++ framework/test/Volo.Abp.AspNetCore.Mvc.Tests/Volo/Abp/AspNetCore/Mvc/Contracts/ClassSerialization_Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Volo.Abp.Modularity;
+using Volo.Abp.Reflection;
+using Volo.Abp.Serialization;
+using Xunit;
+
+namespace Volo.Abp.AspNetCore.Mvc.Contracts
+{
+    public class ClassSerialization_Tests : AspNetCoreMvcTestBase
+    {
+
+
+        [Fact]
+        public void Should_Have_Serializable_Attribute()
+        {
+            var assemblyFinder = GetRequiredService<IAssemblyFinder>();
+
+            var assembly = assemblyFinder.Assemblies
+                                         .Where(x => x.ManifestModule.Name == "Volo.Abp.AspNetCore.Mvc.Contracts.dll")
+                                         .Single();
+
+            var classes = assembly.DefinedTypes
+                                  .Where(x => x.IsClass)
+                                  .Where(x => !x.IsAssignableTo<AbpModule>())
+                                  .ToList();
+
+            foreach (var typeInfo in classes)
+            {
+                var attr = typeInfo.GetCustomAttribute<SerializableAttribute>();
+                if (attr == null)
+                    throw new Exception($"Class {typeInfo.FullName} does not contain [Serializable] attribute.");
+            }
+        }
+    }
+}
