acf84e5c402efa6ff5069e2f3e9f85a6f870c2b4
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-03-31 18:09:01 +0300

    Respect to multitenancy side while getting & setting permissions.

--- modules/permission-management/src/Volo.Abp.PermissionManagement.Domain/Volo/Abp/PermissionManagement/PermissionDataSeedContributor.cs
+++ modules/permission-management/src/Volo.Abp.PermissionManagement.Domain/Volo/Abp/PermissionManagement/PermissionDataSeedContributor.cs
@@ -3,28 +3,34 @@ using System.Threading.Tasks;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
 
 namespace Volo.Abp.PermissionManagement
 {
     public class PermissionDataSeedContributor : IDataSeedContributor, ITransientDependency
     {
+        protected ICurrentTenant CurrentTenant { get; }
+
         protected IPermissionDefinitionManager PermissionDefinitionManager { get; }
         protected IPermissionDataSeeder PermissionDataSeeder { get; }
 
         public PermissionDataSeedContributor(
             IPermissionDefinitionManager permissionDefinitionManager,
-            IPermissionDataSeeder permissionDataSeeder)
+            IPermissionDataSeeder permissionDataSeeder,
+            ICurrentTenant currentTenant)
         {
             PermissionDefinitionManager = permissionDefinitionManager;
             PermissionDataSeeder = permissionDataSeeder;
+            CurrentTenant = currentTenant;
         }
 
-        public Task SeedAsync(DataSeedContext context)
+        public virtual Task SeedAsync(DataSeedContext context)
         {
+            var multiTenancySide = CurrentTenant.GetMultiTenancySide();
             var permissionNames = PermissionDefinitionManager
                 .GetPermissions()
+                .Where(p => p.MultiTenancySide.HasFlag(multiTenancySide))
                 .Select(p => p.Name)
-                //TODO: Filter host/tenant permissions!
                 .ToArray();
 
             return PermissionDataSeeder.SeedAsync(
