50f08a74f8a7ac29c3834fa99d6512e46d9cfaf5
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-04-07 19:30:04 +0300

    remove unused namespace

--- modules/client-simulation/src/Volo.ClientSimulation/Volo/ClientSimulation/Scenarios/ScenarioStep.cs
+++ modules/client-simulation/src/Volo.ClientSimulation/Volo/ClientSimulation/Scenarios/ScenarioStep.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Resources;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
