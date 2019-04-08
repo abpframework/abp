c4ae50dbec5067c47c0160d26b7fe7ed441e7ff7
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-04-08 14:28:56 +0300

    #979 Make Post.CreatorId nullable.

--- modules/blogging/src/Volo.Blogging.Application/Volo/Blogging/Posts/PostAppService.cs
+++ modules/blogging/src/Volo.Blogging.Application/Volo/Blogging/Posts/PostAppService.cs
@@ -156,7 +156,6 @@ namespace Volo.Blogging.Posts
             var post = new Post(
                 id: GuidGenerator.Create(),
                 blogId: input.BlogId,
-                creatorId: CurrentUser.Id,
                 title: input.Title,
                 coverImage: input.CoverImage,
                 url: input.Url
