f91da9ea9030ce65d3ec3e0789e7657ec20c410d
maliming <malimings@gmail.com>
2019-04-09 14:11:25 +0800

    PermissionManagement  application unit tests.

---
+++ modules/permission-management/test/Volo.Abp.PermissionManagement.Application.Tests/Volo/Abp/PermissionManagement/AbpPermissionManagementApplicationTestModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.PermissionManagement
+{
+    [DependsOn(
+        typeof(AbpPermissionManagementApplicationModule),
+        typeof(AbpPermissionManagementTestModule)
+    )]
+    public class AbpPermissionManagementApplicationTestModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAlwaysAllowAuthorization();
+
+            context.Services.Configure<PermissionManagementOptions>(options =>
+            {
+                options.ProviderPolicies["Test"] = "Test";
+                options.ManagementProviders.Add<TestPermissionManagementProvider>();
+
+            });
+        }
+    }
+}
