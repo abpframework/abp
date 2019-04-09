d75c98dcb8a6875bad5cb66ec0fcd0dfb4147685
Alper Ebicoglu <ebicoglu@users.noreply.github.com>
2019-04-09 09:53:53 +0300

    removed configuration page.

--- modules/docs/app/VoloDocs/Pages/Index.cshtml.cs
+++ modules/docs/app/VoloDocs/Pages/Index.cshtml.cs
@@ -1,11 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
-using Volo.Abp.Application.Dtos;
 using Volo.Abp.Data;
 using Volo.Abp.Users;
 using Volo.Docs;
@@ -16,14 +13,11 @@ namespace VoloDocs.Pages
     public class IndexModel : PageModel
     {
         public IReadOnlyList<ProjectDto> Projects { get; set; }
-        public string ConnectionString { get; set; }
         public string CreateProjectLink { get; set; }
-
-
 
         private readonly IProjectAppService _projectAppService;
         private readonly DbConnectionOptions _dbConnectionOptions;
-        private readonly  ICurrentUser _currentUser;
+        private readonly ICurrentUser _currentUser;
 
         public IndexModel(IProjectAppService projectAppService,
             IOptionsSnapshot<DbConnectionOptions> dbConnectionOptions, ICurrentUser currentUser)
@@ -35,41 +29,21 @@ namespace VoloDocs.Pages
 
         public async Task<IActionResult> OnGet()
         {
-            ConnectionString = _dbConnectionOptions.ConnectionStrings.Default;
-            if (string.IsNullOrWhiteSpace(ConnectionString))
-            {
-                return RedirectToPage("./Configure");
-            }
-
             CreateProjectLink = _currentUser.Id.HasValue
                 ? "/Docs/Admin/Projects"
                 : "/Account/Login?returnUrl=/Docs/Admin/Projects";
-           
+
             Projects = (await _projectAppService.GetListAsync()).Items;
+
             if (Projects.Count == 1)
             {
-                return RedirectToPage("./Project/Index", new
+                return RedirectToPage("./Documents/Project/Index", new
                 {
                     projectName = Projects[0].ShortName,
                     version = DocsAppConsts.Latest,
                     documentName = Projects[0].DefaultDocumentName
                 });
             }
-
-          
-
-            
-            //if (!Projects.Any())
-            //{
-            //    if (_currentUser.Id.HasValue)
-            //    {
-            //        return Redirect("./Docs/Admin/Projects");
-            //    }
-
-            //    return Redirect("./account/login?returnUrl=/Docs/Admin/Projects");
-            //}
-
-           
 
             return Page();
         }
