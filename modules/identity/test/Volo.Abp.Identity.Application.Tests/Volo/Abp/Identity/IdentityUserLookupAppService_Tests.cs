0eeb8381b365fba34e8757e2430ce92f096f34f2
maliming <malimings@gmail.com>
2019-04-10 15:57:30 +0800

    Add Volo.Abp.Identity unit tests.

---
+++ modules/identity/test/Volo.Abp.Identity.Application.Tests/Volo/Abp/Identity/IdentityUserLookupAppService_Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Shouldly;
+using Xunit;
+
+namespace Volo.Abp.Identity
+{
+    public class IdentityUserLookupAppService_Tests : AbpIdentityApplicationTestBase
+    {
+        private readonly IIdentityUserLookupAppService _identityUserLookupAppService;
+        private readonly IIdentityUserRepository _identityUserRepository;
+        private readonly ILookupNormalizer _lookupNormalizer;
+
+        public IdentityUserLookupAppService_Tests()
+        {
+            _identityUserLookupAppService = GetRequiredService<IIdentityUserLookupAppService>();
+            _identityUserRepository = GetRequiredService<IIdentityUserRepository>();
+            _lookupNormalizer = GetRequiredService<ILookupNormalizer>();
+        }
+
+        [Fact]
+        public async Task FindByIdAsync()
+        {
+            var user = await _identityUserRepository.FindByNormalizedUserNameAsync(_lookupNormalizer.Normalize("john.nash"));
+            user.ShouldNotBeNull();
+
+            (await _identityUserLookupAppService.FindByIdAsync(user.Id)).UserName.ShouldBe(user.UserName);
+        }
+
+        [Fact]
+        public async Task FindById_NotExist_Should_Return_Null()
+        {
+            var user = await _identityUserLookupAppService.FindByIdAsync(Guid.NewGuid());
+            user.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task FindByUserNameAsync()
+        {
+            var user = await _identityUserRepository.FindByNormalizedUserNameAsync(_lookupNormalizer.Normalize("john.nash"));
+            user.ShouldNotBeNull();
+
+            (await _identityUserLookupAppService.FindByUserNameAsync(user.UserName)).UserName.ShouldBe(user.UserName);
+        }
+
+        [Fact]
+        public async Task FindByUserName_NotExist_Should_Return_Null()
+        {
+            var user = await _identityUserLookupAppService.FindByUserNameAsync(Guid.NewGuid().ToString());
+            user.ShouldBeNull();
+        }
+    }
+}
---
+++ modules/identity/test/Volo.Abp.Identity.Domain.Tests/Volo/Abp/Identity/IdentityUserManager_Tests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Shouldly;
+using Volo.Abp.Uow;
+using Xunit;
+
+namespace Volo.Abp.Identity
+{
+    public class IdentityUserManager_Tests : AbpIdentityDomainTestBase
+    {
+        private readonly IdentityUserManager _identityUserManager;
+        private readonly IIdentityUserRepository _identityUserRepository;
+        private readonly IIdentityRoleRepository _identityRoleRepository;
+        private readonly ILookupNormalizer _lookupNormalizer;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+        private readonly IdentityTestData _testData;
+
+        public IdentityUserManager_Tests()
+        {
+            _identityUserManager = GetRequiredService<IdentityUserManager>();
+            _identityUserRepository = GetRequiredService<IIdentityUserRepository>();
+            _identityRoleRepository = GetRequiredService<IIdentityRoleRepository>();
+            _lookupNormalizer = GetRequiredService<ILookupNormalizer>();
+            _testData = GetRequiredService<IdentityTestData>();
+            _unitOfWorkManager = GetRequiredService<IUnitOfWorkManager>();
+        }
+
+        [Fact]
+        public async Task GetByIdAsync()
+        {
+            var user = await _identityUserManager.GetByIdAsync(_testData.UserJohnId);
+
+            user.ShouldNotBeNull();
+            user.UserName.ShouldBe("john.nash");
+        }
+
+        [Fact]
+        public async Task SetRolesAsync()
+        {
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var user = await _identityUserRepository.FindByNormalizedUserNameAsync(
+                    _lookupNormalizer.Normalize("david"));
+                user.ShouldNotBeNull();
+
+                var identityResult = await _identityUserManager.SetRolesAsync(user, new List<string>()
+                {
+                    "moderator",
+                });
+
+                identityResult.Succeeded.ShouldBeTrue();
+                user.Roles.ShouldContain(x => x.RoleId == _testData.RoleModeratorId);
+
+                await uow.CompleteAsync();
+            }
+        }
+
+        [Fact]
+        public async Task SetRoles_Should_Remove_Other_Roles()
+        {
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var roleSupporter =
+                    await _identityRoleRepository.FindByNormalizedNameAsync(_lookupNormalizer.Normalize("supporter"));
+                roleSupporter.ShouldNotBeNull();
+
+                var user = await _identityUserRepository.FindByNormalizedUserNameAsync(
+                    _lookupNormalizer.Normalize("john.nash"));
+                user.ShouldNotBeNull();
+
+                var identityResult = await _identityUserManager.SetRolesAsync(user, new List<string>()
+                {
+                    "admin",
+                });
+
+                identityResult.Succeeded.ShouldBeTrue();
+                user.Roles.ShouldNotContain(x => x.RoleId == _testData.RoleModeratorId);
+                user.Roles.ShouldNotContain(x => x.RoleId == roleSupporter.Id);
+
+                await uow.CompleteAsync();
+            }
+        }
+    }
+}
