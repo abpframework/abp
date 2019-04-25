1e2c0d1ee132a483e32855904d6c620f8bfd2158
杨涛 <webdiyer@gmail.com>
2019-04-25 11:19:19 +0800

    UrlEncode blog short name within Redirect method

--- modules/blogging/src/Volo.Blogging.Web/Pages/Blog/Posts/New.cshtml.cs
+++ modules/blogging/src/Volo.Blogging.Web/Pages/Blog/Posts/New.cshtml.cs
@@ -54,7 +54,7 @@ namespace Volo.Blogging.Pages.Blog.Posts
             var postWithDetailsDto = await _postAppService.CreateAsync(ObjectMapper.Map<CreatePostViewModel,CreatePostDto>(Post));
 
             //TODO: Try Url.Page(...)
-            return Redirect(Url.Content($"~/blog/{blog.ShortName}/{WebUtility.UrlEncode(postWithDetailsDto.Url)}"));
+            return Redirect(Url.Content($"~/blog/{WebUtility.UrlEncode(blog.ShortName)}/{WebUtility.UrlEncode(postWithDetailsDto.Url)}"));
         }
 
         public class CreatePostViewModel
