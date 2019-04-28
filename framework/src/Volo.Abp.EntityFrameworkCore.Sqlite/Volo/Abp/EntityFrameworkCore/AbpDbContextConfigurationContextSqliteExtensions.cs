5ba01578e67548ba4c89673999215d3afcc89a7e
Dong Guo <v-doguo@microsoft.com>
2019-04-28 10:51:59 +0800

    add sqlite support

---
+++ framework/src/Volo.Abp.EntityFrameworkCore.Sqlite/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextSqliteExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Volo.Abp.EntityFrameworkCore.DependencyInjection;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextConfigurationContextSqliteExtensions
+    {
+        public static DbContextOptionsBuilder UseSqlite(
+            [NotNull] this AbpDbContextConfigurationContext context,
+            [CanBeNull] Action<SqliteDbContextOptionsBuilder> sqliteOptionsAction = null)
+        {
+            if (context.ExistingConnection != null)
+            {
+                return context.DbContextOptions.UseSqlite(context.ExistingConnection, sqliteOptionsAction);
+            }
+            else
+            {
+                return context.DbContextOptions.UseSqlite(context.ConnectionString, sqliteOptionsAction);
+            }
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.EntityFrameworkCore.Sqlite/Volo/Abp/EntityFrameworkCore/Sqlite/AbpEntityFrameworkCoreSqliteModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.EntityFrameworkCore.Sqlite
+{
+    [DependsOn(
+        typeof(AbpEntityFrameworkCoreModule)
+        )]
+    public class AbpEntityFrameworkCoreSqliteModule : AbpModule
+    {
+    }
+}
