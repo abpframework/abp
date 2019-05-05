b5a58b4ad848e5639c814be19f448d8f2eababf9
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-05-05 21:29:49 +0300

    Fix mvc module path.

--- build-all.ps1
+++ build-all.ps1
@@ -20,7 +20,7 @@ $solutionPaths = (
     "modules/background-jobs",
     "modules/client-simulation",
     "abp_io",
-    "templates/module",
+    "templates/mvc-module",
     "templates/service",
     "templates/mvc",
     "samples/MicroserviceDemo"
