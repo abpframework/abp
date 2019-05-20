afa01500479aa1955b50ed9146fc53ddba73f09a
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-05-20 12:01:22 +0300

    Added widgets and dashboards packages.

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Dashboards/Volo/Abp/AspNetCore/Mvc/UI/Dashboards/AbpAspNetCoreMvcUiDashboardsModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Dashboards
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreMvcUiWidgetsModule)
+        )]
+    public class AbpAspNetCoreMvcUiDashboardsModule : AbpModule
+    {
+
+    }
+}
