4d79878f7edf9b9eac3859fbb048fbb568480c81
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-05-20 12:44:23 +0300

    Added widget renderer

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Widgets/Volo/Abp/AspNetCore/Mvc/UI/Widgets/IWidgetRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Widgets
+{
+    public interface IWidgetRenderer : ITransientDependency
+    {
+        Task<object> RenderAsync(string mywidget);
+    }
+
+    public class WidgetRenderer : IWidgetRenderer
+    {
+        public Task<object> RenderAsync(string mywidget)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Widgets/Volo/Abp/AspNetCore/Mvc/UI/Widgets/WidgetDefinition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using JetBrains.Annotations;
+using Volo.Abp.Localization;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Widgets
+{
+    public class WidgetDefinition
+    {
+        /// <summary>
+        /// Unique name of the widget.
+        /// </summary>
+        [NotNull]
+        public string Name { get; }
+
+        /// <summary>
+        /// Display name of the widget.
+        /// </summary>
+        [NotNull]
+        public ILocalizableString DisplayName
+        {
+            get => _displayName;
+            set => _displayName = Check.NotNull(value, nameof(value));
+        }
+        private ILocalizableString _displayName;
+
+        public Type ViewComponentType { get; set; }
+
+        public WidgetDefinition(
+            [NotNull] string name,
+            [NotNull] Type viewComponentType,
+            [CanBeNull] ILocalizableString displayName)
+        {
+            Name = Check.NotNullOrWhiteSpace(name, nameof(name));
+            ViewComponentType = Check.NotNull(viewComponentType, nameof(viewComponentType));
+            DisplayName = displayName ?? new FixedLocalizableString(name);
+        }
+    }
+}
