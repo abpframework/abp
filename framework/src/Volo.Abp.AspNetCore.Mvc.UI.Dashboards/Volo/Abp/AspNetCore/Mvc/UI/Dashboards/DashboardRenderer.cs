c13898682ca2d40b4543633730c1d450834cc2b5
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-05-22 16:55:40 +0300

    Update DashboardRenderer.cs

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Dashboards/Volo/Abp/AspNetCore/Mvc/UI/Dashboards/DashboardRenderer.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Dashboards/Volo/Abp/AspNetCore/Mvc/UI/Dashboards/DashboardRenderer.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using Volo.Abp.AspNetCore.Mvc.UI.Dashboards.Components.Dashboard;
-using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
 
 namespace Volo.Abp.AspNetCore.Mvc.UI.Dashboards
 {
