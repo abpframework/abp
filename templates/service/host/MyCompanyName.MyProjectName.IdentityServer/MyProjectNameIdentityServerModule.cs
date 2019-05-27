7378cb8ab54fb326773bd8844b0a358c1b513458
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-05-27 14:25:45 +0300

    Added projects from the module template.

---
+++ templates/service/host/MyCompanyName.MyProjectName.IdentityServer/MyProjectNameIdentityServerModule.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MyCompanyName.MyProjectName.MultiTenancy;
+using StackExchange.Redis;
+using Volo.Abp;
+using Volo.Abp.Account.Web;
+using Volo.Abp.AspNetCore.MultiTenancy;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+using Volo.Abp.Auditing;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Data;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.SqlServer;
+using Volo.Abp.Identity.EntityFrameworkCore;
+using Volo.Abp.IdentityServer.EntityFrameworkCore;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+using Volo.Abp.Threading;
+using Volo.Abp.Ui.Navigation.Urls;
+
+namespace MyCompanyName.MyProjectName
+{
+    [DependsOn(
+        typeof(AbpAccountWebIdentityServerModule),
+        typeof(AbpAspNetCoreMultiTenancyModule),
+        typeof(AbpAspNetCoreMvcModule),
+        typeof(AbpAspNetCoreMvcUiBasicThemeModule),
+        typeof(AbpAuditLoggingEntityFrameworkCoreModule),
+        typeof(AbpAutofacModule),
+        typeof(AbpEntityFrameworkCoreSqlServerModule),
+        typeof(AbpIdentityEntityFrameworkCoreModule),
+        typeof(AbpIdentityServerEntityFrameworkCoreModule),
+        typeof(AbpPermissionManagementEntityFrameworkCoreModule),
+        typeof(AbpSettingManagementEntityFrameworkCoreModule)
+        )]
+    public class MyProjectNameIdentityServerModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var hostingEnvironment = context.Services.GetHostingEnvironment();
+            var configuration = context.Services.BuildConfiguration();
+
+            Configure<AbpDbContextOptions>(options =>
+            {
+                options.UseSqlServer();
+            });
+
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Languages.Add(new LanguageInfo("en", "en", "English"));
+                options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
+                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
+                options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
+            });
+
+            Configure<AbpAuditingOptions>(options =>
+            {
+                //options.IsEnabledForGetRequests = true;
+                options.ApplicationName = "AuthServer";
+            });
+
+            Configure<AppUrlOptions>(options =>
+            {
+                options.Applications["MVC"].RootUrl = configuration["AppSelfUrl"];
+            });
+            
+            context.Services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = configuration["Redis:Configuration"];
+            });
+
+            if (!hostingEnvironment.IsDevelopment())
+            {
+                var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
+                context.Services
+                    .AddDataProtection()
+                    .PersistKeysToStackExchangeRedis(redis, "MyProjectName-Protection-Keys");
+            }
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            
+            app.UseVirtualFiles();
+            app.UseAuthentication();
+            if (MultiTenancyConsts.IsEnabled)
+            {
+                app.UseMultiTenancy();
+            }
+            app.UseIdentityServer();
+            app.UseAbpRequestLocalization();
+            app.UseAuditing();
+            app.UseMvcWithDefaultRouteAndArea();
+
+            SeedData(context);
+        }
+
+        private void SeedData(ApplicationInitializationContext context)
+        {
+            AsyncHelper.RunSync(async () =>
+            {
+                using (var scope = context.ServiceProvider.CreateScope())
+                {
+                    await scope.ServiceProvider
+                        .GetRequiredService<IDataSeeder>()
+                        .SeedAsync();
+                }
+            });
+        }
+    }
+}
---
+++ templates/service/host/MyCompanyName.MyProjectName.Web.Host/MultiTenancy/MultiTenancyConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCompanyName.MyProjectName.MultiTenancy
+{
+    public static class MultiTenancyConsts
+    {
+        /* Enable/disable multi-tenancy.
+         */
+        public const bool IsEnabled = false;
+    }
+}
