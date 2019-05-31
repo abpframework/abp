1fb6b1343440ca8192c097f9618bcf0c8c9371aa
徐亮 <xuliang@yhglobal.com>
2019-05-31 19:10:05 +0800

    feature: implement the interfaces for reading/writing LDAP.
    include:
    Authenticate
    Query Organizations
    Add Organization
    Query Users
    Query User

---
+++ framework/src/Volo.Abp.Ldap/Volo/Abp/Ldap/Exceptions/OrganizationNotExistException.cs
@@ -0,0 +1,11 @@
+﻿namespace Volo.Abp.Ldap.Exceptions
+{
+    public class OrganizationNotExistException : BusinessException
+    {
+        public OrganizationNotExistException(string distinguishedName)
+            : base("LDAP:000001", $"the organization distinguished named {distinguishedName} does not exist.")
+        {
+
+        }
+    }
+}
