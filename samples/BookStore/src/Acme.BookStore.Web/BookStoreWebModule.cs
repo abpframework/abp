b707f2e8533dbbf3b0776ffbca7608c91c81704f
maliming <malimings@gmail.com>
2019-06-06 22:45:02 +0800

    fix #1206 Mvc template & BookStore virtual file enhancement.

--- samples/BookStore/src/Acme.BookStore.Web/BookStoreWebModule.cs
+++ samples/BookStore/src/Acme.BookStore.Web/BookStoreWebModule.cs
@@ -105,17 +105,19 @@ namespace Acme.BookStore.Web
 
         private void ConfigureVirtualFileSystem(IHostingEnvironment hostingEnvironment)
         {
-            if (hostingEnvironment.IsDevelopment())
+            Configure<VirtualFileSystemOptions>(options =>
             {
-                Configure<VirtualFileSystemOptions>(options =>
+                options.FileSets.AddEmbedded<BookStoreWebModule>("Acme.BookStore.Web");
+
+                if (hostingEnvironment.IsDevelopment())
                 {
                     options.FileSets.ReplaceEmbeddedByPhysical<BookStoreDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}Acme.BookStore.Domain.Shared", Path.DirectorySeparatorChar)));
                     options.FileSets.ReplaceEmbeddedByPhysical<BookStoreDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}Acme.BookStore.Domain", Path.DirectorySeparatorChar)));
                     options.FileSets.ReplaceEmbeddedByPhysical<BookStoreApplicationContractsModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}Acme.BookStore.Application.Contracts", Path.DirectorySeparatorChar)));
                     options.FileSets.ReplaceEmbeddedByPhysical<BookStoreApplicationModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}Acme.BookStore.Application", Path.DirectorySeparatorChar)));
                     options.FileSets.ReplaceEmbeddedByPhysical<BookStoreWebModule>(hostingEnvironment.ContentRootPath);
-                });
-            }
+                }
+            });
         }
 
         private void ConfigureLocalizationServices()
--- templates/mvc/src/MyCompanyName.MyProjectName.Web/MyProjectNameWebModule.cs
+++ templates/mvc/src/MyCompanyName.MyProjectName.Web/MyProjectNameWebModule.cs
@@ -109,9 +109,11 @@ namespace MyCompanyName.MyProjectName.Web
 
         private void ConfigureVirtualFileSystem(IHostingEnvironment hostingEnvironment)
         {
-            if (hostingEnvironment.IsDevelopment())
+            Configure<VirtualFileSystemOptions>(options =>
             {
-                Configure<VirtualFileSystemOptions>(options =>
+                options.FileSets.AddEmbedded<MyProjectNameWebModule>("MyCompanyName.MyProjectName.Web");
+
+                if (hostingEnvironment.IsDevelopment())
                 {
                     options.FileSets.ReplaceEmbeddedByPhysical<MyProjectNameDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}MyCompanyName.MyProjectName.Domain.Shared", Path.DirectorySeparatorChar)));
                     options.FileSets.ReplaceEmbeddedByPhysical<MyProjectNameDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}MyCompanyName.MyProjectName.Domain", Path.DirectorySeparatorChar)));
@@ -128,8 +130,8 @@ namespace MyCompanyName.MyProjectName.Web
                     options.FileSets.ReplaceEmbeddedByPhysical<AbpIdentityWebModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}..{0}..{0}modules{0}identity{0}src{0}Volo.Abp.Identity.Web", Path.DirectorySeparatorChar)));
                     options.FileSets.ReplaceEmbeddedByPhysical<AbpAccountWebModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}..{0}..{0}modules{0}account{0}src{0}Volo.Abp.Account.Web", Path.DirectorySeparatorChar)));
                     //</TEMPLATE-REMOVE>
-                });
-            }
+                }
+            });
         }
 
         private void ConfigureLocalizationServices()
