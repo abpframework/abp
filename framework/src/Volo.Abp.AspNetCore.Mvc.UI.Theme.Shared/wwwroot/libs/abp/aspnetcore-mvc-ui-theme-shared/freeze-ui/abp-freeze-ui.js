e9af3639213768f2ff38ceff37994a5a1cf034f5
Alper Ebicoglu <ebicoglu@users.noreply.github.com>
2019-06-12 11:14:04 +0300

    add freeze-ui package

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/freeze-ui/abp-freeze-ui.js
@@ -0,0 +1,46 @@
+﻿var abp = abp || {};
+(function ($) {
+    if (!window.FreezeUI || !$) {
+        return;
+    }
+    
+    /* MESSAGE **************************************************/
+    /*Package from https://alexradulescu.github.io/freeze-ui/*/
+
+    abp.ui = abp.ui || {};
+
+    /* UI BLOCK */
+
+    abp.ui.block = function (elm) {
+        if (elm) {
+            window.FreezeUI({ selector: elm });
+        } else {
+            window.FreezeUI();
+        }
+    };
+
+    abp.ui.unblock = function () {
+        window.UnFreezeUI();
+    };
+
+    /* UI BUSY */
+
+    abp.ui.setBusy = function (element, text, freezeDelay) {
+        var opt = { text : text ? text : ' ' };
+
+        if (element) {
+            opt.element = element;
+        }
+
+        if (freezeDelay) {
+            opt.freezeDelay = freezeDelay;
+        }
+         
+        window.FreezeUI(opt);
+    };
+
+    abp.ui.clearBusy = function () {
+        window.UnFreezeUI();
+    };
+
+})(jQuery);
---
+++ npm/packs/freeze-ui/abp.resourcemapping.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    mappings: {
+        "@node_modules/@abp/freeze-ui/lib/freeze-ui.min.js": "@libs/freeze-ui/",       
+        "@node_modules/@abp/freeze-ui/lib/freeze-ui.min.css": "@libs/freeze-ui/"
+    }
+}
