eb9cf68c8b7ef89765e6f9c139dedba70f4054a7
Super <47396430@qq.com>
2019-06-23 02:30:24 +0800

    Update MyProjectNameDbContextModelCreatingExtensions.cs

--- templates/mvc/src/MyCompanyName.MyProjectName.EntityFrameworkCore/EntityFrameworkCore/MyProjectNameDbContextModelCreatingExtensions.cs
+++ templates/mvc/src/MyCompanyName.MyProjectName.EntityFrameworkCore/EntityFrameworkCore/MyProjectNameDbContextModelCreatingExtensions.cs
@@ -27,15 +27,18 @@ namespace MyCompanyName.MyProjectName.EntityFrameworkCore
             where TUser: class, IUser
         {
             var type = typeof(AppUser);
+            var typeMethods = b.GetType().GetMethods();
             var properties = new List<PropertyInfo>();
+            var interfaceProperties = GetPublicProperties(type.GetInterface("IUser"));
+            var baseTypeProperties = type.BaseType.GetProperties();
 
             properties.AddRange(type.GetProperties()
-                .Where(p => !ReflectionHelper.GetPublicProperties(type.GetInterface("IUser")).Select(ip => ip.Name).Contains(p.Name))
-                .Where(p => !type.BaseType.GetProperties().Select(bp => bp.Name).Contains(p.Name)));
+                .Where(p => !interfaceProperties.Select(ip => ip.Name).Contains(p.Name))
+                .Where(p => !baseTypeProperties.Select(bp => bp.Name).Contains(p.Name)));
 
             properties.ForEach(p =>
             {
-                var mi = b.GetType().GetMethods().First(method => method.Name == "Property" && method.IsGenericMethod && method.GetParameters().Length == 1 && method.GetParameters().Single().ParameterType == typeof(string)).MakeGenericMethod(p.PropertyType);
+                var mi = typeMethods.First(method => method.Name == "Property" && method.IsGenericMethod && method.GetParameters().Length == 1 && method.GetParameters().Single().ParameterType == typeof(string)).MakeGenericMethod(p.PropertyType);
                 mi.Invoke(b, new object[] { p.Name });
             });
         }
