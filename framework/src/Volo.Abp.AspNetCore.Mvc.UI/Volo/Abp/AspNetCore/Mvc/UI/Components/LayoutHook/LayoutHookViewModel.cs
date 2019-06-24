63bc860d757a9a953306ba6d62bab1e7b8e78099
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2019-06-24 18:35:57 +0300

    Resolved #1361: Layout Hook Feature for MVC UI

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI/Volo/Abp/AspNetCore/Mvc/UI/Components/LayoutHook/LayoutHookViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Volo.Abp.AspNetCore.Mvc.UI.Components.LayoutHook
+{
+    public class LayoutHookViewModel
+    {
+        public LayoutHookInfo[] Hooks { get; }
+
+        public string Layout { get; }
+
+        public LayoutHookViewModel(LayoutHookInfo[] hooks, string layout)
+        {
+            Hooks = hooks;
+            Layout = layout;
+        }
+    }
+}
