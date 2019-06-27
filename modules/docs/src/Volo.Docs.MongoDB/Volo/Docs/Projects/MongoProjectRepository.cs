03e4cd008467ff74c04d78542d3eb0fed075dbaf
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-06-27 11:44:06 +0300

    Docs module MongoDB support
    resolves https://github.com/abpframework/abp/issues/1364

---
+++ modules/docs/src/Volo.Docs.MongoDB/Volo/Docs/Projects/MongoProjectRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories.MongoDB;
+using Volo.Abp.MongoDB;
+using Volo.Docs.MongoDB;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace Volo.Docs.Projects
+{
+    public class MongoProjectRepository : MongoDbRepository<IDocsMongoDbContext, Project, Guid>, IProjectRepository
+    {
+        public MongoProjectRepository(IMongoDbContextProvider<IDocsMongoDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<List<Project>> GetListAsync(string sorting, int maxResultCount, int skipCount)
+        {
+            var projects = await GetMongoQueryable().OrderBy(sorting ?? "Id desc").As<IMongoQueryable<Project>>()
+                .PageBy<Project, IMongoQueryable<Project>>(skipCount, maxResultCount)
+                .ToListAsync();
+
+            return projects;
+        }
+
+        public async Task<Project> GetByShortNameAsync(string shortName)
+        {
+            var project = await GetMongoQueryable().FirstOrDefaultAsync(p => p.ShortName == shortName);
+
+            if (project == null)
+            {
+                throw new EntityNotFoundException($"Project with the name {shortName} not found!");
+            }
+
+            return project;
+        }
+    }
+}
