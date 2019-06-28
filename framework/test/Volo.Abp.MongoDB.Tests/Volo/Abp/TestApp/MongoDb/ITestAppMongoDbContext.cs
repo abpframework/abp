7d5d33a1b4d00a1c298463ffcc57ef63ed1864b3
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-06-28 09:07:15 +0300

    mongodb namespaces & projects renamed

--- framework/test/Volo.Abp.MongoDB.Tests/Volo/Abp/TestApp/MongoDb/ITestAppMongoDbContext.cs
+++ framework/test/Volo.Abp.MongoDB.Tests/Volo/Abp/TestApp/MongoDb/ITestAppMongoDbContext.cs
@@ -3,7 +3,7 @@ using Volo.Abp.Data;
 using Volo.Abp.MongoDB;
 using Volo.Abp.TestApp.Domain;
 
-namespace Volo.Abp.TestApp.MongoDb
+namespace Volo.Abp.TestApp.MongoDB
 {
     [ConnectionStringName("TestApp")]
     public interface ITestAppMongoDbContext : IAbpMongoDbContext
