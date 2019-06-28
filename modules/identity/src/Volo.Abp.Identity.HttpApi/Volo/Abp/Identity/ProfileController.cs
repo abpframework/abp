6fa00321c045126c8aed832d51caf281bdf9e1b5
maliming <malimings@gmail.com>
2019-06-28 15:48:59 +0800

    Fix inconsistent REST API urls.

--- modules/identity/src/Volo.Abp.Identity.HttpApi/Volo/Abp/Identity/ProfileController.cs
+++ modules/identity/src/Volo.Abp.Identity.HttpApi/Volo/Abp/Identity/ProfileController.cs
@@ -7,6 +7,7 @@ namespace Volo.Abp.Identity
     [RemoteService]
     [Area("identity")]
     [ControllerName("Profile")]
+    [Route("/api/identity/profiles")]
     public class ProfileController : AbpController, IProfileAppService
     {
         private readonly IProfileAppService _profileAppService;
@@ -15,16 +16,21 @@ namespace Volo.Abp.Identity
         {
             _profileAppService = profileAppService;
         }
+
+        [HttpGet]
         public Task<ProfileDto> GetAsync()
         {
             return _profileAppService.GetAsync();
         }
 
+        [HttpPut]
         public Task<ProfileDto> UpdateAsync(UpdateProfileDto input)
         {
             return _profileAppService.UpdateAsync(input);
         }
 
+        [HttpPost]
+        [Route("ChangePassword")]
         public Task ChangePasswordAsync(string currentPassword, string newPassword)
         {
             return _profileAppService.ChangePasswordAsync(currentPassword, newPassword);
