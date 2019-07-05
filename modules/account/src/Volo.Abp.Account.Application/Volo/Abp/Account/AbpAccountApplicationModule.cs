d3ad33ab5ea9fa6c3eabe6572fef339a242b1d88
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-07-05 13:09:06 +0300

    register api for account module

---
+++ modules/account/src/Volo.Abp.Account.Application/Volo/Abp/Account/AbpAccountApplicationModule.cs
@@ -0,0 +1,32 @@
+﻿using Volo.Abp.Emailing;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+using Volo.Abp.Ui.Navigation.Urls;
+using Volo.Abp.UI.Navigation;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Volo.Abp.Account
+{
+    [DependsOn(
+        typeof(AbpAccountApplicationContractsModule),
+        typeof(AbpEmailingModule),
+        typeof(AbpIdentityApplicationModule),
+        typeof(AbpUiNavigationModule)
+    )]
+    public class AbpAccountApplicationModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<VirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpAccountApplicationModule>();
+            });
+
+            Configure<AppUrlOptions>(options =>
+            {
+                options.Applications["MVC"].Urls[AccountUrlNames.PasswordReset] = "Account/ResetPassword";
+                options.Applications["MVC"].Urls[AccountUrlNames.EmailConfirmation] = "Account/EmailConfirmation";
+            });
+        }
+    }
+}
---
+++ modules/account/src/Volo.Abp.Account.Application/Volo/Abp/Account/AccountUrlNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Volo.Abp.Account
+{
+    public static class AccountUrlNames
+    {
+        public const string PasswordReset = "Abp.Account.PasswordReset";
+        public const string EmailConfirmation = "Abp.Account.EmailConfirmation";
+    }
+}
