73ec92f3af01b62907e61ed2506879e4fe8b484c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2019-07-08 15:22:33 +0300

    Introduce ISettingValueProviderManager.

---
+++ framework/src/Volo.Abp.Settings/Volo/Abp/Settings/ISettingValueProviderManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Volo.Abp.Settings
+{
+    public interface ISettingValueProviderManager
+    {
+        List<ISettingValueProvider> Providers { get; }
+    }
+}
