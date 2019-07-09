0ef023c0c96bf1235a62358037c7f218a949f6bd
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-07-09 16:51:45 +0300

    dashboard global filters

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Dashboards/Volo/Abp/AspNetCore/Mvc/UI/Dashboards/Components/Dashboard/DashboardViewComponent.cshtml.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Dashboards/Volo/Abp/AspNetCore/Mvc/UI/Dashboards/Components/Dashboard/DashboardViewComponent.cshtml.cs
@@ -10,18 +10,20 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Dashboards.Components.Dashboard
     {
         private readonly DashboardOptions _dashboardOptions;
         private readonly WidgetOptions _widgetOptions;
+        private readonly GlobalFilterOptions _globalFilterOptions;
 
-        public DashboardViewComponent(IOptions<DashboardOptions> dashboardOptions, IOptions<WidgetOptions> widgetOptions)
+        public DashboardViewComponent(IOptions<DashboardOptions> dashboardOptions, IOptions<WidgetOptions> widgetOptions, IOptions<GlobalFilterOptions> globalFilterOptions)
         {
             _dashboardOptions = dashboardOptions.Value;
             _widgetOptions = widgetOptions.Value;
+            _globalFilterOptions = globalFilterOptions.Value;
         }
 
         public IViewComponentResult Invoke(string dashboardName)
         {
             var dashboard = _dashboardOptions.Dashboards.Single(d => d.Name.Equals(dashboardName));
 
-            var model = new DashboardViewModel(dashboard, _widgetOptions.Widgets);
+            var model = new DashboardViewModel(dashboard, _widgetOptions.Widgets, _globalFilterOptions.GlobalFilters);
 
             return View("~/Volo/Abp/AspNetCore/Mvc/UI/Dashboards/Components/Dashboard/Default.cshtml", model);
         }
