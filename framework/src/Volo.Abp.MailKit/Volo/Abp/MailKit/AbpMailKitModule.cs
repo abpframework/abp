b31814bae051991bec58f9def3e6d39ead3c9fda
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-07-09 08:45:22 +0300

    Update AbpMailKitModule.cs

--- framework/src/Volo.Abp.MailKit/Volo/Abp/MailKit/AbpMailKitModule.cs
+++ framework/src/Volo.Abp.MailKit/Volo/Abp/MailKit/AbpMailKitModule.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp.Emailing;
+﻿using Volo.Abp.Emailing;
 using Volo.Abp.Modularity;
 
 namespace Volo.Abp.MailKit
