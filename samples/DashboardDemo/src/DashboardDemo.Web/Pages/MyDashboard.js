f8f2bdb91d0f826c8f42b4847fff432ce8dee4d0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2019-07-26 12:08:30 +0300

    Update MyDashboard.js

--- samples/DashboardDemo/src/DashboardDemo.Web/Pages/MyDashboard.js
+++ samples/DashboardDemo/src/DashboardDemo.Web/Pages/MyDashboard.js
@@ -1,8 +1,87 @@
 ﻿abp.widgets = abp.widgets || {}; //TODO: Remove later
 (function () {
 
-    abp.WidgetManager = function(opts) {
+    abp.WidgetManager = function (opts) {
+        if (typeof opts === 'string') {
+            opts = {
+                wrapper: opts
+            };
+        }
 
+        var $widgetWrapperDiv;
+        if (typeof opts.wrapper === 'string') {
+            $widgetWrapperDiv = $(opts.wrapper);
+        } else {
+            $widgetWrapperDiv = opts.wrapper;
+        }
+
+        if (!$widgetWrapperDiv.hasClass('abp-widget-wrapper')) {
+            $widgetWrapperDiv = $widgetWrapperDiv.closest('.abp-widget-wrapper');
+        }
+
+        var getFilters = function () {
+            var filters = {};
+
+            if (opts.filterCallback) {
+                filters = opts.filterCallback();
+            }
+
+            var widgetApi = $widgetWrapperDiv.data('abp-widget-api');
+            if (widgetApi && widgetApi.getFilters) {
+                filters = $.extend(filters, widgetApi.getFilters());
+            }
+
+            return filters;
+        };
+        
+        var init = function () {
+            var widgetName = $widgetWrapperDiv.attr('data-widget-name');
+            var widgetApiClass = abp.widgets[widgetName];
+            if (widgetApiClass) {
+                var widgetApi = new widgetApiClass($widgetWrapperDiv);
+                $widgetWrapperDiv.data('abp-widget-api', widgetApi);
+                if (widgetApi.init) {
+                    widgetApi.init(getFilters($widgetWrapperDiv));
+                }
+            }
+        };
+
+        var callRefreshWidgetApi = function () {
+            var widgetApi = $widgetWrapperDiv.data('abp-widget-api');
+            if (widgetApi && widgetApi.refresh) {
+                widgetApi.refresh(getFilters($widgetWrapperDiv));
+            }
+        }
+
+        var refresh = function () {
+            var refreshUrl = $widgetWrapperDiv.attr('data-refresh-url');
+            if (refreshUrl) {
+                abp.ajax({
+                    url: refreshUrl,
+                    type: 'GET',
+                    dataType: 'html',
+                    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                    data: getFilters()
+                }).then(function (result) {
+                    var $newWidgetWrapperDiv = $(result);
+                    $widgetWrapperDiv.replaceWith($newWidgetWrapperDiv);
+                    $widgetWrapperDiv = $newWidgetWrapperDiv;
+                    $widgetWrapperDiv.data('abp-widget-manager', publicApi);
+                    init();
+                });
+            } else {
+                callRefreshWidgetApi();
+            }
+        };
+
+        var publicApi = {
+            init: init,
+            refresh: refresh
+        };
+
+        $widgetWrapperDiv.data('abp-widget-manager', publicApi);
+
+        return publicApi;
     };
 
     abp.DashboardManager = function (opts) {
@@ -20,72 +99,24 @@
             $dashboardWrapper = opts.wrapper;
         }
 
-        var getFilters = function ($widgetWrapperDiv) {
-            var filters = {};
-
-            if (opts.filterCallback) {
-                filters = opts.filterCallback();
-            }
-
-            var widgetApi = $widgetWrapperDiv.data('abp-widget-api');
-            if (widgetApi && widgetApi.getFilters) {
-                filters = $.extend(filters, widgetApi.getFilters());
-            }
-
-            return filters;
-        };
-
-        var initWidget = function($widgetWrapperDiv) {
-            var widgetName = $widgetWrapperDiv.attr('data-widget-name');
-            var widgetApiClass = abp.widgets[widgetName];
-            if (widgetApiClass) {
-                var widgetApi = new widgetApiClass($widgetWrapperDiv);
-                $widgetWrapperDiv.data('abp-widget-api', widgetApi);
-                if (widgetApi.init) {
-                    widgetApi.init(getFilters($widgetWrapperDiv));
-                }
-            }
-        };
-
-        var callRefreshWidgetApi = function ($widgetWrapperDiv) {
-            var widgetApi = $widgetWrapperDiv.data('abp-widget-api');
-            if (widgetApi && widgetApi.refresh) {
-                widgetApi.refresh(getFilters($widgetWrapperDiv));
-            }
-        }
-
-        var refreshWidget = function ($widgetWrapperDiv) {
-            var refreshUrl = $widgetWrapperDiv.attr('data-refresh-url');
-            if (refreshUrl) {
-                abp.ajax({
-                    url: refreshUrl,
-                    type: 'GET',
-                    dataType: 'html',
-                    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-                    data: getFilters($widgetWrapperDiv)
-                }).then(function (result) {
-                    var $newWidgetWrapperDiv = $(result);
-                    $widgetWrapperDiv.replaceWith($newWidgetWrapperDiv);
-                    $widgetWrapperDiv = $newWidgetWrapperDiv;
-                    initWidget($widgetWrapperDiv);
-                });
-            } else {
-                callRefreshWidgetApi($widgetWrapperDiv);
-            }
-        };
-
-        var init = function () {
-            $dashboardWrapper.find('.abp-widget-wrapper').each(function () {
-                initWidget($(this));
-            });
-        };
-
-        var refresh = function() {
+        var init = function() {
             $dashboardWrapper.find('.abp-widget-wrapper').each(function() {
-                refreshWidget($(this));
+                var widgetManager = new abp.WidgetManager({
+                    wrapper: $(this),
+                    filterCallback: opts.filterCallback
+                });
+
+                widgetManager.init();
             });
         };
-        
+
+        var refresh = function () {
+            $dashboardWrapper.find('.abp-widget-wrapper').each(function () {
+                var widgetManager = $(this).data('abp-widget-manager');
+                widgetManager && widgetManager.refresh();
+            });
+        };
+
         return {
             init: init,
             refresh: refresh
@@ -96,14 +127,14 @@
 
     var myDashboard = new abp.DashboardManager({
         wrapper: '#MyDashboardWidgetsArea',
-        filterCallback: function() {
+        filterCallback: function () {
             return filterForm.serializeFormToObject();
         }
     });
 
     myDashboard.init();
 
-    filterForm.submit(function(e) {
+    filterForm.submit(function (e) {
         e.preventDefault();
         myDashboard.refresh();
     });
