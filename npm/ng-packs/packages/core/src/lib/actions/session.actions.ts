86c16b5ee071a556cb613e98b97e1133b53bb85d
TheDiaval <thediavall@gmail.com>
2019-08-02 11:12:18 +0300

    feature(core): add tenantId to session state

--- npm/ng-packs/packages/core/src/lib/actions/session.actions.ts
+++ npm/ng-packs/packages/core/src/lib/actions/session.actions.ts
@@ -2,3 +2,7 @@ export class SessionSetLanguage {
   static readonly type = '[Session] Set Language';
   constructor(public payload: string) {}
 }
+export class SessionSetTenantId {
+  static readonly type = '[Session] Set Tenant Id';
+  constructor(public payload: string) {}
+}
--- npm/ng-packs/packages/core/src/lib/models/session.ts
+++ npm/ng-packs/packages/core/src/lib/models/session.ts
@@ -1,5 +1,6 @@
 export namespace Session {
   export interface State {
     language: string;
+    tenantId: string;
   }
 }
