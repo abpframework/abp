e33c43bef44d9ae9f624a1144beeefb73c409d2e
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-08-05 15:34:14 +0300

    Update WidgetPolicyChecker.cs

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Widgets/Volo/Abp/AspNetCore/Mvc/UI/Widgets/WidgetPolicyChecker.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Widgets/Volo/Abp/AspNetCore/Mvc/UI/Widgets/WidgetPolicyChecker.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Options;
 using Volo.Abp.Users;
 
