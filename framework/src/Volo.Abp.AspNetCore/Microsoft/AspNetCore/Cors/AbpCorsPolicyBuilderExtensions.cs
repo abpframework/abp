5e3d2d21dd6d9601d9dfbd9f4bb0ffeabb88ffa9
maliming <malimings@gmail.com>
2019-08-06 17:20:25 +0800

    Add the AbpExposeHeaders extension method.

---
+++ framework/src/Volo.Abp.AspNetCore/Microsoft/AspNetCore/Cors/AbpCorsPolicyBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace Microsoft.AspNetCore.Cors
+{
+    public static class AbpCorsPolicyBuilderExtensions
+    {
+        public static CorsPolicyBuilder WithAbpExposeHeaders(this CorsPolicyBuilder corsPolicyBuilder)
+        {
+            return corsPolicyBuilder.WithExposedHeaders("_AbpErrorFormat");
+        }
+    }
+}
