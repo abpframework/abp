ad5a83f4407aad6301674a71959fbbf573b4b74a
mehmet-erim <bilgi@mehmeterim.com>
2019-08-28 16:07:14 +0300

    feature(theme-shared): create breadcrumb.component

--- npm/ng-packs/packages/theme-shared/src/lib/components/index.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button/button.component';
+export * from './breadcrumb/breadcrumb.component';
 export * from './change-password/change-password.component';
 export * from './confirmation/confirmation.component';
 export * from './loader-bar/loader-bar.component';
