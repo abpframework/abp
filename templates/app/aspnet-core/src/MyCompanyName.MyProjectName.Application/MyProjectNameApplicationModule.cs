1610a90251e7121a953a39b3c896f227b2de4f6f
maliming <malimings@gmail.com>
2019-09-03 16:34:33 +0800

    Resolve #1708 Use AddMaps instead of AddProfile in startup templates.

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application/MyProjectNameApplicationModule.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application/MyProjectNameApplicationModule.cs
@@ -23,11 +23,7 @@ namespace MyCompanyName.MyProjectName
         {
             Configure<AbpAutoMapperOptions>(options =>
             {
-                /* Use `true` for the `validate` parameter if you want to
-                 * validate the profile on application startup.
-                 * See http://docs.automapper.org/en/stable/Configuration-validation.html for more info
-                 * about the configuration validation. */
-                options.AddProfile<MyProjectNameApplicationAutoMapperProfile>();
+                options.AddMaps<MyProjectNameApplicationModule>();
             });
         }
     }
--- templates/module/aspnet-core/host/MyCompanyName.MyProjectName.Web.Host/MyProjectNameWebHostModule.cs
+++ templates/module/aspnet-core/host/MyCompanyName.MyProjectName.Web.Host/MyProjectNameWebHostModule.cs
@@ -135,12 +135,7 @@ namespace MyCompanyName.MyProjectName
         {
             Configure<AbpAutoMapperOptions>(options =>
             {
-                /* use `true` for the `validate` parameter if you want to
-                 * validate the profile on application startup.
-                 * See http://docs.automapper.org/en/stable/Configuration-validation.html for more
-                 * about configuration validation.
-                 */
-                options.AddProfile<MyProjectNameWebAutoMapperProfile>();
+                options.AddMaps<MyProjectNameWebHostModule>();
             });
         }
 
