7b21d9d2c95b86dfe565e0dfc8fb10746075d20a
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2019-09-17 11:11:56 +0300

    Moved docs module to .net core 3.0

--- modules/docs/app/VoloDocs.Web/VoloDocsWebModule.cs
+++ modules/docs/app/VoloDocs.Web/VoloDocsWebModule.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
@@ -30,6 +29,7 @@ using Volo.Docs.Admin;
 using Volo.Docs.Localization;
 using VoloDocs.EntityFrameworkCore;
 using Localization.Resources.AbpUi;
+using Microsoft.Extensions.Hosting;
 
 namespace VoloDocs.Web
 {
