b722a152372067316d96caf4e9852381713acbd2
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-09-26 12:58:42 +0300

    identity settings isVisibleToClients

--- modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/AbpIdentitySettingDefinitionProvider.cs
+++ modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/AbpIdentitySettingDefinitionProvider.cs
@@ -8,23 +8,22 @@ namespace Volo.Abp.Identity
         public override void Define(ISettingDefinitionContext context)
         {
             context.Add(
+                new SettingDefinition(IdentitySettingNames.Password.RequiredLength, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Password.RequiredUniqueChars, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Password.RequireNonAlphanumeric, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Password.RequireLowercase, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Password.RequireUppercase, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Password.RequireDigit, null, null, null, true),
 
-                new SettingDefinition(IdentitySettingNames.Password.RequiredLength),
-                new SettingDefinition(IdentitySettingNames.Password.RequiredUniqueChars),
-                new SettingDefinition(IdentitySettingNames.Password.RequireNonAlphanumeric),
-                new SettingDefinition(IdentitySettingNames.Password.RequireLowercase),
-                new SettingDefinition(IdentitySettingNames.Password.RequireUppercase),
-                new SettingDefinition(IdentitySettingNames.Password.RequireDigit),
+                new SettingDefinition(IdentitySettingNames.Lockout.AllowedForNewUsers, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Lockout.LockoutDuration, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.Lockout.MaxFailedAccessAttempts, null, null, null, true),
 
-                new SettingDefinition(IdentitySettingNames.Lockout.AllowedForNewUsers),
-                new SettingDefinition(IdentitySettingNames.Lockout.LockoutDuration),
-                new SettingDefinition(IdentitySettingNames.Lockout.MaxFailedAccessAttempts),
+                new SettingDefinition(IdentitySettingNames.SignIn.RequireConfirmedEmail, null, null, null, true),
+                new SettingDefinition(IdentitySettingNames.SignIn.RequireConfirmedPhoneNumber, null, null, null, true),
 
-                new SettingDefinition(IdentitySettingNames.SignIn.RequireConfirmedEmail),
-                new SettingDefinition(IdentitySettingNames.SignIn.RequireConfirmedPhoneNumber),
-
-                new SettingDefinition(IdentitySettingNames.User.IsUserNameUpdateEnabled, "true"),
-                new SettingDefinition(IdentitySettingNames.User.IsEmailUpdateEnabled, "true")
+                new SettingDefinition(IdentitySettingNames.User.IsUserNameUpdateEnabled, "true", null, null, true),
+                new SettingDefinition(IdentitySettingNames.User.IsEmailUpdateEnabled, "true", null, null, true)
 
             );
         }
