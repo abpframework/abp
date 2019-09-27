cecdbfc44ae9186db99cf38ae24d01b15ae4af46
mehmet-erim <bilgi@mehmeterim.com>
2019-09-27 10:48:50 +0300

    fix(tenant-management-config): exports

--- npm/ng-packs/packages/tenant-management-config/src/public-api.ts
+++ npm/ng-packs/packages/tenant-management-config/src/public-api.ts
@@ -1,7 +1,2 @@
-/*
- * Public API Surface of tenant-management-config
- */
-
 export * from './lib/services/tenant-management-config.service';
-export * from './lib/tenant-management-config.component';
 export * from './lib/tenant-management-config.module';
