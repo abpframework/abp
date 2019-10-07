efb6c456f6453f715d65876722ad636bb024d12e
mehmet-erim <bilgi@mehmeterim.com>
2019-10-07 15:43:49 +0300

    fix: some bugs

--- npm/ng-packs/packages/theme-shared/src/lib/components/button/button.component.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/components/button/button.component.ts
@@ -3,17 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'abp-button',
   template: `
-    <button [attr.type]="type" [ngClass]="buttonClass" [disabled]="loading || disabled">
+    <button [attr.type]="buttonType || type" [ngClass]="buttonClass" [disabled]="loading || disabled">
       <i [ngClass]="icon" class="mr-1"></i><ng-content></ng-content>
     </button>
-  `
+  `,
 })
 export class ButtonComponent {
   @Input()
   buttonClass = 'btn btn-primary';
 
   @Input()
-  type = 'button';
+  buttonType = 'button';
 
   @Input()
   iconClass: string;
@@ -23,6 +23,11 @@ export class ButtonComponent {
 
   @Input()
   disabled = false;
+
+  /**
+   * @deprecated Use buttonType instead. To be deleted in v1
+   */
+  @Input() type = 'button';
 
   get icon(): string {
     return `${this.loading ? 'fa fa-pulse fa-spinner' : this.iconClass || 'd-none'}`;
