d7713ee30836cf667ae96fd8bd8a584f377cab82
mehmet-erim <bilgi@mehmeterim.com>
2019-10-08 13:09:19 +0300

    feat(module-template): add settings component
    #1652

--- templates/module/angular/projects/my-project-name-config/src/lib/services/my-project-name-config.service.ts
+++ templates/module/angular/projects/my-project-name-config/src/lib/services/my-project-name-config.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { eLayoutType, addAbpRoutes, ABP } from '@abp/ng.core';
+import { addSettingTab } from '@abp/ng.theme.shared';
+import { MyProjectNameSettingsComponent } from '../components/my-project-name-settings.component';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +14,12 @@ export class MyProjectNameConfigService {
       layout: eLayoutType.application,
       order: 2,
     } as ABP.FullRoute);
+
+    const route = addSettingTab({
+      component: MyProjectNameSettingsComponent,
+      name: 'MyProjectName Settings',
+      order: 1,
+      requiredPolicy: '',
+    });
   }
 }
