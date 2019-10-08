a99496f2efd229b9250334f7eb9c0371b276fa33
mehmet-erim <bilgi@mehmeterim.com>
2019-10-08 09:22:33 +0300

    feat: create module template main app #1652

---
+++ templates/module/angular/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { ABP } from '@abp/ng.core';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    data: {
+      routes: {
+        name: '::Menu:Home',
+      } as ABP.Route,
+    },
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./lazy-libs/account-wrapper.module').then(m => m.AccountWrapperModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
