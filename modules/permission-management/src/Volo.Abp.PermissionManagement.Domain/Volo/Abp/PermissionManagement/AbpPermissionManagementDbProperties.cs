4170a36764de6049956b6fc1d565ec75e02423e4
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2019-10-11 16:07:58 +0300

    PermissionManagement db table prefix revision

---
+++ modules/permission-management/src/Volo.Abp.PermissionManagement.Domain/Volo/Abp/PermissionManagement/AbpPermissionManagementDbProperties.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Data;
+
+namespace Volo.Abp.PermissionManagement
+{
+    public static class AbpPermissionManagementDbProperties
+    {
+        public static string DbTablePrefix { get; } = AbpCommonDbProperties.DbTablePrefix;
+
+        public static string DbSchema { get; } = AbpCommonDbProperties.DbSchema;
+
+        public const string ConnectionStringName = "AbpPermissionManagement";
+    }
+}
