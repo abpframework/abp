2d3645843c92564ab0f55babd8e67cf0314f6f0d
mehmet-erim <bilgi@mehmeterim.com>
2019-10-11 17:08:20 +0300

    refactor(core): remove unnecessary injected service

--- npm/ng-packs/packages/core/src/lib/guards/auth.guard.ts
+++ npm/ng-packs/packages/core/src/lib/guards/auth.guard.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Store } from '@ngxs/store';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { Observable } from 'rxjs';
-import { Navigate } from '@ngxs/router-plugin';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private oauthService: OAuthService, private store: Store, private router: Router) {}
+  constructor(private oauthService: OAuthService, private router: Router) {}
 
   canActivate(_: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean | UrlTree {
     const hasValidAccessToken = this.oauthService.hasValidAccessToken();
