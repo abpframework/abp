4a31ee9e10de53909f357ab182052c897b24ee13
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-10-21 14:51:46 +0300

    Remove_UniqueIndex_From_TenantName
    resolved https://github.com/abpframework/abp/issues/1970

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.EntityFrameworkCore.DbMigrations/Migrations/20191021114822_Remove_UniqueIndex_From_TenantName.Designer.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.EntityFrameworkCore.DbMigrations/Migrations/20191021114822_Remove_UniqueIndex_From_TenantName.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCompanyName.MyProjectName.EntityFrameworkCore;
 
 namespace MyCompanyName.MyProjectName.Migrations
 {
     [DbContext(typeof(MyProjectNameMigrationsDbContext))]
-    partial class MyProjectNameMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191021114822_Remove_UniqueIndex_From_TenantName")]
+    partial class Remove_UniqueIndex_From_TenantName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.EntityFrameworkCore.DbMigrations/Migrations/20191021114822_Remove_UniqueIndex_From_TenantName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCompanyName.MyProjectName.Migrations
+{
+    public partial class Remove_UniqueIndex_From_TenantName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpTenants_Name",
+                table: "AbpTenants");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpTenants_Name",
+                table: "AbpTenants",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpTenants_Name",
+                table: "AbpTenants");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpTenants_Name",
+                table: "AbpTenants",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
