10b5390b1f330c0c0490a38cee4192528dc59b59
windy liu <vincywindy@live.cn>
2019-10-30 22:05:26 +0800

    Add virual appservice method

--- modules/identity/src/Volo.Abp.Identity.Application/Volo/Abp/Identity/ProfileAppService.cs
+++ modules/identity/src/Volo.Abp.Identity.Application/Volo/Abp/Identity/ProfileAppService.cs
@@ -17,14 +17,14 @@ namespace Volo.Abp.Identity
             _userManager = userManager;
         }
 
-        public async Task<ProfileDto> GetAsync()
+        public virtual async Task<ProfileDto> GetAsync()
         {
             return ObjectMapper.Map<IdentityUser, ProfileDto>(
                 await _userManager.GetByIdAsync(CurrentUser.GetId())
             );
         }
 
-        public async Task<ProfileDto> UpdateAsync(UpdateProfileDto input)
+        public virtual async Task<ProfileDto> UpdateAsync(UpdateProfileDto input)
         {
             var user = await _userManager.GetByIdAsync(CurrentUser.GetId());
 
@@ -50,7 +50,7 @@ namespace Volo.Abp.Identity
             return ObjectMapper.Map<IdentityUser, ProfileDto>(user);
         }
 
-        public async Task ChangePasswordAsync(ChangePasswordInput input)
+        public virtual async Task ChangePasswordAsync(ChangePasswordInput input)
         {
             var currentUser = await _userManager.GetByIdAsync(CurrentUser.GetId());
             (await _userManager.ChangePasswordAsync(currentUser, input.CurrentPassword, input.NewPassword)).CheckErrors();
