104ebb4a32584361020a021d01fed6a8ce2e7d2b
mehmet-erim <bilgi@mehmeterim.com>
2019-10-31 09:50:53 +0300

    test: create mock strategy for breadcrumb

--- npm/ng-packs/packages/theme-shared/src/lib/tests/breadcrumb.component.spec.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/tests/breadcrumb.component.spec.ts
@@ -1,15 +1,16 @@
-import { LocalizationPipe } from '@abp/ng.core';
+import { LocalizationPipe, ABP } from '@abp/ng.core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { createComponentFactory, Spectator, SpyObject } from '@ngneat/spectator/jest';
 import { Store } from '@ngxs/store';
 import { BreadcrumbComponent } from '../components/breadcrumb/breadcrumb.component';
+import { Router } from '@angular/router';
 
 describe('BreadcrumbComponent', () => {
   let spectator: Spectator<BreadcrumbComponent>;
   let store: SpyObject<Store>;
   const createComponent = createComponentFactory({
     component: BreadcrumbComponent,
-    mocks: [Store],
+    mocks: [Store, Router],
     imports: [RouterTestingModule],
     declarations: [LocalizationPipe],
     detectChanges: false,
@@ -21,6 +22,9 @@ describe('BreadcrumbComponent', () => {
   });
 
   it('should display the breadcrumb', () => {
-    store.selectSnapshot.andCallFake(selector => selector({ LeptonLayoutState: {} }));
+    const router = spectator.get(Router);
+    (router as any).url = '/identity/users';
+    store.selectSnapshot.andReturn({ name: 'Identity', children: [{ name: 'Users', path: 'users' }] } as ABP.FullRoute);
+    spectator.detectChanges();
   });
 });
