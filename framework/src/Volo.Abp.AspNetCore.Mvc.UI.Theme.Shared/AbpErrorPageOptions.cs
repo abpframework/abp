e743e662af9f16caec5ba664c61b2fce8702cd07
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-11-06 10:38:46 +0300

    added AbpErrorPageOptions and moved the "/error" page to a new controller

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/AbpErrorPageOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared
+{
+    public class AbpErrorPageOptions
+    {
+        public readonly IDictionary<string, string> ErrorPageUrls;
+
+        public AbpErrorPageOptions()
+        {
+            ErrorPageUrls = new Dictionary<string, string>();
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/Pages/Error/AbpErrorPageModel.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Http;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Pages.Error
+{
+    public class AbpErrorPageModel
+    {
+        public RemoteServiceErrorInfo ErrorInfo { get; set; }
+
+        public int HttpStatusCode { get; set; }
+    }
+}
