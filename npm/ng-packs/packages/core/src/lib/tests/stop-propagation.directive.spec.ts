d4b5bd93deadd06d45593ab1f0015f55abedf603
thediaval <thediavall@gmail.com>
2019-11-07 17:28:19 +0300

    feat(core): add stop-propagation directive test

---
+++ npm/ng-packs/packages/core/src/lib/tests/stop-propagation.directive.spec.ts
@@ -0,0 +1,40 @@
+import { createDirectiveFactory, SpectatorDirective } from '@ngneat/spectator/jest';
+import { ClickEventStopPropagationDirective } from '../directives/stop-propagation.directive';
+
+describe('ClickEventStopPropagationDirective', () => {
+  let spectator: SpectatorDirective<ClickEventStopPropagationDirective>;
+  let directive: ClickEventStopPropagationDirective;
+  let link: HTMLAnchorElement;
+  let childClickEventFn = jest.fn(() => null);
+  let parentClickEventFn = jest.fn(() => null);
+  const createDirective = createDirectiveFactory({
+    directive: ClickEventStopPropagationDirective,
+  });
+
+  beforeEach(() => {
+    spectator = createDirective(
+      '<div (click)="parentClickEventFn()"><a (click.stop)="childClickEventFn()" >Link</a></div>',
+      {
+        hostProps: { parentClickEventFn, childClickEventFn },
+      },
+    );
+    directive = spectator.directive;
+    link = spectator.query('a');
+    childClickEventFn.mockClear();
+    parentClickEventFn.mockClear();
+  });
+
+  test('should be created', () => {
+    spectator.click('a');
+    expect(directive).toBeTruthy();
+  });
+
+  test("should not call click event of parent when child element's click event is triggered", done => {
+    spectator.setHostInput({ parentClickEventFn, childClickEventFn });
+    spectator.click('a');
+    spectator.detectChanges();
+    expect(childClickEventFn).toHaveBeenCalled();
+    expect(parentClickEventFn).not.toHaveBeenCalled();
+    done();
+  });
+});
