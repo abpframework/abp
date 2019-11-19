358f5fe76d83117964bdfbb0b7aa796786843501
mehmet-erim <bilgi@mehmeterim.com>
2019-11-19 11:47:25 +0300

    feat(client-generator): improve api defination model

--- npm/packs/client-generator/src/types/api-defination.ts
+++ npm/packs/client-generator/src/types/api-defination.ts
@@ -9,6 +9,45 @@ export namespace APIDefination {
 
   export interface Module {
     rootPath: string;
-    controllers: { [key: string]: any };
+    controllers: { [key: string]: Controller };
+  }
+
+  export interface Controller {
+    controllerName: string;
+    interfaces: { typeAsString: string }[];
+    typeAsString: string;
+    actions: { [key: string]: Action };
+  }
+
+  export interface Action {
+    uniqueName: string;
+    name: string;
+    httpMethod: string;
+    url: string;
+    supportedVersions: any[];
+    parametersOnMethod: ParametersOnMethod[];
+    parameters: Parameter[];
+    returnValue: ReturnValue;
+  }
+
+  export interface Parameter {
+    nameOnMethod: string;
+    name: string;
+    typeAsString: string;
+    isOptional: boolean;
+    defaultValue: null;
+    constraintTypes: null;
+    bindingSourceId: string;
+  }
+
+  export interface ParametersOnMethod {
+    name: string;
+    typeAsString: string;
+    isOptional: boolean;
+    defaultValue: null;
+  }
+
+  export interface ReturnValue {
+    typeAsString: string;
   }
 }
