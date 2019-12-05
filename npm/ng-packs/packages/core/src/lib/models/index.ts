14008e69de218468ee6724a84e419e5a55af77a4
mehmet-erim <bilgi@mehmeterim.com>
2019-12-05 10:55:19 +0300

    feat(core): add application dtos #2222

--- npm/ng-packs/packages/core/src/lib/models/index.ts
+++ npm/ng-packs/packages/core/src/lib/models/index.ts
@@ -1,6 +1,7 @@
 export * from './application-configuration';
 export * from './common';
 export * from './config';
+export * from './dtos';
 export * from './rest';
 export * from './session';
 export * from './profile';
