288070515a56fe56b00bdd8c714679761e1eb79a
YinChang <yincahng0626@gmail.com>
2019-12-08 17:43:23 +0800

    modify several module's SettingDefinitionProvider to support  multi-lingual

--- modules/account/src/Volo.Abp.Account.Web/Settings/AccountSettingDefinitionProvider.cs
+++ modules/account/src/Volo.Abp.Account.Web/Settings/AccountSettingDefinitionProvider.cs
@@ -1,4 +1,6 @@
-﻿using Volo.Abp.Settings;
+﻿using Volo.Abp.Account.Localization;
+using Volo.Abp.Localization;
+using Volo.Abp.Settings;
 
 namespace Volo.Abp.Account.Web.Settings
 {
@@ -7,12 +9,16 @@ namespace Volo.Abp.Account.Web.Settings
         public override void Define(ISettingDefinitionContext context)
         {
             context.Add(
-                new SettingDefinition(AccountSettingNames.IsSelfRegistrationEnabled, "true")
+                new SettingDefinition(AccountSettingNames.IsSelfRegistrationEnabled, "true", L("DisplayName:Abp.Account.IsSelfRegistrationEnabled"), L("Description:Abp.Account.IsSelfRegistrationEnabled"))
             );
 
             context.Add(
-                new SettingDefinition(AccountSettingNames.EnableLocalLogin, "true")
+                new SettingDefinition(AccountSettingNames.EnableLocalLogin, "true", L("DisplayName:Abp.Account.EnableLocalLogin"), L("Description:Abp.Account.EnableLocalLogin"))
             );
+        }
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<AccountResource>(name);
         }
     }
 }
