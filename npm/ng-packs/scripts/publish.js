c5aa981a7967fa0ce1bfb0e59275d793584af85f
mehmet-erim <bilgi@mehmeterim.com>
2019-12-10 09:21:36 +0300

    ci(scripts): add version updating process to publish script #2346

--- npm/ng-packs/scripts/publish.js
+++ npm/ng-packs/scripts/publish.js
@@ -1,12 +1,13 @@
 // ESM syntax is supported.
 import execa from 'execa';
+import fse from 'fs-extra';
 
 const versions = ['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease'];
 let nextSemanticVersion = (process.argv[2] || '').toLowerCase();
 
 if (versions.indexOf(nextSemanticVersion) < 0) {
   console.log(
-    "Please enter the next semantic version like this: 'npm run publish patch'. Available versions: " +
+    "Please enter the next semantic version like this: 'npm run publish patch'. Available versions:\n " +
       JSON.stringify(versions),
   );
 
@@ -16,6 +17,16 @@ if (versions.indexOf(nextSemanticVersion) < 0) {
 (async () => {
   try {
     await execa('yarn', ['install-new-dependencies'], { stdout: 'inherit' });
+
+    await fse.rename('../lerna.version.json', '../lerna.json');
+
+    await execa(
+      'yarn',
+      ['lerna', 'version', nextSemanticVersion, '--yes', '--no-commit-hooks', '--skip-git'],
+      { stdout: 'inherit', cwd: '../' },
+    );
+
+    await fse.rename('../lerna.json', '../lerna.version.json');
   } catch (error) {
     console.error(error.stderr);
     process.exit(1);
