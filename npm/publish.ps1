44b439514bbc0af8e74d7f4ed6b0ae60afe30c97
mehmet-erim <bilgi@mehmeterim.com>
2019-12-12 13:57:36 +0300

    ci: refactor publish.ps1

--- npm/publish.ps1
+++ npm/publish.ps1
@@ -1,10 +1,31 @@
-& cd ng-packs\scripts
-& npm install
-& yarn publish-packages patch
-& cd ../../
-& yarn
-& yarn lerna publish patch --no-push --yes --no-git-reset --no-commit-hooks --no-git-tag-version --force-publish
-& yarn update:templates
-& yarn global add gulp
-& yarn gulp:app
-& yarn gulp:module
+param(
+  [string]$Version
+)
+
+if(-Not $Version) {
+echo 'Please pass a semantic version like this: ./publish.ps1 -Version patch'
+exit
+}
+
+$rootFolder = (Get-Item -Path "./" -Verbose).FullName
+
+$commands = (
+  "cd ng-packs\scripts",
+  "npm install",
+  "npm run publish-packages -- $Version",
+  "cd ../../",
+  "yarn",
+  "yarn lerna publish $Version --no-push --yes --no-git-reset --no-commit-hooks --no-git-tag-version --force-publish",
+  "yarn gulp:app",
+  "yarn gulp:module"
+)
+
+foreach ($command in $commands) { 
+  Write-Host $command
+  Invoke-Expression $command
+  if($LASTEXITCODE -ne '0' -And $command -notlike '*cd *'){
+    Write-Host ("Process failed! " + $command)
+    Set-Location $rootFolder
+    exit $LASTEXITCODE
+  }
+}
