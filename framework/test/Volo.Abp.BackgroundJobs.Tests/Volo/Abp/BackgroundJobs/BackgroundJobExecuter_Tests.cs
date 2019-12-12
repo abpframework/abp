b1eed339622e4a5abba6d0e1c4f29f52f1ab3846
maliming <malimings@gmail.com>
2019-12-12 15:36:38 +0800

    Implement AsyncBackgroundJob.
    Resolve #2374

--- framework/test/Volo.Abp.BackgroundJobs.Tests/Volo/Abp/BackgroundJobs/BackgroundJobExecuter_Tests.cs
+++ framework/test/Volo.Abp.BackgroundJobs.Tests/Volo/Abp/BackgroundJobs/BackgroundJobExecuter_Tests.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Shouldly;
 using Xunit;
 
@@ -28,6 +28,29 @@ namespace Volo.Abp.BackgroundJobs
                     ServiceProvider,
                     typeof(MyJob),
                     new MyJobArgs("42")
+                )
+            );
+
+            //Assert
+
+            jobObject.ExecutedValues.ShouldContain("42");
+        }
+
+        [Fact]
+        public async Task Should_Execute_Async_Tasks()
+        {
+            //Arrange
+
+            var jobObject = GetRequiredService<MyAsyncJob>();
+            jobObject.ExecutedValues.ShouldBeEmpty();
+
+            //Act
+
+            _backgroundJobExecuter.Execute(
+                new JobExecutionContext(
+                    ServiceProvider,
+                    typeof(MyAsyncJob),
+                    new MyAsyncJobArgs("42")
                 )
             );
 
