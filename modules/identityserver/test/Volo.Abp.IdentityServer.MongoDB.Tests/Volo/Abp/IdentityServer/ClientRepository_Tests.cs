18344cbdae4780a372b083dbc4f3129aaef15eeb
Mehmet Tüken <mehmetuken@hotmail.com>
2019-12-18 03:37:57 +0300

    [modules/identityserver] fix mongo.exe dispose

--- modules/identityserver/test/Volo.Abp.IdentityServer.MongoDB.Tests/Volo/Abp/IdentityServer/ClientRepository_Tests.cs
+++ modules/identityserver/test/Volo.Abp.IdentityServer.MongoDB.Tests/Volo/Abp/IdentityServer/ClientRepository_Tests.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xunit;
 
 namespace Volo.Abp.IdentityServer
 {
+    [Collection(MongoTestCollection.Name)]
     public class ClientRepository_Tests : ClientRepository_Tests<AbpIdentityServerMongoDbTestModule>
     {
 
--- modules/identityserver/test/Volo.Abp.IdentityServer.MongoDB.Tests/Volo/Abp/IdentityServer/PersistentGrantRepository_Tests.cs
+++ modules/identityserver/test/Volo.Abp.IdentityServer.MongoDB.Tests/Volo/Abp/IdentityServer/PersistentGrantRepository_Tests.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xunit;
 
 namespace Volo.Abp.IdentityServer
 {
+    [Collection(MongoTestCollection.Name)]
     public class PersistentGrantRepository_Tests : PersistentGrantRepository_Tests<AbpIdentityServerMongoDbTestModule>
     {
 
