efae782ccee7e89812d8415b27f81693785812c3
TheDiaval <thediavall@gmail.com>
2019-12-20 17:06:07 +0300

    feature(permission-management): add actions to state service

--- npm/ng-packs/packages/permission-management/src/lib/services/permission-management-state.service.ts
+++ npm/ng-packs/packages/permission-management/src/lib/services/permission-management-state.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { PermissionManagementState } from '../states/permission-management.state';
+import { PermissionManagement } from '../models';
+import { GetPermissions, UpdatePermissions } from '../actions';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,15 @@ export class PermissionManagementStateService {
   }
   getEntityDisplayName() {
     return this.store.selectSnapshot(PermissionManagementState.getEntityDisplayName);
+  }
+
+  getPermissions(payload: PermissionManagement.GrantedProvider) {
+    return this.store.dispatch(new GetPermissions(payload));
+  }
+
+  updatePermissions(
+    payload: PermissionManagement.GrantedProvider & PermissionManagement.UpdateRequest,
+  ) {
+    return this.store.dispatch(new UpdatePermissions(payload));
   }
 }
