e2fb9384c47b1a3604a8eb00ce2d6850e6630936
TheDiaval <thediavall@gmail.com>
2019-12-20 16:47:50 +0300

    feature(feature-management): add state actions to state service

--- npm/ng-packs/packages/feature-management/src/lib/services/feature-management-state.service.ts
+++ npm/ng-packs/packages/feature-management/src/lib/services/feature-management-state.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { FeatureManagementState } from '../states';
+import { FeatureManagement } from '../models';
+import { GetFeatures, UpdateFeatures } from '../actions';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +12,13 @@ export class FeatureManagementStateService {
 
   getFeatures() {
     return this.store.selectSnapshot(FeatureManagementState.getFeatures);
+  }
+
+  fetchFeatures(payload: FeatureManagement.Provider) {
+    return this.store.dispatch(new GetFeatures(payload));
+  }
+
+  updateFeatures(payload: FeatureManagement.Provider & FeatureManagement.Features) {
+    return this.store.dispatch(new UpdateFeatures(payload));
   }
 }
