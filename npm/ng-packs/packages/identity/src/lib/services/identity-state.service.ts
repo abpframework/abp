b2e5362d835821a49a5ec0314eb1ead67cfd4832
TheDiaval <thediavall@gmail.com>
2019-12-20 16:48:12 +0300

    feature(identity): add actions to state service

--- npm/ng-packs/packages/identity/src/lib/services/identity-state.service.ts
+++ npm/ng-packs/packages/identity/src/lib/services/identity-state.service.ts
@@ -1,5 +1,20 @@
+import { ABP } from '@abp/ng.core';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
+import {
+  CreateRole,
+  CreateUser,
+  DeleteRole,
+  DeleteUser,
+  GetRoleById,
+  GetRoles,
+  GetUserById,
+  GetUsers,
+  UpdateRole,
+  UpdateUser,
+  GetUserRoles,
+} from '../actions/identity.actions';
+import { Identity } from '../models/identity';
 import { IdentityState } from '../states/identity.state';
 
 @Injectable({
@@ -19,5 +34,49 @@ export class IdentityStateService {
   }
   getUsersTotalCount() {
     return this.store.selectSnapshot(IdentityState.getUsersTotalCount);
+  }
+
+  fetchRoles(payload?: ABP.PageQueryParams) {
+    return this.store.dispatch(new GetRoles(payload));
+  }
+
+  fetchRole(payload: string) {
+    return this.store.dispatch(new GetRoleById(payload));
+  }
+
+  deleteRole(payload: string) {
+    return this.store.dispatch(new DeleteRole(payload));
+  }
+
+  createRole(payload: Identity.RoleSaveRequest) {
+    return this.store.dispatch(new CreateRole(payload));
+  }
+
+  updateRole(payload: Identity.RoleItem) {
+    return this.store.dispatch(new UpdateRole(payload));
+  }
+
+  fetchUsers(payload?: ABP.PageQueryParams) {
+    return this.store.dispatch(new GetUsers(payload));
+  }
+
+  fetchUser(payload: string) {
+    return this.store.dispatch(new GetUserById(payload));
+  }
+
+  deleteUser(payload: string) {
+    return this.store.dispatch(new DeleteUser(payload));
+  }
+
+  createUser(payload: Identity.UserSaveRequest) {
+    return this.store.dispatch(new CreateUser(payload));
+  }
+
+  updateUser(payload: Identity.UserItem) {
+    return this.store.dispatch(new UpdateUser(payload));
+  }
+
+  getUserRoles(payload: string) {
+    return this.store.dispatch(new GetUserRoles(payload));
   }
 }
