2f3c43bb26cf0a3c4763c626bb8f4b441eee7dce
TheDiaval <thediavall@gmail.com>
2019-12-24 10:47:20 +0300

    test(core): add missing tests for state services

--- npm/ng-packs/packages/core/src/lib/tests/session-state.service.spec.ts
+++ npm/ng-packs/packages/core/src/lib/tests/session-state.service.spec.ts
@@ -2,6 +2,8 @@ import { createServiceFactory, SpectatorService, SpyObject } from '@ngneat/spect
 import { SessionStateService } from '../services/session-state.service';
 import { SessionState } from '../states/session.state';
 import { Store } from '@ngxs/store';
+import * as SessionActions from '../actions';
+
 describe('SessionStateService', () => {
   let service: SessionStateService;
   let spectator: SpectatorService<SessionStateService>;
@@ -33,6 +35,23 @@ describe('SessionStateService', () => {
           service[fnName]();
           expect(spy).toHaveBeenCalledWith(SessionState[fnName]);
         }
+      });
+  });
+
+  test('should have a dispatch method for every sessionState action', () => {
+    const reg = /(?<=dispatch)(\w+)(?=\()/gm;
+    SessionStateService.toString()
+      .match(reg)
+      .forEach(fnName => {
+        expect(SessionActions[fnName]).toBeTruthy();
+
+        const spy = jest.spyOn(store, 'dispatch');
+        spy.mockClear();
+
+        const params = Array.from(new Array(SessionActions[fnName].length));
+
+        service[`dispatch${fnName}`](...params);
+        expect(spy).toHaveBeenCalledWith(new SessionActions[fnName](...params));
       });
   });
 });
