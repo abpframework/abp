f89bade3a61b1e901766e6d1c2d5cbd10ecb2cb2
TheDiaval <thediavall@gmail.com>
2019-12-24 10:51:57 +0300

    feature(theme-basic): add state action dispatchers to state service

--- npm/ng-packs/packages/theme-basic/src/lib/services/layout-state.service.ts
+++ npm/ng-packs/packages/theme-basic/src/lib/services/layout-state.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { LayoutState } from '../states/layout.state';
+import { AddNavigationElement, RemoveNavigationElementByName } from '../actions';
+import { Layout } from '../models/layout';
 
 @Injectable()
 export class LayoutStateService {
@@ -8,5 +10,13 @@ export class LayoutStateService {
 
   getNavigationElements() {
     return this.store.selectSnapshot(LayoutState.getNavigationElements);
+  }
+
+  dispatchAddNavigationElement(payload: Layout.NavigationElement | Layout.NavigationElement[]) {
+    return this.store.dispatch(new AddNavigationElement(payload));
+  }
+
+  dispatchRemoveNavigationElementByName(name: string) {
+    return this.store.dispatch(new RemoveNavigationElementByName(name));
   }
 }
