f703db96e50bf149e04ef17cde67efbd760dbcde
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2019-12-26 11:53:40 +0300

    Localize exception message on LimitedResultRequestDto
    resolves https://github.com/abpframework/abp/issues/2468

---
+++ framework/src/Volo.Abp.Ddd.Application.Contracts/Volo/Abp/Application/Localization/AbpValidationResource.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Localization;
+
+namespace Volo.Abp.Application.Localization
+{
+    [LocalizationResourceName("AbpValidation")]
+    public class AbpValidationResource
+    {
+    }
+}
