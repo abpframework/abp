af005d6b820934fbeb86a4ab453698b709330fb2
Mehmet Tüken <mehmetuken@hotmail.com>
2019-12-28 00:36:21 +0300

    fix test fails
    ServiceProxyScript minify it's not parameter setter.

--- framework/test/Volo.Abp.AspNetCore.Mvc.Tests/Volo/Abp/AspNetCore/Mvc/ProxyScripting/AbpServiceProxiesController_Tests.cs
+++ framework/test/Volo.Abp.AspNetCore.Mvc.Tests/Volo/Abp/AspNetCore/Mvc/ProxyScripting/AbpServiceProxiesController_Tests.cs
@@ -9,19 +9,8 @@ namespace Volo.Abp.AspNetCore.Mvc.ProxyScripting
         [Fact]
         public async Task GetAll()
         {
-            var script = await GetResponseAsStringAsync("/Abp/ServiceProxyScript?minify=true");
-            script.Length.ShouldBeGreaterThan(0);
-        }
-
-        [Fact]
-        public async Task GetAllWithMinify()
-        {
             var script = await GetResponseAsStringAsync("/Abp/ServiceProxyScript");
-            var minifyScript = await GetResponseAsStringAsync("/Abp/ServiceProxyScript?minify=true");
-
             script.Length.ShouldBeGreaterThan(0);
-            minifyScript.Length.ShouldBeGreaterThan(0);
-            minifyScript.Length.ShouldBeLessThan(script.Length);
         }
     }
 }
