2239d010d451dbaddc168c28dd104ab45fd28cc2
mehmet-erim <bilgi@mehmeterim.com>
2019-12-30 15:41:24 +0300

    feat(account): add tenant-box component inputs and outputs interfaces
    #2404

---
+++ npm/ng-packs/packages/account/src/lib/models/account.ts
@@ -0,0 +1,6 @@
+  // tslint:disable-next-line: no-empty-interface
+  export interface TenantBoxComponentInputs {}
+
+  // tslint:disable-next-line: no-empty-interface
+  export interface TenantBoxComponentOutputs {}
+}
