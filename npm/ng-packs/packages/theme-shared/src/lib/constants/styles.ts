27d9f690566466170e950d93b7bf7d79b5b851f6
TheDiaval <thediavall@gmail.com>
2020-01-02 14:00:45 +0300

    refactor(theme-shared): change modal-backdrop size

--- npm/ng-packs/packages/theme-shared/src/lib/constants/styles.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/constants/styles.ts
@@ -48,8 +48,8 @@ export default `
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   background-color: rgba(0, 0, 0, 0.6);
   z-index: 1040;
 }
