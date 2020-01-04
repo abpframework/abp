50e9868c23670b51c0270187c4ec3abcc1203345
Galip Tolga Erdem <galiperdem@gmail.com>
2020-01-04 23:29:51 +0300

    Added cosmosdb provider

---
+++ framework/src/Volo.Abp.EntityFrameworkCore.CosmosDB/Microsoft/EntityFrameworkCore/CosmosDbContextOptionsExtensions.cs
@@ -0,0 +1,51 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Volo.Abp;
+// Microsoft.EntityFrameworkCore.Cosmos doesn't have single connectionString parameter DbContextOptionsBuilder in {ApplicationName}.EntityFrameworkCore.DbMigrations\{ApplicationName}MigrationsDbContextFactory
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class CosmosDbContextOptionsExtensions
+    {
+        public static DbContextOptionsBuilder<TContext> UseCosmos<TContext>(
+            [NotNull] this DbContextOptionsBuilder<TContext> optionsBuilder,
+            [NotNull] string connectionString,
+            [CanBeNull] Action<CosmosDbContextOptionsBuilder> cosmosOptionsAction = null)
+            where TContext : DbContext
+            => (DbContextOptionsBuilder<TContext>)UseCosmos(
+                (DbContextOptionsBuilder)optionsBuilder,
+                connectionString,
+                cosmosOptionsAction
+                );
+
+        public static DbContextOptionsBuilder UseCosmos(
+            [NotNull] this DbContextOptionsBuilder optionsBuilder,
+            [NotNull] string connectionString,
+            [CanBeNull] Action<CosmosDbContextOptionsBuilder> cosmosOptionsAction = null)
+        {
+            var connStringParts = connectionString.ParseConnectionString();
+            var accountEndpoint = connStringParts["AccountEndpoint"];
+            if (string.IsNullOrEmpty(accountEndpoint))
+                throw new AbpException($"CosmosDb AccountName can not be null or empty!");
+
+            var accountKey = connStringParts["AccountKey"];
+            if (string.IsNullOrEmpty(accountEndpoint))
+                throw new AbpException($"CosmosDb AccountKey can not be null or empty!");
+
+            var databaseName = connStringParts["DatabaseName"];
+            if (string.IsNullOrEmpty(accountEndpoint))
+                throw new AbpException($"CosmosDb DatabaseName can not be null or empty!");
+
+            return optionsBuilder.UseCosmos(accountEndpoint: accountEndpoint, accountKey: accountKey, databaseName: databaseName, cosmosOptionsAction);
+        }
+
+        static Dictionary<string, string> ParseConnectionString(this string connString)
+        {
+            return connString.Split(';')
+            .Select(t => t.Split(new char[] { '=' }, 2))
+            .ToDictionary(t => t[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.EntityFrameworkCore.CosmosDB/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextCosmosDBExtensions.cs
@@ -0,0 +1,63 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Volo.Abp.EntityFrameworkCore.DependencyInjection;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextConfigurationContextCosmosDBExtensions
+    {
+        public static DbContextOptionsBuilder UseCosmos(
+           [NotNull] this AbpDbContextConfigurationContext context,
+           [CanBeNull] Action<CosmosDbContextOptionsBuilder> cosmosOptionsAction = null)
+        {
+            //context.ConnectionStringName = "AbpBackgroundJobs" hits all the time
+            if (context.ExistingConnection != null)
+            {
+                var connStringParts = context.ExistingConnection.ConnectionString.ParseConnectionString();
+
+                var accountEndpoint = connStringParts["AccountEndpoint"];
+                if (string.IsNullOrEmpty(accountEndpoint))
+                    throw new AbpException($"CosmosDb AccountName can not be null or empty!");
+
+                var accountKey = connStringParts["AccountKey"];
+                if (string.IsNullOrEmpty(accountEndpoint))
+                    throw new AbpException($"CosmosDb AccountKey can not be null or empty!");
+
+                var databaseName = connStringParts["DatabaseName"];
+                if (string.IsNullOrEmpty(accountEndpoint))
+                    throw new AbpException($"CosmosDb DatabaseName can not be null or empty!");
+
+                return context.DbContextOptions.UseCosmos(accountEndpoint: accountEndpoint, accountKey: accountKey, databaseName: databaseName, cosmosOptionsAction);
+            }
+            else
+            {
+                var connStringParts = context.ConnectionString.ParseConnectionString();
+
+                var accountEndpoint = connStringParts["AccountEndpoint"];
+                if (string.IsNullOrEmpty(accountEndpoint))
+                    throw new AbpException($"CosmosDb AccountName can not be null or empty!");
+
+                var accountKey = connStringParts["AccountKey"];
+                if (string.IsNullOrEmpty(accountEndpoint))
+                    throw new AbpException($"CosmosDb AccountKey can not be null or empty!");
+
+                var databaseName = connStringParts["DatabaseName"];
+                if (string.IsNullOrEmpty(accountEndpoint))
+                    throw new AbpException($"CosmosDb DatabaseName can not be null or empty!");
+
+                return context.DbContextOptions.UseCosmos(accountEndpoint: accountEndpoint, accountKey: accountKey, databaseName: databaseName, cosmosOptionsAction);
+            }
+        }
+
+        static Dictionary<string, string> ParseConnectionString(this string connString)
+        {
+            return connString.Split(';')
+            .Select(t => t.Split(new char[] { '=' }, 2))
+            .ToDictionary(t => t[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+        }
+    }
+}
