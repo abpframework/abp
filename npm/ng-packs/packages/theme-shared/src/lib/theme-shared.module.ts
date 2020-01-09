13ed5e03aa013b65aa15a42ec133d9a29c4ca803
TheDiaval <thediavall@gmail.com>
2020-01-09 19:21:41 +0300

    feat(theme-shared): import toast-container component to module
    related issue: #2537

--- npm/ng-packs/packages/theme-shared/src/lib/theme-shared.module.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/theme-shared.module.ts
@@ -1,9 +1,10 @@
 import { CoreModule, LazyLoadService } from '@abp/ng.core';
+import { DatePipe } from '@angular/common';
 import { APP_INITIALIZER, Injector, ModuleWithProviders, NgModule } from '@angular/core';
+import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { NgxValidateCoreModule } from '@ngx-validate/core';
 import { MessageService } from 'primeng/components/common/messageservice';
 import { ToastModule } from 'primeng/toast';
-import { forkJoin } from 'rxjs';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ChartComponent } from './components/chart/chart.component';
@@ -13,16 +14,15 @@ import { LoaderBarComponent } from './components/loader-bar/loader-bar.component
 import { ModalComponent } from './components/modal/modal.component';
 import { SortOrderIconComponent } from './components/sort-order-icon/sort-order-icon.component';
 import { TableEmptyMessageComponent } from './components/table-empty-message/table-empty-message.component';
+import { ToastContainerComponent } from './components/toast-container/toast-container.component';
 import { ToastComponent } from './components/toast/toast.component';
 import styles from './constants/styles';
 import { TableSortDirective } from './directives/table-sort.directive';
 import { ErrorHandler } from './handlers/error.handler';
-import { chartJsLoaded$ } from './utils/widget-utils';
 import { RootParams } from './models/common';
-import { HTTP_ERROR_CONFIG, httpErrorConfigFactory } from './tokens/http-error.token';
-import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { httpErrorConfigFactory, HTTP_ERROR_CONFIG } from './tokens/http-error.token';
 import { DateParserFormatter } from './utils/date-parser-formatter';
-import { DatePipe } from '@angular/common';
+import { chartJsLoaded$ } from './utils/widget-utils';
 
 export function appendScript(injector: Injector) {
   const fn = () => {
@@ -47,6 +47,7 @@ export function appendScript(injector: Injector) {
     ModalComponent,
     TableEmptyMessageComponent,
     ToastComponent,
+    ToastContainerComponent,
     SortOrderIconComponent,
     TableSortDirective,
   ],
@@ -59,6 +60,7 @@ export function appendScript(injector: Injector) {
     ModalComponent,
     TableEmptyMessageComponent,
     ToastComponent,
+    ToastContainerComponent,
     SortOrderIconComponent,
     TableSortDirective,
   ],
