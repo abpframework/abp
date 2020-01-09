6ec6a7bb0b7416fe1c9542a3fc8f116d08714ff6
Arkat Erol <arkaterol@outlook.com>
2020-01-09 10:41:58 +0300

    Update generate proxy command

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/GenerateProxyCommand.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/GenerateProxyCommand.cs
@@ -49,43 +49,66 @@ namespace Volo.Abp.Cli.Commands
                 Logger.LogInformation("Downloading api definition...");
                 Logger.LogInformation("Api Url: " + apiUrl);
 
-                JObject data = JObject.Parse(json);
-
-                string rootPath = (string)data["modules"]["app"]["rootPath"];
-
-                System.IO.Directory.CreateDirectory(string.Format("src/app/{0}/shared/models", rootPath));
-                System.IO.Directory.CreateDirectory(string.Format("src/app/{0}/shared/services", rootPath));
+                JObject data = JObject.Parse(json); 
 
                 var serviceIndexList = new List<string>();
+                var modelIndexList = new List<string>();
+
+                var moduleList = new Dictionary<string, string>();
 
                 foreach (var module in data["modules"])
                 {
-                    foreach (var item in module.First["controllers"])
+                    string rootPath = ((string)module.First["rootPath"]).ToLower();
+
+                    if (moduleList.Any(p => p.Key == rootPath))
+                    {
+                        var value = moduleList[rootPath];
+
+                        moduleList[rootPath] = value.TrimEnd('}') + "," + module.First["controllers"].ToString().TrimStart('{');
+                    }
+                    else {
+                        moduleList.Add(rootPath, module.First["controllers"].ToString());
+                    }                    
+                }
+
+
+                foreach (var module in moduleList)
+                {
+                    JObject moduleValue = JObject.Parse(module.Value); 
+
+                    string rootPath = module.Key;
+                    var controllerName = "";
+                    var controllerServiceName = "";
+
+                    Directory.CreateDirectory(string.Format("src/app/{0}/shared/models", rootPath));
+                    Directory.CreateDirectory(string.Format("src/app/{0}/shared/services", rootPath));
+
+                    StringBuilder serviceFileText = new StringBuilder();
+
+                    serviceFileText.AppendLine("firstTypeList");
+                    serviceFileText.AppendLine("import { Injectable } from '@angular/core';");
+                    serviceFileText.AppendLine("import { Observable } from 'rxjs';");
+                    serviceFileText.AppendLine("secondTypeList");
+                    serviceFileText.AppendLine("");
+                    serviceFileText.AppendLine("@Injectable()");
+                    serviceFileText.AppendLine(string.Format("export class {0}Service ", "[ControllerName]") + "{");
+                    serviceFileText.AppendLine("  constructor(private restService: RestService) {}");
+                    serviceFileText.AppendLine("");
+
+                    var firstTypeList = new List<string>();
+                    var secondTypeList = new List<string>();
+
+                    foreach (var item in moduleValue.Root.ToList())
                     {
                         var controller = item.First;
 
-                        var controllerName = (string)controller["controllerName"];
-                        var controllerServiceName = controllerName.PascalToKebabCase() + ".service.ts";
+                        controllerName = (string)controller["controllerName"];
+                        controllerServiceName = controllerName.PascalToKebabCase() + ".service.ts"; 
 
-                        StringBuilder serviceFileText = new StringBuilder();
-
-                        serviceFileText.AppendLine("firstTypeList");
-                        serviceFileText.AppendLine("import { Injectable } from '@angular/core';");
-                        serviceFileText.AppendLine("import { Observable } from 'rxjs';");
-                        serviceFileText.AppendLine("secondTypeList");
-                        serviceFileText.AppendLine("");
-                        serviceFileText.AppendLine("@Injectable()");
-                        serviceFileText.AppendLine(string.Format("export class {0}Service ", controllerName) + "{");
-                        serviceFileText.AppendLine("  constructor(private restService: RestService) {}");
-                        serviceFileText.AppendLine("");
-
-                        var firstTypeList = new List<string>();
-                        var secondTypeList = new List<string>();
-
-                        foreach (var controllerItem in controller["actions"])
+                        foreach (var actionItem in controller["actions"])
                         {
-                            var action = controllerItem.First;
-                            var actionName = (string)action["name"];
+                            var action = actionItem.First;
+                            var actionName = (string)action["uniqueName"];
 
                             actionName = (char.ToLower(actionName[0]) + actionName.Substring(1)).Replace("Async", "").Replace("Controller", "");
 
@@ -182,6 +205,12 @@ namespace Volo.Abp.Cli.Commands
                                     serviceFileText.AppendLine(string.Format(" {0}({1}): Observable<{2}> {{", actionName, parametersText.ToString(), type));
                                     secondTypeList.Add(type);
                                 }
+
+                                var modelIndex = CreateType(data, returnValueType, rootPath);
+
+                                if (!string.IsNullOrWhiteSpace(modelIndex))
+                                    modelIndexList.Add(modelIndex);
+
                             }
 
                             if (modelBindingExtraList != null && modelBindingExtraList.Count > 0)
@@ -194,125 +223,196 @@ namespace Volo.Abp.Cli.Commands
                             serviceFileText.AppendLine(string.Format("   return this.restService.request({{ url: '/{0}', method: '{1}'{2}{3} }});", url, httpMethod, bodyExtra, modelBindingExtra));
 
                             serviceFileText.AppendLine(" }");
-                        }
-
-                        serviceFileText.AppendLine("}");
-
-                        if (firstTypeList != null && firstTypeList.Count > 0)
-                        {
-                            var firstTypeListDistinct = ", " + String.Join(", ", firstTypeList.Where(p => p != "void").Distinct().ToArray());
-                            serviceFileText.Replace("firstTypeList", string.Format("import {{ RestService {0}}} from '@abp/ng.core';", firstTypeListDistinct));
-                        }
-                        else
-                        {
-                            serviceFileText.Replace("firstTypeList", "");
-                        }
-
-                        if (secondTypeList != null && secondTypeList.Count > 0)
-                        {
-                            var secondTypeListDistinct = String.Join(", ", secondTypeList.Where(p => p != "void").Distinct().ToArray());
-                            serviceFileText.Replace("secondTypeList", string.Format("import {{{0}}} from '../models';", secondTypeListDistinct));
-                        }
-                        else
-                        {
-                            serviceFileText.Replace("secondTypeList", "");
-                        }
-
-                        System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/services/{1}", rootPath, controllerServiceName), serviceFileText.ToString());
+                        } 
 
                         serviceIndexList.Add(controllerServiceName.Replace(".ts", ""));
                     }
-                }
 
-                StringBuilder serviceIndexFileText = new StringBuilder();
-
-                foreach (var serviceIndexItem in serviceIndexList)
-                {
-                    serviceIndexFileText.AppendLine(string.Format("export * from './{0}';", serviceIndexItem));
-                } 
-
-                System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/services/index.ts", rootPath), serviceIndexFileText.ToString());
-
-                var modelIndexList = new List<string>();
-
-                foreach (var type in data["types"])
-                {
-                    var typeFullName = ((JProperty)type.First.Parent).Name;
-
-                    var typeNameSplit = typeFullName.Split(".");
-                    var typeName = typeNameSplit[typeNameSplit.Length - 1];
-
-                    var typeModelName = typeName.Replace("<","").Replace(">", "").PascalToKebabCase() + ".ts";
-
-                    StringBuilder modelFileText = new StringBuilder();
-
-
-                    var baseType = (string)type.First["baseType"];
-                    var extends = "";
-
-                    if (!string.IsNullOrWhiteSpace(baseType))
+                    if (firstTypeList != null && firstTypeList.Count > 0)
                     {
-                        var baseTypeSplit = baseType.Split(".");
-                        var baseTypeName = baseTypeSplit[baseTypeSplit.Length - 1];
-                        var baseTypeKebabCase = baseTypeName.PascalToKebabCase();
-
-                        modelFileText.AppendLine(string.Format("import {{ {0} }} from '@abp/ng.core';", baseTypeName, baseTypeKebabCase));
-                        extends = "extends " + baseTypeName;
-                    }
-
-                    modelFileText.AppendLine(string.Format("export class {0} {1} {{", typeName, extends));
-
-                    foreach (var property in type.First["properties"])
-                    {
-                        var propertyName = (string)property["name"];
-                        propertyName = (char.ToLower(propertyName[0]) + propertyName.Substring(1));
-
-                        var typeSimple = (string)property["typeSimple"];
-
-                        modelFileText.AppendLine(string.Format("  {0}: {1};", propertyName, typeSimple));
-                    }
-
-                    modelFileText.AppendLine("");
-
-                    modelFileText.AppendLine(string.Format("  constructor(initialValues: Partial<{0}> = {{}}) {{", typeName));
-
-                    if (!string.IsNullOrWhiteSpace(baseType))
-                    {
-                        modelFileText.AppendLine("    super(initialValues);");
+                        var firstTypeListDistinct = ", " + String.Join(", ", firstTypeList.Where(p => p != "void").Distinct().ToArray());
+                        serviceFileText.Replace("firstTypeList", string.Format("import {{ RestService {0}}} from '@abp/ng.core';", firstTypeListDistinct));
                     }
                     else
                     {
-                        modelFileText.AppendLine("    if (initialValues) {");
-                        modelFileText.AppendLine("      for (const key in initialValues) {");
-                        modelFileText.AppendLine("        if (initialValues.hasOwnProperty(key)) {");
-                        modelFileText.AppendLine("          this[key] = initialValues[key];");
-
-                        modelFileText.AppendLine("        }");
-                        modelFileText.AppendLine("      }");
-                        modelFileText.AppendLine("    }");
-                        modelFileText.AppendLine("  }");
+                        serviceFileText.Replace("firstTypeList", "");
                     }
 
-                    modelFileText.AppendLine("}");
+                    if (secondTypeList != null && secondTypeList.Count > 0)
+                    {
+                        var secondTypeListDistinct = String.Join(", ", secondTypeList.Where(p => p != "void").Distinct().ToArray());
+                        serviceFileText.Replace("secondTypeList", string.Format("import {{{0}}} from '../models';", secondTypeListDistinct));
+                    }
+                    else
+                    {
+                        serviceFileText.Replace("secondTypeList", "");
+                    } 
 
-                    System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/models/{1}", rootPath, typeModelName), modelFileText.ToString());
+                    serviceFileText.AppendLine("}");
 
-                    modelIndexList.Add(typeModelName.Replace(".ts", ""));
+                    serviceFileText.Replace("[ControllerName]", controllerName);
+                    System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/services/{1}", rootPath, controllerServiceName), serviceFileText.ToString());
                 }
 
-                StringBuilder modelIndexFileText = new StringBuilder();
+                //StringBuilder serviceIndexFileText = new StringBuilder();
 
-                foreach (var modelIndexItem in modelIndexList)
-                {
-                    modelIndexFileText.AppendLine(string.Format("export * from './{0}';", modelIndexItem));
-                }
+                //foreach (var serviceIndexItem in serviceIndexList)
+                //{
+                //    serviceIndexFileText.AppendLine(string.Format("export * from './{0}';", serviceIndexItem));
+                //}
 
-                System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/models/index.ts", rootPath), modelIndexFileText.ToString()); 
+                //System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/services/index.ts", rootPath), serviceIndexFileText.ToString());
+
+                //StringBuilder modelIndexFileText = new StringBuilder();
+
+                //foreach (var modelIndexItem in modelIndexList)
+                //{
+                //    modelIndexFileText.AppendLine(string.Format("export * from './{0}';", modelIndexItem));
+                //}
+
+                //System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/models/index.ts", rootPath), modelIndexFileText.ToString());
             }
             catch (Exception ex)
             {
                 throw;
             }
+        }
+
+        private string CreateType(JObject data, string returnValueType, string rootPath)
+        { 
+            var type = data["types"][returnValueType];
+
+            if (returnValueType.StartsWith("Volo.Abp.Application.Dtos")
+                 || returnValueType.StartsWith("System.Collections")
+                 || returnValueType == "System.String"
+                 || returnValueType == "System.Void"
+                 || returnValueType.Contains("IActionResult")
+                 || returnValueType.Contains("IStringValueType")
+                 || returnValueType.Contains("IValueValidator")
+                 )
+                return null;
+
+            var typeNameSplit = returnValueType.Split(".");
+            var typeName = typeNameSplit[typeNameSplit.Length - 1];
+
+            var typeModelName = typeName.Replace("<", "").Replace(">", "").PascalToKebabCase() + ".ts";
+
+            var path = string.Format("src/app/{0}/shared/models/{1}", rootPath, typeModelName);
+            if (File.Exists(path))
+                return null;
+
+
+            StringBuilder modelFileText = new StringBuilder();
+
+
+            var baseType = (string)type["baseType"];
+            var extends = "";
+
+            if (!string.IsNullOrWhiteSpace(baseType) && baseType != "System.Enum")
+            {
+                var baseTypeSplit = baseType.Split(".");
+                var baseTypeName = baseTypeSplit[baseTypeSplit.Length - 1].Replace("<", "").Replace(">", "");
+                var baseTypeKebabCase = "./" + baseTypeName.PascalToKebabCase();
+
+                if (baseType.Contains("Volo.Abp.Application.Dtos.EntityDto"))
+                    baseTypeKebabCase = "@abp/ng.core";
+
+                if (baseTypeName.Contains("guid") || baseTypeName.Contains("Guid"))
+                    baseTypeName = "string";
+
+                modelFileText.AppendLine(string.Format("import {{ {0} }} from '{1}';", baseTypeName, baseTypeKebabCase));
+                extends = "extends " + baseTypeName;
+            }
+
+            if (baseType == "System.Enum" && (string)type.First["isEnum"] == "True")
+            {
+                modelFileText.AppendLine(string.Format("export enum {0} {{", typeName));
+
+                var enumDictionary = new Dictionary<string, string>();
+
+                var enumNameList = type.First["enumNames"].ToArray();
+                var enumValueList = type.First["enumValues"].ToArray();
+
+                for (int i = 0; i < enumNameList.Length; i++)
+                {
+                    modelFileText.AppendLine(string.Format("{0} = {1},", enumNameList[i], enumValueList[i]));
+                }
+
+                modelFileText.AppendLine("}");
+            }
+            else
+            {
+                modelFileText.AppendLine(string.Format("export class {0} {1} {{", typeName, extends));
+
+                foreach (var property in type["properties"])
+                {
+                    var propertyName = (string)property["name"];
+                    propertyName = (char.ToLower(propertyName[0]) + propertyName.Substring(1));
+
+                    var typeSimple = (string)property["typeSimple"];
+
+                    if (typeSimple.IndexOf("[") > -1 && typeSimple.IndexOf("]") > -1)
+                        typeSimple = typeSimple.Replace("[", "").Replace("]", "") + "[]";
+
+                    if (typeSimple.StartsWith("Volo.Abp"))
+                    {
+                        var typeSimpleSplit = typeSimple.Split(".");
+                        typeSimple = typeSimpleSplit[typeSimpleSplit.Length - 1];
+                    }
+
+                    if (typeSimple.StartsWith("System.Object"))
+                    {
+                        typeSimple = typeSimple.Replace("System.Object", "object");
+                    }
+
+                    if (typeSimple.Contains("?"))
+                    {
+                        typeSimple = typeSimple.Replace("?", "");
+                        propertyName += "?";
+                    }
+
+                    if (typeSimple != "boolean"
+                     && typeSimple != "string"
+                     && typeSimple != "number"
+                     && typeSimple != "boolean[]"
+                     && typeSimple != "string[]"
+                     && typeSimple != "number[]"
+                      )
+                    {
+                        typeSimple = "any" + (typeSimple.Contains("[]") ? "[]" : "");
+                    }
+
+                    modelFileText.AppendLine(string.Format("  {0}: {1};", propertyName, typeSimple));
+                }
+
+                modelFileText.AppendLine("");
+
+                modelFileText.AppendLine(string.Format("  constructor(initialValues: Partial<{0}> = {{}}) {{", typeName));
+
+                if (!string.IsNullOrWhiteSpace(baseType))
+                {
+                    modelFileText.AppendLine("    super(initialValues);");
+                    modelFileText.AppendLine("  }");
+                }
+                else
+                {
+                    modelFileText.AppendLine("    if (initialValues) {");
+                    modelFileText.AppendLine("      for (const key in initialValues) {");
+                    modelFileText.AppendLine("        if (initialValues.hasOwnProperty(key)) {");
+                    modelFileText.AppendLine("          this[key] = initialValues[key];");
+
+                    modelFileText.AppendLine("        }");
+                    modelFileText.AppendLine("      }");
+                    modelFileText.AppendLine("    }");
+                    modelFileText.AppendLine("  }");
+                }
+
+                modelFileText.AppendLine("}");
+            }
+
+            System.IO.File.WriteAllText(string.Format("src/app/{0}/shared/models/{1}", rootPath, typeModelName), modelFileText.ToString());
+
+            return typeModelName.Replace(".ts", "");
         }
 
         public string GetUsageInfo()
