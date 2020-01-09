27c565fc1efde688a7c956480acaf9084563e97e
mperk <perkmehmed@gmail.com>
2020-01-09 14:48:20 +0300

    event permissions update when role update

---
+++ modules/identity/src/Volo.Abp.PermissionManagement.Domain.Identity/Volo/Abp/PermissionManagement/Identity/RoleUpdateEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events;
+using Volo.Abp.EventBus;
+using Volo.Abp.Identity;
+
+namespace Volo.Abp.PermissionManagement.Identity
+{
+    public class RoleUpdateEventHandler :
+        ILocalEventHandler<IdentityRoleNameChangedEvent>,
+        ITransientDependency
+    {
+        protected IIdentityRoleRepository RoleRepository { get; }
+        protected IPermissionManager PermissionManager { get; }
+        protected IPermissionGrantRepository PermissionGrantRepository { get; }
+
+        public RoleUpdateEventHandler(
+            IIdentityRoleRepository roleRepository,
+            IPermissionManager permissionManager,
+            IPermissionGrantRepository permissionGrantRepository)
+        {
+            RoleRepository = roleRepository;
+            PermissionManager = permissionManager;
+            PermissionGrantRepository = permissionGrantRepository;
+        }
+
+        public async Task HandleEventAsync(IdentityRoleNameChangedEvent eventData)
+        {
+            var role = await RoleRepository.FindAsync(eventData.IdentityRole.Id, false).ConfigureAwait(false);
+            if (role == null)
+            {
+                return;
+            }
+
+            var permissionGrantsInRole = await PermissionGrantRepository.GetListAsync("R", eventData.OldName).ConfigureAwait(false);
+            foreach (var permissionGrant in permissionGrantsInRole)
+            {
+                await PermissionManager.UpdateProviderKeyAsync(permissionGrant, eventData.IdentityRole.Name).ConfigureAwait(false);
+            }
+        }
+    }
+}
