c1818befe18230bfa4222a0aaa06acd9c6150abc
zhengzhou <netuserzhengzhou@gmail.com>
2020-01-19 09:44:33 +0800

    增加Acme.BookStore.MongoDB项目。

---
+++ samples/DecideStore/src/Acme.BookStore.MongoDB/BookStoreMongoDBDbContext.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Data;
+using Volo.Abp.MongoDB;
+
+namespace Acme.BookStore
+{
+    [ConnectionStringName("MongoDBConnString")]
+    public class BookStoreMongoDBDbContext: AbpMongoDbContext
+    {
+        public IMongoCollection<Book> Questions => Collection<Book>();
+        
+        protected override void CreateModel(IMongoModelBuilder modelBuilder)
+        {
+            base.CreateModel(modelBuilder);
+
+            modelBuilder.Entity<Book>(b =>
+            {
+                b.CollectionName = "Book";
+            });
+        }
+    }
+}
---
+++ samples/DecideStore/src/Acme.BookStore.MongoDB/BookStoreMongoDBModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Volo.Abp.Modularity;
+using Volo.Abp.MongoDB;
+
+namespace Acme.BookStore
+{
+    [DependsOn(typeof(AbpMongoDbModule))]
+    public class BookStoreMongoDBModule: AbpModule
+    {
+    }
+}
