1ba86db4b55e81980392ee3bd55578d51fcd7b59
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2020-02-07 09:23:03 +0300

    Update ProjectConsts.cs

--- modules/docs/src/Volo.Docs.Domain.Shared/Volo/Docs/Projects/ProjectConsts.cs
+++ modules/docs/src/Volo.Docs.Domain.Shared/Volo/Docs/Projects/ProjectConsts.cs
@@ -7,7 +7,6 @@
         public const int MaxDefaultDocumentNameLength = 128;
         public const int MaxNavigationDocumentNameLength = 128;
         public const int MaxParametersDocumentNameLength = 128;
-        public const int MaxPartialTemplatesDocumentNameLength = 128;
         public const int MaxLatestVersionBranchNameLength = 128;
         public const int MaxVersionNameLength = 128;
     }
