061027bda58348bd8eb1e7a80baf1c18dce550bc
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-02-12 14:31:49 +0300

    Add comment for EntityFrameworkCoreMyProjectNameDbSchemaMigrator

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.EntityFrameworkCore.DbMigrations/EntityFrameworkCore/EntityFrameworkCoreMyProjectNameDbSchemaMigrator.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.EntityFrameworkCore.DbMigrations/EntityFrameworkCore/EntityFrameworkCoreMyProjectNameDbSchemaMigrator.cs
@@ -13,13 +13,20 @@ namespace MyCompanyName.MyProjectName.EntityFrameworkCore
     {
         private readonly IServiceProvider _serviceProvider;
 
-        public EntityFrameworkCoreMyProjectNameDbSchemaMigrator(IServiceProvider serviceProvider)
+        public EntityFrameworkCoreMyProjectNameDbSchemaMigrator(
+            IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
         public async Task MigrateAsync()
         {
+            /* We intentionally resolving the MyProjectNameMigrationsDbContext
+             * from IServiceProvider (instead of directly injecting it)
+             * to properly get the connection string of the current tenant in the
+             * current scope.
+             */
+
             await _serviceProvider
                 .GetRequiredService<MyProjectNameMigrationsDbContext>()
                 .Database
