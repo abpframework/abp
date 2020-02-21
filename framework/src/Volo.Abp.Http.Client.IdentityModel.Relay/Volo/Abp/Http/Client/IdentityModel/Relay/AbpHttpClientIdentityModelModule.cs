866a5b8b96857322cf40e4ba582ed57625e583fa
Necati Meral <nm@lis-gmbh.com>
2020-02-21 10:32:13 +0100

    Created new framework package `Volo.Abp.Http.Client.IdentityModel.Relay`
    Package contains the 'pass-through' authenticator-implementation which previously was in `Volo.Abp.Http.Client.IdentityModel.IdentityModelRemoteServiceHttpClientAuthenticator`.
    Since the `IdentityModelRemoteServiceHttpClientAuthenticator` implementation (without requiring `IHttpContextAccessor`) is required for basic clients (which should be able to target net-standard or netfx) this package was splitted into `Volo.Abp.Http.Client.IdentityModel.Relay` to support the known behavior of just passing the token around.
    
    WARNING: This commit contains a breaking change for existing backend applications which use automagic C# clients!

---
+++ framework/src/Volo.Abp.Http.Client.IdentityModel.Relay/Volo/Abp/Http/Client/IdentityModel/Relay/AbpHttpClientIdentityModelModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.IdentityModel;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.Http.Client.IdentityModel.Relay
+{
+    [DependsOn(
+        typeof(AbpHttpClientIdentityModelModule)
+        )]
+    public class AbpHttpClientIdentityModelRelayModule : AbpModule
+    {
+
+    }
+}
