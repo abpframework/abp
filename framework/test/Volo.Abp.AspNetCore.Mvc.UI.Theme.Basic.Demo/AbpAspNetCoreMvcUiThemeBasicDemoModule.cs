10d8e22e9fde06ce97450ec5e287d91b2394574d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-02-25 19:24:52 +0300

    ReplaceEmbeddedByPhysical for the AbpAspNetCoreMvcUiThemeSharedDemoModule

--- framework/test/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Demo/AbpAspNetCoreMvcUiThemeBasicDemoModule.cs
+++ framework/test/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Demo/AbpAspNetCoreMvcUiThemeBasicDemoModule.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
+using Volo.Abp.UI;
+using Volo.Abp.VirtualFileSystem;
 
 namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Demo
 {
@@ -13,6 +17,19 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic.Demo
         )]
     public class AbpAspNetCoreMvcUiThemeBasicDemoModule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var env = context.Services.GetHostingEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                Configure<AbpVirtualFileSystemOptions>(options =>
+                {
+                    options.FileSets.ReplaceEmbeddedByPhysical<AbpAspNetCoreMvcUiThemeSharedDemoModule>(Path.Combine(env.ContentRootPath, string.Format("..{0}..{0}src{0}Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo", Path.DirectorySeparatorChar)));
+                });
+            }
+        }
+
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
         {
             var app = context.GetApplicationBuilder();
