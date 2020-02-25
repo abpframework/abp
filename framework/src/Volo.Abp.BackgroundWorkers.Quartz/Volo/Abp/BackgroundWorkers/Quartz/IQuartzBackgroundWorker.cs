7af6ede0a7d9fd4a99fe66cdb141f9e232acb1b0
liangshiwei <liangshiw@outlook.com>
2020-02-25 13:08:41 +0800

    Code formatting

--- framework/src/Volo.Abp.BackgroundWorkers.Quartz/Volo/Abp/BackgroundWorkers/Quartz/IQuartzBackgroundWorker.cs
+++ framework/src/Volo.Abp.BackgroundWorkers.Quartz/Volo/Abp/BackgroundWorkers/Quartz/IQuartzBackgroundWorker.cs
@@ -5,6 +5,7 @@ namespace Volo.Abp.BackgroundWorkers.Quartz
     public interface IQuartzBackgroundWorker : IBackgroundWorker, IJob
     {
         ITrigger Trigger { get; set; }
+
         IJobDetail JobDetail { get; set; }
     }
 }
--- framework/src/Volo.Abp.BackgroundWorkers.Quartz/Volo/Abp/BackgroundWorkers/Quartz/QuartzBackgroundWorkerBase.cs
+++ framework/src/Volo.Abp.BackgroundWorkers.Quartz/Volo/Abp/BackgroundWorkers/Quartz/QuartzBackgroundWorkerBase.cs
@@ -6,8 +6,9 @@ namespace Volo.Abp.BackgroundWorkers.Quartz
     public abstract class QuartzBackgroundWorkerBase : BackgroundWorkerBase, IQuartzBackgroundWorker
     {
         public ITrigger Trigger { get; set; }
-        public IJobDetail JobDetail { get; set; }
-        public abstract Task Execute(IJobExecutionContext context);
 
+        public IJobDetail JobDetail { get; set; }
+
+        public abstract Task Execute(IJobExecutionContext context);
     }
 }
