1c29ac629f6290645b3c7e2f87e1973fd788904e
mehmet-erim <bilgi@mehmeterim.com>
2020-02-26 15:34:23 +0300

    chore: remove primeng from template
    closes #2924

--- templates/app/angular/src/app/app.component.ts
+++ templates/app/angular/src/app/app.component.ts
@@ -6,25 +6,18 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <abp-loader-bar></abp-loader-bar>
     <router-outlet></router-outlet>
-  `,
+  `
 })
 export class AppComponent implements OnInit {
   constructor(private lazyLoadService: LazyLoadService) {}
-
-  ngOnInit() {
+  async ngOnInit() {
     this.lazyLoadService
       .load(
-        [
-          'primeng.min.css',
-          'primeicons.css',
-          'primeng-nova-light-theme.css',
-          'fontawesome-all.min.css',
-          'fontawesome-v4-shims.min.css',
-        ],
+        ['fontawesome-all.min.css', 'fontawesome-v4-shims.min.css'],
         'style',
         null,
         'head',
-        'afterbegin',
+        'afterbegin'
       )
       .subscribe();
   }
