5815a2a514c94296053b9d62b05d8d8923529827
liangshiwei <liangshiw@outlook.com>
2020-02-28 16:42:17 +0800

    Quartz demoapp switch to ramstore

--- modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp.Quartz/DemoAppQuartzModule.cs
+++ modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp.Quartz/DemoAppQuartzModule.cs
@@ -16,25 +16,6 @@ namespace Volo.Abp.BackgroundJobs.DemoApp.Quartz
     )]
     public class DemoAppQuartzModule : AbpModule
     {
-        public override void PreConfigureServices(ServiceConfigurationContext context)
-        {
-            var configuration = context.Services.GetConfiguration();
-
-            PreConfigure<AbpQuartzPreOptions>(options =>
-            {
-                options.Properties = new NameValueCollection
-                {
-                    ["quartz.jobStore.dataSource"] = "BackgroundJobsDemoApp",
-                    ["quartz.jobStore.type"] = "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz",
-                    ["quartz.jobStore.tablePrefix"] = "QRTZ_",
-                    ["quartz.serializer.type"] = "json",
-                    ["quartz.dataSource.BackgroundJobsDemoApp.connectionString"] = configuration.GetConnectionString("Default"),
-                    ["quartz.dataSource.BackgroundJobsDemoApp.provider"] = "SqlServer",
-                    ["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz",
-                };
-            });
-        }
-
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
         {
             //TODO: Configure console logging
