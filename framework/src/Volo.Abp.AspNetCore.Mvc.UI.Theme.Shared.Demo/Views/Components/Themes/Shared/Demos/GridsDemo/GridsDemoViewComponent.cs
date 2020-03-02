dc81bb7ca57783a8c2fba577fa63bbfa19cb6ad7
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-03-02 15:08:13 +0300

    boostrap demo components added

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo/Views/Components/Themes/Shared/Demos/GridsDemo/GridsDemoViewComponent.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo.Views.Components.Themes.Shared.Demos.GridsDemo
+{
