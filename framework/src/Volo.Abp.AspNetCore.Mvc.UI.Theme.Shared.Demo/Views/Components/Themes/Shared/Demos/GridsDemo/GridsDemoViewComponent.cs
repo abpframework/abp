7714ba2139039067679bad7e663141323c61183a
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-03-02 15:08:27 +0300

    Update GridsDemoViewComponent.cs

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo/Views/Components/Themes/Shared/Demos/GridsDemo/GridsDemoViewComponent.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo/Views/Components/Themes/Shared/Demos/GridsDemo/GridsDemoViewComponent.cs
@@ -3,3 +3,14 @@ using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
 
 namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Demo.Views.Components.Themes.Shared.Demos.GridsDemo
 {
+    [Widget]
+    public class GridsDemoViewComponent : AbpViewComponent
+    {
+        public const string ViewPath = "/Views/Components/Themes/Shared/Demos/GridsDemo/Default.cshtml";
+
+        public IViewComponentResult Invoke()
+        {
+            return View(ViewPath);
+        }
+    }
+}
