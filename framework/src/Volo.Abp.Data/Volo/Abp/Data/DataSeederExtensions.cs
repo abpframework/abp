56c30abdd293bffb68ca6f99ea65105af33a4631
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-03-13 13:22:29 +0300

    Update DataSeederExtensions.cs

--- framework/src/Volo.Abp.Data/Volo/Abp/Data/DataSeederExtensions.cs
+++ framework/src/Volo.Abp.Data/Volo/Abp/Data/DataSeederExtensions.cs
@@ -5,6 +5,11 @@ namespace Volo.Abp.Data
 {
     public static class DataSeederExtensions
     {
+        public static Task SeedAsync(this IDataSeeder seeder, Guid? tenantId = null)
+        {
+            return seeder.SeedAsync(new DataSeedContext(tenantId));
+        }
+
         public static Task SeedAsync(this IDataSeeder seeder, Guid? tenantId, string adminEmailAddress, string adminPassword)
         {
             var context = new DataSeedContext(tenantId)
