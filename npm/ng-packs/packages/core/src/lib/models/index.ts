fffbd06dfe91d8a6e9f3391081cb4a84d88acc70
mehmet-erim <bilgi@mehmeterim.com>
2020-03-18 22:12:34 +0300

    fix: fix lint error

--- npm/ng-packs/packages/core/src/lib/models/index.ts
+++ npm/ng-packs/packages/core/src/lib/models/index.ts
@@ -3,6 +3,6 @@ export * from './common';
 export * from './config';
 export * from './dtos';
 export * from './profile';
-export * from './replaceable-components'; 
+export * from './replaceable-components';
 export * from './rest';
 export * from './session';
