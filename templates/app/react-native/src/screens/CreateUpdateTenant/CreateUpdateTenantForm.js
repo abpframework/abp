2db56d592f319e27640e275a88c95dd79b316f65
mehmet-erim <bilgi@mehmeterim.com>
2020-03-18 21:27:34 +0300

    feat(rn): add admin email and password fields to create tenant form

--- templates/app/react-native/src/screens/CreateUpdateTenant/CreateUpdateTenantForm.js
+++ templates/app/react-native/src/screens/CreateUpdateTenant/CreateUpdateTenantForm.js
@@ -1,8 +1,8 @@
 import { Formik } from 'formik';
 import i18n from 'i18n-js';
-import { Container, Content, Input, InputGroup, Label } from 'native-base';
+import { Container, Content, Input, InputGroup, Label, Item, Icon } from 'native-base';
 import PropTypes from 'prop-types';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 import FormButtons from '../../components/FormButtons/FormButtons';
@@ -15,7 +15,10 @@ const validations = {
 
 function CreateUpdateTenantForm({ editingTenant = {}, submit, remove }) {
   const tenantNameRef = useRef();
+  const adminEmailRef = useRef();
+  const adminPasswordRef = useRef();
 
+  const [showAdminPassword, setShowAdminPassword] = useState(false);
   const hasRemovePermission = usePermission('AbpTenantManagement.Tenants.Delete');
 
   const onSubmit = values => {
@@ -25,11 +28,25 @@ function CreateUpdateTenantForm({ editingTenant = {}, submit, remove }) {
     });
   };
 
+  const adminEmailAddressValidation = Yup.lazy(() =>
+    Yup.string()
+      .required('AbpAccount::ThisFieldIsRequired.')
+      .email('AbpAccount::ThisFieldIsNotAValidEmailAddress.'),
+  );
+
+  const adminPasswordValidation = Yup.lazy(() =>
+    Yup.string().required('AbpAccount::ThisFieldIsRequired.'),
+  );
+
   return (
     <Formik
       enableReinitialize
       validationSchema={Yup.object().shape({
         ...validations,
+        ...(!editingTenant.id && {
+          adminEmailAddress: adminEmailAddressValidation,
+          adminPassword: adminPasswordValidation,
+        }),
       })}
       initialValues={{
         lockoutEnabled: false,
@@ -46,12 +63,55 @@ function CreateUpdateTenantForm({ editingTenant = {}, submit, remove }) {
                 <Input
                   abpInput
                   ref={tenantNameRef}
+                  onSubmitEditing={() =>
+                    !editingTenant.id ? adminEmailRef.current._root.focus() : null
+                  }
+                  returnKeyType={!editingTenant.id ? 'next' : 'done'}
                   onChangeText={handleChange('name')}
                   onBlur={handleBlur('name')}
                   value={values.name}
                 />
               </InputGroup>
               <ValidationMessage>{errors.name}</ValidationMessage>
+              {!editingTenant || !editingTenant.id ? (
+                <>
+                  <InputGroup abpInputGroup>
+                    <Label abpLabel>
+                      {i18n.t('AbpTenantManagement::DisplayName:AdminEmailAddress')}
+                    </Label>
+                    <Input
+                      abpInput
+                      ref={adminEmailRef}
+                      onSubmitEditing={() => adminPasswordRef.current._root.focus()}
+                      returnKeyType="next"
+                      onChangeText={handleChange('adminEmailAddress')}
+                      onBlur={handleBlur('adminEmailAddress')}
+                      value={values.adminEmailAddress}
+                    />
+                  </InputGroup>
+                  <ValidationMessage>{errors.adminEmailAddress}</ValidationMessage>
+                  <InputGroup abpInputGroup>
+                    <Label abpLabel>
+                      {i18n.t('AbpTenantManagement::DisplayName:AdminPassword')}
+                    </Label>
+                    <Item abpInput>
+                      <Input
+                        ref={adminPasswordRef}
+                        returnKeyType="done"
+                        secureTextEntry={!showAdminPassword}
+                        onChangeText={handleChange('adminPassword')}
+                        onBlur={handleBlur('adminPassword')}
+                        value={values.adminPassword}
+                      />
+                      <Icon
+                        name={showAdminPassword ? 'eye-off' : 'eye'}
+                        onPress={() => setShowAdminPassword(!showAdminPassword)}
+                      />
+                    </Item>
+                  </InputGroup>
+                  <ValidationMessage>{errors.adminPassword}</ValidationMessage>
+                </>
+              ) : null}
             </Content>
           </Container>
           <FormButtons
