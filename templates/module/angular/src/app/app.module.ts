8d71b74281dd6fac4279970d14c47806c86e50ba
Arman Ozak <arman@armanozak.com>
2020-03-27 14:31:09 +0300

    style: add prettierrc and rules matching other angular projects

--- templates/module/angular/src/app/app.module.ts
+++ templates/module/angular/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { AccountConfigModule } from "@abp/ng.account.config";
-import { CoreModule } from "@abp/ng.core";
-import { IdentityConfigModule } from "@abp/ng.identity.config";
-import { SettingManagementConfigModule } from "@abp/ng.setting-management.config";
-import { TenantManagementConfigModule } from "@abp/ng.tenant-management.config";
-import { ThemeSharedModule } from "@abp/ng.theme.shared";
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
-import { NgxsModule } from "@ngxs/store";
-import { OAuthModule } from "angular-oauth2-oidc";
-import { environment } from "../environments/environment";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SharedModule } from "./shared/shared.module";
-import { MyProjectNameConfigModule } from "../../projects/my-project-name-config/src/public-api";
+import { AccountConfigModule } from '@abp/ng.account.config';
+import { CoreModule } from '@abp/ng.core';
+import { IdentityConfigModule } from '@abp/ng.identity.config';
+import { SettingManagementConfigModule } from '@abp/ng.setting-management.config';
+import { TenantManagementConfigModule } from '@abp/ng.tenant-management.config';
+import { ThemeSharedModule } from '@abp/ng.theme.shared';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { MyProjectNameConfigModule } from '../../projects/my-project-name-config/src/public-api';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
 
@@ -23,11 +23,11 @@ const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
   imports: [
     ThemeSharedModule.forRoot(),
     CoreModule.forRoot({
-      environment
+      environment,
     }),
     OAuthModule.forRoot(),
     NgxsModule.forRoot([]),
-    AccountConfigModule.forRoot({ redirectUrl: "/" }),
+    AccountConfigModule.forRoot({ redirectUrl: '/' }),
     IdentityConfigModule,
     TenantManagementConfigModule,
     SettingManagementConfigModule,
@@ -37,8 +37,8 @@ const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
     AppRoutingModule,
     SharedModule,
 
-    ...(environment.production ? [] : LOGGERS)
+    ...(environment.production ? [] : LOGGERS),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
