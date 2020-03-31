81600ceb1daf6ef1093e2d004f790abf5a6caf89
mehmet-erim <bilgi@mehmeterim.com>
2020-03-31 10:06:06 +0300

    chore: remove linked list

--- npm/ng-packs/packages/core/src/lib/utils/index.ts
+++ npm/ng-packs/packages/core/src/lib/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './common-utils';
 export * from './generator-utils';
 export * from './initial-utils';
-export * from './linked-list';
 export * from './route-utils';
 export * from './rxjs-utils';
