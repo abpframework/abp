5e7221ca372de1610817a237f9e7581ef5486b45
liangshiwei <liangshiw@outlook.com>
2020-03-31 15:19:27 +0800

    Remove AbpBackgroundJobsHangfireModule code

--- framework/src/Volo.Abp.BackgroundJobs.HangFire/Volo/Abp/BackgroundJobs/Hangfire/AbpBackgroundJobsHangfireModule.cs
+++ framework/src/Volo.Abp.BackgroundJobs.HangFire/Volo/Abp/BackgroundJobs/Hangfire/AbpBackgroundJobsHangfireModule.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp.Hangfire;
+﻿using Volo.Abp.Hangfire;
 using Volo.Abp.Modularity;
 
 namespace Volo.Abp.BackgroundJobs.Hangfire
@@ -10,13 +9,5 @@ namespace Volo.Abp.BackgroundJobs.Hangfire
     )]
     public class AbpBackgroundJobsHangfireModule : AbpModule
     {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            var options = context.Services.ExecutePreConfiguredActions<AbpBackgroundJobOptions>();
-            if (!options.IsJobExecutionEnabled)
-            {
-                context.Services.AddSingleton<IBackgroundJobManager>(x => new NullBackgroundJobManager());
-            }
-        }
     }
 }
