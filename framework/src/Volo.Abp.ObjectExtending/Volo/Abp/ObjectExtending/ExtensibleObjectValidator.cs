2b211c1073e8ab55caa08384c5fd642af7f14f6c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-04-11 16:17:21 +0300

    Add validation for extra properties.

---
+++ framework/src/Volo.Abp.ObjectExtending/Volo/Abp/ObjectExtending/ExtensibleObjectValidator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using JetBrains.Annotations;
+using Volo.Abp.Data;
+using Volo.Abp.DynamicProxy;
+
+namespace Volo.Abp.ObjectExtending
+{
+    public static class ExtensibleObjectValidator
+    {
+        [NotNull]
+        public static List<ValidationResult> GetValidationErrors(
+            [NotNull] IHasExtraProperties extensibleObject,
+            [CanBeNull] IServiceProvider serviceProvider = null)
+        {
+            var validationErrors = new List<ValidationResult>();
+            
+            AddValidationErrors(
+                extensibleObject,
+                new ValidationContext(
+                    extensibleObject,
+                    serviceProvider,
+                    new Dictionary<object, object>()
+                ),
+                validationErrors
+            );
+
+            return validationErrors;
+        }
+
+        public static void AddValidationErrors(
+            [NotNull] IHasExtraProperties extensibleObject,
+            [NotNull] ValidationContext validationContext,
+            [NotNull] List<ValidationResult> validationErrors)
+        {
+            Check.NotNull(extensibleObject, nameof(extensibleObject));
+            Check.NotNull(validationContext, nameof(validationContext));
+            Check.NotNull(validationErrors, nameof(validationErrors));
+
+            var objectType = ProxyHelper.UnProxy(extensibleObject).GetType();
+
+            var objectExtensionInfo = ObjectExtensionManager.Instance
+                .GetOrNull(objectType);
+
+            if (objectExtensionInfo == null)
+            {
+                return;
+            }
+
+            foreach (var propertyInfo in objectExtensionInfo.GetProperties())
+            {
+                if (propertyInfo.ValidationAttributes.IsNullOrEmpty())
+                {
+                    continue;
+                }
+
+                var validationContext2 = new ValidationContext(extensibleObject, validationContext, null)
+                {
+                    DisplayName = propertyInfo.Name,
+                    MemberName = propertyInfo.Name
+                };
+
+                foreach (var attribute in propertyInfo.ValidationAttributes)
+                {
+                    var result = attribute.GetValidationResult(extensibleObject.GetProperty(propertyInfo.Name), validationContext2);
+                    if (result != null)
+                    {
+                        validationErrors.Add(result);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ framework/test/Volo.Abp.ObjectExtending.Tests/Volo/Abp/ObjectExtending/ExtensibleObjectValidator_Tests.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shouldly;
+using Volo.Abp.Data;
+using Volo.Abp.Threading;
+using Xunit;
+
+namespace Volo.Abp.ObjectExtending
+{
+    public class ExtensibleObjectValidator_Tests
+    {
+        private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
+
+        static ExtensibleObjectValidator_Tests()
+        {
+            OneTimeRunner.Run(() =>
+            {
+                ObjectExtensionManager.Instance
+                    .AddOrUpdate<ExtensiblePersonObject>(options =>
+                    {
+                        options.AddOrUpdateProperty<string>("Name", propertyInfo =>
+                        {
+                            propertyInfo.ValidationAttributes.Add(new RequiredAttribute());
+                            propertyInfo.ValidationAttributes.Add(new StringLengthAttribute(64) { MinimumLength = 2 });
+                        });
+
+                        options.AddOrUpdateProperty<string>("Address", propertyInfo =>
+                        {
+                            propertyInfo.ValidationAttributes.Add(new StringLengthAttribute(255));
+                        });
+
+                        options.AddOrUpdateProperty<byte>("Age", propertyInfo =>
+                        {
+                            propertyInfo.ValidationAttributes.Add(new RequiredAttribute());
+                            propertyInfo.ValidationAttributes.Add(new RangeAttribute(18, 99));
+                        });
+
+                        options.AddOrUpdateProperty<bool>("IsMarried", propertyInfo =>
+                        {
+
+                        });
+                    });
+            });
+        }
+
+        [Fact]
+        public void Should_Validate_If_The_Properties_Are_Valid()
+        {
+            ExtensibleObjectValidator
+                .GetValidationErrors(
+                    new ExtensiblePersonObject
+                    {
+                        ExtraProperties =
+                        {
+                            {"Name", "John"},
+                            {"Age", "42"},
+                        }
+                    }
+                ).Count.ShouldBe(0); //All Valid
+        }
+
+        [Fact]
+        public void Should_Not_Validate_If_The_Properties_Are_Not_Valid()
+        {
+            ExtensibleObjectValidator
+                .GetValidationErrors(
+                    new ExtensiblePersonObject()
+                ).Count.ShouldBe(2); //Name & Age
+
+            ExtensibleObjectValidator
+                .GetValidationErrors(
+                    new ExtensiblePersonObject
+                    {
+                        ExtraProperties =
+                        {
+                            {"Address", new string('x', 256) }
+                        }
+                    }
+                ).Count.ShouldBe(3); //Name, Age & Address
+
+            ExtensibleObjectValidator
+                .GetValidationErrors(
+                    new ExtensiblePersonObject
+                    {
+                        ExtraProperties =
+                        {
+                            {"Age", "42" }
+                        }
+                    }
+                ).Count.ShouldBe(1); //Name
+
+            ExtensibleObjectValidator
+                .GetValidationErrors(
+                    new ExtensiblePersonObject
+                    {
+                        ExtraProperties =
+                        {
+                            {"Address", new string('x', 256) },
+                            {"Age", "100" }
+                        }
+            }
+                ).Count.ShouldBe(3); //Name, Age & Address
+        }
+
+        private class ExtensiblePersonObject : ExtensibleObject
+        {
+
+        }
+    }
+}
