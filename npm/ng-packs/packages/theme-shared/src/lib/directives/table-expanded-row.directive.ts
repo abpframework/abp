90f60c5de2dc0ac18a67da0c3b3418e09fff3f03
mehmet-erim <bilgi@mehmeterim.com>
2020-04-16 15:09:36 +0300

    refactor(theme-shared): add ngOnChanges and change the data context type

--- npm/ng-packs/packages/theme-shared/src/lib/directives/table-expanded-row.directive.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/directives/table-expanded-row.directive.ts
@@ -1,18 +1,19 @@
 import {
-  AfterViewInit,
-  ComponentFactory,
-  ComponentFactoryResolver,
   Directive,
-  TemplateRef,
-  ViewContainerRef,
-  ComponentRef,
   EmbeddedViewRef,
   Input,
+  OnChanges,
+  OnInit,
+  SimpleChange,
+  SimpleChanges,
+  TemplateRef,
+  ViewContainerRef,
 } from '@angular/core';
+import compare from 'just-compare';
 import { TableComponent } from '../components/table/table.component';
 
 @Directive({ selector: '[abpTableExpandedRow]', exportAs: 'abpTableExpandedRow' })
-export class TableExpandedRowDirective implements AfterViewInit {
+export class TableExpandedRowDirective implements OnInit, OnChanges {
   @Input('abpTableExpandedRowRecord')
   record: any;
 
@@ -35,41 +36,60 @@ export class TableExpandedRowDirective implements AfterViewInit {
     private tableRef: TableComponent,
   ) {}
 
-  ngAfterViewInit() {
+  ngOnInit() {
     this.vcRef.createEmbeddedView(this.template, {
       toggleExpand: this.toggleExpand,
-      expanded: this.expanded,
+      getExpanded: this.getExpanded,
     });
   }
+
+  getExpanded = (): boolean => {
+    return this.expanded;
+  };
 
   toggleExpand = () => {
     this.expanded = !this.expanded;
 
+    if (this.expanded) {
+      this.createView();
+    } else {
+      this.expandedRowRef.destroy();
+    }
+  };
+
+  createView() {
     const colspan = (this.template.elementRef
       .nativeElement as HTMLElement).nextElementSibling.querySelectorAll('td').length;
 
     const includedKeys = this.includedKeys || this.getIncludedKeys();
 
+    const data = includedKeys.reduce(
+      (acc, key) => [...acc, { key, value: this.record[key], type: typeof this.record[key] }],
+      [],
+    );
+
     const context = {
-      $implicit: this.record,
+      $implicit: data,
       colspan,
-      includedKeys,
     };
 
-    if (this.expanded) {
-      this.expandedRowRef = this.vcRef.createEmbeddedView(
-        this.rowTemplate || this.tableRef.expandedRowRef,
-        context,
-      );
-    } else {
-      this.expandedRowRef.destroy();
-    }
-  };
+    this.expandedRowRef = this.vcRef.createEmbeddedView(
+      this.rowTemplate || this.tableRef.expandedRowRef,
+      context,
+    );
+  }
 
   private getIncludedKeys() {
     const temp = { ...this.record };
     this.excludedKeys.forEach(key => delete temp[key]);
 
     return Object.keys(temp);
+  }
+
+  ngOnChanges({ record: { previousValue, currentValue } = {} as SimpleChange }: SimpleChanges) {
+    if (previousValue && !compare(previousValue, currentValue) && this.expandedRowRef) {
+      this.expandedRowRef.destroy();
+      this.createView();
+    }
   }
 }
