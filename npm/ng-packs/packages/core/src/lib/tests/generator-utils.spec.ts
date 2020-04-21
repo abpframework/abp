faf560b67f399a5158ddf0b7e838636f2be6662a
mehmet-erim <bilgi@mehmeterim.com>
2020-04-21 16:04:32 +0300

    test(core): add new tests to generatePassword describe

--- npm/ng-packs/packages/core/src/lib/tests/generator-utils.spec.ts
+++ npm/ng-packs/packages/core/src/lib/tests/generator-utils.spec.ts
@@ -15,19 +15,22 @@ describe('GeneratorUtils', () => {
     const specials = '!@#$%&*()_+{}<>?[]./';
 
     test.each`
-      name         | charSet     | passwordLength
-      ${'lower'}   | ${lowers}   | ${10}
-      ${'lower'}   | ${lowers}   | ${7}
-      ${'upper'}   | ${uppers}   | ${6}
-      ${'number'}  | ${numbers}  | ${5}
-      ${'special'} | ${specials} | ${4}
-      ${'special'} | ${specials} | ${2}
-      ${'special'} | ${specials} | ${0}
+      name         | charSet     | passedPasswordLength | actualPasswordLength
+      ${'lower'}   | ${lowers}   | ${Infinity}          | ${128}
+      ${'lower'}   | ${lowers}   | ${129}               | ${128}
+      ${'lower'}   | ${lowers}   | ${10}                | ${10}
+      ${'lower'}   | ${lowers}   | ${7}                 | ${7}
+      ${'upper'}   | ${uppers}   | ${6}                 | ${6}
+      ${'number'}  | ${numbers}  | ${5}                 | ${5}
+      ${'special'} | ${specials} | ${4}                 | ${4}
+      ${'special'} | ${specials} | ${2}                 | ${4}
+      ${'special'} | ${specials} | ${0}                 | ${4}
+      ${'special'} | ${specials} | ${undefined}         | ${8}
     `(
       'should have a $name in the password that length is $passwordLength',
-      ({ _, charSet, passwordLength }) => {
-        const password = generatePassword(passwordLength);
-        expect(password).toHaveLength(passwordLength < 4 ? 4 : passwordLength);
+      ({ _, charSet, passedPasswordLength, actualPasswordLength }) => {
+        const password = generatePassword(passedPasswordLength);
+        expect(password).toHaveLength(actualPasswordLength);
         expect(hasChar(charSet, password)).toBe(true);
       },
     );
