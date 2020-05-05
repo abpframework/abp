126acf330d3f0c538535f3b1cdab42d2905f2c02
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-05 04:45:07 +0300

    Add PageToolbar

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/PageToolbars/PageToolbarContributor.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.PageToolbars
+{
+    public abstract class PageToolbarContributor : IPageToolbarContributor
+    {
+        public abstract Task ContributeAsync(PageToolbarContributionContext context);
+    }
+}
