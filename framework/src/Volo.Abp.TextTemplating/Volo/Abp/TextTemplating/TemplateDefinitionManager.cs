a94cdf7b37a0e572998fa62144bfd8b9ebac0416
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-05 17:42:28 +0300

    Make standard layouts inline localized. Also, set IsInlineLocalized on WithVirtualFilePath

--- framework/src/Volo.Abp.TextTemplating/Volo/Abp/TextTemplating/TemplateDefinitionManager.cs
+++ framework/src/Volo.Abp.TextTemplating/Volo/Abp/TextTemplating/TemplateDefinitionManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
-using Volo.Abp.VirtualFileSystem;
 
 namespace Volo.Abp.TextTemplating
 {
@@ -79,38 +78,9 @@ namespace Volo.Abp.TextTemplating
                 {
                     provider.PostDefine(context);
                 }
-
-                SetIsInlineLocalized(
-                    templates,
-                    scope.ServiceProvider
-                );
             }
 
             return templates;
-        }
-
-        protected virtual void SetIsInlineLocalized(
-            Dictionary<string, TemplateDefinition> templates,
-            IServiceProvider serviceProvider)
-        {
-            var virtualFileProvider = serviceProvider.GetRequiredService<IVirtualFileProvider>();
-
-            foreach (var templateDefinition in templates.Values)
-            {
-                var virtualPath = templateDefinition.GetVirtualFilePathOrNull();
-                if (virtualPath == null)
-                {
-                    continue;
-                }
-
-                var fileInfo = virtualFileProvider.GetFileInfo(virtualPath);
-                if (!fileInfo.Exists)
-                {
-                    throw new AbpException("Could not find a file/folder at the location: " + virtualPath);
-                }
-
-                templateDefinition.IsInlineLocalized = !fileInfo.IsDirectory;
-            }
         }
     }
 }
