95bd402a51deb957a5260b7e08f704ac0367fef3
51mono <46481080+zhishile@users.noreply.github.com>
2020-05-14 07:52:47 +0800

    Add docs module client proxies

--- modules/docs/src/Volo.Docs.HttpApi.Client/Volo/Docs/DocsHttpApiClientModule.cs
+++ modules/docs/src/Volo.Docs.HttpApi.Client/Volo/Docs/DocsHttpApiClientModule.cs
@@ -1,11 +1,19 @@
-﻿using Volo.Abp.Modularity;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Http.Client;
+using Volo.Abp.Modularity;
 
 namespace Volo.Docs
 {
     [DependsOn(
-        typeof(DocsApplicationContractsModule))]
+        typeof(DocsApplicationContractsModule),
+        typeof(AbpHttpClientModule)
+    )]
     public class DocsHttpApiClientModule : AbpModule
     {
         //TODO: Create client proxies
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddHttpClientProxies(typeof(DocsApplicationContractsModule).Assembly);
+        }
     }
 }
