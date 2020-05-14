23f304253674e2313c6793aa00bb284940ace568
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-14 04:11:03 +0300

    Implemented SearchAsync for the user lookup

---
+++ modules/identity/src/Volo.Abp.Identity.Application.Contracts/Volo/Abp/Identity/UserLookupSearchInputDto.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Volo.Abp.Identity
+{
+    public class UserLookupSearchInputDto : LimitedResultRequestDto, ISortedResultRequest
+    {
+        public string Sorting { get; set; }
+
+        public string Filter { get; set; }
+    }
+}
