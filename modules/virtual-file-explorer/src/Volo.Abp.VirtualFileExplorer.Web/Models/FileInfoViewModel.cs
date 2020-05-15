ce58bade8af580e1ee2293c4a2c1381a038e48ac
liangshiwei <liangshiw@outlook.com>
2020-05-15 22:47:15 +0800

    Build virtual file explorer module

---
+++ modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web/Models/FileInfoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Volo.Abp.VirtualFileExplorer.Web.Models
+{
+    public class FileInfoViewModel
+    {
+        public string FilePath { get; set; }
+
+        public string Icon { get; set; }
+
+        public string FileType { get; set; }
+
+        public string Length { get; set; }
+
+        public string FileName { get; set; }
+
+        public DateTime LastUpdateTime { get; set; }
+
+        public bool IsDirectory { get; set; }
+    }
+}
---
+++ modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web/Pages/VirtualFileExplorer/VirtualFileExplorerPageModel.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+using Volo.Abp.VirtualFileExplorer.Localization;
+
+namespace Volo.Abp.VirtualFileExplorer.Web.Pages.VirtualFileExplorer
+{
+    /* Inherit your PageModel classes from this class.
+     */
+    public abstract class VirtualFileExplorerPageModel : AbpPageModel
+    {
+        protected VirtualFileExplorerPageModel()
+        {
+            LocalizationResourceType = typeof(VirtualFileExplorerResource);
+            ObjectMapperContext = typeof(AbpVirtualFileExplorerWebModule);
+        }
+    }
+}
