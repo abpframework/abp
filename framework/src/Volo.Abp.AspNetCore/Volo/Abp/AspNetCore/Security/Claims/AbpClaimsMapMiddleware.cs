b70497eb6c6c03e692f5e186d38333b4258ed92c
maliming <6908465+maliming@users.noreply.github.com>
2020-05-17 22:39:10 +0800

    Add AbpClaimsMapMiddleware.
    Resolve #3975

---
+++ framework/src/Volo.Abp.AspNetCore/Volo/Abp/AspNetCore/Security/Claims/AbpClaimsMapMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Security.Claims;
+
+namespace Volo.Abp.AspNetCore.Security.Claims
+{
+    public class AbpClaimsMapMiddleware : IMiddleware, ITransientDependency
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var currentPrincipalAccessor = context.RequestServices.GetRequiredService<ICurrentPrincipalAccessor>();
+            var mapOptions = context.RequestServices.GetRequiredService<IOptions<AbpClaimsMapOptions>>().Value;
+
+            var mapClaims = currentPrincipalAccessor.Principal.Claims.Where(p => mapOptions.Map.Keys.Contains(p.Type));
+            currentPrincipalAccessor.Principal.AddIdentity(new ClaimsIdentity(mapClaims.Select(p => new Claim(
+                mapOptions.Map[p.Type],
+                p.Value,
+                p.ValueType,
+                p.Issuer))));
+
+            await next(context);
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.AspNetCore/Volo/Abp/AspNetCore/Security/Claims/AbpClaimsMapOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Volo.Abp.AspNetCore.Security.Claims
+{
+    public class AbpClaimsMapOptions
+    {
+        public Dictionary<string, string> Map { get; }
+
+        public AbpClaimsMapOptions()
+        {
+            Map = new Dictionary<string, string>();
+        }
+    }
+}
