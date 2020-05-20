5c7904cfe13380cb541f6822fa5fcac39d1d5995
Ahmet <ahmet.cotur@hotmail.com>
2020-05-20 10:58:30 +0300

    Requested changes resolved and tests added.

--- modules/blogging/src/Volo.Blogging.Web/Pages/Blogs/Shared/Helpers/BlogNameControlHelper.cs
+++ modules/blogging/src/Volo.Blogging.Web/Pages/Blogs/Shared/Helpers/BlogNameControlHelper.cs
@@ -5,18 +5,16 @@ namespace Volo.Blogging.Pages.Blogs.Shared.Helpers
 {
     public static class BlogNameControlHelper
     {
-        public static readonly string[] ProhibitedFileExtensions = new string[] {".ico", ".txt", ".php"};
+        public static readonly string[] ProhibitedFileExtensions = new string[] {"ico", "txt", "php"};
 
         public static bool IsProhibitedFileFormatName(string blogShortName)
         {
-            if (!string.IsNullOrWhiteSpace(blogShortName))
+            if (string.IsNullOrWhiteSpace(blogShortName))
             {
-                var fileInfo = new FileInfo(blogShortName);
-
-                return ProhibitedFileExtensions.Contains(fileInfo.Extension);
+                return false;
             }
 
-            return false;
+            return ProhibitedFileExtensions.Any(x => blogShortName.ToLowerInvariant().EndsWith(x));
         }
     }
 }
