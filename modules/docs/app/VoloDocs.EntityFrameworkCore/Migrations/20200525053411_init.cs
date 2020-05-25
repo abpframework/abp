b7baa4639133b1ef6ccf1cbe3b87fcffc1e7cd5d
maliming <6908465+maliming@users.noreply.github.com>
2020-05-25 13:34:34 +0800

    added organization unit migration for Docs.

--- modules/docs/app/VoloDocs.EntityFrameworkCore/Migrations/20200525053411_init.cs
+++ modules/docs/app/VoloDocs.EntityFrameworkCore/Migrations/20200525053411_init.cs
@@ -28,6 +28,36 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "AbpOrganizationUnits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ExtraProperties = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierId = table.Column<Guid>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(nullable: true),
+                    DeletionTime = table.Column<DateTime>(nullable: true),
+                    TenantId = table.Column<Guid>(nullable: true),
+                    ParentId = table.Column<Guid>(nullable: true),
+                    Code = table.Column<string>(maxLength: 95, nullable: false),
+                    DisplayName = table.Column<string>(maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpOrganizationUnits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpOrganizationUnits_AbpOrganizationUnits_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "AbpOrganizationUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "AbpPermissionGrants",
                 columns: table => new
                 {
@@ -164,6 +194,33 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "AbpOrganizationUnitRoles",
+                columns: table => new
+                {
+                    RoleId = table.Column<Guid>(nullable: false),
+                    OrganizationUnitId = table.Column<Guid>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    TenantId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpOrganizationUnitRoles", x => new { x.OrganizationUnitId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AbpOrganizationUnitRoles_AbpOrganizationUnits_OrganizationUnitId",
+                        column: x => x.OrganizationUnitId,
+                        principalTable: "AbpOrganizationUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AbpOrganizationUnitRoles_AbpRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AbpRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "AbpRoleClaims",
                 columns: table => new
                 {
@@ -220,6 +277,33 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                     table.PrimaryKey("PK_AbpUserLogins", x => new { x.UserId, x.LoginProvider });
                     table.ForeignKey(
                         name: "FK_AbpUserLogins_AbpUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpUserOrganizationUnits",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    OrganizationUnitId = table.Column<Guid>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<Guid>(nullable: true),
+                    TenantId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpUserOrganizationUnits", x => new { x.OrganizationUnitId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_AbpUserOrganizationUnits_AbpOrganizationUnits_OrganizationUnitId",
+                        column: x => x.OrganizationUnitId,
+                        principalTable: "AbpOrganizationUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AbpUserOrganizationUnits_AbpUsers_UserId",
                         column: x => x.UserId,
                         principalTable: "AbpUsers",
                         principalColumn: "Id",
@@ -293,6 +377,21 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                 });
 
             migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnitRoles_RoleId_OrganizationUnitId",
+                table: "AbpOrganizationUnitRoles",
+                columns: new[] { "RoleId", "OrganizationUnitId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnits_Code",
+                table: "AbpOrganizationUnits",
+                column: "Code");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnits_ParentId",
+                table: "AbpOrganizationUnits",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_AbpPermissionGrants_Name_ProviderName_ProviderKey",
                 table: "AbpPermissionGrants",
                 columns: new[] { "Name", "ProviderName", "ProviderKey" });
@@ -321,6 +420,11 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                 name: "IX_AbpUserLogins_LoginProvider_ProviderKey",
                 table: "AbpUserLogins",
                 columns: new[] { "LoginProvider", "ProviderKey" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpUserOrganizationUnits_UserId_OrganizationUnitId",
+                table: "AbpUserOrganizationUnits",
+                columns: new[] { "UserId", "OrganizationUnitId" });
 
             migrationBuilder.CreateIndex(
                 name: "IX_AbpUserRoles_RoleId_UserId",
@@ -354,6 +458,9 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                 name: "AbpClaimTypes");
 
             migrationBuilder.DropTable(
+                name: "AbpOrganizationUnitRoles");
+
+            migrationBuilder.DropTable(
                 name: "AbpPermissionGrants");
 
             migrationBuilder.DropTable(
@@ -369,6 +476,9 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
                 name: "AbpUserLogins");
 
             migrationBuilder.DropTable(
+                name: "AbpUserOrganizationUnits");
+
+            migrationBuilder.DropTable(
                 name: "AbpUserRoles");
 
             migrationBuilder.DropTable(
@@ -379,6 +489,9 @@ namespace VoloDocs.EntityFrameworkCore.Migrations
 
             migrationBuilder.DropTable(
                 name: "DocsProjects");
+
+            migrationBuilder.DropTable(
+                name: "AbpOrganizationUnits");
 
             migrationBuilder.DropTable(
                 name: "AbpRoles");
