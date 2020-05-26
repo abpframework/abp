c2fa54729d0eeb34831a9bfa48c733e4ac9727fe
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-26 12:41:14 +0300

    Added test: Default_Settings_With_TenantId

--- framework/test/Volo.Abp.BlobStoring.FileSystem.Tests/Volo/Abp/BlobStoring/FileSystem/BlogFilePathCalculator_Tests.cs
+++ framework/test/Volo.Abp.BlobStoring.FileSystem.Tests/Volo/Abp/BlobStoring/FileSystem/BlogFilePathCalculator_Tests.cs
@@ -8,7 +8,7 @@ namespace Volo.Abp.BlobStoring.FileSystem
     public class BlogFilePathCalculator_Tests : AbpBlobStoringFileSystemTestBase
     {
         private readonly IBlogFilePathCalculator _calculator;
-        
+
         public BlogFilePathCalculator_Tests()
         {
             _calculator = GetRequiredService<IBlogFilePathCalculator>();
@@ -18,26 +18,37 @@ namespace Volo.Abp.BlobStoring.FileSystem
         public void Default_Settings()
         {
             var separator = Path.DirectorySeparatorChar;
-            
+
             _calculator.Calculate(
-                GetArgs($"C:{separator}my-files","my-container","my-blob")
+                GetArgs($"C:{separator}my-files", "my-container", "my-blob")
             ).ShouldBe($"C:{separator}my-files{separator}host{separator}my-container{separator}my-blob");
         }
-        
+
+        [Fact]
+        public void Default_Settings_With_TenantId()
+        {
+            var separator = Path.DirectorySeparatorChar;
+            var tenantId = Guid.NewGuid();
+
+            _calculator.Calculate(
+                GetArgs($"C:{separator}my-files", "my-container", "my-blob", tenantId: tenantId)
+            ).ShouldBe($"C:{separator}my-files{separator}tenants{separator}{tenantId:D}{separator}my-container{separator}my-blob");
+        }
+
         [Fact]
         public void AppendContainerNameToBasePath_Set_To_False()
         {
             var separator = Path.DirectorySeparatorChar;
-            
+
             _calculator.Calculate(
-                GetArgs($"C:{separator}my-files","my-container","my-blob", appendContainerNameToBasePath: false)
+                GetArgs($"C:{separator}my-files", "my-container", "my-blob", appendContainerNameToBasePath: false)
             ).ShouldBe($"C:{separator}my-files{separator}host{separator}my-blob");
         }
 
         private BlobProviderArgs GetArgs(
             string basePath,
-            string containerName, 
-            string blobName, 
+            string containerName,
+            string blobName,
             Guid? tenantId = null,
             bool? appendContainerNameToBasePath = null)
         {
