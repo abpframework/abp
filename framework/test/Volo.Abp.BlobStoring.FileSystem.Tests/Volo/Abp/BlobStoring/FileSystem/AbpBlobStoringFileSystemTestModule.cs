0457322d0ec1e64215e81406ca6acbd2e72eaae6
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-26 07:20:07 +0300

    Added Volo.Abp.BlobStoring.FileSystem package and test project.

---
+++ framework/test/Volo.Abp.BlobStoring.FileSystem.Tests/Volo/Abp/BlobStoring/FileSystem/AbpBlobStoringFileSystemTestModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.BlobStoring.FileSystem
+{
+    [DependsOn(
+        typeof(AbpBlobStoringFileSystemModule)
+        )]
+    public class AbpBlobStoringFileSystemTestModule : AbpModule
+    {
+        
+    }
+}
