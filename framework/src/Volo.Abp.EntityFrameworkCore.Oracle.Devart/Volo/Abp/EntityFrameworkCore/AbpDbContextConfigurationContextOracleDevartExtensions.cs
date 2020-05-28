5a45ad68cb5b207b679cbe356ec7af8b4c2ef68f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-28 22:16:18 +0300

    Implement oracle integration package.

---
+++ framework/src/Volo.Abp.EntityFrameworkCore.Oracle.Devart/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextOracleDevartExtensions.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Devart.Data.Oracle.Entity;
+using Volo.Abp.EntityFrameworkCore.DependencyInjection;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextConfigurationContextOracleDevartExtensions
+    {
+        public static DbContextOptionsBuilder UseOracle(
+           [NotNull] this AbpDbContextConfigurationContext context,
+           [CanBeNull] Action<OracleDbContextOptionsBuilder> oracleOptionsAction = null)
+        {
+            if (context.ExistingConnection != null)
+            {
+                return context.DbContextOptions.UseOracle(context.ExistingConnection, oracleOptionsAction);
+            }
+            else
+            {
+                return context.DbContextOptions.UseOracle(context.ConnectionString, oracleOptionsAction);
+            }
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.EntityFrameworkCore.Oracle.Devart/Volo/Abp/EntityFrameworkCore/AbpDbContextOptionsOracleDevartExtensions.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+using System;
+using Devart.Data.Oracle.Entity;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextOptionsOracleDevartExtensions
+    {
+        public static void UseOracle(
+                [NotNull] this AbpDbContextOptions options,
+                [CanBeNull] Action<OracleDbContextOptionsBuilder> oracleOptionsAction = null)
+        {
+            options.Configure(context =>
+            {
+                context.UseOracle(oracleOptionsAction);
+            });
+        }
+
+        public static void UseOracle<TDbContext>(
+            [NotNull] this AbpDbContextOptions options,
+            [CanBeNull] Action<OracleDbContextOptionsBuilder> oracleOptionsAction = null)
+            where TDbContext : AbpDbContext<TDbContext>
+        {
+            options.Configure<TDbContext>(context =>
+            {
+                context.UseOracle(oracleOptionsAction);
+            });
+        }
+    }
+}
