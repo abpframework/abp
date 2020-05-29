f0f7ea25dc721abbc94e0ad8bf95fb9bbcdcf667
Ahmet <ahmet.cotur@hotmail.com>
2020-05-29 14:59:29 +0300

    MongoDB Container Tests removed

--- modules/blob-storing-database/test/Volo.Abp.BlobStoring.Database.MongoDB.Tests/MongoDB/MongoDbContainer_Test.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Volo.Abp.BlobStoring.Database.MongoDB
-{
-    public class MongoDbContainer_Test : BlobStoringDatabaseTestBase<BlobStoringDatabaseMongoDbTestModule>
-    {
-        
-    }
-}
