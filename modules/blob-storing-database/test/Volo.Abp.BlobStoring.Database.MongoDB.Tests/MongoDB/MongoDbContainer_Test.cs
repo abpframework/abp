9da08249804964164eb8a42b08016f88c9984dc6
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-05-30 22:46:13 +0300

    Create MongoDbContainer_Test.cs

---
+++ modules/blob-storing-database/test/Volo.Abp.BlobStoring.Database.MongoDB.Tests/MongoDB/MongoDbContainer_Test.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.BlobStoring.Database.MongoDB;
+using Xunit;
+
+namespace Volo.Abp.BlobStoring.Database.EntityFrameworkCore
+{
+    [Collection(MongoTestCollection.Name)]
+    public class MongoDbContainer_Test : BlobContainer_Tests<BlobStoringDatabaseMongoDbTestModule>
+    {
+        
+    }
+}
