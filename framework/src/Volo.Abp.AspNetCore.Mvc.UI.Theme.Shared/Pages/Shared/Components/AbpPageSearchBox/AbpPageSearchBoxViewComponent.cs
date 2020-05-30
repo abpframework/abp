bb9d6141afc5160c017ca4c3beadd8d9399220b7
Akın Sabri Çam <sabricam2016@gmail.com>
2020-05-30 05:29:44 +0300

    added search box view component

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/Pages/Shared/Components/AbpPageSearchBox/AbpPageSearchBoxViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Pages.Shared.Components.AbpPageSearchBox
+{
+    public class AbpPageSearchBoxViewComponent : AbpViewComponent
+    {
+        public string PlaceHolder { get; set; }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+            string placeHolder,
+            string formClassName,
+            string textBoxClassName)
+        {
+            return await Task.FromResult(View("~/Pages/Shared/Components/AbpPageSearchBox/Default.cshtml",
+                new SearchBoxViewModel
+                {
+                    PlaceHolder = placeHolder,
+                    FormClassName = formClassName,
+                    TextBoxClassName = textBoxClassName
+                }));
+            ;
+        }
+    }
+
+    public class SearchBoxViewModel
+    {
+        public string PlaceHolder { get; set; }
+
+        public string FormClassName { get; set; }
+
+        public string TextBoxClassName { get; set; }
+    }
+}
