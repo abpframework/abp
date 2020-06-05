493bf50037590cfe10fe989411f5b5cf12dac8af
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2020-06-05 14:12:40 +0300

    blog admin separation: application & httpApi

---
+++ modules/blogging/src/Volo.Blogging.Admin.HttpApi/Volo/Blogging/Admin/BlogManagementController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Blogging.Admin.Blogs;
+
+namespace Volo.Blogging.Admin
+{
+    [RemoteService(Name = BloggingAdminRemoteServiceConsts.RemoteServiceName)]
+    [Area("bloggingAdmin")]
+    [Route("api/blogging/blogs/admin")]
+    public class BlogManagementController : AbpController, IBlogManagementAppService
+    {
+        private readonly IBlogManagementAppService _blogManagementAppService;
+
+        public BlogManagementController(IBlogManagementAppService blogManagementAppService)
+        {
+            _blogManagementAppService = blogManagementAppService;
+        }
+
+        [HttpGet]
+        public async Task<ListResultDto<BlogDto>> GetListAsync()
+        {
+            return await _blogManagementAppService.GetListAsync();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<BlogDto> GetAsync(Guid id)
+        {
+            return await _blogManagementAppService.GetAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<BlogDto> Create(CreateBlogDto input)
+        {
+            return await _blogManagementAppService.Create(input);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<BlogDto> Update(Guid id, UpdateBlogDto input)
+        {
+            return await _blogManagementAppService.Update(id, input);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task Delete(Guid id)
+        {
+            await _blogManagementAppService.Delete(id);
+        }
+    }
+}
