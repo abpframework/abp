313944ebe4e2aa3db7738859a5f6e473e9ce4d1a
mehmet-erim <bilgi@mehmeterim.com>
2020-06-08 15:19:01 +0300

    build: add post-install script

---
+++ npm/ng-packs/scripts/post-install.ts
@@ -0,0 +1,26 @@
+import execa from 'execa';
+import fse from 'fs-extra';
+
+(async () => {
+  rename(true);
+
+  try {
+    await execa('yarn', ['ngcc'], {
+      stdout: 'inherit',
+      cwd: '../',
+    });
+  } catch (error) {
+    rename(false);
+  }
+
+  rename(false);
+})();
+
+async function rename(prodToMain: boolean) {
+  try {
+    await fse.rename('../tsconfig.json', `../tsconfig.${prodToMain ? 'temp' : 'prod'}.json`);
+    await fse.rename(`../tsconfig.${prodToMain ? 'prod' : 'temp'}.json`, '../tsconfig.json');
+  } catch (error) {
+    rename(!prodToMain);
+  }
+}
