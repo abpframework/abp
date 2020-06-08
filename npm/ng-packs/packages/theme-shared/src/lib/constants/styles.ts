e81930100e80bcc10088e96b65cf771daafc62aa
Arman Ozak <arman@armanozak.com>
2020-06-08 17:42:06 +0300

    feat: add bordered style for datatable

--- npm/ng-packs/packages/theme-shared/src/lib/constants/styles.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/constants/styles.ts
@@ -45,6 +45,11 @@ export default `
   background: #8a8686;
 }
 
+.bordered .datatable-body-row {
+  border-top: 1px solid #eee;
+  margin-top: -1px;
+}
+
 .abp-ellipsis-inline {
   display: inline-block;
   overflow: hidden;
