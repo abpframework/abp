c2fc5f1671f34ffea60c6ef5ddba02c9d1627573
liangshiwei <liangshiw@outlook.com>
2020-06-13 00:29:05 +0800

    Update IIdentityClaimTypeRepository & IIdentityRoleRepository

--- modules/identity/src/Volo.Abp.Identity.MongoDB/Volo/Abp/Identity/MongoDB/MongoIdentityClaimTypeRepository.cs
+++ modules/identity/src/Volo.Abp.Identity.MongoDB/Volo/Abp/Identity/MongoDB/MongoIdentityClaimTypeRepository.cs
@@ -5,6 +5,7 @@ using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using System.Threading;
 using Volo.Abp.Domain.Repositories.MongoDB;
 using Volo.Abp.MongoDB;
 
@@ -16,23 +17,31 @@ namespace Volo.Abp.Identity.MongoDB
         {
         }
 
-        public virtual async Task<bool> AnyAsync(string name, Guid? ignoredId = null)
+        public virtual async Task<bool> AnyAsync(
+            string name,
+            Guid? ignoredId = null,
+            CancellationToken cancellationToken = default)
         {
             if (ignoredId == null)
             {
                 return await GetMongoQueryable()
                     .Where(ct => ct.Name == name)
-                    .AnyAsync();
+                    .AnyAsync(GetCancellationToken(cancellationToken));
             }
             else
             {
                 return await GetMongoQueryable()
                     .Where(ct => ct.Id != ignoredId && ct.Name == name)
-                    .AnyAsync();
+                    .AnyAsync(GetCancellationToken(cancellationToken));
             }
         }
 
-        public virtual async Task<List<IdentityClaimType>> GetListAsync(string sorting, int maxResultCount, int skipCount, string filter)
+        public virtual async Task<List<IdentityClaimType>> GetListAsync(
+            string sorting,
+            int maxResultCount,
+            int skipCount,
+            string filter,
+            CancellationToken cancellationToken = default)
         {
             return await GetMongoQueryable()
                 .WhereIf<IdentityClaimType, IMongoQueryable<IdentityClaimType>>(
@@ -43,7 +52,21 @@ namespace Volo.Abp.Identity.MongoDB
                 .OrderBy(sorting ?? nameof(IdentityClaimType.Name))
                 .As<IMongoQueryable<IdentityClaimType>>()
                 .PageBy<IdentityClaimType, IMongoQueryable<IdentityClaimType>>(skipCount, maxResultCount)
-                .ToListAsync();
+                .ToListAsync(GetCancellationToken(cancellationToken));
+        }
+
+        public async Task<long> GetCountAsync(
+            string filter = null,
+            CancellationToken cancellationToken = default)
+        {
+            return await GetMongoQueryable()
+                .WhereIf<IdentityClaimType, IMongoQueryable<IdentityClaimType>>(
+                    !filter.IsNullOrWhiteSpace(),
+                    u =>
+                        u.Name.Contains(filter)
+                )
+                .As<IMongoQueryable<IdentityClaimType>>()
+                .LongCountAsync(GetCancellationToken(cancellationToken));
         }
     }
 }
