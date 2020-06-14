10c00be2dc39e6d2c39328466ee1b77bad890e95
Necati Meral <necati_meral@yahoo.de>
2020-06-14 22:20:11 +0200

    Implemented rudimentary readonly appservices

---
+++ framework/src/Volo.Abp.Ddd.Application/Volo/Abp/Application/Services/ReadOnlyAppService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Auditing;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace Volo.Abp.Application.Services
+{
+    public abstract class ReadOnlyAppService<TEntity, TGetOutputDto, TGetListOutputDto, TKey, TGetListInput>
+        : AbstractKeyReadOnlyAppService<TEntity, TGetOutputDto, TGetListOutputDto, TKey, TGetListInput>
+        where TEntity : class, IEntity<TKey>
+        where TGetOutputDto : IEntityDto<TKey>
+        where TGetListOutputDto : IEntityDto<TKey>
+    {
+        protected new IRepository<TEntity, TKey> Repository { get; }
+
+        protected ReadOnlyAppService(IRepository<TEntity, TKey> repository)
+        : base(repository)
+        {
+            Repository = repository;
+        }
+
+        protected override async Task<TEntity> GetEntityByIdAsync(TKey id)
+        {
+            return await Repository.GetAsync(id);
+        }
+
+        protected override IQueryable<TEntity> ApplyDefaultSorting(IQueryable<TEntity> query)
+        {
+            if (typeof(TEntity).IsAssignableTo<ICreationAuditedObject>())
+            {
+                return query.OrderByDescending(e => ((ICreationAuditedObject)e).CreationTime);
+            }
+            else
+            {
+                return query.OrderByDescending(e => e.Id);
+            }
+        }
+    }
+}
