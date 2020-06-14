a4dda7a9fc981b1beb4df4b55b969d4a65e4a012
Necati Meral <necati_meral@yahoo.de>
2020-06-14 21:31:24 +0200

    Splitted `ICrudAppService` interface

---
+++ framework/src/Volo.Abp.Ddd.Application.Contracts/Volo/Abp/Application/Services/IDeleteAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.Application.Services
+{
+    public interface IDeleteAppService<in TKey> : IApplicationService
+    {
+        Task DeleteAsync(TKey id);
+    }
+}
---
+++ framework/src/Volo.Abp.Ddd.Application.Contracts/Volo/Abp/Application/Services/IUpdateAppService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.Application.Services
+{
+    public interface IUpdateAppService<TEntityDto, in TKey>
+        : IUpdateAppService<TEntityDto, TKey, TEntityDto>
+    {
+
+    }
+
+    public interface IUpdateAppService<TGetOutputDto, in TKey, in TUpdateInput>
+        : IApplicationService
+    {
+        Task<TGetOutputDto> UpdateAsync(TKey id, TUpdateInput input);
+    }
+}
