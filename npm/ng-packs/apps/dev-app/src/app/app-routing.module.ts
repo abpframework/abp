e88d5b4cba80e18a51ffe7bfdf83fdd59fdf04b8
mehmet-erim <bilgi@mehmeterim.com>
2020-06-19 14:21:30 +0300

    feat: define DynamicLayoutComponent in root level

--- npm/ng-packs/apps/dev-app/src/app/app-routing.module.ts
+++ npm/ng-packs/apps/dev-app/src/app/app-routing.module.ts
@@ -1,36 +1,37 @@
-import { ABP } from '@abp/ng.core';
+import { ABP, DynamicLayoutComponent } from '@abp/ng.core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
-    data: {
-      routes: {
-        name: '::Menu:Home',
-        order: 1,
-      } as ABP.Route,
-    },
-  },
-  {
-    path: 'account',
-    loadChildren: () =>
-      import('@abp/ng.account').then(m => m.AccountModule.forLazy({ redirectUrl: '/' })),
-  },
-  {
-    path: 'identity',
-    loadChildren: () => import('@abp/ng.identity').then(m => m.IdentityModule.forLazy()),
-  },
-  {
-    path: 'tenant-management',
-    loadChildren: () =>
-      import('@abp/ng.tenant-management').then(m => m.TenantManagementModule.forLazy()),
-  },
-  {
-    path: 'setting-management',
-    loadChildren: () =>
-      import('@abp/ng.setting-management').then(m => m.SettingManagementModule.forLazy()),
+    component: DynamicLayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: 'account',
+        loadChildren: () =>
+          import('@abp/ng.account').then(m => m.AccountModule.forLazy({ redirectUrl: '/' })),
+      },
+      {
+        path: 'identity',
+        loadChildren: () => import('@abp/ng.identity').then(m => m.IdentityModule.forLazy()),
+      },
+      {
+        path: 'tenant-management',
+        loadChildren: () =>
+          import('@abp/ng.tenant-management').then(m => m.TenantManagementModule.forLazy()),
+      },
+      {
+        path: 'setting-management',
+        loadChildren: () =>
+          import('@abp/ng.setting-management').then(m => m.SettingManagementModule.forLazy()),
+      },
+    ],
   },
 ];
 
