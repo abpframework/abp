2328363030a91ccee2d9e81151b983613509bbc1
Arman Ozak <arman@armanozak.com>
2020-06-22 13:31:49 +0300

    feat: import ThemeBasicModule instead of SharedModule

--- npm/ng-packs/apps/dev-app/src/app/app.module.ts
+++ npm/ng-packs/apps/dev-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { CoreModule } from '@abp/ng.core';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
+import { ThemeBasicModule } from '@abp/ng.theme.basic';
 import { ThemeSharedModule } from '@abp/ng.theme.shared';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,7 +13,6 @@ import { NgxsModule } from '@ngxs/store';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
 const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
 
@@ -32,7 +32,7 @@ const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: false })];
     TenantManagementConfigModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
     NgxsModule.forRoot(),
-    SharedModule,
+    ThemeBasicModule.forRoot(),
     ...(environment.production ? [] : LOGGERS),
   ],
   declarations: [AppComponent],
