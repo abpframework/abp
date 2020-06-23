da545f3d8d793e3c9cf0f9a99f4396bf21fa312a
Galip Tolga Erdem <galiperdem@gmail.com>
2020-06-23 19:28:47 +0300

    Added LoggedOut view

---
+++ modules/account/src/Volo.Abp.Account.Web/Pages/Account/LoggedOut.js
@@ -0,0 +1,6 @@
+﻿(function ($) {
+    //TODO:gterdem There should be an option to set the redirect delay here
+    setTimeout(function () {
+        window.location = $('.redirectButton').attr('href');
+    }, 3000)
+})(JQuery)
