184a696d2cadcceaf5af08938f712db11702657c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-06-23 10:42:12 +0300

    Create Volo.Abp.EntityFrameworkCore.Oracle package.

---
+++ framework/src/Volo.Abp.EntityFrameworkCore.Oracle/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextOracleExtensions.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Oracle.EntityFrameworkCore.Infrastructure;
+using Volo.Abp.EntityFrameworkCore.DependencyInjection;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextConfigurationContextOracleExtensions
+    {
+        public static DbContextOptionsBuilder UseOracle(
+           [NotNull] this AbpDbContextConfigurationContext context,
+           [CanBeNull] Action<OracleDbContextOptionsBuilder> oracleOptionsAction = null,
+           bool useExistingConnectionIfAvailable = false)
+        {
+            if (useExistingConnectionIfAvailable && context.ExistingConnection != null)
+            {
+                return context.DbContextOptions.UseOracle(context.ExistingConnection, oracleOptionsAction);
+            }
+            else
+            {
+                return context.DbContextOptions.UseOracle(context.ConnectionString, oracleOptionsAction);
+            }
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.EntityFrameworkCore.Oracle/Volo/Abp/EntityFrameworkCore/AbpDbContextOptionsOracleExtensions.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using System;
+using Oracle.EntityFrameworkCore.Infrastructure;
+
+namespace Volo.Abp.EntityFrameworkCore
+{
+    public static class AbpDbContextOptionsOracleExtensions
+    {
+        public static void UseOracle(
+                [NotNull] this AbpDbContextOptions options,
+                [CanBeNull] Action<OracleDbContextOptionsBuilder> oracleOptionsAction = null,
+                bool useExistingConnectionIfAvailable = false)
+        {
+            options.Configure(context =>
+            {
+                context.UseOracle(oracleOptionsAction, useExistingConnectionIfAvailable);
+            });
+        }
+
+        public static void UseOracle<TDbContext>(
+            [NotNull] this AbpDbContextOptions options,
+            [CanBeNull] Action<OracleDbContextOptionsBuilder> oracleOptionsAction = null,
+            bool useExistingConnectionIfAvailable = false)
+            where TDbContext : AbpDbContext<TDbContext>
+        {
+            options.Configure<TDbContext>(context =>
+            {
+                context.UseOracle(oracleOptionsAction, useExistingConnectionIfAvailable);
+            });
+        }
+    }
+}
