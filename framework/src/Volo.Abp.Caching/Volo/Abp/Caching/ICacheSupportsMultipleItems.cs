8d3a145b7b8f9d029be74e3a32e1b4990a97907b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-06-24 20:45:49 +0300

    Resolved #4482 Add SetMany and GetMany to the IDistributedCache

---
+++ framework/src/Volo.Abp.Caching/Volo/Abp/Caching/ICacheSupportsMultipleItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Volo.Abp.Caching
+{
+    public interface ICacheSupportsMultipleItems
+    {
+        byte[][] GetMany(
+            IEnumerable<string> keys
+        );
+
+        Task<byte[][]> GetManyAsync(
+            IEnumerable<string> keys,
+            CancellationToken token = default
+        );
+
+        void SetMany(
+            IEnumerable<KeyValuePair<string, byte[]>> items,
+            DistributedCacheEntryOptions options
+        );
+
+        Task SetManyAsync(
+            IEnumerable<KeyValuePair<string, byte[]>> items,
+            DistributedCacheEntryOptions options,
+            CancellationToken token = default
+        ); 
+    }
+}
--- framework/src/Volo.Abp.Caching/Volo/Abp/Caching/IDistributedCache.cs
+++ framework/src/Volo.Abp.Caching/Volo/Abp/Caching/IDistributedCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
@@ -33,6 +34,39 @@ namespace Volo.Abp.Caching
         TCacheItem Get(
             TCacheKey key,
             bool? hideErrors = null
+        );
+        
+        /// <summary>
+        /// Gets multiple cache items with the given keys.
+        ///
+        /// The returned list contains exactly the same count of items specified in the given keys.
+        /// An item in the return list can not be null, but an item in the list has null value
+        /// if the related key not found in the cache. 
+        /// </summary>
+        /// <param name="keys">The keys of cached items to be retrieved from the cache.</param>
+        /// <param name="hideErrors">Indicates to throw or hide the exceptions for the distributed cache.</param>
+        /// <returns>List of cache items.</returns>
+        KeyValuePair<TCacheKey, TCacheItem>[] GetMany(
+            IEnumerable<TCacheKey> keys,
+            bool? hideErrors = null
+        );
+        
+        /// <summary>
+        /// Gets multiple cache items with the given keys.
+        ///
+        /// The returned list contains exactly the same count of items specified in the given keys.
+        /// An item in the return list can not be null, but an item in the list has null value
+        /// if the related key not found in the cache.
+        /// 
+        /// </summary>
+        /// <param name="keys">The keys of cached items to be retrieved from the cache.</param>
+        /// <param name="hideErrors">Indicates to throw or hide the exceptions for the distributed cache.</param>
+        /// /// <param name="token">The <see cref="T:System.Threading.CancellationToken" /> for the task.</param>
+        /// <returns>List of cache items.</returns>
+        Task<KeyValuePair<TCacheKey, TCacheItem>[]> GetManyAsync(
+            IEnumerable<TCacheKey> keys,
+            bool? hideErrors = null,
+            CancellationToken token = default
         );
 
         /// <summary>
@@ -109,6 +143,35 @@ namespace Volo.Abp.Caching
             [NotNull] TCacheKey key,
             [NotNull] TCacheItem value,
             [CanBeNull] DistributedCacheEntryOptions options = null,
+            bool? hideErrors = null,
+            CancellationToken token = default
+        );
+
+        /// <summary>
+        /// Sets multiple cache items.
+        /// Based on the implementation, this can be more efficient than setting multiple items individually.
+        /// </summary>
+        /// <param name="items">Items to set on the cache</param>
+        /// <param name="options">The cache options for the value.</param>
+        /// <param name="hideErrors">Indicates to throw or hide the exceptions for the distributed cache.</param>
+        void SetMany(
+            IEnumerable<KeyValuePair<TCacheKey, TCacheItem>> items,
+            DistributedCacheEntryOptions options = null,
+            bool? hideErrors = null
+        );
+        
+        /// <summary>
+        /// Sets multiple cache items.
+        /// Based on the implementation, this can be more efficient than setting multiple items individually.
+        /// </summary>
+        /// <param name="items">Items to set on the cache</param>
+        /// <param name="options">The cache options for the value.</param>
+        /// <param name="hideErrors">Indicates to throw or hide the exceptions for the distributed cache.</param>
+        /// <param name="token">The <see cref="T:System.Threading.CancellationToken" /> for the task.</param>
+        /// <returns>The <see cref="T:System.Threading.Tasks.Task" /> indicating that the operation is asynchronous.</returns>
+        Task SetManyAsync(
+            IEnumerable<KeyValuePair<TCacheKey, TCacheItem>> items,
+            DistributedCacheEntryOptions options = null,
             bool? hideErrors = null,
             CancellationToken token = default
         );
