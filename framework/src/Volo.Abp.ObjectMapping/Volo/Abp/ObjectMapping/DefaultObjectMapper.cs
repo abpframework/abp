a50b379ba2e19d790613459ac26a00aa0cf089b7
stimocin <sercan.timocin@nesine.com>
2020-06-25 22:00:33 +0300

    #94 resolved

--- framework/src/Volo.Abp.ObjectMapping/Volo/Abp/ObjectMapping/DefaultObjectMapper.cs
+++ framework/src/Volo.Abp.ObjectMapping/Volo/Abp/ObjectMapping/DefaultObjectMapper.cs
@@ -1,5 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Volo.Abp.DependencyInjection;
 
 namespace Volo.Abp.ObjectMapping
@@ -7,10 +10,10 @@ namespace Volo.Abp.ObjectMapping
     public class DefaultObjectMapper<TContext> : DefaultObjectMapper, IObjectMapper<TContext>
     {
         public DefaultObjectMapper(
-            IServiceProvider serviceProvider, 
+            IServiceProvider serviceProvider,
             IAutoObjectMappingProvider<TContext> autoObjectMappingProvider
             ) : base(
-                serviceProvider, 
+                serviceProvider,
                 autoObjectMappingProvider)
         {
 
@@ -29,7 +32,7 @@ namespace Volo.Abp.ObjectMapping
             AutoObjectMappingProvider = autoObjectMappingProvider;
             ServiceProvider = serviceProvider;
         }
-        
+
         //TODO: It can be slow to always check if service is available. Test it and optimize if necessary.
 
         public virtual TDestination Map<TSource, TDestination>(TSource source)
@@ -60,12 +63,18 @@ namespace Volo.Abp.ObjectMapping
                     //TODO: Check if TDestination has a proper constructor which takes TSource
                     //TODO: Check if TDestination has an empty constructor (in this case, use MapFrom)
 
-                    return (TDestination) Activator.CreateInstance(typeof(TDestination), source);
+                    return (TDestination)Activator.CreateInstance(typeof(TDestination), source);
                 }
                 catch
                 {
                     //TODO: Remove catch when TODOs are implemented above
                 }
+            }
+
+            if (IsTypeSupportInterface(source.GetType(), typeof(ICollection<>)) && IsTypeSupportInterface(typeof(TDestination), typeof(ICollection<>)))
+            {
+                TDestination destination = (TDestination)Activator.CreateInstance(typeof(TDestination));
+                return CollectionMap(source, destination);
             }
 
             return AutoMap<TSource, TDestination>(source);
@@ -99,6 +108,11 @@ namespace Volo.Abp.ObjectMapping
                 return destination;
             }
 
+            if (IsTypeSupportInterface(source.GetType(), typeof(ICollection<>)) && IsTypeSupportInterface(typeof(TDestination), typeof(ICollection<>)))
+            {
+                return CollectionMap<TSource, TDestination>(source, destination);
+            }
+
             return AutoMap(source, destination);
         }
 
@@ -110,6 +124,78 @@ namespace Volo.Abp.ObjectMapping
         protected virtual TDestination AutoMap<TSource, TDestination>(TSource source, TDestination destination)
         {
             return AutoObjectMappingProvider.Map<TSource, TDestination>(source, destination);
+        }
+
+        private TDestination CollectionMap<TSource, TDestination>(TSource source, TDestination destination)
+        {
+            Type destinationType = typeof(TDestination);
+            Type sourceGenericType = source.GetType().GetGenericArguments().Single();
+            Type destinationGenericType = destinationType.GetGenericArguments().Single();
+            Type mappingDefinationClassType = typeof(IObjectMapper<,>).MakeGenericType(sourceGenericType, destinationGenericType);
+
+            object mapperObject = null;
+
+            using (var scope = ServiceProvider.CreateScope())
+            {
+                mapperObject = scope.ServiceProvider.GetService(mappingDefinationClassType);
+
+                if (mapperObject == null)
+                {
+                    // Dependcy evil 1
+                    return AutoMap<TSource, TDestination>(source);
+                }
+            }
+
+            // Dependcy evil 2
+            var mapMethodInfo = mappingDefinationClassType.GetMethod("Map",
+                                  BindingFlags.Public | BindingFlags.Instance,
+                                  null,
+                                  CallingConventions.Any,
+                                  new Type[] { sourceGenericType },
+                                  null);
+
+            var sourceCollection = (IEnumerable<object>)source;
+
+            // Dependcy evil 3
+            var addMethodInfo = destinationType.GetMethod("Add",
+                              BindingFlags.Public | BindingFlags.Instance,
+                              null,
+                              CallingConventions.Any,
+                              new Type[] { destinationGenericType },
+                              null);
+
+            if (addMethodInfo == null)
+            {
+                return default;
+            }
+
+            return ConvertToDestinationCollection(destination, mapperObject, mapMethodInfo, sourceCollection, addMethodInfo);
+        }
+
+        private TDestination ConvertToDestinationCollection<TDestination>(TDestination destination, object mapperObject, MethodInfo mapMethodInfo, IEnumerable<object> sourceCollection, MethodInfo addMethodInfo)
+        {
+            foreach (var item in sourceCollection)
+            {
+                var mappedObj = mapMethodInfo.Invoke(mapperObject, new object[] { item });
+                addMethodInfo.Invoke(destination, new object[] { mappedObj });
+            }
+
+            return destination;
+        }
+
+        private bool IsTypeSupportInterface(Type type, Type inter)
+        {
+            if (inter.IsAssignableFrom(type))
+            {
+                return true;
+            }
+
+            if (type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == inter))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- framework/test/Volo.Abp.AutoMapper.Tests/Volo/Abp/AutoMapper/ObjectMapperExtensions_Tests.cs
+++ framework/test/Volo.Abp.AutoMapper.Tests/Volo/Abp/AutoMapper/ObjectMapperExtensions_Tests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Volo.Abp.AutoMapper.SampleClasses;
 using Volo.Abp.ObjectMapping;
 using Volo.Abp.Testing;
@@ -27,6 +29,108 @@ namespace Volo.Abp.AutoMapper
                 }
             );
 
+            dto.As<MyEntityDto>().Number.ShouldBe(42);
+        }
+
+        [Fact]
+        public void Should_Map_Objects_Using_IObjectMapper_Map_Method_With_Source()
+        {
+            var dto = _objectMapper.Map<MyEntity, MyEntityDto2>(
+                new MyEntity
+                {
+                    Number = 42
+                }
+            );
+
+            dto.As<MyEntityDto2>().Number.ShouldBe(43);
+        }
+
+        [Fact]
+        public void Should_Map_List_To_List_Using_IObjectMapper_Map_Method_With_Source()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new List<MyEntity>();
+            entities.Add(entity);
+
+            var dtos = _objectMapper.Map<List<MyEntity>, List<MyEntityDto2>>(entities);
+
+            var dto = dtos.First();
+            dto.As<MyEntityDto2>().Number.ShouldBe(43);
+        }
+
+        [Fact]
+        public void Should_Map_List_To_List_Using_IObjectMapper_Map_Method_With_Source_And_Destination()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new List<MyEntity>();
+            entities.Add(entity);
+
+            var dtos = _objectMapper.Map<List<MyEntity>, List<MyEntityDto2>>(entities, new List<MyEntityDto2>());
+
+            var dto = dtos.First();
+            dto.As<MyEntityDto2>().Number.ShouldBe(43);
+        }
+
+        [Fact]
+        public void Should_Map_LinkedList_To_List_Using_IObjectMapper_Map_Method_With_Source()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new LinkedList<MyEntity>();
+            entities.AddFirst(entity);
+
+            var dtos = _objectMapper.Map<LinkedList<MyEntity>, List<MyEntityDto2>>(entities);
+
+            var dto = dtos.First();
+            dto.As<MyEntityDto2>().Number.ShouldBe(43);
+        }
+
+        [Fact]
+        public void Should_Map_LinkedList_To_List_Using_IObjectMapper_Map_Method_With_Source_And_Destination()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new LinkedList<MyEntity>();
+            entities.AddFirst(entity);
+
+            var dtos = _objectMapper.Map<LinkedList<MyEntity>, List<MyEntityDto2>>(entities, new List<MyEntityDto2>());
+
+            var dto = dtos.First();
+            dto.As<MyEntityDto2>().Number.ShouldBe(43);
+        }
+
+        [Fact]
+        public void Should_Not_Map_List_To_LinkedList_Using_IObjectMapper_Map_Method_With_Source()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new List<MyEntity>();
+            entities.AddFirst(entity);
+
+            var dtos = _objectMapper.Map<List<MyEntity>, LinkedList<MyEntityDto2>>(entities);
+
+            dtos.ShouldBeNull();
+        }
+
+        [Fact]
+        public void Should_Not_Map_List_To_LinkedList_Using_IObjectMapper_Map_Method_With_Source_And_Destination()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new List<MyEntity>();
+            entities.AddFirst(entity);
+
+            var dtos = _objectMapper.Map(entities, new LinkedList<MyEntityDto2>());
+
+            dtos.ShouldBeNull();
+        }
+
+        [Fact]
+        public void Should_Map_List_To_List_With_AutoMap()
+        {
+            var entity = new MyEntity { Number = 42 };
+            var entities = new List<MyEntity>();
+            entities.AddFirst(entity);
+
+            var dtos = _objectMapper.Map<List<MyEntity>, List<MyEntityDto>>(entities);
+
+            var dto = dtos.First();
             dto.As<MyEntityDto>().Number.ShouldBe(42);
         }
     }
