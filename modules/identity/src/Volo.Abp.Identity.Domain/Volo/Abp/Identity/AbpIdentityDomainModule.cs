6d5f169cfc0d18f869459d3c960ae433c3e5e21b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-06-25 12:09:02 +0300

    Define OrganizationUnitEto

--- modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/AbpIdentityDomainModule.cs
+++ modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/AbpIdentityDomainModule.cs
@@ -34,6 +34,7 @@ namespace Volo.Abp.Identity
                 options.EtoMappings.Add<IdentityUser, UserEto>(typeof(AbpIdentityDomainModule));
                 options.EtoMappings.Add<IdentityClaimType, IdentityClaimTypeEto>(typeof(AbpIdentityDomainModule));
                 options.EtoMappings.Add<IdentityRole, IdentityRoleEto>(typeof(AbpIdentityDomainModule));
+                options.EtoMappings.Add<OrganizationUnit, OrganizationUnitEto>(typeof(AbpIdentityDomainModule));
             });
             
             var identityBuilder = context.Services.AddAbpIdentity(options =>
