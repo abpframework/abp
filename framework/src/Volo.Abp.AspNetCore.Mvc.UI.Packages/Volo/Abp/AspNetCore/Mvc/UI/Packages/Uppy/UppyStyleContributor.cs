fff0a6f5557dc49e955d9d159c8a6fcd11e1bbbe
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-06-26 16:01:08 +0300

    Resolved #4503: Introduce @libs/uppy npm package.

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Packages/Volo/Abp/AspNetCore/Mvc/UI/Packages/Uppy/UppyStyleContributor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Packages.Uppy
+{
+    public class UppyStyleContributor : BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files.AddIfNotContains("/libs/uppy/uppy.min.css");
+        }
+    }
+}
