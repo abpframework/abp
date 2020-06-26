013782436101d7c5a7feee866e900b9819831256
liangshiwei <liangshiw@outlook.com>
2020-06-26 18:03:25 +0800

    Implement the ICacheSupportsMultipleItems for the AbpRedisCache

--- framework/src/Volo.Abp.Caching.StackExchangeRedis/Volo/Abp/Caching/StackExchangeRedis/AbpRedisCache.cs
+++ framework/src/Volo.Abp.Caching.StackExchangeRedis/Volo/Abp/Caching/StackExchangeRedis/AbpRedisCache.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Microsoft.Extensions.Options;
 using StackExchange.Redis;
@@ -10,40 +12,179 @@ using Volo.Abp.DependencyInjection;
 namespace Volo.Abp.Caching.StackExchangeRedis
 {
     [DisableConventionalRegistration]
-    public class AbpRedisCache : RedisCache
+    public class AbpRedisCache : RedisCache, ICacheSupportsMultipleItems
     {
-        private static readonly FieldInfo RedisDatabaseField;
-        private static readonly MethodInfo ConnectMethod;
-        private static readonly MethodInfo ConnectAsyncMethod;
-
-        protected IDatabase RedisDatabase => RedisDatabaseField.GetValue(this) as IDatabase;
-
-        static AbpRedisCache()
-        {
-            RedisDatabaseField = typeof(RedisCache)
-                .GetField("_cache", BindingFlags.Instance | BindingFlags.NonPublic);
-            
-            ConnectMethod = typeof(RedisCache)
-                .GetMethod("Connect", BindingFlags.Instance | BindingFlags.NonPublic);
-            
-            ConnectAsyncMethod = typeof(RedisCache)
-                .GetMethod("ConnectAsync", BindingFlags.Instance | BindingFlags.NonPublic);
-        }
-
         public AbpRedisCache(IOptions<RedisCacheOptions> optionsAccessor)
             : base(optionsAccessor)
         {
-            
         }
 
-        protected virtual void Connect()
+        public byte[][] GetMany(
+            IEnumerable<string> keys)
         {
-            ConnectMethod.Invoke(this, Array.Empty<object>());
+            keys = Check.NotNull(keys, nameof(keys));
+
+            return GetAndRefreshMany(keys, true);
         }
 
-        protected virtual Task ConnectAsync(CancellationToken token = default)
+        public async Task<byte[][]> GetManyAsync(
+            IEnumerable<string> keys,
+            CancellationToken token = default)
         {
-            return (Task) ConnectAsyncMethod.Invoke(this, new object[] {token});
+            keys = Check.NotNull(keys, nameof(keys));
+
+            return await GetAndRefreshManyAsync(keys, true, token);
+        }
+
+        public void SetMany(
+            IEnumerable<KeyValuePair<string, byte[]>> items,
+            DistributedCacheEntryOptions options)
+        {
+            Connect();
+
+            Task.WaitAll(PipelineSetMany(items, options));
+        }
+
+        public async Task SetManyAsync(
+            IEnumerable<KeyValuePair<string, byte[]>> items,
+            DistributedCacheEntryOptions options,
+            CancellationToken token = default)
+        {
+            token.ThrowIfCancellationRequested();
+
+            await ConnectAsync(token);
+
+            await Task.WhenAll(PipelineSetMany(items, options));
+        }
+
+        protected virtual byte[][] GetAndRefreshMany(
+            IEnumerable<string> keys,
+            bool getData)
+        {
+            Connect();
+
+            var keyArray = keys.Select(key => Instance + key).ToArray();
+            RedisValue[][] results;
+
+            if (getData)
+            {
+                results = Cache.HashMemberGetMany(keyArray, AbsoluteExpirationKey,
+                    SlidingExpirationKey, DataKey);
+            }
+            else
+            {
+                results = Cache.HashMemberGetMany(keyArray, AbsoluteExpirationKey,
+                    SlidingExpirationKey);
+            }
+
+            Task.WaitAll(GetAndRefreshMany(keyArray, results, out var bytes));
+
+            return bytes;
+        }
+
+        protected virtual async Task<byte[][]> GetAndRefreshManyAsync(
+            IEnumerable<string> keys,
+            bool getData,
+            CancellationToken token = default)
+        {
+            token.ThrowIfCancellationRequested();
+
+            await ConnectAsync(token);
+
+            var keyArray = keys.Select(key => Instance + key).ToArray();
+            RedisValue[][] results;
+
+            if (getData)
+            {
+                results = await Cache.HashMemberGetManyAsync(keyArray, AbsoluteExpirationKey,
+                    SlidingExpirationKey, DataKey);
+            }
+            else
+            {
+                results = await Cache.HashMemberGetManyAsync(keyArray, AbsoluteExpirationKey,
+                    SlidingExpirationKey);
+            }
+
+            await Task.WhenAll(GetAndRefreshMany(keyArray, results, out var bytes));
+
+            return bytes;
+        }
+
+        private Task[] GetAndRefreshMany(string[] keys, RedisValue[][] results, out byte[][] bytes)
+        {
+            bytes = new byte[keys.Length][];
+            var tasks = new Task[keys.Length];
+            for (var i = 0; i < keys.Length; i++)
+            {
+                if (results[i].Length >= 2)
+                {
+                    MapMetadata(results[i], out DateTimeOffset? absExpr, out TimeSpan? sldExpr);
+                    tasks[i] = PipelineRefresh(keys[i], absExpr, sldExpr);
+                }
+
+                if (results[i].Length >= 3 && results[i][2].HasValue)
+                {
+                    bytes[i] = results[i][2];
+                }
+                else
+                {
+                    bytes[i] = null;
+                }
+            }
+
+            return tasks;
+        }
+
+        private Task PipelineRefresh(
+            string key,
+            DateTimeOffset? absExpr,
+            TimeSpan? sldExpr)
+        {
+            if (sldExpr.HasValue)
+            {
+                TimeSpan? expr;
+
+                if (absExpr.HasValue)
+                {
+                    var relExpr = absExpr.Value - DateTimeOffset.Now;
+                    expr = relExpr <= sldExpr.Value ? relExpr : sldExpr;
+                }
+                else
+                {
+                    expr = sldExpr;
+                }
+
+                return Cache.KeyExpireAsync(key, expr);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task[] PipelineSetMany(
+            IEnumerable<KeyValuePair<string, byte[]>> items,
+            DistributedCacheEntryOptions options)
+        {
+            items = Check.NotNull(items, nameof(items));
+            options = Check.NotNull(options, nameof(options));
+
+            var itemArray = items.ToArray();
+            var tasks = new Task[itemArray.Length];
+            var creationTime = DateTimeOffset.UtcNow;
+            var absoluteExpiration = GetAbsoluteExpiration(creationTime, options);
+
+            for (var i = 0; i < itemArray.Length; i++)
+            {
+                tasks[i] = Cache.ScriptEvaluateAsync(SetScript, new RedisKey[] {Instance + itemArray[i].Key},
+                    new RedisValue[]
+                    {
+                        absoluteExpiration?.Ticks ?? NotPresent,
+                        options.SlidingExpiration?.Ticks ?? NotPresent,
+                        GetExpirationInSeconds(creationTime, absoluteExpiration, options) ?? NotPresent,
+                        itemArray[i].Value
+                    });
+            }
+
+            return tasks;
         }
     }
 }
---
+++ framework/src/Volo.Abp.Caching.StackExchangeRedis/Volo/Abp/Caching/StackExchangeRedis/AbpRedisExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Volo.Abp.Caching.StackExchangeRedis
+{
+    public static class AbpRedisExtensions
+    {
+        private const string HmGetManyScript = (@"
+                                                local res = {};
+                                                for i , key in ipairs(KEYS) do
+                                                    res[#res+1] = redis.call('HMGET', key,unpack(ARGV))
+                                                end
+                                                return res");
+
+        public static RedisValue[][] HashMemberGetMany(
+            this IDatabase cache,
+            string[] keys,
+            params string[] members)
+        {
+            var results = cache.ScriptEvaluate(
+                HmGetManyScript,
+                keys.Select(key => (RedisKey) key).ToArray(),
+                members.Select(member => (RedisValue) member).ToArray());
+
+            return ((RedisResult[]) results).Select(result => (RedisValue[]) result).ToArray();
+        }
+
+        public static async Task<RedisValue[][]> HashMemberGetManyAsync(
+            this IDatabase cache,
+            string[] keys,
+            params string[] members)
+        {
+            var results = await cache.ScriptEvaluateAsync(
+                HmGetManyScript,
+                keys.Select(key => (RedisKey) key).ToArray(),
+                members.Select(member => (RedisValue) member).ToArray());
+
+            return ((RedisResult[]) results).Select(result => (RedisValue[]) result).ToArray();
+        }
+    }
+}
