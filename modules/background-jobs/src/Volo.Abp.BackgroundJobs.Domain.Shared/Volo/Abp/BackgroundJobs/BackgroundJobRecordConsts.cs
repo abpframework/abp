90958e56ec0670f72bab4c6f364cb90c117fba4f
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-06-29 15:04:28 +0300

    Update BackgroundJobRecordConsts.cs

--- modules/background-jobs/src/Volo.Abp.BackgroundJobs.Domain.Shared/Volo/Abp/BackgroundJobs/BackgroundJobRecordConsts.cs
+++ modules/background-jobs/src/Volo.Abp.BackgroundJobs.Domain.Shared/Volo/Abp/BackgroundJobs/BackgroundJobRecordConsts.cs
@@ -2,8 +2,14 @@
 {
     public static class BackgroundJobRecordConsts
     {
+        /// <summary>
+        /// Default value: 128
+        /// </summary>
         public static int MaxJobNameLength { get; set; } = 128;
 
+        /// <summary>
+        /// Default value: 1024 * 1024
+        /// </summary>
         public static int MaxJobArgsLength { get; set; } = 1024 * 1024;
     }
 }
