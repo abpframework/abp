47d180cf8e7b54ffb1524f62ebd087ea19b5a7a0
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-07-01 12:36:11 +0300

    Blogging module - Used GenerateEmbeddedFiles option

--- modules/blogging/src/Volo.Blogging.Admin.Application.Contracts/Volo/Blogging/Admin/BloggingAdminApplicationContractsModule.cs
+++ modules/blogging/src/Volo.Blogging.Admin.Application.Contracts/Volo/Blogging/Admin/BloggingAdminApplicationContractsModule.cs
@@ -14,6 +14,7 @@ namespace Volo.Blogging.Admin
             {
                 options.FileSets.AddEmbedded<BloggingAdminApplicationContractsModule>();
             });
+            
             Configure<AbpLocalizationOptions>(options =>
             {
                 options.Resources
