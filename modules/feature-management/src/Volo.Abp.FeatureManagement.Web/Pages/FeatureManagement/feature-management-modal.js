d8dd4f7468cd53c946ff85a38a0045a6170c68ea
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-07-09 14:52:16 +0300

    Feature Management - prettier settings added and js files reformatted

--- modules/feature-management/src/Volo.Abp.FeatureManagement.Web/Pages/FeatureManagement/feature-management-modal.js
+++ modules/feature-management/src/Volo.Abp.FeatureManagement.Web/Pages/FeatureManagement/feature-management-modal.js
@@ -1,17 +1,14 @@
 ﻿var abp = abp || {};
 (function ($) {
-    abp.modals = abp.modals || {};
+	abp.modals = abp.modals || {};
 
-    abp.modals.FeatureManagement = function () {
-
-        $('.FeatureValueCheckbox').change(function () {
-            if (this.checked) {
-                $(this).val("true");
-            }
-            else {
-                $(this).val("false");
-            }
-        });
-
-    };
+	abp.modals.FeatureManagement = function () {
+		$('.FeatureValueCheckbox').change(function () {
+			if (this.checked) {
+				$(this).val('true');
+			} else {
+				$(this).val('false');
+			}
+		});
+	};
 })(jQuery);
