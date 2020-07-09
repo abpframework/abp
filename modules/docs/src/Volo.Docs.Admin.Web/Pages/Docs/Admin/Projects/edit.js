2c36e51ff3a87e4e6294e7cd16a7ff3b50732e26
Ahmet Çotur <ahmet.cotur@hotmail.com>
2020-07-09 14:51:53 +0300

    Docs Module - prettier settings added and js files reformatted

--- modules/docs/src/Volo.Docs.Admin.Web/Pages/Docs/Admin/Projects/edit.js
+++ modules/docs/src/Volo.Docs.Admin.Web/Pages/Docs/Admin/Projects/edit.js
@@ -1,14 +1,12 @@
 ﻿var abp = abp || {};
 $(function () {
-    abp.modals.projectEdit = function () {
-        var initModal = function (publicApi, args) {
-            var $form = publicApi.getForm();
+	abp.modals.projectEdit = function () {
+		var initModal = function (publicApi, args) {
+			var $form = publicApi.getForm();
+		};
 
-
-        };
-
-        return {
-            initModal: initModal
-        }
-    };
+		return {
+			initModal: initModal,
+		};
+	};
 });
