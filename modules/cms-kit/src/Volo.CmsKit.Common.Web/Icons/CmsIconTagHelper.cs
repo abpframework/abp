a7ddabac05dec6d24272ec8d378409884404d811
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-15 16:43:22 +0300

    Fix CmsIconTagHelper.

--- modules/cms-kit/src/Volo.CmsKit.Common.Web/Icons/CmsIconTagHelper.cs
+++ modules/cms-kit/src/Volo.CmsKit.Common.Web/Icons/CmsIconTagHelper.cs
@@ -3,7 +3,7 @@ using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers;
 
 namespace Volo.CmsKit.Web.Icons
 {
-    [HtmlTargetElement(TagStructure = TagStructure.WithoutEndTag)]
+    [HtmlTargetElement("cms-icon",TagStructure = TagStructure.WithoutEndTag)]
     public class CmsIconTagHelper : AbpTagHelper<CmsIconTagHelper, CmsIconTagHelperService>
     {
         public string Name { get; set; }
