37453bf0788fa29dd5d22253825a3805e314b670
nuck.tang <nuck.tang@ctsmed.com>
2020-07-15 10:30:45 +0800

    Hiding MinioBlobContainer_Tests code

--- framework/test/Volo.Abp.BlobStoring.Minio.Tests/Volo/Abp/BlobStoring/Minio/MinioBlobContainer_Tests.cs
+++ framework/test/Volo.Abp.BlobStoring.Minio.Tests/Volo/Abp/BlobStoring/Minio/MinioBlobContainer_Tests.cs
@@ -2,9 +2,8 @@
 
 namespace Volo.Abp.BlobStoring.Minio
 {
-     
+     /*
     //Please set the correct connection string in secrets.json and continue the test.
-
     public class MinioBlobContainer_Tests : BlobContainer_Tests<AbpBlobStoringMinioTestModule>
     {
         public MinioBlobContainer_Tests()
@@ -13,5 +12,5 @@ namespace Volo.Abp.BlobStoring.Minio
 
         }
     }
-    
+    */
 }
