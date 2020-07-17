0bce76cc341020c4c1004ef20b345af35844b539
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-17 22:13:25 +0300

    Show popover to react.

--- modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/ReactionSelection/default.js
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/ReactionSelection/default.js
@@ -1,19 +1,30 @@
 ﻿(function ($) {
+
+    var l = abp.localization.getResource('CmsKit');
+
+    var myDefaultWhiteList = $.fn.tooltip.Constructor.Default.whiteList;
+
+    if (myDefaultWhiteList.img.indexOf('data-name') < 0) {
+        myDefaultWhiteList['img'].push('data-name');
+    }
+
     $(document).ready(function () {
 
         abp.widgets.CmsReactionSelection = function ($widget) {
-            var $reactionSelection = $widget.find('.cms-reaction-selection');
             var widgetManager = $widget.data('abp-widget-manager');
+            var $reactionArea = $widget.find('.cms-reaction-area');
+            var $selectIcon = $widget.find('.cms-reaction-select-icon');
+            var $popoverContent = $widget.find('.cms-reaction-selection-popover-content');
 
             function getFilters() {
                 return {
-                    entityType: $reactionSelection.attr('data-entity-type'),
-                    entityId: $reactionSelection.attr('data-entity-id')
+                    entityType: $reactionArea.attr('data-entity-type'),
+                    entityId: $reactionArea.attr('data-entity-id')
                 };
             }
 
-            function init() {
-                $widget.find('.cms-reaction-icon').each(function () {
+            function registerClickOfReactionIcons($container) {
+                $container.find('.cms-reaction-icon').each(function () {
                     var $icon = $(this);
                     $icon.click(function () {
                         var methodName = $icon.hasClass('cms-reaction-icon-selected') ? 'delete' : 'create';
@@ -22,10 +33,26 @@
                                 reactionName: $icon.attr('data-name')
                             })
                         ).then(function () {
+                            $selectIcon.popover('hide');
                             widgetManager.refresh($widget);
                         });
                     });
                 });
+            }
+
+            function init() {
+
+                $selectIcon.popover({
+                    placement: 'right',
+                    html: true,
+                    title: l('PickYourReaction'),
+                    content: $popoverContent.html()
+                }).on('shown.bs.popover', function () {
+                    var $popover = $('#' + $selectIcon.attr('aria-describedby'));
+                    registerClickOfReactionIcons($popover);
+                });
+
+                registerClickOfReactionIcons($widget);
             }
 
             return {
@@ -34,12 +61,13 @@
             };
         };
 
-        $('.abp-widget-wrapper[data-widget-name="CmsReactionSelection"]').each(function () {
-            var widgetManager = new abp.WidgetManager({
-                wrapper: $(this),
-            });
+        $('.abp-widget-wrapper[data-widget-name="CmsReactionSelection"]')
+            .each(function () {
+                var widgetManager = new abp.WidgetManager({
+                    wrapper: $(this),
+                });
 
-            widgetManager.init();
-        });
+                widgetManager.init();
+            });
     });
 })(jQuery);
