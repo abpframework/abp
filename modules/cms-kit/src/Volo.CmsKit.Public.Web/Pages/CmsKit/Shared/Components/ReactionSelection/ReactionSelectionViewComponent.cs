849f7377995a80f1ad26f56d6ce07a5ffa585e1c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-20 00:00:43 +0300

    Move ReactionSelectionViewModel and ReactionViewModel into ReactionSelectionViewComponent as nested classes.

--- modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/ReactionSelection/ReactionSelectionViewComponent.cs
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/ReactionSelection/ReactionSelectionViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Volo.Abp.AspNetCore.Mvc;
@@ -55,6 +56,31 @@ namespace Volo.CmsKit.Web.Pages.CmsKit.Shared.Components.ReactionSelection
             }
 
             return View("~/Pages/CmsKit/Shared/Components/ReactionSelection/Default.cshtml", viewModel);
+        }
+
+        public class ReactionSelectionViewModel
+        {
+            public string EntityType { get; set; }
+
+            public string EntityId { get; set; }
+
+            public List<ReactionViewModel> Reactions { get; set; }
+        }
+
+        public class ReactionViewModel
+        {
+            [NotNull]
+            public string Name { get; set; }
+
+            [CanBeNull]
+            public string DisplayName { get; set; }
+
+            [NotNull]
+            public string Icon { get; set; }
+
+            public int Count { get; set; }
+
+            public bool IsSelectedByCurrentUser { get; set; }
         }
     }
 }
