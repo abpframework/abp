c2b1cb9c5b0417b7471bf44195581c041359b663
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-07-26 17:49:12 +0300

    Revert #4630: Remove AutoMapper to the Application.Contracts project.

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application.Contracts/MyProjectNameApplicationContractsModule.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application.Contracts/MyProjectNameApplicationContractsModule.cs
@@ -1,5 +1,4 @@
 ﻿using Volo.Abp.Account;
-using Volo.Abp.AutoMapper;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
@@ -16,22 +15,13 @@ namespace MyCompanyName.MyProjectName
         typeof(AbpIdentityApplicationContractsModule),
         typeof(AbpPermissionManagementApplicationContractsModule),
         typeof(AbpTenantManagementApplicationContractsModule),
-        typeof(AbpObjectExtendingModule),
-        typeof(AbpAutoMapperModule)
+        typeof(AbpObjectExtendingModule)
     )]
     public class MyProjectNameApplicationContractsModule : AbpModule
     {
         public override void PreConfigureServices(ServiceConfigurationContext context)
         {
             MyProjectNameDtoExtensions.Configure();
-        }
-        
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            Configure<AbpAutoMapperOptions>(options =>
-            {
-                options.AddMaps<MyProjectNameApplicationContractsModule>();
-            });
         }
     }
 }
