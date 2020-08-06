c232b8d761136c8efc6bac893ac57455934e72d7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-08-06 14:18:32 +0300

    Create and register AbpSignInManager.

---
+++ modules/identity/src/Volo.Abp.Identity.AspNetCore/Volo/Abp/Identity/AspNetCore/AbpSignInManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Volo.Abp.Identity.AspNetCore
+{
+    public class AbpSignInManager : SignInManager<IdentityUser>
+    {
+        public AbpSignInManager(
+            UserManager<IdentityUser> userManager,
+            IHttpContextAccessor contextAccessor,
+            IUserClaimsPrincipalFactory<IdentityUser> claimsFactory,
+            IOptions<IdentityOptions> optionsAccessor,
+            ILogger<SignInManager<IdentityUser>> logger,
+            IAuthenticationSchemeProvider schemes,
+            IUserConfirmation<IdentityUser> confirmation
+        ) : base(
+            userManager,
+            contextAccessor,
+            claimsFactory,
+            optionsAccessor,
+            logger,
+            schemes,
+            confirmation)
+        {
+            
+        }
+
+        public override Task<SignInResult> PasswordSignInAsync(
+            string userName,
+            string password,
+            bool isPersistent,
+            bool lockoutOnFailure)
+        {
+            return base.PasswordSignInAsync(userName, password, isPersistent, lockoutOnFailure);
+        }
+    }
+}
