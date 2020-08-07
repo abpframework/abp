beef6af3a1d98dac178a3a6266361e9f90da2ab0
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2020-08-07 10:53:18 +0300

    Cmskit mongodb support

--- modules/cms-kit/src/Volo.CmsKit.MongoDB/Volo/CmsKit/MongoDB/CmsKitMongoDbModule.cs
+++ modules/cms-kit/src/Volo.CmsKit.MongoDB/Volo/CmsKit/MongoDB/CmsKitMongoDbModule.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Modularity;
 using Volo.Abp.MongoDB;
+using Volo.Abp.Users.MongoDB;
+using Volo.CmsKit.Comments;
+using Volo.CmsKit.MongoDB.Comments;
+using Volo.CmsKit.MongoDB.Reactions;
+using Volo.CmsKit.MongoDB.Users;
+using Volo.CmsKit.Reactions;
+using Volo.CmsKit.Users;
 
 namespace Volo.CmsKit.MongoDB
 {
     [DependsOn(
         typeof(CmsKitDomainModule),
+        typeof(AbpUsersMongoDbModule),
         typeof(AbpMongoDbModule)
         )]
     public class CmsKitMongoDbModule : AbpModule
@@ -14,6 +22,9 @@ namespace Volo.CmsKit.MongoDB
         {
             context.Services.AddMongoDbContext<CmsKitMongoDbContext>(options =>
             {
+                options.AddRepository<UserReaction, MongoUserReactionRepository>();
+                options.AddRepository<Comment, MongoCommentRepository>();
+                options.AddRepository<CmsUser, MongoCmsUserRepository>();
                 /* Add custom repositories here. Example:
                  * options.AddRepository<Question, MongoQuestionRepository>();
                  */
