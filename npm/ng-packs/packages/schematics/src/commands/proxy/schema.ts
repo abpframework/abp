e00b09b445b73394e41b0a4419b8f808ee6d75da
Arman Ozak <arman@armanozak.com>
2020-08-12 14:11:17 +0300

    feat: add project parameter to proxy command

--- npm/ng-packs/packages/schematics/src/commands/proxy/schema.ts
+++ npm/ng-packs/packages/schematics/src/commands/proxy/schema.ts
@@ -1,16 +1,21 @@
 export interface Schema {
   /**
-   * The URL to get API configuration from
-   */
-  apiUrl?: string;
-
-  /**
    * The name of the module to generate code for
    */
   module?: string;
 
   /**
+   * The project to place the generated code in
+   */
+  project?: string;
+
+  /**
    * The path to place the generated code at
    */
-  out?: string;
+  path?: string;
+
+  /**
+   * The URL to get API configuration from
+   */
+  source?: string;
 }
