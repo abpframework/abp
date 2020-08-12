e8d5afb3bc3952676a32f31b2e2150638bc58854
Arman Ozak <arman@armanozak.com>
2020-08-12 22:38:28 +0300

    feat: get source json for proxy generation

--- npm/ng-packs/packages/schematics/src/commands/proxy/index.ts
+++ npm/ng-packs/packages/schematics/src/commands/proxy/index.ts
@@ -1,9 +1,15 @@
 import { chain, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { Schema as GenerateProxySchema } from './schema';
+import { getSourceJson, getSourceUrl, resolveProject } from '../../utils';
+import type { Schema as GenerateProxySchema } from './schema';
 
-export default function(_params: GenerateProxySchema) {
+export default function(params: GenerateProxySchema) {
   return chain([
-    async (_tree: Tree, _context: SchematicContext) => {
+    async (tree: Tree, _context: SchematicContext) => {
+      const project = await resolveProject(tree, params.module!);
+      const url = params.source || getSourceUrl(tree, project.definition);
+      const data = await getSourceJson(url);
+
+      console.log(Object.keys(data.types));
       return chain([]);
     },
   ]);
