fcaefac06e0da3cc300b45dad141f2c7fb0607e0
wakuflair <wakuflair@163.com>
2020-08-14 18:02:07 +0800

    Add `MyProjectNameMenus.cs` for the app template

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Web/Menus/MyProjectNameMenuContributor.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Web/Menus/MyProjectNameMenuContributor.cs
@@ -28,7 +28,7 @@ namespace MyCompanyName.MyProjectName.Web.Menus
 
             var l = context.GetLocalizer<MyProjectNameResource>();
 
-            context.Menu.Items.Insert(0, new ApplicationMenuItem("MyProjectName.Home", l["Menu:Home"], "~/"));
+            context.Menu.Items.Insert(0, new ApplicationMenuItem(MyProjectNameMenus.Home, l["Menu:Home"], "~/"));
         }
     }
 }
