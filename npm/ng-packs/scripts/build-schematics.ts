0e780789c9af3700ce9f33d96106461a0c38534c
Arman Ozak <arman@armanozak.com>
2020-08-14 16:37:30 +0300

    build: clear previous schematics build artifacts

--- npm/ng-packs/scripts/build-schematics.ts
+++ npm/ng-packs/scripts/build-schematics.ts
@@ -23,6 +23,7 @@ const FILES_TO_COPY_AFTER_BUILD: (FileCopy | string)[] = [
   { src: 'src/commands/proxy/files-enum', dest: 'commands/proxy/files-enum' },
   { src: 'src/commands/proxy/files-model', dest: 'commands/proxy/files-model' },
   { src: 'src/commands/proxy/files-service', dest: 'commands/proxy/files-service' },
+  { src: 'src/commands/proxy/schema.json', dest: 'commands/proxy/schema.json' },
   { src: 'src/collection.json', dest: 'collection.json' },
   'package.json',
   'README.md',
@@ -44,6 +45,8 @@ async function* copyPackageFiles(packageName: string) {
 }
 
 (async () => {
+  await fse.remove(`../dist/${PACKAGE_TO_BUILD}`);
+
   await execa(
     'tsc',
     ['-p', `packages/${PACKAGE_TO_BUILD}/tsconfig.json`, '--outDir', `dist/${PACKAGE_TO_BUILD}`],
