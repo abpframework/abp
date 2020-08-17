c1807a49ec1938c60cc95d032db6322444c91c82
Arman Ozak <arman@armanozak.com>
2020-08-17 22:59:30 +0300

    feat: add NoApiDefinition exception to schematics

--- npm/ng-packs/packages/schematics/src/enums/exception.ts
+++ npm/ng-packs/packages/schematics/src/enums/exception.ts
@@ -2,6 +2,7 @@ export const enum Exception {
   FileNotFound = '[File Not Found] There is no file at "{0}" path.',
   InvalidWorkspace = '[Invalid Workspace] The angular.json should be a valid JSON file.',
   NoApi = '[API Not Available] Please double-check the URL in the source project environment and make sure your application is up and running.',
+  NoApiDefinition = '[API Definition Not Found] There is no valid API definition file at "{0}".',
   NoProject = '[Project Not Found] Either define a default project in your workspace or specify the project name in schematics options.',
   NoWorkspace = '[Workspace Not Found] Make sure you are running schematics at the root directory of your workspace and it has an angular.json file.',
   NoEnvironment = '[Environment Not Found] An environment file cannot be located in "{0}" project.',
