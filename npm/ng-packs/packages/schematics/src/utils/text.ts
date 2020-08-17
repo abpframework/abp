92484c3a16a47996a74ed507976a0292ffce0e0e
Arman Ozak <arman@armanozak.com>
2020-08-17 22:59:03 +0300

    feat: update schematics text utils

--- npm/ng-packs/packages/schematics/src/utils/text.ts
+++ npm/ng-packs/packages/schematics/src/utils/text.ts
@@ -1,9 +1,15 @@
 import { strings } from '@angular-devkit/core';
 
-export const camel = strings.camelize;
-export const kebab = strings.dasherize;
 export const lower = (text: string) => text.toLowerCase();
-export const macro = (text: string) => strings.underscore(text).toUpperCase();
-export const pascal = strings.classify;
-export const snake = strings.underscore;
 export const upper = (text: string) => text.toUpperCase();
+export const camel = (text: string) => strings.camelize(_(text));
+export const pascal = (text: string) => strings.classify(_(text));
+export const kebab = (text: string) => strings.dasherize(_(text));
+export const snake = (text: string) => strings.underscore(_(text));
+export const macro = (text: string) => upper(snake(text));
+export const dir = (text: string) =>
+  strings.dasherize(text.replace(/\./g, '/').replace(/\/\//g, '/'));
+
+function _(text: string): string {
+  return text.replace(/\./g, '_');
+}
