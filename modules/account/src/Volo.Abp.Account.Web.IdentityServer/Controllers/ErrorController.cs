249e64c660351df09e9fdea7b7cba2d7e891ecc3
liangshiwei <deawback@hotmail.com>
2020-08-17 16:00:00 +0800

    Add Identity service error controller to account module

---
+++ modules/account/src/Volo.Abp.Account.Web.IdentityServer/Controllers/ErrorController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Views.Error;
+using Volo.Abp.Http;
+
+namespace Volo.Abp.Account.Web.Controllers
+{
+    [Area("account")]
+    public class ErrorController : AbpController
+    {
+        private readonly IIdentityServerInteractionService _interaction;
+        private readonly IWebHostEnvironment _environment;
+
+        public ErrorController(
+            IIdentityServerInteractionService interaction,
+            IWebHostEnvironment environment)
+        {
+            _interaction = interaction;
+            _environment = environment;
+        }
+
+        public async Task<IActionResult> Index(string errorId)
+        {
+            var errorMessage = await _interaction.GetErrorContextAsync(errorId) ?? new ErrorMessage
+            {
+                Error = L["Error"]
+            };
+
+            if (!_environment.IsDevelopment())
+            {
+                // Only show in development
+                errorMessage.ErrorDescription = null;
+            }
+
+
+            // ReSharper disable once Mvc.ViewNotResolved
+            return View("~/Views/Error/Default.cshtml", new AbpErrorViewModel
+            {
+                ErrorInfo = new RemoteServiceErrorInfo(errorMessage.Error, errorMessage.ErrorDescription),
+                HttpStatusCode = 500
+            });
+        }
+    }
+}
