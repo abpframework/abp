0e8b9857f1e639a356af8939cf5906c5345a303c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-08-18 20:44:00 +0300

    Resolved #5109: Inherit BackgroundEmailSendingJob form AsyncBackgroundJob.

--- framework/src/Volo.Abp.Emailing/Volo/Abp/Emailing/BackgroundEmailSendingJobArgs.cs
+++ framework/src/Volo.Abp.Emailing/Volo/Abp/Emailing/BackgroundEmailSendingJobArgs.cs
@@ -5,12 +5,14 @@ namespace Volo.Abp.Emailing
     [Serializable]
     public class BackgroundEmailSendingJobArgs
     {
+        public string From { get; set; }
+
         public string To { get; set; }
 
         public string Subject { get; set; }
 
         public string Body { get; set; }
-        
+
         /// <summary>
         /// Default: true.
         /// </summary>
