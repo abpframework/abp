d50f9a72c575b28b9240a6360811d852f2e928db
EngincanV <enginveske@gmail.com>
2020-08-24 14:21:09 +0300

    Rating widget created

---
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/Rating/RatingViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace Volo.CmsKit.Public.Web.Pages.CmsKit.Shared.Components.Rating
+{
+    [ViewComponent(Name = "CmsRating")]
+    [Widget(
+        StyleTypes = new[] {typeof(RatingStyleBundleContributor)},
+        ScriptTypes = new[] {typeof(RatingScriptBundleContributor)},
+        RefreshUrl = "/CmsKitPublicWidgets/Rating"
+    )]
+    public class RatingViewComponent : AbpViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Pages/CmsKit/Shared/Components/Rating/Default.cshtml");
+        }
+    }
+}
