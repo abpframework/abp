6afc5ca2e71cbeabfdd619d866f13d57887ce9d7
maliming <6908465+maliming@users.noreply.github.com>
2020-08-25 11:29:12 +0800

    Add Identity Two-factor features and settings.

---
+++ modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/IdentitySettingProviderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp.Identity.Features;
+using Volo.Abp.Identity.Settings;
+using Volo.Abp.Settings;
+
+namespace Volo.Abp.Identity
+{
+    public static class IdentitySettingProviderExtensions
+    {
+        public static async Task<IdentityTwoFactorBehaviour> GetIdentityTwoFactorBehaviour([NotNull] this ISettingProvider settingProvider)
+        {
+            Check.NotNull(settingProvider, nameof(settingProvider));
+
+            var value = await settingProvider.GetOrNullAsync(IdentitySettingNames.TwoFactor.Behaviour);
+            if (value.IsNullOrWhiteSpace() || !Enum.TryParse<IdentityTwoFactorBehaviour>(value, out var behaviour))
+            {
+                throw new AbpException($"{IdentitySettingNames.TwoFactor.Behaviour} setting value is invalid");
+            }
+
+            return behaviour;
+        }
+    }
+}
