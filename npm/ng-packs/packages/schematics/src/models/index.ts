012137a4853bfb8f04468bcdec82ae15d0590712
Arman Ozak <arman@armanozak.com>
2020-08-28 00:47:41 +0300

    feat: add proxy config interface to schematics

--- npm/ng-packs/packages/schematics/src/models/index.ts
+++ npm/ng-packs/packages/schematics/src/models/index.ts
@@ -3,5 +3,6 @@ export * from './import';
 export * from './method';
 export * from './model';
 export * from './project';
+export * from './proxy-config';
 export * from './service';
 export * from './util';
