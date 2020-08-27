f4ad9b9eab9086649a3b13503fab9182efa0f81f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-08-27 16:22:23 +0300

    Add prompt parameter to the GenerateProxyCommand

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/GenerateProxyCommand.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/GenerateProxyCommand.cs
@@ -16,11 +16,31 @@ namespace Volo.Abp.Cli.Commands
             CheckAngularJsonFile();
             CheckNgSchematics();
 
-            var module = commandLineArgs.Options.GetOrNull(Options.Module.Short, Options.Module.Long) ?? "__default";
-            var source = commandLineArgs.Options.GetOrNull(Options.Source.Short, Options.Source.Long) ?? "__default";
-            var target = commandLineArgs.Options.GetOrNull(Options.Target.Short, Options.Target.Long) ?? "__default";
+            var prompt = commandLineArgs.Options.ContainsKey("p") || commandLineArgs.Options.ContainsKey("prompt");
+            var defaultValue = prompt ? null : "__default";
 
-            CmdHelper.RunCmd($"npx ng g @abp/ng.schematics:proxy --module {module} --source {source} --target {target}");
+            var module = commandLineArgs.Options.GetOrNull(Options.Module.Short, Options.Module.Long) ?? defaultValue;
+            var source = commandLineArgs.Options.GetOrNull(Options.Source.Short, Options.Source.Long) ?? defaultValue;
+            var target = commandLineArgs.Options.GetOrNull(Options.Target.Short, Options.Target.Long) ?? defaultValue;
+
+            var commandBuilder = new StringBuilder("npx ng g @abp/ng.schematics:proxy");
+
+            if (module != null)
+            {
+                commandBuilder.Append($" --module {module}");
+            }
+
+            if (source != null)
+            {
+                commandBuilder.Append($" --source {source}");
+            }
+
+            if (target != null)
+            {
+                commandBuilder.Append($" --target {target}");
+            }
+
+            CmdHelper.RunCmd(commandBuilder.ToString());
 
             return Task.CompletedTask;
         }
@@ -73,9 +93,12 @@ namespace Volo.Abp.Cli.Commands
             sb.AppendLine("");
             sb.AppendLine("  abp generate-proxy");
             sb.AppendLine("");
-            sb.AppendLine("Examples:");
+            sb.AppendLine("Options:");
             sb.AppendLine("");
-            sb.AppendLine("  abp generate-proxy");
+            sb.AppendLine("-m|--module <module-name>          (default: 'app') The name of the backend module you wish to generate proxies for.");
+            sb.AppendLine("-s|--source <source-name>          (default: 'defaultProject') Angular project name to resolve the root namespace & API definition URL from.");
+            sb.AppendLine("-t|--target <target-name>          (default: 'defaultProject') Angular project name to place generated code in.");
+            sb.AppendLine("-p|--prompt                        Asks the options from the command line prompt (for the missing options)");
             sb.AppendLine("");
             sb.AppendLine("See the documentation for more info: https://docs.abp.io/en/abp/latest/CLI");
 
@@ -105,6 +128,12 @@ namespace Volo.Abp.Cli.Commands
             {
                 public const string Short = "t";
                 public const string Long = "target";
+            }
+
+            public static class Prompt
+            {
+                public const string Short = "p";
+                public const string Long = "prompt";
             }
         }
     }
