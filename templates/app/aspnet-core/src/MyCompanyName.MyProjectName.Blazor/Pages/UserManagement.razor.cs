9228e72650fc99f4a11dca67fe52915e815b35ad
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-08-30 17:09:16 +0300

    Use codebehind class for the the user management component.

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor/Pages/UserManagement.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Volo.Abp.Identity;
+
+namespace MyCompanyName.MyProjectName.Blazor.Pages
+{
+    public partial class UserManagement : ComponentBase
+    {
+        private long _totalUserCount;
+        private IReadOnlyList<IdentityUserDto> _users;
+        
+        private async Task GetUsersAsync()
+        {
+            var result = await UserAppService.GetListAsync(new GetIdentityUsersInput());
+            _totalUserCount = result.TotalCount;
+            _users = result.Items;
+        }
+    }
+}
