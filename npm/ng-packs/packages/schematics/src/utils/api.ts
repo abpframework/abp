97027b170163876a550af3bcba105fdadeda26ec
Arman Ozak <arman@armanozak.com>
2020-08-31 15:39:42 +0300

    fix: remove source param overwrite in createApisGenerator

--- npm/ng-packs/packages/schematics/src/utils/api.ts
+++ npm/ng-packs/packages/schematics/src/utils/api.ts
@@ -2,10 +2,5 @@ import { chain, schematic } from '@angular-devkit/schematics';
 import { GenerateProxySchema } from '../models';
 
 export function createApisGenerator(schema: GenerateProxySchema, generated: string[]) {
-  return chain(
-    generated.map(m => {
-      const apiSchema = { ...schema, source: schema.target, module: m };
-      return schematic('api', apiSchema);
-    }),
-  );
+  return chain(generated.map(m => schematic('api', { ...schema, module: m })));
 }
