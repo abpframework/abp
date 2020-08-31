49ccc4dbde3c47c22a66df749f14ab02aeaf5f95
Arman Ozak <arman@armanozak.com>
2020-08-31 21:56:44 +0300

    docs: update proxy folder warning

--- npm/ng-packs/packages/schematics/src/constants/proxy.ts
+++ npm/ng-packs/packages/schematics/src/constants/proxy.ts
@@ -5,14 +5,14 @@ export const PROXY_WARNING_PATH = `${PROXY_PATH}/README.md`;
 export const PROXY_WARNING = `# Proxy Generation Output
 
 This directory includes the output of the latest proxy generation.
-The \`services\`, \`models\`, and \`enums\` folders will be overwritten when proxy generation is run again.
-Therefore, please do not place your own content in those folders.
+The files and folders in it will be overwritten when proxy generation is run again.
+Therefore, please do not place your own content in this folder.
 
 In addition, \`generate-proxy.json\` works like a lock file.
 It includes information used by the proxy generator, so please do not delete or modify it.
 
-Finally, the name of this folder should not be changed for two reasons:
-- Proxy generator will keep creating this folder and you will have multiple copies of the same content.
-- ABP Suite generates imports from this folder and uses the path \`/proxy\` when doing so.
+Finally, the name of the files and folders should not be changed for two reasons:
+- Proxy generator will keep creating them at those paths and you will have multiple copies of the same content.
+- ABP Suite generates files which include imports from this folder.
 
 `;
