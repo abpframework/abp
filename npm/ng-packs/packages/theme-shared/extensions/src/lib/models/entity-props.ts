284214ae03c4f0dcd449a8d3c1c19d644c07f49b
bnymncoskuner <bnymncoskuner@gmail.com>
2020-09-03 16:34:53 +0300

    feat: remove actionProvided

--- npm/ng-packs/packages/theme-shared/extensions/src/lib/models/entity-props.ts
+++ npm/ng-packs/packages/theme-shared/extensions/src/lib/models/entity-props.ts
@@ -28,7 +28,6 @@ export class EntityProp<R = any> extends Prop<R> {
   readonly sortable: boolean;
   readonly valueResolver: PropCallback<R, Observable<any>>;
   readonly action: ActionCallback<R>;
-  readonly actionProvided: boolean;
 
   constructor(options: EntityPropOptions<R>) {
     super(
@@ -43,8 +42,7 @@ export class EntityProp<R = any> extends Prop<R> {
     this.columnWidth = options.columnWidth;
     this.sortable = options.sortable || false;
     this.valueResolver = options.valueResolver || (data => of(data.record[this.name]));
-    this.action = options.action || (_ => {});
-    this.actionProvided = !!options.action;
+    this.action = options.action;
   }
 
   static create<R = any>(options: EntityPropOptions<R>) {
