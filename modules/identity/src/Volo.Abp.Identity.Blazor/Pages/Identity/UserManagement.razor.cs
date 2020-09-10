96631c1eef3e4e9f0bdbf00b1d13a38189330da9
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-09-10 17:05:51 +0300

    Created empty user management page.

---
+++ modules/identity/src/Volo.Abp.Identity.Blazor/Pages/Identity/UserManagement.razor.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.PermissionManagement.Blazor.Components;
+
+namespace Volo.Abp.Identity.Blazor.Pages.Identity
+{
+    public partial class UserManagement
+    {
+        private const string PermissionProviderName = "U";
+
+        private PermissionManagementModal PermissionManagementModal;
+
+        public UserManagement()
+        {
+            ObjectMapperContext = typeof(AbpIdentityBlazorModule);
+        }
+    }
+}
