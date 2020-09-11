950040d7a80b0daaf36f39ec99ac723f36487cf0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-09-11 18:00:18 +0300

    Introduce IBrandingProvider in the Volo.Abp.UI package

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor/BrandingProvider.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Ui.Branding;
+
+namespace MyCompanyName.MyProjectName.Blazor
+{
+    public class BrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "MyProjectName";
+    }
+}
