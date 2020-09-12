d5c7012215feb325a8b9180bd1e9de72518041f7
mehmet-erim <bilgi@mehmeterim.com>
2020-09-12 10:27:47 +0300

    fix: lint erorrs

--- npm/ng-packs/packages/feature-management/src/lib/proxy/feature-management/features.service.ts
+++ npm/ng-packs/packages/feature-management/src/lib/proxy/feature-management/features.service.ts
@@ -11,16 +11,16 @@ export class FeaturesService {
   get = (providerName: string, providerKey: string) =>
     this.restService.request<any, GetFeatureListResultDto>({
       method: 'GET',
-      url: `/api/feature-management/features`,
-      params: { providerName: providerName, providerKey: providerKey },
+      url: '/api/feature-management/features',
+      params: { providerName, providerKey },
     },
     { apiName: this.apiName });
 
   update = (providerName: string, providerKey: string, input: UpdateFeaturesDto) =>
     this.restService.request<any, void>({
       method: 'PUT',
-      url: `/api/feature-management/features`,
-      params: { providerName: providerName, providerKey: providerKey },
+      url: '/api/feature-management/features',
+      params: { providerName, providerKey },
       body: input,
     },
     { apiName: this.apiName });
