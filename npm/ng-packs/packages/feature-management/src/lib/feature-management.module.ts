379605c20efb3559712af634fd1745c3285409ba
mehmet-erim <bilgi@mehmeterim.com>
2020-09-14 10:46:37 +0300

    fix: build error

--- npm/ng-packs/packages/feature-management/src/lib/feature-management.module.ts
+++ npm/ng-packs/packages/feature-management/src/lib/feature-management.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { FeatureManagementComponent } from './components/feature-management/feature-management.component';
 import { NgxsModule } from '@ngxs/store';
 import { FeatureManagementState } from './states/feature-management.state';
-import { TreeModule } from '@abp/ng.components/tree';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -12,7 +11,6 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     CoreModule,
     ThemeSharedModule,
-    TreeModule,
     NgbNavModule,
     NgxsModule.forFeature([FeatureManagementState]),
   ],
