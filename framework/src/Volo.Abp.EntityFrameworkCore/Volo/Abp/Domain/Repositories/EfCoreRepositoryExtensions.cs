b4a3279ab5f0917c42c27417b429831adbdba7ed
emrekara37 <emre.kara5@ogr.sakarya.edu.tr>
2020-09-16 13:30:00 +0300

    GetDbContext and GetDbSet implementation for IRepository

--- framework/src/Volo.Abp.EntityFrameworkCore/Volo/Abp/Domain/Repositories/EfCoreRepositoryExtensions.cs
+++ framework/src/Volo.Abp.EntityFrameworkCore/Volo/Abp/Domain/Repositories/EfCoreRepositoryExtensions.cs
@@ -5,8 +5,6 @@ using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
 
 namespace Volo.Abp.Domain.Repositories
 {
-    //TODO: Should work for any IRepository implementation
-
     public static class EfCoreRepositoryExtensions
     {
         public static DbContext GetDbContext<TEntity, TKey>(this IReadOnlyBasicRepository<TEntity, TKey> repository)
@@ -14,22 +12,39 @@ namespace Volo.Abp.Domain.Repositories
         {
             return repository.ToEfCoreRepository().DbContext;
         }
-
+        public static DbContext GetDbContext<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
+            where TEntity : class, IEntity
+        {
+            return repository.ToEfCoreRepository().DbContext;
+        }
         public static DbSet<TEntity> GetDbSet<TEntity, TKey>(this IReadOnlyBasicRepository<TEntity, TKey> repository)
             where TEntity : class, IEntity<TKey>
         {
             return repository.ToEfCoreRepository().DbSet;
         }
-
+        public static DbSet<TEntity> GetDbSet<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
+            where TEntity : class, IEntity
+        {
+            return repository.ToEfCoreRepository().DbSet;
+        }
         public static IEfCoreRepository<TEntity, TKey> ToEfCoreRepository<TEntity, TKey>(this IReadOnlyBasicRepository<TEntity, TKey> repository)
             where TEntity : class, IEntity<TKey>
         {
-            var efCoreRepository = repository as IEfCoreRepository<TEntity, TKey>;
-            if (efCoreRepository == null)
+            if (!(repository is IEfCoreRepository<TEntity, TKey> efCoreRepository))
             {
                 throw new ArgumentException("Given repository does not implement " + typeof(IEfCoreRepository<TEntity, TKey>).AssemblyQualifiedName, nameof(repository));
             }
 
+            return efCoreRepository;
+        }
+        public static IEfCoreRepository<TEntity> ToEfCoreRepository<TEntity>(
+            this IReadOnlyBasicRepository<TEntity> repository)
+            where TEntity : class, IEntity
+        {
+            if (!(repository is IEfCoreRepository<TEntity> efCoreRepository))
+            {
+                throw new ArgumentException("Given repository does not implement " + typeof(IEfCoreRepository<TEntity>).AssemblyQualifiedName, nameof(repository));
+            }
             return efCoreRepository;
         }
     }
