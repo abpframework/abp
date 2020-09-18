37514cef5a866b128c194b7efaba99715a8a5338
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-09-18 09:53:34 +0300

    Example implementation for the repository async extensions.

---
+++ framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Repositories/RepositoryAsyncExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp.Domain.Entities;
+
+namespace Volo.Abp.Domain.Repositories
+{
+    public static class RepositoryAsyncExtensions
+    {
+        #region Count/LongCount
+
+        public static Task<int> CountAsync<T>(
+            [NotNull] this IReadOnlyRepository<T> repository,
+            CancellationToken cancellationToken = default)
+            where T : class, IEntity
+        {
+            return repository.AsyncExecuter.CountAsync(
+                repository,
+                cancellationToken
+            );
+        }
+
+        public static Task<int> CountAsync<T>(
+            [NotNull] this IReadOnlyRepository<T> repository,
+            [NotNull] Expression<Func<T, bool>> predicate,
+            CancellationToken cancellationToken = default)
+            where T : class, IEntity
+        {
+            return repository.AsyncExecuter.CountAsync(
+                repository,
+                predicate,
+                cancellationToken
+            );
+        }
+
+        #endregion
+    }
+}
