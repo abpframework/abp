29f5662694cacc162f929eb5cd0cdeff90c187aa
Ahmet <ahmet.cotur@hotmail.com>
2020-09-24 13:29:06 +0300

    Added empty TenantManagement Blazor project

---
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.Blazor/AbpTenantManagementBlazorModule.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyCompanyName.MyProjectName.Blazor;
+using MyCompanyName.MyProjectName.Blazor.Menus;
+using Volo.Abp.AspNetCore.Components.WebAssembly.Theming.Routing;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.Modularity;
+using Volo.Abp.UI.Navigation;
+
+namespace Volo.Abp.TenantManagement.Blazor
+{
+    [DependsOn(
+        typeof(AbpAutoMapperModule),
+        typeof(AbpTenantManagementHttpApiClientModule)
+        )]
+    public class AbpTenantManagementBlazorModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAutoMapperObjectMapper<AbpTenantManagementBlazorModule>();
+
+            Configure<AbpAutoMapperOptions>(options =>
+            {
+                options.AddProfile<AbpTenantManagementBlazorAutoMapperProfile>(validate: true);
+            });
+
+            Configure<AbpNavigationOptions>(options =>
+            {
+                options.MenuContributors.Add(new MyProjectNameMenuContributor());
+            });
+
+            Configure<AbpRouterOptions>(options =>
+            {
+                options.AdditionalAssemblies.Add(typeof(AbpTenantManagementBlazorModule).Assembly);
+            });
+        }
+    }
+}
---
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.Blazor/Menus/MyProjectNameMenus.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCompanyName.MyProjectName.Blazor.Menus
+{
+    public class MyProjectNameMenus
+    {
+        private const string Prefix = "MyProjectName";
+
+        //Add your menu items here...
+        //public const string Home = Prefix + ".MyNewMenuItem";
+
+    }
+}
