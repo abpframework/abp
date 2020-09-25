0178b6c324e86c5ece4a1aeb460c53e1c9a1096f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-09-25 11:36:04 +0300

    Added IAbpUtilsService

---
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/IAbpUtilsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Volo.Abp.AspNetCore.Components.WebAssembly.Theming
+{
+    public interface IAbpUtilsService
+    {
+        ValueTask AddClassToTagAsync(string tagName, string className);
+        ValueTask RemoveClassFromTagAsync(string tagName, string className);
+        ValueTask<bool> HasClassOnTag(string tagName, string className);
+    }
+}
