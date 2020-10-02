59688af816f54968742f15426247b8ea3b2e10f5
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-02 10:55:13 +0300

    Created empty IUiNotificationService for Blazor

---
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/IUiNotificationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Volo.Abp.AspNetCore.Components.WebAssembly
+{
+    public interface IUiNotificationService
+    {
+        Task Info(string message);
+    }
+}
---
+++ framework/src/Volo.Abp.BlazoriseUI/Volo/Abp/BlazoriseUI/BlazoriseUiNotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Components.WebAssembly;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.BlazoriseUI
+{
+    [Dependency(ReplaceServices = true)]
+    public class BlazoriseUiNotificationService : IUiNotificationService, ITransientDependency
+    {
+        public Task Info(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
