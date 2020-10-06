3c00231d3fb16fdbf07b233000611eae1ed31056
İlkay İlknur <ilkay@ilkayilknur.com>
2020-10-06 12:08:39 +0300

    additional methods added to the UINotificationService.

--- framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/IUiNotificationService.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/IUiNotificationService.cs
@@ -5,5 +5,8 @@ namespace Volo.Abp.AspNetCore.Components.WebAssembly
     public interface IUiNotificationService
     {
         Task Info(string message);
+        Task Success(string message);
+        Task Warn(string message);
+        Task Error(string message);
     }
 }
