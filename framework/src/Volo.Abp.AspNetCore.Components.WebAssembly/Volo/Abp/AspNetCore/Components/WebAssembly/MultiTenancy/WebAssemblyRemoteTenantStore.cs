29d583743c648a38a372a6f5f10bc19bab4987f8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-14 13:45:39 +0300

    Resolved #5401: Implement caching for WebAssemblyRemoteTenantStore

--- framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/MultiTenancy/WebAssemblyRemoteTenantStore.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/MultiTenancy/WebAssemblyRemoteTenantStore.cs
@@ -6,44 +6,55 @@ using Volo.Abp.Http.Client.DynamicProxying;
 using Volo.Abp.MultiTenancy;
 using Volo.Abp.Threading;
 
-namespace Volo.Abp.AspNetCore.Components.WebAssembly
+namespace Volo.Abp.AspNetCore.Components.WebAssembly.MultiTenancy
 {
     public class WebAssemblyRemoteTenantStore : ITenantStore, ITransientDependency
     {
         protected IHttpClientProxy<IAbpTenantAppService> Proxy { get; }
+        protected WebAssemblyRemoteTenantStoreCache Cache { get; }
 
         public WebAssemblyRemoteTenantStore(
-            IHttpClientProxy<IAbpTenantAppService> proxy)
+            IHttpClientProxy<IAbpTenantAppService> proxy,
+            WebAssemblyRemoteTenantStoreCache cache)
         {
             Proxy = proxy;
+            Cache = cache;
         }
 
         public async Task<TenantConfiguration> FindAsync(string name)
         {
-            //TODO: Cache
+            var cached = Cache.Find(name);
+            if (cached != null)
+            {
+                return cached;
+            }
 
-            return CreateTenantConfiguration(await Proxy.Service.FindTenantByNameAsync(name));
+            var tenant = CreateTenantConfiguration(await Proxy.Service.FindTenantByNameAsync(name));
+            Cache.Set(tenant);
+            return tenant;
         }
 
         public async Task<TenantConfiguration> FindAsync(Guid id)
         {
-            //TODO: Cache
+            var cached = Cache.Find(id);
+            if (cached != null)
+            {
+                return cached;
+            }
 
-            return CreateTenantConfiguration(await Proxy.Service.FindTenantByIdAsync(id));
+            var tenant = CreateTenantConfiguration(await Proxy.Service.FindTenantByIdAsync(id));
+            Cache.Set(tenant);
+            return tenant;
         }
 
         public TenantConfiguration Find(string name)
         {
-            //TODO: Cache
-
-            return AsyncHelper.RunSync(async () => CreateTenantConfiguration(await Proxy.Service.FindTenantByNameAsync(name)));
+            return AsyncHelper.RunSync(() => FindAsync(name));
         }
 
         public TenantConfiguration Find(Guid id)
         {
-            //TODO: Cache
-
-            return AsyncHelper.RunSync(async () => CreateTenantConfiguration(await Proxy.Service.FindTenantByIdAsync(id)));
+            return AsyncHelper.RunSync(() => FindAsync(id));
         }
 
         protected virtual TenantConfiguration CreateTenantConfiguration(FindTenantResultDto tenantResultDto)
@@ -54,16 +65,6 @@ namespace Volo.Abp.AspNetCore.Components.WebAssembly
             }
 
             return new TenantConfiguration(tenantResultDto.TenantId.Value, tenantResultDto.Name);
-        }
-
-        protected virtual string CreateCacheKey(string tenantName)
-        {
-            return $"RemoteTenantStore_Name_{tenantName}";
-        }
-
-        protected virtual string CreateCacheKey(Guid tenantId)
-        {
-            return $"RemoteTenantStore_Id_{tenantId:N}";
         }
     }
 }
