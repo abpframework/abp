40d8bb91dc3762c9f6e3f39354d232f1f93139b3
İlkay İlknur <ilkay@ilkayilknur.com>
2020-10-14 16:57:39 +0300

    ICookieService implementation.

---
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/CookieService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.AspNetCore.Components.WebAssembly
+{
+    [Dependency(ReplaceServices = true)]
+    public class CookieService : ICookieService, ITransientDependency
+    {
+        public IJSRuntime JsRuntime { get; }
+
+        public CookieService(IJSRuntime jsRuntime)
+        {
+            JsRuntime = jsRuntime;
+        }
+
+        public async ValueTask SetAsync(string key, string value, DateTimeOffset? expireDate = null, string path = null)
+        {
+            await JsRuntime.InvokeVoidAsync("abp.utils.setCookieValue", key, value, expireDate?.ToString("r"), path);
+        }
+
+        public async ValueTask<string> GetAsync(string key)
+        {
+            return await JsRuntime.InvokeAsync<string>("abp.utils.getCookieValue", key);
+        }
+
+        public async ValueTask DeleteAsync(string key, string path = null)
+        {
+            await JsRuntime.InvokeVoidAsync("abp.utils.deleteCookie", key);
+        }
+    }
+}
