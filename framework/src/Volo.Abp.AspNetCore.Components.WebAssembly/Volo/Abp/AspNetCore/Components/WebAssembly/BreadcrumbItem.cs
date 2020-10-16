c804c0ab36ed708824b023d7d5705a67f5b2d3f5
İlkay İlknur <ilkay@ilkayilknur.com>
2020-10-16 15:35:49 +0300

    page header initial implementation.

---
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/BreadcrumbItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Volo.Abp.AspNetCore.Components.WebAssembly
+{
+    public class BreadcrumbItem
+    {
+        public string Text { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Url { get; set; }
+
+        public BreadcrumbItem(string text, string url = null, string icon = null)
+        {
+            Text = text;
+            Url = url;
+            Icon = icon;
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.BlazoriseUI/Components/PageHeader.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Volo.Abp.AspNetCore.Components.WebAssembly;
+
+namespace Volo.Abp.BlazoriseUI.Components
+{
+    public partial class PageHeader : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public bool BreadcrumbShowHome { get; set; } = true;
+
+        [Parameter]
+        public bool BreadcrumbShowCurrent { get; set; } = true;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected List<BreadcrumbItem> BreadcrumbItems { get; set; }
+
+        public PageHeader()
+        {
+            BreadcrumbItems = new List<BreadcrumbItem>();
+        }
+
+        public void AddBreadcrumbItem(string text, string url = null, string icon = null)
+        {
+            BreadcrumbItems.Add(new BreadcrumbItem(text, url, icon));
+            StateHasChanged();
+        }
+    }
+}
