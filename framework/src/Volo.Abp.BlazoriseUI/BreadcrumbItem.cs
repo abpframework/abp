a208f7809ea8e34ebeff1182e1f2d8cdc80d79e2
İlkay İlknur <ilkay@ilkayilknur.com>
2020-10-19 21:17:05 +0300

    convert icon property to object so that developer can spesify custom name.

--- framework/src/Volo.Abp.BlazoriseUI/BreadcrumbItem.cs
+++ framework/src/Volo.Abp.BlazoriseUI/BreadcrumbItem.cs
@@ -6,11 +6,11 @@ namespace Volo.Abp.BlazoriseUI
     {
         public string Text { get; set; }
 
-        public IconName? Icon { get; set; }
+        public object Icon { get; set; }
 
         public string Url { get; set; }
 
-        public BreadcrumbItem(string text, string url = null, IconName? icon = null)
+        public BreadcrumbItem(string text, string url = null, object icon = null)
         {
             Text = text;
             Url = url;
