8e37d0c8fcce6f435c56c8de19a4e22471918be7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-20 17:14:04 +0300

    Toggle collapse on blazor ui mobile view

---
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly.BasicTheme/Themes/Basic/MainLayout.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace Volo.Abp.AspNetCore.Components.WebAssembly.BasicTheme.Themes.Basic
+{
+    public partial class MainLayout
+    {
+        private bool IsCollapseShown { get; set; }
+
+        private void ToggleCollapse()
+        {
+            IsCollapseShown = !IsCollapseShown;
+        }
+    }
+}
