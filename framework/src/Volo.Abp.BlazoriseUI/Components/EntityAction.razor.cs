b3ba2e3186e078607ff0f6489022eb20c45cfaa6
İlkay İlknur <ilkay@ilkayilknur.com>
2020-10-21 17:33:54 +0300

    entity actions component implementation.

---
+++ framework/src/Volo.Abp.BlazoriseUI/Components/EntityAction.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Volo.Abp.BlazoriseUI.Components
+{
+    public partial class EntityAction<TItem> : ComponentBase
+    {
+        [Parameter]
+        public bool IsVisible { get; set; }
+        [Parameter]
+        public string Text { get; set; }
+
+        [Parameter]
+        public bool Primary { get; set; }
+
+        [Parameter]
+        public EventCallback<TItem> Clicked { get; set; }
+
+        [CascadingParameter]
+        public EntityActions<TItem> ParentActions { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            ParentActions.AddAction(this);
+        }
+
+        protected virtual async Task ActionClickedAsync()
+        {
+            await Clicked.InvokeAsync(ParentActions.Entity);
+        }
+    }
+}
---
+++ framework/src/Volo.Abp.BlazoriseUI/Components/EntityActions.razor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Blazorise;
+using Microsoft.AspNetCore.Components;
+
+namespace Volo.Abp.BlazoriseUI.Components
+{
+    public partial class EntityActions<TItem> : ComponentBase
+    {
+        protected List<EntityAction<TItem>> Actions = new List<EntityAction<TItem>>();
+        protected bool HasPrimaryAction => Actions.Any(t => t.Primary);
+        protected EntityAction<TItem> PrimaryAction => Actions.FirstOrDefault(t => t.Primary);
+        protected internal ActionType Type => Actions.Count(t => t.IsVisible) > 1 ? ActionType.Dropdown : ActionType.Button;
+
+        [Parameter]
+        public Color ToggleColor { get; set; }
+
+        [Parameter]
+        public string ToggleText { get; set; }
+
+        [Parameter]
+        public TItem Entity { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        internal void AddAction(EntityAction<TItem> action)
+        {
+            Actions.Add(action);
+            StateHasChanged();
+        }
+    }
+
+    public enum ActionType
+    {
+        Dropdown,
+        Button
+    }
+}
