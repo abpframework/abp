9310db485bb4520207318b83eb80d6aa1b0cab7d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-23 14:49:00 +0300

    #5523 Get current user claims from AuthenticationStateProvider

--- framework/src/Volo.Abp.Http.Client.IdentityModel.WebAssembly/Volo/Abp/Http/Client/IdentityModel/WebAssembly/AbpHttpClientIdentityModelWebAssemblyModule.cs
+++ framework/src/Volo.Abp.Http.Client.IdentityModel.WebAssembly/Volo/Abp/Http/Client/IdentityModel/WebAssembly/AbpHttpClientIdentityModelWebAssemblyModule.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using IdentityModel;
 using Volo.Abp.Modularity;
+using Volo.Abp.Security.Claims;
 
 namespace Volo.Abp.Http.Client.IdentityModel.WebAssembly
 {
@@ -8,6 +9,14 @@ namespace Volo.Abp.Http.Client.IdentityModel.WebAssembly
     )]
     public class AbpHttpClientIdentityModelWebAssemblyModule : AbpModule
     {
-
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            AbpClaimTypes.UserName = JwtClaimTypes.PreferredUserName;
+            //AbpClaimTypes.Name = ...; //TODO
+            //AbpClaimTypes.SurName = ...; //TODO
+            AbpClaimTypes.UserId = JwtClaimTypes.Subject;
+            AbpClaimTypes.Role = JwtClaimTypes.Role;
+            AbpClaimTypes.Email = JwtClaimTypes.Email;
+        }
     }
 }
