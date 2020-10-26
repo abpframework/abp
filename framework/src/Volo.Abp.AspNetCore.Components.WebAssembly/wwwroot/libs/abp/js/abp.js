0edd75b2f8013b21d174cb0ea63614fb4b6e5ca8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-10-26 11:57:05 +0300

    Add abp.domReady

--- framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/wwwroot/libs/abp/js/abp.js
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/wwwroot/libs/abp/js/abp.js
@@ -2,6 +2,16 @@
 (function () {
     abp.utils = abp.utils || {};
 
+    // DOM READY /////////////////////////////////////////////////////
+
+    abp.domReady = function (fn) {
+        if (document.readyState === "complete" || document.readyState === "interactive") {
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    };
+
     // COOKIES ///////////////////////////////////////////////////////
 
     /**
