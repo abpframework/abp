b4f6ef6d03e973c940e222b7e43b3f1d8d37fd23
Mladen Macanovic <mladen.macanovic@gmail.com>
2020-10-28 10:25:32 +0100

    Implemented UiNotificationService

---
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly/Volo/Abp/AspNetCore/Components/WebAssembly/UiNotificationOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace Volo.Abp.AspNetCore.Components.WebAssembly
+{
+    /// <summary>
+    /// Options to override notification appearance.
+    /// </summary>
+    public class UiNotificationOptions
+    {
+        /// <summary>
+        /// Custom text for the Ok button.
+        /// </summary>
+        public string OkButtonText { get; set; }
+
+        /// <summary>
+        /// Custom icon for the Ok button.
+        /// </summary>
+        public object OkButtonIcon { get; set; }
+    }
+}
--- framework/src/Volo.Abp.BlazoriseUI/BlazoriseUiNotificationService.cs
+++ framework/src/Volo.Abp.BlazoriseUI/BlazoriseUiNotificationService.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Localization.Resources.AbpUi;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Volo.Abp.AspNetCore.Components.WebAssembly;
@@ -7,34 +10,79 @@ using Volo.Abp.DependencyInjection;
 namespace Volo.Abp.BlazoriseUI
 {
     [Dependency(ReplaceServices = true)]
-    public class BlazoriseUiNotificationService : IUiNotificationService, ITransientDependency
+    public class BlazoriseUiNotificationService : IUiNotificationService, IScopedDependency
     {
+        /// <summary>
+        /// An event raised after the notification is received.
+        /// </summary>
+        public event EventHandler<UiNotificationEventArgs> NotificationReceived;
+
+        private readonly IStringLocalizer<AbpUiResource> localizer;
+
         public ILogger<BlazoriseUiNotificationService> Logger { get; set; }
 
-        public BlazoriseUiNotificationService()
+        public BlazoriseUiNotificationService(
+            IStringLocalizer<AbpUiResource> localizer)
         {
+            this.localizer = localizer;
+
             Logger = NullLogger<BlazoriseUiNotificationService>.Instance;
         }
 
-        public Task Info(string message)
+        public Task Info(string message, string title = null, Action<UiNotificationOptions> options = null)
         {
+            var uiNotificationOptions = CreateDefaultOptions();
+            options?.Invoke(uiNotificationOptions);
+
             Logger.LogInformation(message);
+
+            NotificationReceived?.Invoke(this, new UiNotificationEventArgs(UiNotificationType.Info, message, title, uiNotificationOptions));
+
             return Task.CompletedTask;
         }
 
-        public Task Success(string message)
+        public Task Success(string message, string title = null, Action<UiNotificationOptions> options = null)
         {
+            var uiNotificationOptions = CreateDefaultOptions();
+            options?.Invoke(uiNotificationOptions);
+
+            Logger.LogInformation(message);
+
+            NotificationReceived?.Invoke(this, new UiNotificationEventArgs(UiNotificationType.Success, message, title, uiNotificationOptions));
+
             return Task.CompletedTask;
         }
 
-        public Task Warn(string message)
+        public Task Warn(string message, string title = null, Action<UiNotificationOptions> options = null)
         {
+            var uiNotificationOptions = CreateDefaultOptions();
+            options?.Invoke(uiNotificationOptions);
+
+            Logger.LogWarning(message);
+
+            NotificationReceived?.Invoke(this, new UiNotificationEventArgs(UiNotificationType.Warning, message, title, uiNotificationOptions));
+
             return Task.CompletedTask;
         }
 
-        public Task Error(string message)
+        public Task Error(string message, string title = null, Action<UiNotificationOptions> options = null)
         {
+            var uiNotificationOptions = CreateDefaultOptions();
+            options?.Invoke(uiNotificationOptions);
+
+            Logger.LogError(message);
+
+            NotificationReceived?.Invoke(this, new UiNotificationEventArgs(UiNotificationType.Error, message, title, uiNotificationOptions));
+
             return Task.CompletedTask;
+        }
+
+        protected virtual UiNotificationOptions CreateDefaultOptions()
+        {
+            return new UiNotificationOptions
+            {
+                OkButtonText = localizer["Ok"],
+            };
         }
     }
 }
