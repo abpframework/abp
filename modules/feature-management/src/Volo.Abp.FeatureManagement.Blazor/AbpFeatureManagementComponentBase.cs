e3bfda349280ae44e23a65e751562561645a9736
Ahmet <ahmet.cotur@hotmail.com>
2020-10-30 11:34:15 +0300

    FeatureManagement Blazor Refactored
    Added FeatureManagementComponentBase: #5961

---
+++ modules/feature-management/src/Volo.Abp.FeatureManagement.Blazor/AbpFeatureManagementComponentBase.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Components;
+using Volo.Abp.FeatureManagement.Localization;
+
+namespace Volo.Abp.FeatureManagement.Blazor
+{
+    public class AbpFeatureManagementComponentBase : AbpComponentBase
+    {
+        public AbpFeatureManagementComponentBase()
+        {
+            LocalizationResource = typeof(AbpFeatureManagementResource);
+        }
+    }
+}
