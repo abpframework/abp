a8799bd6eb4a368d9edb2ce80622756d8812e578
Ahmet <ahmet.cotur@hotmail.com>
2020-10-30 11:28:28 +0300

    Account Blazor refactored
    Added AccountComponentBase: #5961
    Replace Abstract Base: #5962

---
+++ modules/account/src/Volo.Abp.Account.Blazor/AbpAccountComponentBase.cs
@@ -0,0 +1,14 @@
+﻿using Volo.Abp.Account.Localization;
+using Volo.Abp.AspNetCore.Components;
+
+namespace Volo.Abp.Account.Blazor
+{
+    public class AbpAccountComponentBase : AbpComponentBase
+    {
+        public AbpAccountComponentBase()
+        {
+            LocalizationResource = typeof(AccountResource);
+            ObjectMapperContext = typeof(AbpAccountBlazorModule);
+        }
+    }
+}
