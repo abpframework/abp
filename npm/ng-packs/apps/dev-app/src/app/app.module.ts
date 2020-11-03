4e8f10f95301ef9097f70ff676acc3e5bd031788
mehmet-erim <bilgi@mehmeterim.com>
2020-11-03 15:27:10 +0300

    feat: pass registerLocaleFn to CoreModule in app.module files

--- npm/ng-packs/apps/dev-app/src/app/app.module.ts
+++ npm/ng-packs/apps/dev-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { AccountConfigModule } from '@abp/ng.account/config';
 import { CoreModule } from '@abp/ng.core';
+import { registerLocale } from '@abp/ng.core/locale';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
@@ -24,6 +25,7 @@ const LOGGERS = [NgxsLoggerPluginModule.forRoot({ disabled: true })];
     AppRoutingModule,
     CoreModule.forRoot({
       environment,
+      registerLocaleFn: registerLocale(),
       sendNullsAsQueryParam: false,
       skipGetAppConfiguration: false,
     }),
