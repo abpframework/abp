f7b84cf09dac4a306bf96350dd7077cdac26addd
Ilkay Ilknur <ilkay@ilkayilknur.com>
2020-11-03 19:05:51 +0300

    flag icon library removed from theming project.

--- framework/src/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/BundleContributer.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/BundleContributer.cs
@@ -11,7 +11,6 @@ namespace Volo.Abp.AspNetCore.Components.WebAssembly.Theming
             {
                 "_content/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/libs/bootstrap/css/bootstrap.min.css",
                 "_content/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/libs/fontawesome/css/all.css",
-                "_content/Volo.Abp.AspNetCore.Components.WebAssembly.Theming/libs/flag-icon/css/flag-icon.css"
             };
         }
     }
