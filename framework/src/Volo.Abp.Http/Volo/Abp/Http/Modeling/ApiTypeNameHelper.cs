e1d89aabde032712f77ba191c53541b4cfc59f16
maliming <liming.ma@volosoft.com>
2020-11-05 11:43:44 +0800

    Update ApiTypeNameHelper.cs

--- framework/src/Volo.Abp.Http/Volo/Abp/Http/Modeling/ApiTypeNameHelper.cs
+++ framework/src/Volo.Abp.Http/Volo/Abp/Http/Modeling/ApiTypeNameHelper.cs
@@ -6,8 +6,6 @@ namespace Volo.Abp.Http.Modeling
 {
     public static class ApiTypeNameHelper
     {
-        private static ITypeList _cycleType = new TypeList();
-
         public static string GetTypeName(Type type)
         {
             if (TypeHelper.IsDictionary(type, out var keyType, out var valueType))
