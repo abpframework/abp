37073c59bef659139f9be7319af294987ec35697
maliming <malimings@gmail.com>
2020-11-06 21:43:17 +0800

    Add AbpStringToEnumConverter & AbpStringToBooleanConverter.

---
+++ framework/src/Volo.Abp.Json/Volo/Abp/Json/SystemTextJson/JsonConverters/AbpStringToEnumConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Volo.Abp.Json.SystemTextJson.JsonConverters
+{
+    public class AbpStringToEnumConverter : JsonConverter<object>
+    {
+        private readonly JsonStringEnumConverter _innerJsonStringEnumConverter;
+
+        public AbpStringToEnumConverter()
+            : this(namingPolicy: null, allowIntegerValues: true)
+        {
+        }
+
+        public AbpStringToEnumConverter(JsonNamingPolicy namingPolicy = null, bool allowIntegerValues = true)
+        {
+            _innerJsonStringEnumConverter = new JsonStringEnumConverter(namingPolicy, allowIntegerValues);
+        }
+
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert.IsEnum;
+        }
+
+        public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var newOptions = new JsonSerializerOptions(options);
+            newOptions.Converters.Remove(this);
+            newOptions.Converters.Add(_innerJsonStringEnumConverter.CreateConverter(typeToConvert, options));
+            return JsonSerializer.Deserialize(ref reader, typeToConvert, newOptions);
+        }
+
+        public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        {
+            var newOptions = new JsonSerializerOptions(options);
+            newOptions.Converters.Remove(this);
+            JsonSerializer.Serialize(writer, value, newOptions);
+        }
+    }
+}
