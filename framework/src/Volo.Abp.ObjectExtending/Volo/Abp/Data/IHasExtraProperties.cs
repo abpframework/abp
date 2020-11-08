7d4b876935f4cbc96df426d45556283d2e5eea12
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-11-08 20:51:51 +0300

    Move ExtraPropertyDictionary to its own file.

--- framework/src/Volo.Abp.ObjectExtending/Volo/Abp/Data/IHasExtraProperties.cs
+++ framework/src/Volo.Abp.ObjectExtending/Volo/Abp/Data/IHasExtraProperties.cs
@@ -1,23 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace Volo.Abp.Data
+﻿namespace Volo.Abp.Data
 {
     //TODO: Move to Volo.Abp.Data.ObjectExtending namespace at 4.0?
 
     public interface IHasExtraProperties
     {
         ExtraPropertyDictionary ExtraProperties { get; }
-    }
-
-    public class ExtraPropertyDictionary : Dictionary<string, object>
-    {
-        public ExtraPropertyDictionary()
-        {
-        }
-
-        public ExtraPropertyDictionary(IDictionary<string, object> dictionary)
-            : base(dictionary)
-        {
-        }
     }
 }
