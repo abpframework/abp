f54af894b8947e0b291c7937ca71fe663e963568
mehmet-erim <bilgi@mehmeterim.com>
2020-11-09 08:36:31 +0300

    chore: remove session.actions

--- npm/ng-packs/packages/core/src/lib/actions/index.ts
+++ npm/ng-packs/packages/core/src/lib/actions/index.ts
@@ -3,4 +3,3 @@ export * from './loader.actions';
 export * from './profile.actions';
 export * from './replaceable-components.actions';
 export * from './rest.actions';
-export * from './session.actions';
