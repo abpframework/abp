44176eb37ad92a355243a04de03b57479406a8ca
enisn <iletisim@enisnecipoglu.com>
2020-11-10 16:25:49 +0300

    🧪 Add tests for AutoFilterer

---
+++ framework/test/Volo.Abp.AutoFilterer.Tests/Volo/Abp/AutoFilterer/AutoFilterer_ModluleInitialization_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.AutoFilterer.Tests.Volo.Abp.AutoFilterer
+{
+    public class AutoFilterer_ModluleInitialization_Tests
+    {
+        
+    }
+}
