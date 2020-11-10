cdbc520e00889771eaca2df47bf58d0175c3d3bc
Galip Tolga Erdem <galiperdem@gmail.com>
2020-11-10 17:06:38 +0300

    Added vue and vee-validate package and contributors

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Packages/Volo/Abp/AspNetCore/Mvc/UI/Packages/VeeValidate/VeeValidateScriptContributor.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Packages.VeeValidate
+{
+    public class VeeValidateScriptContributor : BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files.Add("/libs/vee-validate.js/vee-validate.full.js");
+        }
+    }
+}
