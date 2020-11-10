6df041f7761f11184d33a499aacd64540091009d
enisn <iletisim@enisnecipoglu.com>
2020-11-10 16:24:35 +0300

    ✨ Add AutoFilterer module

---
+++ framework/src/Volo.Abp.AutoFilterer/Volo/Abp/AutoFilterer/AbpAutoFiltererModule.cs
@@ -0,0 +1,21 @@
+﻿using AutoFilterer.Swagger;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.AutoFilterer
+{
+    public class AbpAutoFiltererModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.Configure<SwaggerGenOptions>(cfg => cfg.UseAutoFiltererParameters());
+            base.ConfigureServices(context);
+        }
+    }
+}
