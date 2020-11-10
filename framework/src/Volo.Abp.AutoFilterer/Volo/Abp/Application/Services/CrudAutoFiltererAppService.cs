ccad8f22b86f9638a0938ec990ec9956592e0b7d
enisn <iletisim@enisnecipoglu.com>
2020-11-10 16:25:04 +0300

    ✨ Add CrudAutoFiltererAppService and implement AutoFilterer

---
+++ framework/src/Volo.Abp.AutoFilterer/Volo/Abp/Application/Services/CrudAutoFiltererAppService.cs
@@ -0,0 +1,67 @@
+﻿using AutoFilterer.Abstractions;
+using AutoFilterer.Extensions;
+using AutoFilterer.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace Volo.Abp.Application.Services
+{
+    public class CrudAutoFiltererAppService<TEntity, TEntityDto, TKey>
+        : CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, PaginationFilterBase>
+        where TEntity : class, IEntity<TKey>
+        where TEntityDto : IEntityDto<TKey>
+    {
+        protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
+        {
+        }
+    }
+
+    public class CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput>
+        : CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput, TEntityDto>
+        where TEntity : class, IEntity<TKey>
+        where TEntityDto : IEntityDto<TKey>
+        where TGetListInput : IFilter
+    {
+        protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
+        {
+        }
+    }
+
+    public class CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput>
+        : CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput, TCreateInput>
+        where TEntity : class, IEntity<TKey>
+        where TEntityDto : IEntityDto<TKey>
+        where TGetListInput : IFilter
+    {
+        protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
+        {
+        }
+    }
+
+    public class CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput, TUpdateInput>
+        : CrudAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput, TUpdateInput>
+        where TEntity : class, IEntity<TKey>
+        where TEntityDto : IEntityDto<TKey>
+        where TGetListInput : IFilter
+    {
+        protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
+        {
+        }
+
+        protected override IQueryable<TEntity> ApplyPaging(IQueryable<TEntity> query, TGetListInput input) 
+            => query;
+
+        protected override IQueryable<TEntity> ApplySorting(IQueryable<TEntity> query, TGetListInput input) 
+            => query.ApplyFilter(input);
+
+        protected override IQueryable<TEntity> ApplyDefaultSorting(IQueryable<TEntity> query)
+            => query;
+    }
+}
