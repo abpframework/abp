b285c93e4d1d03476568eabefbba9705a08e3791
enisn <iletisim@enisnecipoglu.com>
2020-11-11 17:30:34 +0300

    🧪🐛 Fix swagger dependency of AutoFilterer test

--- framework/test/Volo.Abp.AutoFilterer.Tests/Volo/Abp/AutoFilterer/ApplicationService_AutoFilterer_Tests.cs
+++ framework/test/Volo.Abp.AutoFilterer.Tests/Volo/Abp/AutoFilterer/ApplicationService_AutoFilterer_Tests.cs
@@ -1,9 +1,12 @@
 ﻿using AutoFilterer.Swagger.OperationFilters;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Shouldly;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
+using Volo.Abp.Swashbuckle;
 using Volo.Abp.Testing;
 using Xunit;
 
@@ -14,16 +17,37 @@ namespace Volo.Abp.AutoFilterer.Tests.Volo.Abp.AutoFilterer
         [Fact]
         public void Should_Have_SwaggerOperatorFilters()
         {
-            var options = ServiceProvider.GetService<SwaggerGenOptions>();
+            var options = ServiceProvider.GetService<IOptions<SwaggerGenOptions>>();
 
-            options.OperationFilterDescriptors.ShouldContain(x => x.Type == typeof(OrderableEnumOperationFilter), "OrderableEnumOperationFilter couldn't found. Make sure the method 'UseAutoFiltererParameters()' is called.");
-            options.OperationFilterDescriptors.ShouldContain(x => x.Type == typeof(InnerFilterPropertiesOperationFilter), "OrderableEnumOperationFilter couldn't found. Make sure the method 'UseAutoFiltererParameters()' is called.");
+            options.Value.ShouldNotBeNull("Looks like swagger didn't initialized. Check test project.");
+
+            options.Value.OperationFilterDescriptors.ShouldContain(x => x.Type == typeof(OrderableEnumOperationFilter), "OrderableEnumOperationFilter couldn't found. Make sure the method 'UseAutoFiltererParameters()' is called.");
+            options.Value.OperationFilterDescriptors.ShouldContain(x => x.Type == typeof(InnerFilterPropertiesOperationFilter), "OrderableEnumOperationFilter couldn't found. Make sure the method 'UseAutoFiltererParameters()' is called.");
         }
 
-        [DependsOn(typeof(AbpAutofacModule))]
-        [DependsOn(typeof(AbpAutoFiltererModule))]
+        [DependsOn(
+            typeof(AbpAutofacModule),
+            typeof(AbpSwashbuckleModule),
+            typeof(AbpAutoFiltererModule)
+            )]
         public class TestModule : AbpModule
         {
+            public override void ConfigureServices(ServiceConfigurationContext context)
+            {
+                ConfigureSwaggerServices(context.Services);
+            }
+
+            private void ConfigureSwaggerServices(IServiceCollection services)
+            {
+                services.AddSwaggerGen(
+                    options =>
+                    {
+                        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Bookstore API", Version = "v1" });
+                        options.DocInclusionPredicate((docName, description) => true);
+                        options.CustomSchemaIds(type => type.FullName);
+                    }
+                );
+            }
         }
     }
 }
