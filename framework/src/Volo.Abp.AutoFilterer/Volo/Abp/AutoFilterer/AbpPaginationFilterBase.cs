2dc453fa949acc6b3c6e1f1b724db5dbec53da2f
enisn <iletisim@enisnecipoglu.com>
2020-11-13 12:05:51 +0300

    ✨ Add AbpPaginationFilterBase into AutoFilterer implementation

---
+++ framework/src/Volo.Abp.AutoFilterer/Volo/Abp/AutoFilterer/AbpPaginationFilterBase.cs
@@ -0,0 +1,41 @@
+﻿using AutoFilterer.Enums;
+using AutoFilterer.Types;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Volo.Abp.AutoFilterer.Volo.Abp.AutoFilterer
+{
+    public class AbpPaginationFilterBase : PaginationFilterBase, IPagedAndSortedResultRequest
+    {
+        public AbpPaginationFilterBase()
+        {
+            // As default.
+            this.Sort = nameof(AuditedAggregateRoot.CreationTime);
+            this.SortBy = Sorting.Descending;
+        }
+
+        int IPagedResultRequest.SkipCount { get => Page; set => Page = value; }
+        int ILimitedResultRequest.MaxResultCount { get => PerPage; set => PerPage = value; }
+        string ISortedResultRequest.Sorting
+        {
+            get => base.Sort + " " + GetAbpStringKeyword(base.SortBy);
+            set => SetSortingByAbpKeyword(value);
+        }
+
+        private void SetSortingByAbpKeyword(string keyword)
+        {
+            var splitted = keyword.Split(' ');
+            this.Sort = splitted[0];
+            this.SortBy = GetSortingFromKeyword(splitted.Length > 0 ? splitted[1] : default);
+        }
+
+        private static string GetAbpStringKeyword(Sorting sorting)
+        {
+            return sorting == Sorting.Descending ? "DESC" : "ASC";
+        }
+        private static Sorting GetSortingFromKeyword(string keyword)
+        {
+            return keyword == "DESC" ? Sorting.Descending : Sorting.Ascending;
+        }
+    }
+}
