f488d2e671f8bddb2a00ae145d1f7a50456b713a
enisn <iletisim@enisnecipoglu.com>
2020-11-13 16:20:54 +0300

    🔨 Fix namespace for AbpPaginationFilterBase

--- framework/src/Volo.Abp.AutoFilterer/Volo/Abp/AutoFilterer/AbpPaginationFilterBase.cs
+++ framework/src/Volo.Abp.AutoFilterer/Volo/Abp/AutoFilterer/AbpPaginationFilterBase.cs
@@ -4,7 +4,7 @@ using AutoFilterer.Types;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Domain.Entities.Auditing;
 
-namespace Volo.Abp.AutoFilterer.Volo.Abp.AutoFilterer
+namespace Volo.Abp.AutoFilterer
 {
     [PossibleSortings(typeof(AuditedAggregateRoot))]
     public class AbpPaginationFilterBase : PaginationFilterBase, IPagedAndSortedResultRequest
