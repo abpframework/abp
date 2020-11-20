5887bec0169d2dd698eab24950fece1e036092a1
enisn <iletisim@enisnecipoglu.com>
2020-11-20 17:51:10 +0300

    ♻ Refactor CrudAutoFiltererAppService according to AbpFilterBase

--- framework/src/Volo.Abp.AutoFilterer/Volo/Abp/Application/Services/CrudAutoFiltererAppService.cs
+++ framework/src/Volo.Abp.AutoFilterer/Volo/Abp/Application/Services/CrudAutoFiltererAppService.cs
@@ -1,4 +1,5 @@
-﻿using AutoFilterer.Extensions;
+﻿using AutoFilterer.Abstractions;
+using AutoFilterer.Extensions;
 using System.Linq;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.AutoFilterer;
@@ -21,7 +22,7 @@ namespace Volo.Abp.Application.Services
         : CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput, TEntityDto>
         where TEntity : class, IEntity<TKey>
         where TEntityDto : IEntityDto<TKey>
-        where TGetListInput : AbpPaginationFilterBase
+        where TGetListInput : IFilter
     {
         protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
         {
@@ -32,7 +33,7 @@ namespace Volo.Abp.Application.Services
         : CrudAutoFiltererAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput, TCreateInput>
         where TEntity : class, IEntity<TKey>
         where TEntityDto : IEntityDto<TKey>
-        where TGetListInput : AbpPaginationFilterBase
+        where TGetListInput : IFilter
     {
         protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
         {
@@ -43,19 +44,42 @@ namespace Volo.Abp.Application.Services
         : CrudAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput, TUpdateInput>
         where TEntity : class, IEntity<TKey>
         where TEntityDto : IEntityDto<TKey>
-        where TGetListInput : AbpPaginationFilterBase
+        where TGetListInput : IFilter
     {
         protected CrudAutoFiltererAppService(IRepository<TEntity, TKey> repository) : base(repository)
         {
         }
 
-        protected override IQueryable<TEntity> ApplyPaging(IQueryable<TEntity> query, TGetListInput input) 
-            => query.ToPaged(input.Page, input.PerPage);
+        protected override IQueryable<TEntity> ApplyPaging(IQueryable<TEntity> query, TGetListInput input)
+        {
+            if (input is IPaginationFilter filter)
+                return query.ToPaged(filter.Page, filter.PerPage);
 
-        protected override IQueryable<TEntity> ApplySorting(IQueryable<TEntity> query, TGetListInput input) 
-            => input.ApplyOrder(query);
+            return base.ApplyPaging(query, input);
+        }
 
-        protected override IQueryable<TEntity> CreateFilteredQuery(TGetListInput input) 
-            => input.ApplyFilterWithoutOrdering(base.CreateFilteredQuery(input));
+        protected override IQueryable<TEntity> ApplySorting(IQueryable<TEntity> query, TGetListInput input)
+        {
+            if (input is IOrderable orderableFilter)
+                return orderableFilter.ApplyOrder(query);
+
+            return base.ApplySorting(query, input);
+        }
+
+        protected override IQueryable<TEntity> CreateFilteredQuery(TGetListInput input)
+        {
+            var query = base.CreateFilteredQuery(input);
+
+            if (input is IOrderablePaginationFilter fullFilter)
+            {
+                return fullFilter.ApplyFilterWithoutPaginationAndOrdering(query);
+            }
+            else if (input is IFilter filter)
+            {
+                return filter.ApplyFilterTo(query);
+            }
+
+            return query;
+        }
     }
 }
