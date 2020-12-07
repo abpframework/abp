f168fd27ca8eb5a8569eec3696ef2dc211d1af8c
maliming <malimings@gmail.com>
2020-12-07 15:06:46 +0800

    Localize exception messages in AbpAuthorizationModule..

--- framework/test/Volo.Abp.Features.Tests/Volo/Abp/Features/FeatureCheckerExtensions_Tests.cs
+++ framework/test/Volo.Abp.Features.Tests/Volo/Abp/Features/FeatureCheckerExtensions_Tests.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Shouldly;
+﻿using Shouldly;
 using Volo.Abp.AspNetCore.ExceptionHandling;
 using Volo.Abp.Authorization;
 using Volo.Abp.Localization;
@@ -9,51 +8,32 @@ namespace Volo.Abp.Features
 {
     public class FeatureCheckerExtensions_Tests : FeatureTestBase
     {
-        private readonly IFeatureChecker _featureChecker;
         private readonly IExceptionToErrorInfoConverter _exceptionToErrorInfoConverter;
 
         public FeatureCheckerExtensions_Tests()
         {
-            _featureChecker = GetRequiredService<IFeatureChecker>();
             _exceptionToErrorInfoConverter = GetRequiredService<IExceptionToErrorInfoConverter>();
         }
 
         [Fact]
-        public async Task CheckEnabledAsync()
+        public void Test_AbpAuthorizationException_Localization()
         {
             using (CultureHelper.Use("zh-Hans"))
             {
-                var ex = await Assert.ThrowsAsync<AbpAuthorizationException>(async () =>
-                    await _featureChecker.CheckEnabledAsync("BooleanTestFeature1"));
+                var exception = new AbpAuthorizationException(code: AbpFeatureErrorCodes.FeatureIsNotEnabled)
+                    .WithData("FeatureName", "my_feature_name");
+                var errorInfo = _exceptionToErrorInfoConverter.Convert(exception, false);
+                errorInfo.Message.ShouldBe("功能未启用: my_feature_name");
 
-                var errorInfo = _exceptionToErrorInfoConverter.Convert(ex, false);
-                errorInfo.Message.ShouldBe("功能未启用: BooleanTestFeature1");
-            }
-        }
+                exception = new AbpAuthorizationException(code: AbpFeatureErrorCodes.AllOfTheseFeaturesMustBeEnabled)
+                    .WithData("FeatureNames", "my_feature_name, my_feature_name2");
+                errorInfo = _exceptionToErrorInfoConverter.Convert(exception, false);
+                errorInfo.Message.ShouldBe("必要的功能未启用. 这些功能需要启用: my_feature_name, my_feature_name2");
 
-        [Fact]
-        public async Task CheckEnabled_RequiresAll()
-        {
-            using (CultureHelper.Use("zh-Hans"))
-            {
-                var ex = await Assert.ThrowsAsync<AbpAuthorizationException>(async () =>
-                    await _featureChecker.CheckEnabledAsync(true, "BooleanTestFeature1", "BooleanTestFeature2"));
-
-                var errorInfo = _exceptionToErrorInfoConverter.Convert(ex, false);
-                errorInfo.Message.ShouldBe("必要的功能未启用. 这些功能需要启用: BooleanTestFeature1, BooleanTestFeature2");
-            }
-        }
-
-        [Fact]
-        public async Task CheckEnabled_Not_RequiresAll()
-        {
-            using (CultureHelper.Use("zh-Hans"))
-            {
-                var ex = await Assert.ThrowsAsync<AbpAuthorizationException>(async () =>
-                    await _featureChecker.CheckEnabledAsync(false, "BooleanTestFeature1", "BooleanTestFeature2"));
-
-                var errorInfo = _exceptionToErrorInfoConverter.Convert(ex, false);
-                errorInfo.Message.ShouldBe("必要的功能未启用. 需要启用这些功能中的一项：BooleanTestFeature1, BooleanTestFeature2");
+                exception = new AbpAuthorizationException(code: AbpFeatureErrorCodes.AtLeastOneOfTheseFeaturesMustBeEnabled)
+                    .WithData("FeatureNames", "my_feature_name, my_feature_name2");
+                errorInfo = _exceptionToErrorInfoConverter.Convert(exception, false);
+                errorInfo.Message.ShouldBe("必要的功能未启用. 需要启用这些功能中的一项：my_feature_name, my_feature_name2");
             }
         }
     }
