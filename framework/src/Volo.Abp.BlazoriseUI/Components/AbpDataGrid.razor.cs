3a77152e5dd26623cf738285028e5cdd09fb5d04
Ilkay Ilknur <ilkay@ilkayilknur.com>
2020-12-10 16:42:49 +0300

    abp datagrid implementation.

---
+++ framework/src/Volo.Abp.BlazoriseUI/Components/AbpDataGrid.razor.cs
@@ -0,0 +1,31 @@
+﻿using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using Volo.Abp.AspNetCore.Components.Extensibility.TableColumns;
+
+namespace Volo.Abp.BlazoriseUI.Components
+{
+    public partial class AbpDataGrid<TItem> : ComponentBase
+    {
+        protected Dictionary<string, DataGridEntityActionsColumn<TItem>> ActionColumns = new Dictionary<string, DataGridEntityActionsColumn<TItem>>();
+
+        [Parameter]
+        public IEnumerable<TItem> Data { get; set; }
+
+        [Parameter]
+        public EventCallback<DataGridReadDataEventArgs<TItem>> ReadData { get; set; }
+
+        [Parameter]
+        public int? TotalItems { get; set; }
+
+        [Parameter]
+        public bool ShowPager { get; set; }
+
+        [Parameter]
+        public int PageSize { get; set; }
+
+        [Parameter]
+        public List<TableColumn> Columns { get; set; }
+
+    }
+}
