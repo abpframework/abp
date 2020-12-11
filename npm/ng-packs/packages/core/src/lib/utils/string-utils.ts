7c3a2452a989d389518af1dc9cde82ac0326b0d1
Arman Ozak <arman@armanozak.com>
2020-12-11 13:07:11 +0300

    fix: avoid lint error in createTokenParser

--- npm/ng-packs/packages/core/src/lib/utils/string-utils.ts
+++ npm/ng-packs/packages/core/src/lib/utils/string-utils.ts
@@ -1,12 +1,12 @@
 export function createTokenParser(format: string) {
-  return (string: string) => {
+  return (str: string) => {
     const tokens: string[] = [];
     const regex = format.replace(/\./g, '\\.').replace(/\{\s?([0-9a-zA-Z]+)\s?\}/g, (_, token) => {
       tokens.push(token);
       return '(.+)';
     });
 
-    const matches = (string.match(regex) || []).slice(1);
+    const matches = (str.match(regex) || []).slice(1);
 
     return matches.reduce((acc, v, i) => {
       const key = tokens[i];
