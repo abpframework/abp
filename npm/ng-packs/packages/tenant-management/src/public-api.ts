93b9bc2d5a5f256a95cb30c307fef7eb58e331a7
mehmet-erim <bilgi@mehmeterim.com>
2020-12-14 15:56:07 +0300

    chore: export guards folder

--- npm/ng-packs/packages/tenant-management/src/public-api.ts
+++ npm/ng-packs/packages/tenant-management/src/public-api.ts
@@ -2,6 +2,7 @@ export * from './lib/tenant-management.module';
 export * from './lib/actions';
 export * from './lib/components';
 export * from './lib/enums';
+export * from './lib/guards';
 export * from './lib/models';
 export * from './lib/services';
 export * from './lib/states';
