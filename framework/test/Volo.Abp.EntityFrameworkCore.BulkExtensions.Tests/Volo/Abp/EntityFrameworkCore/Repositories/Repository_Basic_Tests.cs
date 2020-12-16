fe1a521262c0cd03870f1652969400517d0c4bb9
enisn <iletisim@enisnecipoglu.com>
2020-12-16 18:23:12 +0300

    EfCore BulkExtensions Tests

---
+++ framework/test/Volo.Abp.EntityFrameworkCore.BulkExtensions.Tests/Volo/Abp/EntityFrameworkCore/Repositories/Repository_Basic_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.TestApp.Testing;
+
+namespace Volo.Abp.EntityFrameworkCore.BulkExtensions.Tests.Volo.Abp.EntityFrameworkCore.Repositories
+{
+    public class Repository_Basic_Tests : Repository_Basic_Tests<AbpEntityFrameworkCoreBulkExtensionsTestModule>
+    {
+    }
+}
