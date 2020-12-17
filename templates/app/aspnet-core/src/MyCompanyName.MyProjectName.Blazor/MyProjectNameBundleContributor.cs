095d6138071b2232256aa02c06bda3e20915d08a
maliming <malimings@gmail.com>
2020-12-17 11:19:58 +0800

    Update MyProjectNameBundleContributor.
    https://github.com/abpframework/abp/pull/6668

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor/MyProjectNameBundleContributor.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor/MyProjectNameBundleContributor.cs
@@ -4,11 +4,12 @@ namespace MyCompanyName.MyProjectName.Blazor
 {
     public class MyProjectNameBundleContributor : IBundleContributor
     {
-        public void AddScripts(BundleContext context, BundleParameterDictionary parameters)
+        public void AddScripts(BundleContext context)
         {
+
         }
 
-        public void AddStyles(BundleContext context, BundleParameterDictionary parameters)
+        public void AddStyles(BundleContext context)
         {
             context.Add("main.css");
         }
