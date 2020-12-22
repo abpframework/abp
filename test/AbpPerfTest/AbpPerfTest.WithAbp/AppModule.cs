333540e5e848e711f4d347d8971d53074d465e9e
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-12-22 16:50:19 +0300

    Added initial AbpPerfTest.WithAbp application.

---
+++ test/AbpPerfTest/AbpPerfTest.WithAbp/AppModule.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp.EntityFrameworkCore.SqlServer;
+using Volo.Abp.Modularity;
+
+namespace AbpPerfTest.WithAbp
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreMvcModule),
+        typeof(AbpAutofacModule),
+        typeof(AbpEntityFrameworkCoreSqlServerModule)
+        )]
+    public class AppModule : AbpModule
+    {
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseConfiguredEndpoints();
+        }
+    }
+}
