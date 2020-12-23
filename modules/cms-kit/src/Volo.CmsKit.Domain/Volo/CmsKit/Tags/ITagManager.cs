1ff9ba3adc8fc826ccc19407fca51196640c4a05
YinChang0626 <yinchang0626@gmail.com>
2020-12-23 22:20:47 +0800

    fix namespace

--- modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Tags/ITagManager.cs
+++ modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Tags/ITagManager.cs
@@ -3,9 +3,8 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp.Domain.Services;
-using Volo.CmsKit.Tags;
 
-namespace Volo.CmsKit.Domain.Volo.CmsKit.Tags
+namespace Volo.CmsKit.Tags
 {
     public interface ITagManager : IDomainService
     {
--- modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Tags/TagManager.cs
+++ modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Tags/TagManager.cs
@@ -7,9 +7,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp;
 using Volo.Abp.Domain.Services;
-using Volo.CmsKit.Tags;
 
-namespace Volo.CmsKit.Domain.Volo.CmsKit.Tags
+namespace Volo.CmsKit.Tags
 {
     public class TagManager : DomainService, ITagManager
     {
