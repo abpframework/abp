4fc4172b514f2fcd6950874fd02a3e5c334a6312
Arman Ozak <arman@armanozak.com>
2020-12-24 18:47:23 +0300

    provide mock rest service in CoreTestingModule

--- npm/ng-packs/packages/core/testing/src/lib/core-testing.module.ts
+++ npm/ng-packs/packages/core/testing/src/lib/core-testing.module.ts
@@ -4,12 +4,14 @@ import {
   coreOptionsFactory,
   CORE_OPTIONS,
   LocalizationPipe,
+  RestService,
 } from '@abp/ng.core';
 import { APP_BASE_HREF } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { provideRoutes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockLocalizationPipe } from './pipes/mock-localization.pipe';
+import { MockRestService } from './services/mock-rest.service';
 
 /**
  * CoreTestingModule is the module that will be used in tests
@@ -40,6 +42,10 @@ export class CoreTestingModule {
         {
           provide: LocalizationPipe,
           useClass: MockLocalizationPipe,
+        },
+        {
+          provide: RestService,
+          useClass: MockRestService,
         },
         provideRoutes(routes),
       ],
