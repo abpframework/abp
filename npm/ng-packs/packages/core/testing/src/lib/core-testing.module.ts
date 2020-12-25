91b5b74b95bcb2807a15a5365e92c07eed9b34ae
Arman Ozak <arman@armanozak.com>
2020-12-25 18:22:40 +0300

    remove MockLocalizationPipe from core testing package

--- npm/ng-packs/packages/core/testing/src/lib/core-testing.module.ts
+++ npm/ng-packs/packages/core/testing/src/lib/core-testing.module.ts
@@ -1,16 +1,8 @@
-import {
-  ABP,
-  BaseCoreModule,
-  coreOptionsFactory,
-  CORE_OPTIONS,
-  LocalizationPipe,
-  RestService,
-} from '@abp/ng.core';
+import { ABP, BaseCoreModule, coreOptionsFactory, CORE_OPTIONS, RestService } from '@abp/ng.core';
 import { APP_BASE_HREF } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { provideRoutes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MockLocalizationPipe } from './pipes/mock-localization.pipe';
 import { MockRestService } from './services/mock-rest.service';
 
 /**
@@ -18,9 +10,8 @@ import { MockRestService } from './services/mock-rest.service';
  * and it provides mock alternatives
  */
 @NgModule({
-  exports: [RouterTestingModule, BaseCoreModule, MockLocalizationPipe],
+  exports: [RouterTestingModule, BaseCoreModule],
   imports: [RouterTestingModule, BaseCoreModule],
-  declarations: [MockLocalizationPipe],
 })
 export class CoreTestingModule {
   static withConfig(
@@ -38,10 +29,6 @@ export class CoreTestingModule {
           provide: CORE_OPTIONS,
           useFactory: coreOptionsFactory,
           deps: ['CORE_OPTIONS'],
-        },
-        {
-          provide: LocalizationPipe,
-          useClass: MockLocalizationPipe,
         },
         {
           provide: RestService,
