f819e3c679c156247eb3706ede39cea56f6ee859
Arman Ozak <arman@armanozak.com>
2020-12-29 09:16:39 +0300

    fix condition for recursive DeepPartial type

--- npm/ng-packs/packages/core/src/lib/models/utility.ts
+++ npm/ng-packs/packages/core/src/lib/models/utility.ts
@@ -1,13 +1,8 @@
 import { TemplateRef, Type } from '@angular/core';
 
 export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends Serializable ? DeepPartial<T[P]> : T[P];
+  [P in keyof T]?: T[P] extends Record<string | number | symbol, any> ? DeepPartial<T[P]> : T[P];
 };
-
-type Serializable = Record<
-  string | number | symbol,
-  string | number | boolean | Record<string | number | symbol, any>
->;
 
 export type InferredInstanceOf<T> = T extends Type<infer U> ? U : never;
 export type InferredContextOf<T> = T extends TemplateRef<infer U> ? U : never;
