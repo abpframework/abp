af22522028048b4f027d61a7ce40b68ce1f31a1a
enisn <iletisim@enisnecipoglu.com>
2020-12-31 14:30:36 +0300

    CmsKit - Add ContentController

---
+++ modules/cms-kit/src/Volo.CmsKit.Admin.HttpApi/Volo/CmsKit/Admin/Contents/ContentController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.CmsKit.Admin.Contents;
+
+namespace Volo.CmsKit.Admin.HttpApi.Volo.CmsKit.Admin.Contents
+{
+    [RemoteService(Name = CmsKitCommonRemoteServiceConsts.RemoteServiceName)]
+    [Area("cms-kit")]
+    [Route("api/cms-kit-admin/contents")]
+    public class ContentController : CmsKitAdminController, IContentAdminAppService
+    {
+        public ContentController(IContentAdminAppService contentAdminAppService)
+        {
+            ContentAdminAppService = contentAdminAppService;
+        }
+
+        protected IContentAdminAppService ContentAdminAppService { get; }
+
+        public Task<ContentDto> CreateAsync(ContentCreateDto input)
+        {
+            return ContentAdminAppService.CreateAsync(input);
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            return ContentAdminAppService.DeleteAsync(id);
+        }
+
+        public Task<ContentDto> GetAsync(Guid id)
+        {
+            return ContentAdminAppService.GetAsync(id);
+        }
+
+        public Task<PagedResultDto<ContentDto>> GetListAsync(ContentGetListInput input)
+        {
+            return ContentAdminAppService.GetListAsync(input);
+        }
+
+        public Task<ContentDto> UpdateAsync(Guid id, ContentUpdateDto input)
+        {
+            return ContentAdminAppService.UpdateAsync(id, input);
+        }
+    }
+}
