9616ada96074d210f89042351a2ede05b00ad218
enisn <iletisim@enisnecipoglu.com>
2020-12-31 15:36:46 +0300

    CmsKit - Add missing Content Permissions

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Permissions/CmsKitAdminPermissionDefinitionProvider.cs
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Permissions/CmsKitAdminPermissionDefinitionProvider.cs
@@ -1,5 +1,7 @@
 ﻿using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.GlobalFeatures;
 using Volo.Abp.Localization;
+using Volo.CmsKit.GlobalFeatures;
 using Volo.CmsKit.Localization;
 
 namespace Volo.CmsKit.Permissions
@@ -15,6 +17,14 @@ namespace Volo.CmsKit.Permissions
                     .AddChild(CmsKitAdminPermissions.Tags.Create, L("Permission:TagManagement.Create"))
                     .AddChild(CmsKitAdminPermissions.Tags.Update, L("Permission:TagManagement.Update"))
                     .AddChild(CmsKitAdminPermissions.Tags.Delete, L("Permission:TagManagement.Delete"));
+
+            if (GlobalFeatureManager.Instance.IsEnabled<ContentsFeature>())
+            {
+                var contentGroup = cmsGroup.AddPermission(CmsKitAdminPermissions.Contents.Default, L("Permission:Contents"));
+                contentGroup.AddChild(CmsKitAdminPermissions.Contents.Create, L("Permission:Contents.Create"));
+                contentGroup.AddChild(CmsKitAdminPermissions.Contents.Update, L("Permission:Contents.Update"));
+                contentGroup.AddChild(CmsKitAdminPermissions.Contents.Delete, L("Permission:Contents.Delete"));
+            }
         }
 
         private static LocalizableString L(string name)
