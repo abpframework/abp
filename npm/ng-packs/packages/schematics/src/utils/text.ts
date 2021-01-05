4a253fc1b9be836d718a4369a9aee0d7f08e4677
PM Extra <pm@jubeat.net>
2021-01-05 18:01:19 +0800

    Fix for lint.

--- npm/ng-packs/packages/schematics/src/utils/text.ts
+++ npm/ng-packs/packages/schematics/src/utils/text.ts
@@ -11,7 +11,7 @@ export const dir = (text: string) =>
   strings.dasherize(text.replace(/\./g, '/').replace(/\/\//g, '/'));
 
 export const quote = (value: number | string) =>
-  typeof value === 'string' ? `'${value.replace(/'/g, `\\'`)}'` : value;
+  typeof value === 'string' ? `'${value.replace(/'/g, '\\\'')}'` : value;
 
 function _(text: string): string {
   return text.replace(/\./g, '_');
