a8c5186bde1ca44f00638491b3f2639e17c05b34
maliming <malimings@gmail.com>
2021-01-05 10:15:32 +0800

    Update the entity after changing its properties.
    Resolve #7000

--- modules/identity/src/Volo.Abp.Identity.Application/Volo/Abp/Identity/IdentityUserAppService.cs
+++ modules/identity/src/Volo.Abp.Identity.Application/Volo/Abp/Identity/IdentityUserAppService.cs
@@ -84,7 +84,9 @@ namespace Volo.Abp.Identity
             input.MapExtraPropertiesTo(user);
 
             (await UserManager.CreateAsync(user, input.Password)).CheckErrors();
+
             await UpdateUserByInput(user, input);
+            (await UserManager.UpdateAsync(user)).CheckErrors();
 
             await CurrentUnitOfWork.SaveChangesAsync();
 
@@ -174,6 +176,7 @@ namespace Volo.Abp.Identity
 
             user.Name = input.Name;
             user.Surname = input.Surname;
+            (await UserManager.UpdateAsync(user)).CheckErrors();
 
             if (input.RoleNames != null)
             {
