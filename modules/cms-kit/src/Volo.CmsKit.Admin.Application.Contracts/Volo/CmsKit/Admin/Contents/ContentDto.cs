fa05c14dbcfc4fe8a42b354c00af4efc81f31c68
enisn <iletisim@enisnecipoglu.com>
2021-01-06 12:15:15 +0300

    CmsKit - Add EntityId to ContentDto

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Contents/ContentDto.cs
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Contents/ContentDto.cs
@@ -6,6 +6,9 @@ namespace Volo.CmsKit.Admin.Contents
     public class ContentDto : EntityDto<Guid>
     {
         public string EntityType { get; set; }
+
+        public string EntityId { get; set; }
+
         public string Value { get; set; }
     }
 }
