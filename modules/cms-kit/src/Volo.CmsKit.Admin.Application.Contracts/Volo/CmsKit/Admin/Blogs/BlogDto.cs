80e556e1e84a7ba6a7908a77d208f121bdd34c05
enisn <iletisim@enisnecipoglu.com>
2021-01-15 13:34:38 +0300

    CmsKit - Add BlogAdminAppService

---
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Blogs/BlogDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Volo.CmsKit.Admin.Blogs
+{
+    public class BlogDto : EntityDto<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application/Volo/CmsKit/Admin/Blogs/BlogAdminAppService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+using Volo.CmsKit.Domain.Volo.CmsKit.Blogs;
+using Volo.CmsKit.Permissions;
+
+namespace Volo.CmsKit.Admin.Blogs
+{
+    public class BlogAdminAppService : CrudAppService<Blog, BlogDto, Guid>, IBlogAdminAppService
+    {
+        public BlogAdminAppService(IRepository<Blog, Guid> repository) : base(repository)
+        {
+            GetListPolicyName = CmsKitAdminPermissions.Blogs.Default;
+            GetPolicyName = CmsKitAdminPermissions.Blogs.Default;
+            CreatePolicyName = CmsKitAdminPermissions.Blogs.Create;
+            UpdatePolicyName = CmsKitAdminPermissions.Blogs.Update;
+            DeletePolicyName = CmsKitAdminPermissions.Blogs.Delete;
+        }
+    }
+}
