888a01d1cfb05c019d2d7a19e244a3e5a6ec60f9
enisn <iletisim@enisnecipoglu.com>
2021-01-19 18:15:24 +0300

    CmsKit - namespace fix for ContentAdminAppService

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application/Volo/CmsKit/Admin/Contents/ContentAdminAppService.cs
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application/Volo/CmsKit/Admin/Contents/ContentAdminAppService.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 using Volo.CmsKit.Contents;
-using Volo.CmsKit.Domain.Volo.CmsKit.Contents;
 using Volo.CmsKit.Permissions;
 
 namespace Volo.CmsKit.Admin.Contents
