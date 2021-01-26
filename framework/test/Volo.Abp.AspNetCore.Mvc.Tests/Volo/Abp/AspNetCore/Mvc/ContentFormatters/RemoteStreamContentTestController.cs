e7dc50fcd28563a087a7120a47deaa35d3818239
maliming <malimings@gmail.com>
2021-01-26 16:08:00 +0800

    Refactor IRemoteStreamContent related functions.

---
+++ framework/test/Volo.Abp.AspNetCore.Mvc.Tests/Volo/Abp/AspNetCore/Mvc/ContentFormatters/RemoteStreamContentTestController.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.Content;
+
+namespace Volo.Abp.AspNetCore.Mvc.ContentFormatters
+{
+    [Route("api/remote-stream-content-test")]
+    public class RemoteStreamContentTestController : AbpController
+    {
+        [HttpGet]
+        [Route("Download")]
+        public async Task<IRemoteStreamContent> DownloadAsync()
+        {
+            var memoryStream = new MemoryStream();
+            await memoryStream.WriteAsync(Encoding.UTF8.GetBytes("stream"));
+
+            return new RemoteStreamContent(memoryStream)
+            {
+                ContentType = "audio/mpeg"
+            };
+        }
+
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<string> UploadAsync([FromBody]IRemoteStreamContent streamContent)
+        {
+            using (var reader = new StreamReader(streamContent.GetStream()))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ framework/test/Volo.Abp.AspNetCore.Mvc.Tests/Volo/Abp/AspNetCore/Mvc/ContentFormatters/RemoteStreamContentTestController_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Volo.Abp.AspNetCore.Mvc.ContentFormatters
+{
+    public class RemoteStreamContentTestController_Tests : AspNetCoreMvcTestBase
+    {
+        [Fact]
+        public async Task DownloadAsync()
+        {
+            var result = await GetResponseAsync("/api/remote-stream-content-test/download");
+            result.Content.Headers.ContentType?.ToString().ShouldBe("audio/mpeg");
+            (await result.Content.ReadAsStringAsync()).ShouldBe("stream");
+        }
+
+        [Fact]
+        public async Task UploadAsync()
+        {
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/api/remote-stream-content-test/upload"))
+            {
+                var memoryStream = new MemoryStream();
+                await memoryStream.WriteAsync(Encoding.UTF8.GetBytes("upload"));
+
+                memoryStream.Position = 0;
+                requestMessage.Content = new StreamContent(memoryStream);
+                requestMessage.Content.Headers.Add("Content-Type", "*/*");
+
+                var response = await Client.SendAsync(requestMessage);
+
+                (await response.Content.ReadAsStringAsync()).ShouldBe("upload");
+            }
+        }
+    }
+}
