f4edd897adbabb9d210d70da13d945cb5a0cdd95
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-01-28 16:05:36 +0300

    IgnoreMultiTenancy for ITenantManagementDbContext

--- modules/tenant-management/src/Volo.Abp.TenantManagement.EntityFrameworkCore/Volo/Abp/TenantManagement/EntityFrameworkCore/ITenantManagementDbContext.cs
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.EntityFrameworkCore/Volo/Abp/TenantManagement/EntityFrameworkCore/ITenantManagementDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Data;
 using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.MultiTenancy;
 
 namespace Volo.Abp.TenantManagement.EntityFrameworkCore
 {
+    [IgnoreMultiTenancy]
     [ConnectionStringName(AbpTenantManagementDbProperties.ConnectionStringName)]
     public interface ITenantManagementDbContext : IEfCoreDbContext
     {
