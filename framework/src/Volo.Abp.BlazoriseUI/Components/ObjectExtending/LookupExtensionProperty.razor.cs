b79d88ea43787d03a89d2567a4bf0990aa354080
Ilkay Ilknur <ilkay@ilkayilknur.com>
2021-01-29 13:58:49 +0300

    lookup property HttpClient implementation

--- framework/src/Volo.Abp.BlazoriseUI/Components/ObjectExtending/LookupExtensionProperty.razor.cs
+++ framework/src/Volo.Abp.BlazoriseUI/Components/ObjectExtending/LookupExtensionProperty.razor.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Volo.Abp.Data;
+using Volo.Abp.Http;
+using Volo.Abp.Http.Client;
+using Volo.Abp.Http.Client.Authentication;
+using Volo.Abp.MultiTenancy;
 using Volo.Abp.ObjectExtending;
 
 namespace Volo.Abp.BlazoriseUI.Components.ObjectExtending
@@ -19,14 +26,23 @@ namespace Volo.Abp.BlazoriseUI.Components.ObjectExtending
         [Inject]
         public IStringLocalizerFactory StringLocalizerFactory { get; set; }
 
-        [Inject]
-        public HttpClient Client { get; set; }
-
         [Parameter]
         public TEntity Entity { get; set; }
 
         [Parameter]
         public ObjectExtensionPropertyInfo PropertyInfo { get; set; }
+
+        [Inject]
+        public IRemoteServiceHttpClientAuthenticator ClientAuthenticator { get; set; }
+
+        [Inject]
+        public IHttpClientFactory HttpClientFactory { get; set; }
+
+        [Inject]
+        public ICurrentTenant CurrentTenant { get; set; }
+
+        [Inject]
+        public IOptions<AbpRemoteServiceOptions> RemoteServiceOptions { get; set; }
 
         public object SelectedValue
         {
@@ -63,14 +79,33 @@ namespace Volo.Abp.BlazoriseUI.Components.ObjectExtending
         {
             var selectItems = new List<SelectItem<object>>();
             var url = PropertyInfo.Lookup.Url;
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
             if (!filter.IsNullOrEmpty())
             {
-                url += $"{PropertyInfo.Lookup.FilterParamName}={filter.Trim()}";
+                if (uri.Query.IsNullOrEmpty())
+                {
+                    url += $"?{PropertyInfo.Lookup.FilterParamName}={filter.Trim()}";
+                }
+                else
+                {
+                    url += $"&{PropertyInfo.Lookup.FilterParamName}={filter.Trim()}";
+                }
             }
 
-            var responseStream = await Client.GetStreamAsync(url);
+            var client = HttpClientFactory.CreateClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            AddHeaders(requestMessage);
 
-            var document = await JsonDocument.ParseAsync(responseStream);
+            if (!uri.IsAbsoluteUri)
+            {
+                var remoteServiceConfig = RemoteServiceOptions.Value.RemoteServices.GetConfigurationOrDefault("Default");
+                client.BaseAddress = new Uri(remoteServiceConfig.BaseUrl);
+                await ClientAuthenticator.Authenticate(new RemoteServiceHttpClientAuthenticateContext(client, requestMessage, new RemoteServiceConfiguration(remoteServiceConfig.BaseUrl), string.Empty));
+            }
+
+            var response = await client.SendAsync(requestMessage);
+
+            var document = await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
             var itemsArrayProp = document.RootElement.GetProperty(PropertyInfo.Lookup.ResultListPropertyName);
             foreach (var item in itemsArrayProp.EnumerateArray())
             {
@@ -84,9 +119,30 @@ namespace Volo.Abp.BlazoriseUI.Components.ObjectExtending
             return selectItems;
         }
 
+        protected virtual void SelectedValueChanged(object selectedItem)
+        {
+            SelectedValue = selectedItem;
+        }
+
         protected virtual async Task SearchFilterChangedAsync(string filter)
         {
             lookupItems = await GetLookupItemsAsync(filter);
+        }
+
+        protected virtual void AddHeaders(HttpRequestMessage requestMessage)
+        {
+            if (CurrentTenant.Id.HasValue)
+            {
+                requestMessage.Headers.Add(TenantResolverConsts.DefaultTenantKey, CurrentTenant.Id.Value.ToString());
+            }
+
+            var currentCulture = CultureInfo.CurrentUICulture.Name ?? CultureInfo.CurrentCulture.Name;
+            if (!currentCulture.IsNullOrEmpty())
+            {
+                requestMessage.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue(currentCulture));
+            }
+
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(MimeTypes.Application.Json));
         }
     }
 }
