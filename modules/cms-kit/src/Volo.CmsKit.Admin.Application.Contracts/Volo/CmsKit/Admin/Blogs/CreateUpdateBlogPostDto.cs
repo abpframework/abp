80ad8a12239cfa5c7dadf97f10146389d39733ca
enisn <iletisim@enisnecipoglu.com>
2021-02-01 16:36:50 +0300

    CmsKit - Add missing Validations for CreateUpdateBlogPostDto

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Blogs/CreateUpdateBlogPostDto.cs
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Blogs/CreateUpdateBlogPostDto.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Volo.Abp.Validation;
+using Volo.CmsKit.Blogs;
 
 namespace Volo.CmsKit.Admin.Blogs
 {
     public class CreateUpdateBlogPostDto
     {
+        [Required]
         public Guid BlogId { get; set; }
 
+        [Required]
+        [DynamicMaxLength(typeof(BlogPostConsts), nameof(BlogPostConsts.MaxTitleLength))]
         public string Title { get; set; }
 
+        [Required]
+        [DynamicStringLength(
+            typeof(BlogPostConsts),
+            nameof(BlogPostConsts.MaxUrlSlugLength),
+            nameof(BlogPostConsts.MinUrlSlugLength))]
         public string UrlSlug { get; set; }
 
+        [DynamicMaxLength(typeof(BlogPostConsts), nameof(BlogPostConsts.MaxShortDescriptionLength))]
         public string ShortDescription { get; set; }
     }
 }
