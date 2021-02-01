4b23cb616addb4e653039c3d9e497c2e12377e5a
enisn <iletisim@enisnecipoglu.com>
2021-02-01 15:03:34 +0300

    CmsKit - Add missing validations to BlogDto

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Blogs/BlogDto.cs
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Blogs/BlogDto.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using Volo.Abp.Application.Dtos;
+using Volo.Abp.Validation;
+using Volo.CmsKit.Blogs;
 
 namespace Volo.CmsKit.Admin.Blogs
 {
     public class BlogDto : EntityDto<Guid>
     {
+        [DynamicMaxLength(typeof(BlogConsts), nameof(BlogConsts.MaxNameLength))]
         public string Name { get; set; }
     }
 }
