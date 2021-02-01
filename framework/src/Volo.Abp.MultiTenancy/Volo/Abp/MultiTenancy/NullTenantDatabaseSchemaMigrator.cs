d48e65d23ed912c1e554e4fb49ace48e4ac3e87b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-02-01 20:46:43 +0300

    Introduce ITenantDatabaseSchemaMigrator.

---
+++ framework/src/Volo.Abp.MultiTenancy/Volo/Abp/MultiTenancy/NullTenantDatabaseSchemaMigrator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.MultiTenancy
+{
+    public class NullTenantDatabaseSchemaMigrator : ITenantDatabaseSchemaMigrator, ISingletonDependency
+    {
+        public Task MigrateAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
