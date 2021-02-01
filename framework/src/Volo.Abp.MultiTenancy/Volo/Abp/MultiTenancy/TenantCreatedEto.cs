6d50fc998d4ed8238fa5495c6f2299a22a579f81
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-02-01 20:54:25 +0300

    Define TenantCreatedEto

---
+++ framework/src/Volo.Abp.MultiTenancy/Volo/Abp/MultiTenancy/TenantCreatedEto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.EventBus;
+
+namespace Volo.Abp.MultiTenancy
+{
+    [EventName("abp.multi_tenancy.tenant.created")]
+    public class TenantCreatedEto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
