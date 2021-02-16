2912af84456244ad75e7bf400ee23f101422c751
enisn <iletisim@enisnecipoglu.com>
2021-02-16 13:11:16 +0300

    CmsKit - Add BlogFeatureAdminController & Permissions

---
+++ modules/cms-kit/src/Volo.CmsKit.Admin.HttpApi/Volo/CmsKit/Admin/Blogs/BlogFeatureAdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.GlobalFeatures;
+using Volo.CmsKit.GlobalFeatures;
+using Volo.CmsKit.Permissions;
+
+namespace Volo.CmsKit.Admin.Blogs
+{
+    [RequiresGlobalFeature(typeof(BlogsFeature))]
+    [RemoteService(Name = CmsKitCommonRemoteServiceConsts.RemoteServiceName)]
+    [Area("cms-kit")]
+    [Authorize(CmsKitAdminPermissions.Blogs.Features)]
+    [Route("api/cms-kit-admin/blogs/{blogId}/features")]
+    public class BlogFeatureAdminController : CmsKitAdminController, IBlogFeatureAdminAppService
+    {
+        protected IBlogFeatureAdminAppService BlogFeatureAdminAppService { get; }
+
+        public BlogFeatureAdminController(IBlogFeatureAdminAppService blogFeatureAdminAppService)
+        {
+            BlogFeatureAdminAppService = blogFeatureAdminAppService;
+        }
+
+        [HttpGet]
+        public Task<List<BlogFeatureDto>> GetListAsync(Guid blogId)
+        {
+            return BlogFeatureAdminAppService.GetListAsync(blogId);
+        }
+
+        [HttpPut]
+        public Task SetAsync(Guid blogId, BlogFeatureDto dto)
+        {
+            return BlogFeatureAdminAppService.SetAsync(blogId, dto);
+        }
+    }
+}
