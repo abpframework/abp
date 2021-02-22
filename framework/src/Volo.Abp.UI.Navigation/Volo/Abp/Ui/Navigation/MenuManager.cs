3aaa1d84236b16c4b3b19a9007a128bcc1cafe08
maliming <malimings@gmail.com>
2021-02-22 16:46:44 +0800

    Add RequiredPermissionName to ApplicationMenuItem.

--- framework/src/Volo.Abp.UI.Navigation/Volo/Abp/Ui/Navigation/MenuManager.cs
+++ framework/src/Volo.Abp.UI.Navigation/Volo/Abp/Ui/Navigation/MenuManager.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.DependencyInjection;
 
 namespace Volo.Abp.UI.Navigation
@@ -13,7 +15,7 @@ namespace Volo.Abp.UI.Navigation
         protected IHybridServiceScopeFactory ServiceScopeFactory { get; }
 
         public MenuManager(
-            IOptions<AbpNavigationOptions> options, 
+            IOptions<AbpNavigationOptions> options,
             IHybridServiceScopeFactory serviceScopeFactory)
         {
             ServiceScopeFactory = serviceScopeFactory;
@@ -32,11 +34,59 @@ namespace Volo.Abp.UI.Navigation
                 {
                     await contributor.ConfigureMenuAsync(context);
                 }
+
+                await CheckPermissionsAsync(scope.ServiceProvider, menu);
             }
 
             NormalizeMenu(menu);
 
             return menu;
+        }
+
+        protected virtual async Task CheckPermissionsAsync(IServiceProvider serviceProvider, IHasMenuItems menuWithItems)
+        {
+            var requiredPermissionMenus = new List<ApplicationMenuItem>();
+            GetRequiredPermissionNameMenus(menuWithItems, requiredPermissionMenus);
+
+            if (requiredPermissionMenus.Any())
+            {
+                var permissionChecker = serviceProvider.GetRequiredService<IPermissionChecker>();
+                var grantResult = await permissionChecker.IsGrantedAsync(requiredPermissionMenus.Select(x => x.RequiredPermissionName).ToArray());
+
+                var toBeDeletedMenus = new List<ApplicationMenuItem>();
+                foreach (var menu in requiredPermissionMenus)
+                {
+                    if (grantResult.Result[menu.RequiredPermissionName!] != PermissionGrantResult.Granted)
+                    {
+                        toBeDeletedMenus.Add(menu);
+                    }
+                }
+
+                RemoveMenus(menuWithItems, toBeDeletedMenus);
+            }
+        }
+
+        protected virtual void GetRequiredPermissionNameMenus(IHasMenuItems menuWithItems, List<ApplicationMenuItem> output)
+        {
+            foreach (var menuItem in menuWithItems.Items)
+            {
+                if (!menuItem.RequiredPermissionName.IsNullOrWhiteSpace())
+                {
+                    output.Add(menuItem);
+                }
+
+                GetRequiredPermissionNameMenus(menuItem, output);
+            }
+        }
+
+        protected virtual void RemoveMenus(IHasMenuItems menuWithItems, List<ApplicationMenuItem> toBeDeleted)
+        {
+            menuWithItems.Items.RemoveAll(toBeDeleted.Contains);
+
+            foreach (var menuItem in menuWithItems.Items)
+            {
+                RemoveMenus(menuItem, toBeDeleted);
+            }
         }
 
         protected virtual void NormalizeMenu(IHasMenuItems menuWithItems)
