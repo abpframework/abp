9e12ae0a0b8306fffd2f3cce0b94fe3627eed50d
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2021-02-22 14:27:03 +0800

    UseAutofac

--- framework/src/Volo.Abp.Autofac/Volo/Abp/AbpAutofacAbpApplicationCreationOptionsExtensions.cs
+++ framework/src/Volo.Abp.Autofac/Volo/Abp/AbpAutofacAbpApplicationCreationOptionsExtensions.cs
@@ -1,27 +1,32 @@
 ﻿using Autofac;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
 using Volo.Abp.Autofac;
 
 namespace Volo.Abp
 {
     public static class AbpAutofacAbpApplicationCreationOptionsExtensions
     {
+        static NullLogger<object> Logger = NullLogger<object>.Instance;
+
         public static void UseAutofac(this AbpApplicationCreationOptions options)
         {
+            System.Diagnostics.Debug.WriteLine("装载 Autofac");
             options.Services.AddAutofacServiceProviderFactory();
         }
 
         public static AbpAutofacServiceProviderFactory AddAutofacServiceProviderFactory(this IServiceCollection services)
         {
-            return services.AddAutofacServiceProviderFactory(new ContainerBuilder());
+            ContainerBuilder container = new ContainerBuilder();
+            return services.AddAutofacServiceProviderFactory(container);
         }
 
         public static AbpAutofacServiceProviderFactory AddAutofacServiceProviderFactory(this IServiceCollection services, ContainerBuilder containerBuilder)
         {
-            var factory = new AbpAutofacServiceProviderFactory(containerBuilder);
+            AbpAutofacServiceProviderFactory factory = new AbpAutofacServiceProviderFactory(containerBuilder);
 
             services.AddObjectAccessor(containerBuilder);
-            services.AddSingleton((IServiceProviderFactory<ContainerBuilder>) factory);
+            services.AddSingleton((IServiceProviderFactory<ContainerBuilder>)factory);
 
             return factory;
         }
--- modules/blogging/app/Volo.BloggingTestApp/Controllers/HomeController.cs
+++ modules/blogging/app/Volo.BloggingTestApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
 using Volo.Abp.AspNetCore.Mvc;
 using Volo.Blogging;
 
@@ -15,8 +16,7 @@ namespace Volo.BloggingTestApp.Controllers
         }
         public ActionResult Index()
         {
-            var urlPrefix = _blogOptions.RoutePrefix;
-            return Redirect(urlPrefix);
+            return Json(new {Now=DateTime.Now });
         }
     }
 }
