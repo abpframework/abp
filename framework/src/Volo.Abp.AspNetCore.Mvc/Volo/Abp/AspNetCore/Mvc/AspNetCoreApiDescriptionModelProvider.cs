8e72d54d3f4328cb7536e5419a0d2d2ea8fb3efe
maliming <malimings@gmail.com>
2021-02-24 16:56:03 +0800

    Add JsonName to ParameterApiDescriptionModel.

--- framework/src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/AspNetCoreApiDescriptionModelProvider.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc/Volo/Abp/AspNetCore/Mvc/AspNetCoreApiDescriptionModelProvider.cs
@@ -272,6 +272,7 @@ namespace Volo.Abp.AspNetCore.Mvc
 
                 actionModel.AddParameter(ParameterApiDescriptionModel.Create(
                         parameterDescription.Name,
+                        _options.ApiParameterNameGenerator?.Invoke(parameterDescription),
                         matchedMethodParamName,
                         parameterDescription.Type,
                         parameterDescription.RouteInfo?.IsOptional ?? false,
