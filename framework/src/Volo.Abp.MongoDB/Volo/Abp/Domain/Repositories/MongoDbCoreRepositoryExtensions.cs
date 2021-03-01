149755af7170321f9fbf7ca00fa020097ff8a613
PM Extra <pm@jubeat.net>
2021-03-01 16:37:08 +0800

    Replace IBasicRepository with IReadOnlyBasicRepository.

--- framework/src/Volo.Abp.MongoDB/Volo/Abp/Domain/Repositories/MongoDbCoreRepositoryExtensions.cs
+++ framework/src/Volo.Abp.MongoDB/Volo/Abp/Domain/Repositories/MongoDbCoreRepositoryExtensions.cs
@@ -11,51 +11,51 @@ namespace Volo.Abp.Domain.Repositories
     public static class MongoDbCoreRepositoryExtensions
     {
         [Obsolete("Use GetDatabaseAsync method.")]
-        public static IMongoDatabase GetDatabase<TEntity>(this IBasicRepository<TEntity> repository)
+        public static IMongoDatabase GetDatabase<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().Database;
         }
 
-        public static Task<IMongoDatabase> GetDatabaseAsync<TEntity>(this IBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
+        public static Task<IMongoDatabase> GetDatabaseAsync<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().GetDatabaseAsync(cancellationToken);
         }
 
         [Obsolete("Use GetCollectionAsync method.")]
-        public static IMongoCollection<TEntity> GetCollection<TEntity>(this IBasicRepository<TEntity> repository)
+        public static IMongoCollection<TEntity> GetCollection<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().Collection;
         }
 
-        public static Task<IMongoCollection<TEntity>> GetCollectionAsync<TEntity>(this IBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
+        public static Task<IMongoCollection<TEntity>> GetCollectionAsync<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().GetCollectionAsync(cancellationToken);
         }
 
         [Obsolete("Use GetMongoQueryableAsync method.")]
-        public static IMongoQueryable<TEntity> GetMongoQueryable<TEntity>(this IBasicRepository<TEntity> repository)
+        public static IMongoQueryable<TEntity> GetMongoQueryable<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().GetMongoQueryable();
         }
 
-        public static Task<IMongoQueryable<TEntity>> GetMongoQueryableAsync<TEntity>(this IBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
+        public static Task<IMongoQueryable<TEntity>> GetMongoQueryableAsync<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().GetMongoQueryableAsync(cancellationToken);
         }
 
-        public static Task<IAggregateFluent<TEntity>> GetAggregateAsync<TEntity>(this IBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
+        public static Task<IAggregateFluent<TEntity>> GetAggregateAsync<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, CancellationToken cancellationToken = default)
             where TEntity : class, IEntity
         {
             return repository.ToMongoDbRepository().GetAggregateAsync(cancellationToken);
         }
 
-        public static IMongoDbRepository<TEntity> ToMongoDbRepository<TEntity>(this IBasicRepository<TEntity> repository)
+        public static IMongoDbRepository<TEntity> ToMongoDbRepository<TEntity>(this IReadOnlyBasicRepository<TEntity> repository)
             where TEntity : class, IEntity
         {
             if (repository is IMongoDbRepository<TEntity> mongoDbRepository)
