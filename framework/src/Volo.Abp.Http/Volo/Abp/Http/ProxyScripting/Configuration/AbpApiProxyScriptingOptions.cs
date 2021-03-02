36097d2e402df345eaf16b98ae9a577f81612f13
Ahmet <ahmet.cotur@hotmail.com>
2021-03-02 18:06:30 +0300

    PropertyNameGenerator moved to static class

--- framework/src/Volo.Abp.Http/Volo/Abp/Http/ProxyScripting/Configuration/AbpApiProxyScriptingOptions.cs
+++ framework/src/Volo.Abp.Http/Volo/Abp/Http/ProxyScripting/Configuration/AbpApiProxyScriptingOptions.cs
@@ -1,36 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 
 namespace Volo.Abp.Http.ProxyScripting.Configuration
 {
     public class AbpApiProxyScriptingOptions
     {
         public IDictionary<string, Type> Generators { get; }
-
-        public static Func<PropertyInfo, string> PropertyNameGenerator { get; set; }
-
-        static AbpApiProxyScriptingOptions()
-        {
-            PropertyNameGenerator = propertyInfo =>
-            {
-                var jsonPropertyNameAttribute = propertyInfo.GetSingleAttributeOrNull<System.Text.Json.Serialization.JsonPropertyNameAttribute>(true);
-
-                if (jsonPropertyNameAttribute != null)
-                {
-                    return jsonPropertyNameAttribute.Name;
-                }
-                
-                var jsonPropertyAttribute = propertyInfo.GetSingleAttributeOrNull<Newtonsoft.Json.JsonPropertyAttribute>(true);
-
-                if (jsonPropertyAttribute != null)
-                {
-                    return jsonPropertyAttribute.PropertyName;
-                }
-
-                return null;
-            };
-        }
 
         public AbpApiProxyScriptingOptions()
         {
