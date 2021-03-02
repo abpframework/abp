5e32bfd579220ae0779032b5ce13e84d789789e8
enisn <iletisim@enisnecipoglu.com>
2021-03-02 13:29:07 +0300

    CmsKit - Refactoring for DefaultMediaDescriptorDefinitionStore

--- modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/MediaDescriptors/DefaultMediaDescriptorDefinitionStore.cs
+++ modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/MediaDescriptors/DefaultMediaDescriptorDefinitionStore.cs
@@ -25,7 +25,7 @@ namespace Volo.CmsKit.MediaDescriptors
         /// <param name="entityType">EntityType to get definition.</param>
         /// <exception cref="EntityCantHaveMediaException">Thrown when EntityType is not configured as taggable.</exception>
         /// <exception cref="InvalidOperationException">More than one element satisfies the condition in predicate.</exception>
-        public Task<MediaDescriptorDefinition> GetDefinitionAsync([NotNull] string entityType)
+        public virtual Task<MediaDescriptorDefinition> GetDefinitionAsync([NotNull] string entityType)
         {
             Check.NotNullOrWhiteSpace(entityType, nameof(entityType));
 
@@ -35,7 +35,7 @@ namespace Volo.CmsKit.MediaDescriptors
             return Task.FromResult(result);
         }
 
-        public Task<bool> IsDefinedAsync([NotNull] string entityType)
+        public virtual Task<bool> IsDefinedAsync([NotNull] string entityType)
         {
             Check.NotNullOrEmpty(entityType, nameof(entityType));
 
