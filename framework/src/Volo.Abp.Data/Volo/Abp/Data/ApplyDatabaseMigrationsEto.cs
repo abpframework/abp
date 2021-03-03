9e1ae1171bc086e315d8051d5d909a833bfa4315
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-03-03 11:44:48 +0300

    Inherit ApplyDatabaseMigrationsEto from EtoBase

--- framework/src/Volo.Abp.Data/Volo/Abp/Data/ApplyDatabaseMigrationsEto.cs
+++ framework/src/Volo.Abp.Data/Volo/Abp/Data/ApplyDatabaseMigrationsEto.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Volo.Abp.Domain.Entities.Events.Distributed;
 using Volo.Abp.EventBus;
 
 namespace Volo.Abp.Data
 {
     [Serializable]
     [EventName("abp.data.apply_database_migrations")]
-    public class ApplyDatabaseMigrationsEto
+    public class ApplyDatabaseMigrationsEto : EtoBase
     {
         public Guid? TenantId { get; set; }
         
