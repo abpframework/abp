06a318fca3199b0d127db3cfcb58492ae3538688
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-03-04 11:37:02 +0300

    Use distributed events to handle role delete & rename for permission management.

--- modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/IdentityRoleNameChangedEvent.cs
+++ modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/IdentityRoleNameChangedEvent.cs
@@ -1,5 +1,8 @@
-﻿namespace Volo.Abp.Identity
+﻿using System;
+
+namespace Volo.Abp.Identity
 {
+    [Obsolete("Use the distributed event (IdentityRoleNameChangedEto) instead.")]
     public class IdentityRoleNameChangedEvent
     {
         public IdentityRole IdentityRole { get; set; }
