2b9b56a75bca7adc4242fa411398ba2ce6318aed
liangshiwei <liangshiw@outlook.com>
2021-03-04 21:54:25 +0800

    Add unit tests

--- modules/identity/src/Volo.Abp.PermissionManagement.Domain.Identity/Volo/Abp/PermissionManagement/Identity/RoleDeletedEventHandler.cs
+++ modules/identity/src/Volo.Abp.PermissionManagement.Domain.Identity/Volo/Abp/PermissionManagement/Identity/RoleDeletedEventHandler.cs
@@ -3,21 +3,22 @@ using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Entities.Events.Distributed;
 using Volo.Abp.EventBus;
+using Volo.Abp.EventBus.Distributed;
 using Volo.Abp.Identity;
 
 namespace Volo.Abp.PermissionManagement.Identity
 {
     public class RoleDeletedEventHandler :
-        ILocalEventHandler<EntityDeletedEto<IdentityRoleEto>>,
+        IDistributedEventHandler<EntityDeletedEto<IdentityRoleEto>>,
         ITransientDependency
     {
         protected IPermissionManager PermissionManager { get; }
-    
+
         public RoleDeletedEventHandler(IPermissionManager permissionManager)
         {
             PermissionManager = permissionManager;
         }
-    
+
         public async Task HandleEventAsync(EntityDeletedEto<IdentityRoleEto> eventData)
         {
             await PermissionManager.DeleteAsync(RolePermissionValueProvider.ProviderName, eventData.Entity.Name);
