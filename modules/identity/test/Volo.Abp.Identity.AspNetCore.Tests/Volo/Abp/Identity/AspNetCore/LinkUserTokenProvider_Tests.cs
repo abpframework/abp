c85dd1df0b0e78515a61de2ab2414d4a4d093ef9
maliming <malimings@gmail.com>
2021-03-06 14:25:42 +0800

    Fix GenerateAndVerifyLinkTokenAsync test.

--- modules/identity/test/Volo.Abp.Identity.AspNetCore.Tests/Volo/Abp/Identity/AspNetCore/LinkUserTokenProvider_Tests.cs
+++ modules/identity/test/Volo.Abp.Identity.AspNetCore.Tests/Volo/Abp/Identity/AspNetCore/LinkUserTokenProvider_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Shouldly;
 using Xunit;
@@ -7,6 +8,19 @@ namespace Volo.Abp.Identity.AspNetCore
 {
     public class LinkUserTokenProvider_Tests : AbpIdentityAspNetCoreTestBase
     {
+        protected IIdentityUserRepository UserRepository { get; }
+        protected IIdentityLinkUserRepository IdentityLinkUserRepository { get; }
+        protected IdentityLinkUserManager IdentityLinkUserManager { get; }
+        protected IdentityTestData TestData { get; }
+
+        public LinkUserTokenProvider_Tests()
+        {
+            UserRepository = GetRequiredService<IIdentityUserRepository>();
+            IdentityLinkUserRepository = GetRequiredService<IIdentityLinkUserRepository>();
+            IdentityLinkUserManager = GetRequiredService<IdentityLinkUserManager>();
+            TestData = GetRequiredService<IdentityTestData>();
+        }
+
         [Fact]
         public void LinkUserTokenProvider_Should_Be_Register()
         {
@@ -15,6 +29,16 @@ namespace Volo.Abp.Identity.AspNetCore
             identityOptions.Tokens.ProviderMap.ShouldContain(x =>
                 x.Key == LinkUserTokenProviderConsts.LinkUserTokenProviderName &&
                 x.Value.ProviderType == typeof(LinkUserTokenProvider));
+        }
+
+        [Fact]
+        public virtual async Task GenerateAndVerifyLinkTokenAsync()
+        {
+            var john = await UserRepository.GetAsync(TestData.UserJohnId);
+            var token = await IdentityLinkUserManager.GenerateLinkTokenAsync(new IdentityLinkUserInfo(john.Id, john.TenantId));
+            (await IdentityLinkUserManager.VerifyLinkTokenAsync(new IdentityLinkUserInfo(john.Id, john.TenantId), token)).ShouldBeTrue();
+
+            (await IdentityLinkUserManager.VerifyLinkTokenAsync(new IdentityLinkUserInfo(john.Id, john.TenantId), "123123")).ShouldBeFalse();
         }
     }
 }
