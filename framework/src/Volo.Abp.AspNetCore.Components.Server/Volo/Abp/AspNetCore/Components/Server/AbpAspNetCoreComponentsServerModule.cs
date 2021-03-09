4400ba39a4dc02b1932cd14fd6525adffa8fcf4e
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-03-09 14:01:01 +0300

    Create Volo.Abp.AspNetCore.Components.Server package.

---
+++ framework/src/Volo.Abp.AspNetCore.Components.Server/Volo/Abp/AspNetCore/Components/Server/AbpAspNetCoreComponentsServerModule.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Modularity;
+
+namespace Volo.Abp.AspNetCore.Components.Server
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreModule)
+        )]
+    public class AbpAspNetCoreComponentsServerModule : AbpModule
+    {
+    }
+}
