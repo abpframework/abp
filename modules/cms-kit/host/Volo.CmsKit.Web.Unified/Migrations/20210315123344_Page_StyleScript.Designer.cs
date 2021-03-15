fcfc8a02260a6a54b543f2de63f8a0f0b0357831
enisn <iletisim@enisnecipoglu.com>
2021-03-15 16:04:43 +0300

    CmsKit - Add migrations for PAge Style & Script

--- modules/cms-kit/host/Volo.CmsKit.Web.Unified/Migrations/20210315123344_Page_StyleScript.Designer.cs
+++ modules/cms-kit/host/Volo.CmsKit.Web.Unified/Migrations/20210315123344_Page_StyleScript.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.CmsKit.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using Volo.CmsKit.EntityFrameworkCore;
 namespace Volo.CmsKit.Migrations
 {
     [DbContext(typeof(UnifiedDbContext))]
-    partial class UnifiedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210315123344_Page_StyleScript")]
+    partial class Page_StyleScript
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
