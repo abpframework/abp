f2c545c556fc422f58bb333124cb9d74101426c3
enisn <iletisim@enisnecipoglu.com>
2021-03-16 15:55:34 +0300

    CmsKit - Add CmsUserFeature

---
+++ modules/cms-kit/src/Volo.CmsKit.Domain.Shared/Volo/CmsKit/GlobalFeatures/CmsUserFeature.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using Volo.Abp.GlobalFeatures;
+
+namespace Volo.CmsKit.GlobalFeatures
+{
+    [GlobalFeatureName(Name)]
+    public class CmsUserFeature : GlobalFeature
+    {
+        public const string Name = "CmsKit.User";
+
+        internal CmsUserFeature([NotNull] GlobalModuleFeatures module) : base(module)
+        {
+        }
+    }
+}
