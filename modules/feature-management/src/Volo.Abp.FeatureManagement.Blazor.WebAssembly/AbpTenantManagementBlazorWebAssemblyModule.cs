d1e010af266d21818501b273025023d79eecf3f5
liangshiwei <liangshiw@outlook.com>
2021-03-18 10:59:07 +0800

    Introduce WebAssembly and Server packages for the Feature Management Blazor UI

---
+++ modules/feature-management/src/Volo.Abp.FeatureManagement.Blazor.WebAssembly/AbpTenantManagementBlazorWebAssemblyModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.FeatureManagement.Blazor.WebAssembly
+{
+    [DependsOn(
+        typeof(AbpFeatureManagementBlazorModule),
+        typeof(AbpAspNetCoreComponentsWebAssemblyThemingModule)
+    )]
+    public class AbpTenantManagementBlazorWebAssemblyModule : AbpModule
+    {
+    }
+}
