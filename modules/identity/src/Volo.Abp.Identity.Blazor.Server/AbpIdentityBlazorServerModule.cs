21eb8cfb1510f78bfdbe4c7d72e24e59afa7b6a6
liangshiwei <liangshiw@outlook.com>
2021-03-18 11:32:31 +0800

    Introduce WebAssembly and Server packages for the Identity Blazor UI

---
+++ modules/identity/src/Volo.Abp.Identity.Blazor.Server/AbpIdentityBlazorServerModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Components.Server.Theming;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.Identity.Blazor.Server
+{
+    [DependsOn(
+        typeof(AbpIdentityBlazorModule),
+        typeof(AbpAspNetCoreComponentsServerThemingModule)
+    )]
+    public class AbpIdentityBlazorServerModule : AbpModule
+    {
+    }
+}
---
+++ modules/identity/src/Volo.Abp.Identity.Blazor.WebAssembly/AbpIdentityBlazorWebAssemblyModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.Identity.Blazor.WebAssembly
+{
+    [DependsOn(
+        typeof(AbpIdentityBlazorModule),
+        typeof(AbpAspNetCoreComponentsWebAssemblyThemingModule)
+    )]
+    public class AbpIdentityBlazorWebAssemblyModule : AbpModule
+    {
+    }
+}
