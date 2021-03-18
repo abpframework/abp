fbcd1636832a206e9d737701ceb2dac7b8ed0c22
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-03-18 20:55:16 +0300

    Added initial blazor server tiered project for the startup template.

---
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor.Server.Tiered/Menus/MyProjectNameMenuContributor.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using MyCompanyName.MyProjectName.Localization;
+using Volo.Abp.UI.Navigation;
+
+namespace MyCompanyName.MyProjectName.Blazor.Server.Tiered.Menus
+{
+    public class MyProjectNameMenuContributor : IMenuContributor
+    {
+        public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+        {
+            if (context.Menu.Name == StandardMenus.Main)
+            {
+                await ConfigureMainMenuAsync(context);
+            }
+        }
+
+        private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+        {
+            var l = context.GetLocalizer<MyProjectNameResource>();
+
+            context.Menu.Items.Insert(
+                0,
+                new ApplicationMenuItem(
+                    MyProjectNameMenus.Home,
+                    l["Menu:Home"],
+                    "/",
+                    icon: "fas fa-home"
+                )
+            );
+
+            return Task.CompletedTask;
+        }
+    }
+}
