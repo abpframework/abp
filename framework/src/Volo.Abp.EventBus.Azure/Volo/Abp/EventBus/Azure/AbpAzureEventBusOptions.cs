08e7aedbd9213b55d213363bbcef28a0a7ee8c28
Gideon de Swardt <gideon@fusedigital.io>
2021-04-03 22:31:37 +0100

    Implement Distribute Events Bus with Azure
    Implement a new instance of the IDistributedEventBus that uses the
    AzureServiceBus module.
    
    Added a service that will replace the existing instance in the service
    registry for the distributed event bus with the Azure instance.
    
    Added configuration options for the Azure Event Bus.

---
+++ framework/src/Volo.Abp.EventBus.Azure/Volo/Abp/EventBus/Azure/AbpAzureEventBusOptions.cs
@@ -0,0 +1,11 @@
+namespace Volo.Abp.EventBus.Azure
+{
+    public class AbpAzureEventBusOptions
+    {
+        public string ConnectionName { get; set; }
+
+        public string SubscriberName { get; set; }
+
+        public string TopicName { get; set; }
+    }
+}
---
+++ framework/src/Volo.Abp.EventBus.Azure/Volo/Abp/EventBus/Azure/AbpEventBusAzureModule.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.AzureServiceBus;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.EventBus.Azure
+{
+    [DependsOn(
+        typeof(AbpEventBusModule),
+        typeof(AbpAzureServiceBusModule)
+    )]
+    public class AbpEventBusAzureModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+
+            Configure<AbpAzureEventBusOptions>(configuration.GetSection("Azure:EventBus"));
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            context
+                .ServiceProvider
+                .GetRequiredService<AzureDistributedEventBus>()
+                .Initialize();
+        }
+    }
+}
