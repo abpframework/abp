ba02106bf6ecdf72ff8ae8a1a85fc16903d8dab2
maliming <malimings@gmail.com>
2021-04-05 16:13:13 +0800

    Depend on AbpTextTemplatingScribanModule.

--- framework/src/Volo.Abp.TextTemplating/Volo/Abp/TextTemplating/AbpTextTemplatingModule.cs
+++ framework/src/Volo.Abp.TextTemplating/Volo/Abp/TextTemplating/AbpTextTemplatingModule.cs
@@ -1,7 +1,11 @@
 ﻿using Volo.Abp.Modularity;
+using Volo.Abp.TextTemplating.Scriban;
 
 namespace Volo.Abp.TextTemplating
 {
+    [DependsOn(
+        typeof(AbpTextTemplatingScribanModule)
+    )]
     public class AbpTextTemplatingModule : AbpModule
     {
 
