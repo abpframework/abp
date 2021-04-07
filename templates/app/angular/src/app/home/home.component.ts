ba007b3672c94cf3aec32c0c46c87318052c3c8f
mehmet-erim <bilgi@mehmeterim.com>
2021-04-07 16:24:46 +0300

    rename home.component.css as home.component.scss

--- templates/app/angular/src/app/home/home.component.ts
+++ templates/app/angular/src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { OAuthService } from 'angular-oauth2-oidc';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   get hasLoggedIn(): boolean {
