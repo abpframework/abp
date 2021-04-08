7b1cfad3ba58e7f8e06abb5332ea50f8a3921dec
maliming <malimings@gmail.com>
2021-04-08 21:56:38 +0800

    Add state check to permissions.
    Resolve #8518

---
+++ framework/src/Volo.Abp.Authorization/Volo/Abp/Authorization/Permissions/PermissionDefinitionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Volo.Abp.Authorization.Permissions
+{
+    public static class PermissionDefinitionExtensions
+    {
+        public const string PropertyName = "_AbpPermissionStateProviders";
+
+        public static PermissionDefinition AddStateProvider(
+            [NotNull] this PermissionDefinition permissionDefinition,
+            [NotNull] params IPermissionStateProvider[] permissionStateProviders)
+        {
+            var stateProviders = permissionDefinition.GetStateProvidersInternal();
+
+            foreach (var provider in permissionStateProviders)
+            {
+                stateProviders.AddIfNotContains(provider);
+            }
+
+            return permissionDefinition;
+        }
+
+        public static IReadOnlyList<IPermissionStateProvider> GetStateProviders([NotNull] this PermissionDefinition permissionDefinition)
+        {
+            return permissionDefinition.GetStateProvidersInternal();
+        }
+
+        private static List<IPermissionStateProvider> GetStateProvidersInternal([NotNull] this PermissionDefinition permissionDefinition)
+        {
+            return (List<IPermissionStateProvider>) permissionDefinition.Properties.GetOrAdd(PropertyName, () => new List<IPermissionStateProvider>());
+        }
+    }
+}
--- modules/permission-management/test/Volo.Abp.PermissionManagement.Domain.Tests/Volo/Abp/PermissionManagement/PermissionChecker_User_Tests.cs
+++ modules/permission-management/test/Volo.Abp.PermissionManagement.Domain.Tests/Volo/Abp/PermissionManagement/PermissionChecker_User_Tests.cs
@@ -11,10 +11,12 @@ namespace Volo.Abp.PermissionManagement
     public class PermissionChecker_User_Tests : PermissionTestBase
     {
         private readonly IPermissionChecker _permissionChecker;
+        private readonly ICurrentPrincipalAccessor _currentPrincipalAccessor;
 
         public PermissionChecker_User_Tests()
         {
             _permissionChecker = GetRequiredService<IPermissionChecker>();
+            _currentPrincipalAccessor = GetRequiredService<ICurrentPrincipalAccessor>();
         }
 
         [Fact]
@@ -62,6 +64,27 @@ namespace Volo.Abp.PermissionManagement
                 CreatePrincipal(PermissionTestDataBuilder.User1Id, Guid.NewGuid()),
                 "MyPermission3"
             )).ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task Should_Return_False_For_Granted_Current_User_If_The_Permission_State_Is_Disabled()
+        {
+            (await _permissionChecker.IsGrantedAsync(
+                CreatePrincipal(PermissionTestDataBuilder.User1Id, Guid.NewGuid()),
+                "MyPermission5"
+            )).ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task Should_Return_True_For_Granted_Current_User_If_The_Permission_State_Is_Enabled()
+        {
+            using (_currentPrincipalAccessor.Change(new Claim(AbpClaimTypes.Role, "super-admin")))
+            {
+                (await _permissionChecker.IsGrantedAsync(
+                    CreatePrincipal(PermissionTestDataBuilder.User1Id, Guid.NewGuid()),
+                    "MyPermission5"
+                )).ShouldBeTrue();
+            }
         }
 
         private static ClaimsPrincipal CreatePrincipal(Guid? userId, Guid? tenantId = null)
