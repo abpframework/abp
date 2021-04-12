4a8f4b39fb16aa0cdca390269107f01ff00fab49
maliming <malimings@gmail.com>
2021-04-12 16:56:03 +0800

    Refactor.

--- modules/docs/src/Volo.Docs.Domain/Volo/Docs/Documents/FullSearch/Elastic/DocsElasticSearchBasicAuthenticationOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Volo.Docs.Documents.FullSearch.Elastic
-{
-    public class DocsElasticSearchBasicAuthenticationOptions
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
