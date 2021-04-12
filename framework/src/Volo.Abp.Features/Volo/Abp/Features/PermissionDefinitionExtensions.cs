cbe48196941e0ba60d2c123de6938a1a370ec90f
maliming <malimings@gmail.com>
2021-04-12 15:10:28 +0800

    Return PermissionDefinition in extension methods.

--- framework/src/Volo.Abp.Features/Volo/Abp/Features/PermissionDefinitionExtensions.cs
+++ framework/src/Volo.Abp.Features/Volo/Abp/Features/PermissionDefinitionExtensions.cs
@@ -4,11 +4,11 @@ namespace Volo.Abp.Features
 {
     public static class FeatureDefinitionExtensions
     {
-        public static void RequireFeatures(this PermissionDefinition permissionDefinition, params string[] features)
+        public static PermissionDefinition RequireFeatures(this PermissionDefinition permissionDefinition, params string[] features)
         {
             Check.NotNullOrEmpty(features, nameof(features));
 
-            permissionDefinition.AddStateProvider(new RequireFeaturesPermissionStateProvider(features));
+            return permissionDefinition.AddStateProvider(new RequireFeaturesPermissionStateProvider(features));
         }
     }
 }
--- framework/src/Volo.Abp.GlobalFeatures/Volo/Abp/GlobalFeatures/GlobalFeatureDefinitionExtensions.cs
+++ framework/src/Volo.Abp.GlobalFeatures/Volo/Abp/GlobalFeatures/GlobalFeatureDefinitionExtensions.cs
@@ -4,11 +4,11 @@ namespace Volo.Abp.GlobalFeatures
 {
     public static class GlobalFeatureDefinitionExtensions
     {
-        public static void RequireGlobalFeatures(this PermissionDefinition permissionDefinition, params string[] globalFeatures)
+        public static PermissionDefinition RequireGlobalFeatures(this PermissionDefinition permissionDefinition, params string[] globalFeatures)
         {
             Check.NotNullOrEmpty(globalFeatures, nameof(globalFeatures));
 
-            permissionDefinition.AddStateProvider(new RequireGlobalFeaturesPermissionStateProvider(globalFeatures));
+            return permissionDefinition.AddStateProvider(new RequireGlobalFeaturesPermissionStateProvider(globalFeatures));
         }
     }
 }
