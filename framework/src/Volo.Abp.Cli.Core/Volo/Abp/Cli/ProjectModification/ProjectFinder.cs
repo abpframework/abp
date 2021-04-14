a36fe74269866cc6f94cddda37fdccb402b44839
Alper Ebicoglu <alper.ebicoglu@volosoft.com>
2021-04-14 03:20:34 +0300

    refactor  BlazorProjectTypeChecker #8573

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectModification/ProjectFinder.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectModification/ProjectFinder.cs
@@ -51,10 +51,16 @@ namespace Volo.Abp.Cli.ProjectModification
                     return FindProjectEndsWith(projectFiles, assemblyNames, ".Blazor");
                 case NuGetPackageTarget.BlazorWebAssembly:
                     var BlazorWebAssemblyTargetProject = FindProjectEndsWith(projectFiles, assemblyNames, ".Blazor");
-                    return BlazorWebAssemblyTargetProject != null && !BlazorProjectTypeChecker.IsBlazorServerProject(BlazorWebAssemblyTargetProject) ? BlazorWebAssemblyTargetProject : null;
+                    return BlazorWebAssemblyTargetProject != null &&
+                           !BlazorProjectTypeChecker.IsBlazorServerProject(BlazorWebAssemblyTargetProject)
+                        ? BlazorWebAssemblyTargetProject
+                        : null;
                 case NuGetPackageTarget.BlazorServer:
                     var BlazorServerTargetProject = FindProjectEndsWith(projectFiles, assemblyNames, ".Blazor");
-                    return BlazorServerTargetProject != null && BlazorProjectTypeChecker.IsBlazorServerProject(BlazorServerTargetProject) ? BlazorServerTargetProject : null;
+                    return BlazorServerTargetProject != null &&
+                           BlazorProjectTypeChecker.IsBlazorServerProject(BlazorServerTargetProject)
+                        ? BlazorServerTargetProject
+                        : null;
                 default:
                     return null;
             }
@@ -95,7 +101,9 @@ namespace Volo.Abp.Cli.ProjectModification
             }
 
             project = FindProjectEndsWith(projectFiles, assemblyNames, ".Blazor");
-            if (project != null && BlazorProjectTypeChecker.IsBlazorServerProject(project))
+            if (project != null &&
+                File.Exists(project) &&
+                BlazorProjectTypeChecker.IsBlazorServerProject(project))
             {
                 projects.Add(project);
             }
@@ -126,8 +134,8 @@ namespace Volo.Abp.Cli.ProjectModification
             for (var i = 0; i < assemblyNames.Length; i++)
             {
                 var assemblyName = assemblyNames[i];
-                if(assemblyName.EndsWith(postfix, StringComparison.OrdinalIgnoreCase) &&
-                   (excludePostfix == null || !assemblyName.EndsWith(excludePostfix)))
+                if (assemblyName.EndsWith(postfix, StringComparison.OrdinalIgnoreCase) &&
+                    (excludePostfix == null || !assemblyName.EndsWith(excludePostfix)))
                 {
                     return projectFiles[i];
                 }
