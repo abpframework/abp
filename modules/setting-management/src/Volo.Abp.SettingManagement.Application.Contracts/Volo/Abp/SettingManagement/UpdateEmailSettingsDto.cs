7467db126ebe4dc98ba6e3e07daa632fa0c30881
Alper Ebicoglu <ebicoglu@users.noreply.github.com>
2021-04-14 17:58:55 +0300

    increase string field max sizes

--- modules/setting-management/src/Volo.Abp.SettingManagement.Application.Contracts/Volo/Abp/SettingManagement/UpdateEmailSettingsDto.cs
+++ modules/setting-management/src/Volo.Abp.SettingManagement.Application.Contracts/Volo/Abp/SettingManagement/UpdateEmailSettingsDto.cs
@@ -11,27 +11,28 @@ namespace Volo.Abp.SettingManagement
         [Range(1, 65535)]
         public int SmtpPort { get; set; }
 
-        [MaxLength(128)]
+        [MaxLength(1024)]
         public string SmtpUserName { get; set; }
 
-        [MaxLength(64)]
+        [MaxLength(1024)]
         [DataType(DataType.Password)]
         [DisableAuditing]
         public string SmtpPassword { get; set; }
 
-        [MaxLength(256)]
+        [MaxLength(1024)]
         public string SmtpDomain { get; set; }
 
         public bool SmtpEnableSsl { get; set; }
 
         public bool SmtpUseDefaultCredentials { get; set; }
 
-        [MaxLength(256)]
+        [MaxLength(1024)]
         [Required]
         public string DefaultFromAddress { get; set; }
 
-        [MaxLength(64)]
+        [MaxLength(1024)]
         [Required]
         public string DefaultFromDisplayName { get; set; }
     }
 }
+
