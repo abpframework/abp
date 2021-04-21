d18094a997b097927d2f1b83db25d5ac9240dafe
enisn <info@enisnecipoglu.com>
2021-04-21 16:57:06 +0300

    Add GetUrlOrDefaultAsync to IAppUrlProvider

--- framework/src/Volo.Abp.UI.Navigation/Volo/Abp/Ui/Navigation/Urls/IAppUrlProvider.cs
+++ framework/src/Volo.Abp.UI.Navigation/Volo/Abp/Ui/Navigation/Urls/IAppUrlProvider.cs
@@ -7,6 +7,8 @@ namespace Volo.Abp.UI.Navigation.Urls
     {
         Task<string> GetUrlAsync([NotNull] string appName, [CanBeNull] string urlName = null);
 
+        Task<string> GetUrlOrDefaultAsync([NotNull] string appName, [CanBeNull] string urlName = null);
+
         bool IsRedirectAllowedUrl(string url);
     }
 }
