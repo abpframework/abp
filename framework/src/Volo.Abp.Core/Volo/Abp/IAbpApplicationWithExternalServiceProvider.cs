78a47a81c030ba30c9212cd3e614f8361fed3efe
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-29 23:55:21 +0300

    Added ability to set/create service provider without initializing the ABP application.

--- framework/src/Volo.Abp.Core/Volo/Abp/IAbpApplicationWithExternalServiceProvider.cs
+++ framework/src/Volo.Abp.Core/Volo/Abp/IAbpApplicationWithExternalServiceProvider.cs
@@ -5,6 +5,16 @@ namespace Volo.Abp
 {
     public interface IAbpApplicationWithExternalServiceProvider : IAbpApplication
     {
+        /// <summary>
+        /// Sets the service provider, but not initializes the modules.
+        /// </summary>
+        void SetServiceProvider([NotNull] IServiceProvider serviceProvider);
+        
+        /// <summary>
+        /// Sets the service provider and initializes all the modules.
+        /// If <see cref="SetServiceProvider"/> was called before, the same
+        /// <see cref="serviceProvider"/> instance should be passed to this method.
+        /// </summary>
         void Initialize([NotNull] IServiceProvider serviceProvider);
     }
 }
