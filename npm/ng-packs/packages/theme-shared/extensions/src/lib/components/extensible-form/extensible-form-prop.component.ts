f26c9380e088f4e9de23ab9bc11c367c0943c453
mehmet-erim <bilgi@mehmeterim.com>
2021-06-22 11:56:41 +0300

    trigger focus on first element of extensible form

--- npm/ng-packs/packages/theme-shared/extensions/src/lib/components/extensible-form/extensible-form-prop.component.ts
+++ npm/ng-packs/packages/theme-shared/extensions/src/lib/components/extensible-form/extensible-form-prop.component.ts
@@ -1,13 +1,22 @@
-import { ABP, AbpValidators, ConfigStateService, TrackByService } from '@abp/ng.core';
 import {
+  ABP,
+  AbpValidators,
+  AutofocusDirective,
+  ConfigStateService,
+  TrackByService,
+} from '@abp/ng.core';
+import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ElementRef,
   Input,
   OnChanges,
   Optional,
   SimpleChanges,
   SkipSelf,
+  ViewChild,
 } from '@angular/core';
 import {
   ControlContainer,
@@ -43,10 +52,14 @@ import { addTypeaheadTextSuffix } from '../../utils/typeahead.util';
     { provide: NgbTimeAdapter, useClass: TimeAdapter },
   ],
 })
-export class ExtensibleFormPropComponent implements OnChanges {
+export class ExtensibleFormPropComponent implements OnChanges, AfterViewInit {
   @Input() data: PropData;
 
   @Input() prop: FormProp;
+
+  @Input() first: boolean;
+
+  @ViewChild('field') private fieldRef: ElementRef<HTMLElement>;
 
   asterisk = '';
 
@@ -114,6 +127,13 @@ export class ExtensibleFormPropComponent implements OnChanges {
 
   private setAsterisk() {
     this.asterisk = this.validators.some(isRequired) ? '*' : '';
+  }
+
+  ngAfterViewInit() {
+    if (this.first && this.fieldRef) {
+      const autofocusDirective = new AutofocusDirective(this.fieldRef);
+      autofocusDirective.ngAfterViewInit();
+    }
   }
 
   getComponent(prop: FormProp): string {
