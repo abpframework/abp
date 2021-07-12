553a9578e0094c2265401f726d29ccd4752dd258
bnymncoskuner <bnymncoskuner@gmail.com>
2021-07-12 18:47:12 +0300

    feat: add token for tenant key and resolve it from url

--- npm/ng-packs/packages/core/src/lib/services/multi-tenancy.service.ts
+++ npm/ng-packs/packages/core/src/lib/services/multi-tenancy.service.ts
@@ -1,31 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { switchMap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ABP } from '../models/common';
-import {
-  CurrentTenantDto,
-  FindTenantResultDto,
-} from '../proxy/volo/abp/asp-net-core/mvc/multi-tenancy/models';
+import { FindTenantResultDto } from '../proxy/volo/abp/asp-net-core/mvc/multi-tenancy/models';
 import { RestService } from './rest.service';
+import { AbpTenantService } from '../proxy/pages/abp/multi-tenancy';
+import { ConfigStateService } from './config-state.service';
 import { SessionStateService } from './session-state.service';
+import { TENANT_KEY } from '../tokens/tenant-key.token';
 
 @Injectable({ providedIn: 'root' })
 export class MultiTenancyService {
-  private _domainTenant: CurrentTenantDto = null;
-
-  set domainTenant(value: CurrentTenantDto) {
-    this._domainTenant = value;
-    this.sessionState.setTenant(value);
-  }
-
-  get domainTenant() {
-    return this._domainTenant;
-  }
-
   isTenantBoxVisible = true;
 
   apiName = 'abp';
 
-  constructor(private restService: RestService, private sessionState: SessionStateService) {}
+  private setTenantToState = (tenant: FindTenantResultDto) => {
+    this.sessionState.setTenant({ id: tenant.tenantId, name: tenant.name, isAvailable: true });
+    return this.configStateService.refreshAppState().pipe(map(_ => tenant));
+  };
+
+  constructor(
+    private restService: RestService,
+    private sessionState: SessionStateService,
+    private tenantService: AbpTenantService,
+    private configStateService: ConfigStateService,
+    @Inject(TENANT_KEY) public tenantKey: string,
+  ) {}
 
   /**
    * @deprecated Use AbpTenantService.findTenantByName method instead. To be deleted in v5.0.
@@ -49,5 +50,17 @@ export class MultiTenancyService {
       { url: `/api/abp/multi-tenancy/tenants/by-id/${id}`, method: 'GET', headers },
       { apiName: this.apiName },
     );
+  }
+
+  setTenantByName(tenantName: string) {
+    return this.tenantService
+      .findTenantByName(tenantName, { [this.tenantKey]: '' })
+      .pipe(switchMap(this.setTenantToState));
+  }
+
+  setTenantById(tenantId: string) {
+    return this.tenantService
+      .findTenantById(tenantId, { [this.tenantKey]: '' })
+      .pipe(switchMap(this.setTenantToState));
   }
 }
