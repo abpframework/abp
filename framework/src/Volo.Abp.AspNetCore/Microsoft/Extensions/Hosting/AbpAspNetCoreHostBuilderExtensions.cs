11b5319b247a254af674634996f254f8ff3d4f5f
Alper Ebicoglu <alper.ebicoglu@volosoft.com>
2021-07-13 16:00:29 +0300

    Create AbpAspNetCoreHostBuilderExtensions.cs

---
+++ framework/src/Volo.Abp.AspNetCore/Microsoft/Extensions/Hosting/AbpAspNetCoreHostBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class AbpAspNetCoreHostBuilderExtensions
+    {
+        public const string AppSettingsSecretJsonPath = "appsettings.secrets.json";
+
+        public static IHostBuilder AddAppSettingsSecretsJson(
+            this IHostBuilder hostBuilder,
+            bool optional = true,
+            bool reloadOnChange = true,
+            string path = AppSettingsSecretJsonPath)
+        {
+            return hostBuilder.ConfigureAppConfiguration(appConfig =>
+            {
+                appConfig.AddJsonFile(
+                    path: AppSettingsSecretJsonPath,
+                    optional: optional,
+                    reloadOnChange: reloadOnChange
+                );
+            });
+        }
+    }
+}
