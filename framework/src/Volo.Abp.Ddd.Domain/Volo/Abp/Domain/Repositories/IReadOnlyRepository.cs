ce6736fd9abb0d4df48011db33c49e3f3cf10334
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-07-25 12:14:10 +0300

    Resolved #9646: Move GetListAsync from IReadOnlyBasicRepository to IReadOnlyRepository

--- framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Repositories/IReadOnlyRepository.cs
+++ framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Repositories/IReadOnlyRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Volo.Abp.Domain.Entities;
 using Volo.Abp.Linq;
 
@@ -23,6 +26,18 @@ namespace Volo.Abp.Domain.Repositories
         Task<IQueryable<TEntity>> WithDetailsAsync(params Expression<Func<TEntity, object>>[] propertySelectors); //TODO: CancellationToken
 
         Task<IQueryable<TEntity>> GetQueryableAsync(); //TODO: CancellationToken
+        
+        /// <summary>
+        /// Gets a list entities by the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="predicate">A condition to find the entity</param>
+        /// <param name="includeDetails">Set true to include all children of this entity</param>
+        /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+        /// <returns>Entity</returns>
+        Task<List<TEntity>> GetListAsync(
+            [NotNull] Expression<Func<TEntity, bool>> predicate,
+            bool includeDetails = false,
+            CancellationToken cancellationToken = default);
     }
 
     public interface IReadOnlyRepository<TEntity, TKey> : IReadOnlyRepository<TEntity>, IReadOnlyBasicRepository<TEntity, TKey>
