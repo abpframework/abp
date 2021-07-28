121280829d73705294ddceec28e024c73ad792f7
maliming <liming.ma@volosoft.com>
2021-07-28 16:33:13 +0800

    Update ICancellationTokenProvider.cs

--- framework/src/Volo.Abp.Threading/Volo/Abp/Threading/ICancellationTokenProvider.cs
+++ framework/src/Volo.Abp.Threading/Volo/Abp/Threading/ICancellationTokenProvider.cs
@@ -6,6 +6,7 @@ namespace Volo.Abp.Threading
     public interface ICancellationTokenProvider
     {
         CancellationToken Token { get; }
+
         IDisposable Use(CancellationToken cancellationToken);
     }
 }
