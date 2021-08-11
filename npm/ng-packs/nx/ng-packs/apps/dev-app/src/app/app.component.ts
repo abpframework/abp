8954f5b41ca19c176fb6518e66c39a8f64bf9a34
mehmet-erim <bilgi@mehmeterim.com>
2021-08-11 16:21:59 +0300

    create nx workspace

---
+++ npm/ng-packs/nx/ng-packs/apps/dev-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-packs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dev-app';
+}
---
+++ npm/ng-packs/nx/ng-packs/libs/core/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'core',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/libs/core',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
