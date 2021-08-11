5bb1c1bd627ca2bdff2bb63a016638b2d0e0a19b
liangshiwei <liangshiw@outlook.com>
2021-08-11 10:43:00 +0800

    Update MyProjectNamePermissionDefinitionProvider.cs

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application.Contracts/Permissions/MyProjectNamePermissionDefinitionProvider.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Application.Contracts/Permissions/MyProjectNamePermissionDefinitionProvider.cs
@@ -9,30 +9,8 @@ namespace MyCompanyName.MyProjectName.Permissions
         public override void Define(IPermissionDefinitionContext context)
         {
             var myGroup = context.AddGroup(MyProjectNamePermissions.GroupName);
-
             //Define your own permissions here. Example:
-            myGroup.AddPermission("permission1", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission2", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission3", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission4", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission5", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission6", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission7", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission8", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission9", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission10", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission11", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission12", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission13", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission14", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission15", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission16", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission17", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission18", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission19", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission20", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission21", L("Permission:MyPermission1"));
-            myGroup.AddPermission("permission22", L("Permission:MyPermission1"));
+            //myGroup.AddPermission(MyProjectNamePermissions.MyPermission1, L("Permission:MyPermission1"));
         }
 
         private static LocalizableString L(string name)
