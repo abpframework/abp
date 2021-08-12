1b4349dcd0bf4ce26978e9560dbfbf15f0123a99
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2021-08-12 13:34:18 +0300

    Added İdentity.Installer

---
+++ modules/identity/src/Volo.Abp.Identity.Installer/Volo/Abp/Identity/AbpIdentityInstallerModule.cs
@@ -0,0 +1,21 @@
+﻿using Volo.Abp.Studio;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Volo.Abp.Identity
+{
+    [DependsOn(
+        typeof(AbpStudioModuleInstallerModule),
+        typeof(AbpVirtualFileSystemModule)
+        )]
+    public class AbpIdentityInstallerModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpIdentityInstallerModule>();
+            });
+        }
+    }
+}
---
+++ modules/identity/src/Volo.Abp.Identity.Installer/Volo/Abp/Identity/IdentityInstallerPipelineBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Studio.ModuleInstalling;
+
+namespace Volo.Abp.Identity
+{
+    public class IdentityInstallerPipelineBuilder : ModuleInstallingPipelineBuilderBase, IModuleInstallingPipelineBuilder, ITransientDependency
+    {
+        public ModuleInstallingPipeline Build(ModuleInstallingContext context)
+        {
+            return GetBasePipeline(context);
+        }
+    }
+}
