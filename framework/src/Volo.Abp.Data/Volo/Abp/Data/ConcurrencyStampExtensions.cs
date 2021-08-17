c253b97f223d0214ddd6372d053832d0850426c3
enisn <info@enisnecipoglu.com>
2021-08-17 11:22:23 +0300

    framework - Add SetConcurrencyStamp method

---
+++ framework/src/Volo.Abp.Data/Volo/Abp/Data/ConcurrencyStampExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Domain.Entities;
+
+namespace Volo.Abp.Data
+{
+    public static class ConcurrencyStampExtensions
+    {
+        public static void SetConcurrencyStamp(this IHasConcurrencyStamp entity, string concurrencyStamp)
+        {
+            if (!concurrencyStamp.IsNullOrEmpty())
+            {
+                entity.ConcurrencyStamp = concurrencyStamp;
+            }
+        }
+    }
+}
