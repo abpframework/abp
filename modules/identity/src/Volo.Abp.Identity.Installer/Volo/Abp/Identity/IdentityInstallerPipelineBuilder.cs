349734fbd91114e908d18868a732c4a0767ee42c
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2021-08-24 15:21:56 +0300

    Update IdentityInstallerPipelineBuilder.cs

--- modules/identity/src/Volo.Abp.Identity.Installer/Volo/Abp/Identity/IdentityInstallerPipelineBuilder.cs
+++ modules/identity/src/Volo.Abp.Identity.Installer/Volo/Abp/Identity/IdentityInstallerPipelineBuilder.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Studio.ModuleInstalling;
 
 namespace Volo.Abp.Identity
 {
+    [Dependency(ServiceLifetime.Transient, ReplaceServices = true)]
+    [ExposeServices(typeof(IModuleInstallingPipelineBuilder))]
     public class IdentityInstallerPipelineBuilder : ModuleInstallingPipelineBuilderBase, IModuleInstallingPipelineBuilder, ITransientDependency
     {
         public async Task<ModuleInstallingPipeline> BuildAsync(ModuleInstallingContext context)
