f18b26fbaf0be1b2e46e9e7d5aca2487311788c5
maliming <malimings@gmail.com>
2021-08-26 17:35:51 +0800

    Update libs.

--- npm/packs/bootstrap/abp.resourcemapping.js
+++ npm/packs/bootstrap/abp.resourcemapping.js
@@ -2,8 +2,9 @@
     mappings: {
         "@node_modules/bootstrap/dist/css/bootstrap.css*": "@libs/bootstrap/css/",
         "@node_modules/bootstrap/dist/css/bootstrap.min.css*": "@libs/bootstrap/css/",
-        "@node_modules/bootstrap-v4-rtl/dist/css/bootstrap-rtl*": "@libs/bootstrap/css/",
+        "@node_modules/bootstrap/dist/css/bootstrap.rtl.css*": "@libs/bootstrap/css/",
+        "@node_modules/bootstrap/dist/css/bootstrap.rtl.min.css*": "@libs/bootstrap/css/",
         "@node_modules/bootstrap/dist/js/bootstrap.bundle*": "@libs/bootstrap/js/",
-        "@node_modules/bootstrap/src/*.*": "@libs/bootstrap/js/"
+        "@node_modules/@abp/bootstrap/src/*.*": "@libs/bootstrap/js/"
     }
 }
