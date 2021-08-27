3c436c2edf218617c08781e38958e909fc7b9d0b
liangshiwei <liangshiw@outlook.com>
2021-08-27 17:50:53 +0800

    Add UsageInfo

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/RemoveProxyCommand.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/RemoveProxyCommand.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Text;
+using Microsoft.Extensions.Options;
 using Volo.Abp.Cli.ServiceProxy;
 using Volo.Abp.DependencyInjection;
 
@@ -15,6 +16,20 @@ namespace Volo.Abp.Cli.Commands
             IHybridServiceScopeFactory serviceScopeFactory)
             : base(serviceProxyOptions, serviceScopeFactory)
         {
+        }
+
+        public override string GetUsageInfo()
+        {
+            var sb = new StringBuilder(base.GetUsageInfo());
+
+            sb.AppendLine("");
+            sb.AppendLine("Examples:");
+            sb.AppendLine("");
+            sb.AppendLine("  abp remove-proxy -t ng");
+            sb.AppendLine("  abp remove-proxy -t js -m identity -o Pages/Identity/client-proxies.js");
+            sb.AppendLine("  abp remove-proxy -t csharp --folder MyProxies/InnerFolder");
+
+            return sb.ToString();
         }
 
         public override string GetShortDescription()
