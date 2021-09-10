09d3d355f7f93e544cf1549c3978f4dfead0e75c
mehmet-erim <bilgi@mehmeterim.com>
2021-09-10 12:41:45 +0300

    create http error reporter service

---
+++ npm/ng-packs/packages/core/src/lib/services/http-error-reporter.service.ts
@@ -0,0 +1,29 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, of, Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class HttpErrorReporterService {
+  private _reporter$ = new Subject();
+  private _errors$ = new BehaviorSubject<HttpErrorResponse[]>([]);
+
+  get reporter$() {
+    return this._reporter$.asObservable();
+  }
+
+  get erros$() {
+    return this._errors$.asObservable();
+  }
+
+  get errors() {
+    return this._errors$.value;
+  }
+
+  reportError = (error: HttpErrorResponse) => {
+    this._reporter$.next(error);
+    this._errors$.next([...this.errors, error]);
+    return of();
+  };
+
+  constructor() {}
+}
--- npm/ng-packs/packages/core/src/lib/services/index.ts
+++ npm/ng-packs/packages/core/src/lib/services/index.ts
@@ -4,6 +4,7 @@ export * from './config-state.service';
 export * from './content-projection.service';
 export * from './dom-insertion.service';
 export * from './environment.service';
+export * from './http-error-reporter.service';
 export * from './http-wait.service';
 export * from './lazy-load.service';
 export * from './list.service';
