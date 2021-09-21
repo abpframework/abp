7b83275315df5ec10ace15cdddb1c14a4e25f816
bnymncoskuner <bnymncoskuner@gmail.com>
2021-09-21 16:13:50 +0300

    feat: move tenants service to proxy entry point

--- npm/ng-packs/packages/tenant-management/src/public-api.ts
+++ npm/ng-packs/packages/tenant-management/src/public-api.ts
@@ -2,6 +2,5 @@ export * from './lib/components';
 export * from './lib/enums';
 export * from './lib/guards';
 export * from './lib/models';
-export * from './lib/proxy';
 export * from './lib/tenant-management.module';
 export * from './lib/tokens';
