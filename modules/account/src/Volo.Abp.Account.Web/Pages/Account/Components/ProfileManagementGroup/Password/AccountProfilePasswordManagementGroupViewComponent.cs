a9aeeba4a4f749f1cbd6c28a5a198b36111b573f
liangshiwei <liangshiw@outlook.com>
2021-09-23 10:24:28 +0800

    Add ProfileManagementScriptBundleContributor

--- modules/account/src/Volo.Abp.Account.Web/Pages/Account/Components/ProfileManagementGroup/Password/AccountProfilePasswordManagementGroupViewComponent.cs
+++ modules/account/src/Volo.Abp.Account.Web/Pages/Account/Components/ProfileManagementGroup/Password/AccountProfilePasswordManagementGroupViewComponent.cs
@@ -2,12 +2,16 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
 using Volo.Abp.Auditing;
 using Volo.Abp.Identity;
 using Volo.Abp.Validation;
 
 namespace Volo.Abp.Account.Web.Pages.Account.Components.ProfileManagementGroup.Password
 {
+    [Widget(
+        ScriptTypes = new[] { typeof(ProfileManagementScriptBundleContributor) }
+    )]
     public class AccountProfilePasswordManagementGroupViewComponent : AbpViewComponent
     {
         private readonly IProfileAppService _profileAppService;
---
+++ modules/account/src/Volo.Abp.Account.Web/Pages/Account/Components/ProfileManagementGroup/ProfileManagementScriptBundleContributor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace Volo.Abp.Account.Web.Pages.Account.Components.ProfileManagementGroup
+{
+    public class ProfileManagementScriptBundleContributor: BundleContributor
+    {
+        public override void ConfigureBundle(BundleConfigurationContext context)
+        {
+            context.Files.AddIfNotContains("/client-proxies/identity-proxy.js");
+        }
+    }
+}
