e821c8502cfc03bd518d8c90dbd7f1247a704968
maliming <malimings@gmail.com>
2021-09-24 14:51:54 +0800

    Solve the case of duplicate parameter types

--- framework/src/Volo.Abp.Http.Client/Volo/Abp/Http/Client/ClientProxying/ClientProxyRequestTypeValue.cs
+++ framework/src/Volo.Abp.Http.Client/Volo/Abp/Http/Client/ClientProxying/ClientProxyRequestTypeValue.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 
 namespace Volo.Abp.Http.Client.ClientProxying
 {
-    public class ClientProxyRequestTypeValue : Dictionary<Type, object>
+    public class ClientProxyRequestTypeValue
     {
+        public List<KeyValuePair<Type, object>> Values { get; private set; }
 
+        public ClientProxyRequestTypeValue()
+        {
+            Values = new List<KeyValuePair<Type, object>>();
+        }
+
+        public void Add(Type type, object value)
+        {
+            Values.Add(new KeyValuePair<Type, object>(type, value));
+        }
     }
 }
