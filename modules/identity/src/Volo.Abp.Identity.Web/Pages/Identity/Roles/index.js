07c43aecd5ed4bff07d1af77742855cc09124246
maliming <malimings@gmail.com>
2021-09-25 15:29:18 +0800

    Update Role page of Identity.

--- modules/identity/src/Volo.Abp.Identity.Web/Pages/Identity/Roles/index.js
+++ modules/identity/src/Volo.Abp.Identity.Web/Pages/Identity/Roles/index.js
@@ -88,13 +88,13 @@
                             var name = '<span>' + $.fn.dataTable.render.text().display(data) + '</span>'; //prevent against possible XSS
                             if (row.isDefault) {
                                 name +=
-                                    '<span class="badge rounded-pill bg-success ml-1">' +
+                                    '<span class="badge rounded-pill bg-success ms-1">' +
                                     l('DisplayName:IsDefault') +
                                     '</span>';
                             }
                             if (row.isPublic) {
                                 name +=
-                                    '<span class="badge rounded-pill bg-info ml-1">' +
+                                    '<span class="badge rounded-pill bg-info ms-1">' +
                                     l('DisplayName:IsPublic') +
                                     '</span>';
                             }
