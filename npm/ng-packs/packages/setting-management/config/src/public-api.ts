0dce9e8feb949f77053fc2e269a5d14cbf511ae8
Mehmet Erim <34455572+mehmet-erim@users.noreply.github.com>
2021-09-28 17:17:50 +0300

    Update public-api.ts

--- npm/ng-packs/packages/setting-management/config/src/public-api.ts
+++ npm/ng-packs/packages/setting-management/config/src/public-api.ts
@@ -2,5 +2,5 @@ export * from './components/email-setting-group/email-setting-group.component';
 export * from './enums';
 export * from './providers';
 export * from './proxy';
-export * from './services/index';
+export * from './services';
 export * from './setting-management-config.module';
