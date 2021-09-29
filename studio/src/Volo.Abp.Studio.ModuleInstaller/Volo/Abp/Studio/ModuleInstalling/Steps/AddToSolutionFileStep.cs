71c6b71ebd330e08b3008dc16f516cfd404389ea
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2021-09-29 13:51:55 +0300

    Created ISolutionFileModuleAdder

--- studio/src/Volo.Abp.Studio.ModuleInstaller/Volo/Abp/Studio/ModuleInstalling/Steps/AddToSolutionFileStep.cs
+++ studio/src/Volo.Abp.Studio.ModuleInstaller/Volo/Abp/Studio/ModuleInstalling/Steps/AddToSolutionFileStep.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using Volo.Abp.Studio.Modules;
 using Volo.Abp.Studio.Solution;
 
 namespace Volo.Abp.Studio.ModuleInstalling.Steps
@@ -11,28 +8,9 @@ namespace Volo.Abp.Studio.ModuleInstalling.Steps
     {
         public override async Task ExecuteAsync(ModuleInstallingContext context)
         {
-            var slnFile = context.TargetModule.RemovePostFix(ModuleConsts.FileExtension) + ".sln";
-            var moduleSrcFolder = Path.Combine(context.GetTargetSourceCodeFolder(), "src");
-            var moduleTestFolder = Path.Combine(context.GetTargetSourceCodeFolder(), "test");
+            var _solutionFileModuleAdder = context.ServiceProvider.GetRequiredService<ISolutionFileModuleAdder>();
 
-            await AddProjectsUnderDirectoryToSolutionFile(context, slnFile, moduleSrcFolder, $"modules/{context.ModuleName}");
-            await AddProjectsUnderDirectoryToSolutionFile(context, slnFile, moduleTestFolder, $"test/{context.ModuleName}.Tests");
-        }
-
-        private static async Task AddProjectsUnderDirectoryToSolutionFile(
-            ModuleInstallingContext context,
-            string slnFile,
-            string directory,
-            string slnTargetFolder)
-        {
-            var _dotnetSlnFileModifier = context.ServiceProvider.GetRequiredService<IDotnetSlnFileModifierService>();
-
-            var projects = Directory.GetFiles(directory, "*.csproj", SearchOption.AllDirectories);
-
-            foreach (var project in projects)
-            {
-                await _dotnetSlnFileModifier.AddProjectAsync(slnFile, project, slnTargetFolder);
-            }
+            await _solutionFileModuleAdder.AddAsync(context.TargetModule, context.ModuleName);
         }
     }
 }
