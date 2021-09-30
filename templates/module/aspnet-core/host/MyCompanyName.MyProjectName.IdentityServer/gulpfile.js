7f3ec72e1325e345cd01b075887d0991f771d3e9
voloagent <account@volosoft.com>
2021-09-30 11:44:16 +0300

    NPM_Packages

--- templates/module/aspnet-core/host/MyCompanyName.MyProjectName.IdentityServer/gulpfile.js
+++ templates/module/aspnet-core/host/MyCompanyName.MyProjectName.IdentityServer/gulpfile.js
@@ -1,9 +1,9 @@
-﻿"use strict";
+"use strict";
 
 var gulp = require("gulp"),
     path = require('path'),
     copyResources = require('./node_modules/@abp/aspnetcore.mvc.ui/gulp/copy-resources.js');
 
-exports.default = function(){
+exports.default = async function(){
     return copyResources(path.resolve('./'));
 };
