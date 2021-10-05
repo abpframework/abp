89e08a0e189a75e06b369a3d719499301a1984ed
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2021-10-05 13:32:40 +0300

    Created MyCompanyName.MyProjectName.Installer

---
+++ templates/module/aspnet-core/src/MyCompanyName.MyProjectName.Installer/MyCompanyName/MyProjectName/MyProjectNameInstallerPipelineBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Studio.ModuleInstalling;
+
+namespace Volo.Abp.Identity
+{
+    [Dependency(ServiceLifetime.Transient, ReplaceServices = true)]
+    [ExposeServices(typeof(IModuleInstallingPipelineBuilder))]
+    public class MyProjectNameInstallerPipelineBuilder : ModuleInstallingPipelineBuilderBase, IModuleInstallingPipelineBuilder, ITransientDependency
+    {
+        public async Task<ModuleInstallingPipeline> BuildAsync(ModuleInstallingContext context)
+        {
+            return GetBasePipeline(context);
+        }
+    }
+}
