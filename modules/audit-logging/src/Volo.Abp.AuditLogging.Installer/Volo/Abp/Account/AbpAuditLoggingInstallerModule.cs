4e0801b6bac6552f30efe037db918cc798f9c439
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-10-07 00:20:15 +0300

    added AuditLogging module installer package

---
+++ modules/audit-logging/src/Volo.Abp.AuditLogging.Installer/Volo/Abp/Account/AbpAuditLoggingInstallerModule.cs
@@ -0,0 +1,21 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.Studio;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Volo.Abp.Account
+{
+    [DependsOn(
+        typeof(AbpStudioModuleInstallerModule),
+        typeof(AbpVirtualFileSystemModule)
+        )]
+    public class AbpAuditLoggingInstallerModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpAuditLoggingInstallerModule>();
+            });
+        }
+    }
+}
