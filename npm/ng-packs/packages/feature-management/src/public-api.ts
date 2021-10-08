55dbc12e8641ea42b0604dd2956ca0ba891c1a17
Maik Stegemann <ms@lis-gmbh.com>
2021-10-08 11:08:01 +0200

    export /models/index.ts from public-api.ts

--- npm/ng-packs/packages/feature-management/src/public-api.ts
+++ npm/ng-packs/packages/feature-management/src/public-api.ts
@@ -2,3 +2,4 @@ export * from './lib/components';
 export * from './lib/directives';
 export * from './lib/enums/components';
 export * from './lib/feature-management.module';
+export * from './lib/models';
