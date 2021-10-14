a10d15f8000311585c9b36bb5aa6291c64f2c94a
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2021-10-14 08:27:02 +0300

    Update PackageConsts.cs

--- studio/src/Volo.Abp.Studio.ModuleInstaller.Abstractions/Volo/Abp/Studio/Package/PackageConsts.cs
+++ studio/src/Volo.Abp.Studio.ModuleInstaller.Abstractions/Volo/Abp/Studio/Package/PackageConsts.cs
@@ -3,7 +3,10 @@
     public static class PackageConsts
     {
         public const string FileExtension = ".abppkg.json";
-        
+
+        //todo: use this everywhere!
+        public const string InstallerExtension = ".Installer";
+
         public const string RoleProperty = "role";
     }
 }
