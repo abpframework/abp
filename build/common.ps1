ab6598ffcdb1f0eb18fe8d415b9c6f375a35e374
maliming <liming.ma@volosoft.com>
2021-10-20 22:12:54 +0800

    Update common.ps1

--- build/common.ps1
+++ build/common.ps1
@@ -6,38 +6,12 @@ $rootFolder = (Get-Item -Path "./" -Verbose).FullName
 
 # List of solutions used only in development mode
 $solutionPaths = @(
-		"../framework",
-		"../studio",
-		"../modules/basic-theme",
-		"../modules/users",
-		"../modules/permission-management",
-		"../modules/setting-management",
-		"../modules/feature-management",
-		"../modules/identity",
-		"../modules/identityserver",
-		"../modules/tenant-management",
-		"../modules/audit-logging",
-		"../modules/background-jobs",
-		"../modules/account",
-		"../modules/cms-kit",
-		"../modules/blob-storing-database",
-		"../studio"
+		"../framework"
 	)
 
 if ($full -eq "-f")
 {
-	# List of additional solutions required for full build
-	$solutionPaths += (
-		"../modules/client-simulation",
-		"../modules/virtual-file-explorer",
-		"../modules/docs",
-		"../modules/blogging",
-		"../templates/module/aspnet-core",
-		"../templates/app/aspnet-core",
-		"../templates/console",
-		"../templates/wpf",
-		"../abp_io/AbpIoLocalization"
-	) 
+
 }else{ 
 	Write-host ""
 	Write-host ":::::::::::::: !!! You are in development mode !!! ::::::::::::::" -ForegroundColor red -BackgroundColor  yellow
