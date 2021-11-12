13034bfb91b18b9c09946781d2d90af9dca80e67
Alper Ebicoglu <alper.ebicoglu@volosoft.com>
2021-11-12 11:55:52 +0300

    find PRO packages more accurate. closes #8351

---
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/NuGet/CommercialPackages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Volo.Abp.Cli.NuGet
+{
+    internal static class CommercialPackages
+    {
+        private static readonly HashSet<string> _packages = new()
+        {
+            "volo.abp.suite"
+            //other PRO packages can be added to this list...
+        };
+
+        public static bool IsCommercial(string packageId)
+        {
+            return _packages.Contains(packageId.ToLowerInvariant());
+        }
+    }
+}
