3afa5f1a5cf818fdb500020634bc584635873bdf
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-11-21 23:33:59 +0300

    use file-scoped namespacing for featureManagement module

--- modules/feature-management/test/Volo.Abp.FeatureManagement.TestBase/Volo/Abp/FeatureManagement/TestEditionIds.cs
+++ modules/feature-management/test/Volo.Abp.FeatureManagement.TestBase/Volo/Abp/FeatureManagement/TestEditionIds.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 
-namespace Volo.Abp.FeatureManagement
-{
-    public static class TestEditionIds
-    {
-        public static Guid Regular { get; }
-        public static Guid Enterprise { get; }
-        public static Guid Ultimate { get; }
+namespace Volo.Abp.FeatureManagement;
 
-        static TestEditionIds()
-        {
-            Regular = Guid.Parse("532599ab-c0c0-4345-a04a-e322867b6e15");
-            Enterprise = Guid.Parse("27e50758-1feb-436a-be4f-cae8519e0cb2");
-            Ultimate = Guid.Parse("6ea78c22-be32-497e-aaba-a2332c564c5e");
-        }
+public static class TestEditionIds
+{
+    public static Guid Regular { get; }
+    public static Guid Enterprise { get; }
+    public static Guid Ultimate { get; }
+
+    static TestEditionIds()
+    {
+        Regular = Guid.Parse("532599ab-c0c0-4345-a04a-e322867b6e15");
+        Enterprise = Guid.Parse("27e50758-1feb-436a-be4f-cae8519e0cb2");
+        Ultimate = Guid.Parse("6ea78c22-be32-497e-aaba-a2332c564c5e");
     }
 }
