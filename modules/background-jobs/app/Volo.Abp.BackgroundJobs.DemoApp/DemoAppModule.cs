bd939aaf7e920b9a01f1ed5b5b1f0c9a092739ff
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-11-21 23:22:47 +0300

    use file-scoped namespacing for backgroundJobs module

--- modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp/DemoAppModule.cs
+++ modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp/DemoAppModule.cs
@@ -5,42 +5,41 @@ using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.SqlServer;
 using Volo.Abp.Modularity;
 
-namespace Volo.Abp.BackgroundJobs.DemoApp
-{
-    [DependsOn(
-        typeof(DemoAppSharedModule),
-        typeof(AbpBackgroundJobsEntityFrameworkCoreModule),
-        typeof(AbpAutofacModule),
-        typeof(AbpEntityFrameworkCoreSqlServerModule)
-        )]
-    public class DemoAppModule : AbpModule
-    {
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            Configure<AbpDbContextOptions>(options =>
-            {
-                options.Configure(opts =>
-                {
-                    opts.UseSqlServer();
-                });
-            });
+namespace Volo.Abp.BackgroundJobs.DemoApp;
 
-            Configure<AbpBackgroundJobWorkerOptions>(options =>
+[DependsOn(
+    typeof(DemoAppSharedModule),
+    typeof(AbpBackgroundJobsEntityFrameworkCoreModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpEntityFrameworkCoreSqlServerModule)
+    )]
+public class DemoAppModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpDbContextOptions>(options =>
+        {
+            options.Configure(opts =>
             {
+                opts.UseSqlServer();
+            });
+        });
+
+        Configure<AbpBackgroundJobWorkerOptions>(options =>
+        {
                 //Configure for fast running
                 options.JobPollPeriod = 1000;
-                options.DefaultFirstWaitDuration = 1;
-                options.DefaultWaitFactor = 1;
-            });
-        }
+            options.DefaultFirstWaitDuration = 1;
+            options.DefaultWaitFactor = 1;
+        });
+    }
 
-        public override void OnApplicationInitialization(ApplicationInitializationContext context)
-        {
-            //TODO: Configure console logging
-            //context
-            //    .ServiceProvider
-            //    .GetRequiredService<ILoggerFactory>()
-            //    .AddConsole(LogLevel.Debug);
-        }
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        //TODO: Configure console logging
+        //context
+        //    .ServiceProvider
+        //    .GetRequiredService<ILoggerFactory>()
+        //    .AddConsole(LogLevel.Debug);
     }
 }
