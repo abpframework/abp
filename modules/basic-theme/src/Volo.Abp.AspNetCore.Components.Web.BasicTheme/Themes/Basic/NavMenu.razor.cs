2f672afa540633fcc778526f6f9a394129dc4ded
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-11-21 23:25:57 +0300

    use file-scoped namespacing for basicTheme module

--- modules/basic-theme/src/Volo.Abp.AspNetCore.Components.Web.BasicTheme/Themes/Basic/NavMenu.razor.cs
+++ modules/basic-theme/src/Volo.Abp.AspNetCore.Components.Web.BasicTheme/Themes/Basic/NavMenu.razor.cs
@@ -4,33 +4,32 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Volo.Abp.UI.Navigation;
 
-namespace Volo.Abp.AspNetCore.Components.Web.BasicTheme.Themes.Basic
+namespace Volo.Abp.AspNetCore.Components.Web.BasicTheme.Themes.Basic;
+
+public partial class NavMenu : IDisposable
 {
-    public partial class NavMenu : IDisposable
+    [Inject]
+    protected IMenuManager MenuManager { get; set; }
+
+    [Inject]
+    protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    protected ApplicationMenu Menu { get; set; }
+
+    protected override async Task OnInitializedAsync()
     {
-        [Inject]
-        protected IMenuManager MenuManager { get; set; }
-        
-        [Inject]
-        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        Menu = await MenuManager.GetMainMenuAsync();
+        AuthenticationStateProvider.AuthenticationStateChanged += AuthenticationStateProviderOnAuthenticationStateChanged;
+    }
 
-        protected ApplicationMenu Menu { get; set; }
+    public void Dispose()
+    {
+        AuthenticationStateProvider.AuthenticationStateChanged -= AuthenticationStateProviderOnAuthenticationStateChanged;
+    }
 
-        protected override async Task OnInitializedAsync()
-        {
-            Menu = await MenuManager.GetMainMenuAsync();
-            AuthenticationStateProvider.AuthenticationStateChanged += AuthenticationStateProviderOnAuthenticationStateChanged;
-        }
-        
-        public void Dispose()
-        {
-            AuthenticationStateProvider.AuthenticationStateChanged -= AuthenticationStateProviderOnAuthenticationStateChanged;
-        }
-
-        private async void AuthenticationStateProviderOnAuthenticationStateChanged(Task<AuthenticationState> task)
-        {
-            Menu = await MenuManager.GetMainMenuAsync();
-            await InvokeAsync(StateHasChanged);
-        }
+    private async void AuthenticationStateProviderOnAuthenticationStateChanged(Task<AuthenticationState> task)
+    {
+        Menu = await MenuManager.GetMainMenuAsync();
+        await InvokeAsync(StateHasChanged);
     }
 }
