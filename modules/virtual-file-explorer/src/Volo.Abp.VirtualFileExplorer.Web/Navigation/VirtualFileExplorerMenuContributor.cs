73fadc0e02dd0ae903347077f681bfdea5ecde1b
Ahmet Çotur <ahmet.cotur@hotmail.com>
2021-11-21 23:44:28 +0300

    use file-scoped namespacing for virtualFileExplorer module

--- modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web/Navigation/VirtualFileExplorerMenuContributor.cs
+++ modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web/Navigation/VirtualFileExplorerMenuContributor.cs
@@ -4,22 +4,21 @@ using Microsoft.Extensions.Localization;
 using Volo.Abp.UI.Navigation;
 using Volo.Abp.VirtualFileExplorer.Web.Localization;
 
-namespace Volo.Abp.VirtualFileExplorer.Web.Navigation
+namespace Volo.Abp.VirtualFileExplorer.Web.Navigation;
+
+public class VirtualFileExplorerMenuContributor : IMenuContributor
 {
-    public class VirtualFileExplorerMenuContributor : IMenuContributor
+    public virtual Task ConfigureMenuAsync(MenuConfigurationContext context)
     {
-        public virtual Task ConfigureMenuAsync(MenuConfigurationContext context)
+        if (context.Menu.Name != StandardMenus.Main)
         {
-            if (context.Menu.Name != StandardMenus.Main)
-            {
-                return Task.CompletedTask;
-            }
-
-            var l = context.GetLocalizer<VirtualFileExplorerResource>();
-
-            context.Menu.Items.Add(new ApplicationMenuItem(VirtualFileExplorerMenuNames.Index, l["Menu:VirtualFileExplorer"], icon: "fa fa-file", url: "~/VirtualFileExplorer"));
-
             return Task.CompletedTask;
         }
+
+        var l = context.GetLocalizer<VirtualFileExplorerResource>();
+
+        context.Menu.Items.Add(new ApplicationMenuItem(VirtualFileExplorerMenuNames.Index, l["Menu:VirtualFileExplorer"], icon: "fa fa-file", url: "~/VirtualFileExplorer"));
+
+        return Task.CompletedTask;
     }
 }
--- modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web/Pages/VirtualFileExplorer/Index.cshtml.cs
+++ modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web/Pages/VirtualFileExplorer/Index.cshtml.cs
@@ -9,107 +9,106 @@ using Volo.Abp.VirtualFileExplorer.Web.Models;
 using Volo.Abp.VirtualFileSystem;
 using Volo.Abp.VirtualFileSystem.Embedded;
 
-namespace Volo.Abp.VirtualFileExplorer.Web.Pages.VirtualFileExplorer
+namespace Volo.Abp.VirtualFileExplorer.Web.Pages.VirtualFileExplorer;
+
+public class IndexModel : VirtualFileExplorerPageModel
 {
-    public class IndexModel : VirtualFileExplorerPageModel
+    [BindProperty(SupportsGet = true)]
+    public string Path { get; set; } = "/";
+
+    [BindProperty(SupportsGet = true)]
+    public int CurrentPage { get; set; } = 1;
+
+    [BindProperty(SupportsGet = true)]
+    public int PageSize { get; set; } = 10;
+
+    public List<FileInfoViewModel> FileInfoList { get; set; }
+
+    public PagerModel PagerModel { get; set; }
+
+    public string PathNavigation { get; set; }
+
+    protected IVirtualFileProvider VirtualFileProvider { get; }
+
+    public IndexModel(IVirtualFileProvider virtualFileProvider)
     {
-        [BindProperty(SupportsGet = true)]
-        public string Path { get; set; } = "/";
+        VirtualFileProvider = virtualFileProvider;
+    }
 
-        [BindProperty(SupportsGet = true)]
-        public int CurrentPage { get; set; } = 1;
+    public virtual IActionResult OnGet()
+    {
+        var query = VirtualFileProvider.GetDirectoryContents(Path)
+            .Where(d => VirtualFileExplorerConsts.AllowFileInfoTypes.Contains(d.GetType().Name))
+            .OrderByDescending(f => f.IsDirectory).ToList();
 
-        [BindProperty(SupportsGet = true)]
-        public int PageSize { get; set; } = 10;
+        PagerModel = new PagerModel(query.Count, PageSize, CurrentPage, PageSize, $"{Url.Content("~/")}VirtualFileExplorer?Path={Path}&PageSize={PageSize}");
 
-        public List<FileInfoViewModel> FileInfoList { get; set; }
+        SetViewModel(query.Skip((CurrentPage - 1) * PageSize).Take(PageSize));
+        SetPathNavigation();
 
-        public PagerModel PagerModel { get; set; }
+        return Page();
+    }
 
-        public string PathNavigation { get; set; }
+    private void SetViewModel(IEnumerable<IFileInfo> fileInfos)
+    {
+        FileInfoList = new List<FileInfoViewModel>();
 
-        protected IVirtualFileProvider VirtualFileProvider { get; }
-
-        public IndexModel(IVirtualFileProvider virtualFileProvider)
+        foreach (var fileInfo in fileInfos)
         {
-            VirtualFileProvider = virtualFileProvider;
-        }
-
-        public virtual IActionResult OnGet()
-        {
-            var query = VirtualFileProvider.GetDirectoryContents(Path)
-                .Where(d => VirtualFileExplorerConsts.AllowFileInfoTypes.Contains(d.GetType().Name))
-                .OrderByDescending(f => f.IsDirectory).ToList();
-
-            PagerModel = new PagerModel(query.Count, PageSize, CurrentPage, PageSize, $"{Url.Content("~/")}VirtualFileExplorer?Path={Path}&PageSize={PageSize}");
-
-            SetViewModel(query.Skip((CurrentPage - 1) * PageSize).Take(PageSize));
-            SetPathNavigation();
-
-            return Page();
-        }
-
-        private void SetViewModel(IEnumerable<IFileInfo> fileInfos)
-        {
-            FileInfoList = new List<FileInfoViewModel>();
-
-            foreach (var fileInfo in fileInfos)
+            var fileInfoViewModel = new FileInfoViewModel()
             {
-                var fileInfoViewModel = new FileInfoViewModel()
-                {
-                    IsDirectory = fileInfo.IsDirectory,
-                    Icon = "fas fa-file",
-                    FileType = "file",
-                    Length = fileInfo.Length+" bytes",
-                    FileName = fileInfo.Name,
-                    LastUpdateTime = fileInfo.LastModified.LocalDateTime
-                };
+                IsDirectory = fileInfo.IsDirectory,
+                Icon = "fas fa-file",
+                FileType = "file",
+                Length = fileInfo.Length + " bytes",
+                FileName = fileInfo.Name,
+                LastUpdateTime = fileInfo.LastModified.LocalDateTime
+            };
 
-                var filePath =  fileInfo.PhysicalPath ?? $"{Path.EnsureEndsWith('/')}{fileInfo.Name}";;
+            var filePath = fileInfo.PhysicalPath ?? $"{Path.EnsureEndsWith('/')}{fileInfo.Name}"; ;
 
-                if (fileInfo.IsDirectory)
+            if (fileInfo.IsDirectory)
+            {
+                fileInfoViewModel.Icon = "fas fa-folder";
+                fileInfoViewModel.FileType = "folder";
+                fileInfoViewModel.Length = "/";
+                fileInfoViewModel.FileName = $"<a href='{Url.Content("~/")}VirtualFileExplorer?path={filePath}'>{fileInfo.Name}</a>";
+            }
+            else
+            {
+                if (fileInfo is EmbeddedResourceFileInfo embeddedResourceFileInfo)
                 {
-                    fileInfoViewModel.Icon = "fas fa-folder";
-                    fileInfoViewModel.FileType = "folder";
-                    fileInfoViewModel.Length = "/";
-                    fileInfoViewModel.FileName =$"<a href='{Url.Content("~/")}VirtualFileExplorer?path={filePath}'>{fileInfo.Name}</a>";
+                    fileInfoViewModel.FilePath = embeddedResourceFileInfo.VirtualPath;
                 }
                 else
                 {
-                    if (fileInfo is EmbeddedResourceFileInfo embeddedResourceFileInfo)
-                    {
-                        fileInfoViewModel.FilePath = embeddedResourceFileInfo.VirtualPath;
-                    }
-                    else
-                    {
-                        fileInfoViewModel.FilePath = filePath;
-                    }
-
+                    fileInfoViewModel.FilePath = filePath;
                 }
 
-                FileInfoList.Add(fileInfoViewModel);
             }
-        }
 
-        private void SetPathNavigation()
+            FileInfoList.Add(fileInfoViewModel);
+        }
+    }
+
+    private void SetPathNavigation()
+    {
+        var navigationBuild = new StringBuilder();
+        var pathArray = Path.Split('/').Where(p => !p.IsNullOrWhiteSpace());
+        var href = $"{Url.Content("~/")}VirtualFileExplorer?path=";
+
+        navigationBuild.Append($"<nav aria-label='breadcrumb'>" +
+                               $" <ol class='breadcrumb'>" +
+                               $"<li class='breadcrumb-item'><a href='{href}/'>{L["BackToRoot"]}</a></li>");
+
+        foreach (var item in pathArray)
         {
-            var navigationBuild = new StringBuilder();
-            var pathArray = Path.Split('/').Where(p => !p.IsNullOrWhiteSpace());
-            var href = $"{Url.Content("~/")}VirtualFileExplorer?path=";
-
-            navigationBuild.Append($"<nav aria-label='breadcrumb'>" +
-                                   $" <ol class='breadcrumb'>" +
-                                   $"<li class='breadcrumb-item'><a href='{href}/'>{L["BackToRoot"]}</a></li>");
-
-            foreach (var item in pathArray)
-            {
-                href += "/" + item;
-                navigationBuild.Append($"<li class='breadcrumb-item'><a href='{href}'>{item}</a></li>");
-            }
-
-            navigationBuild.Append("</ol></nav>");
-
-            PathNavigation = navigationBuild.ToString();
+            href += "/" + item;
+            navigationBuild.Append($"<li class='breadcrumb-item'><a href='{href}'>{item}</a></li>");
         }
+
+        navigationBuild.Append("</ol></nav>");
+
+        PathNavigation = navigationBuild.ToString();
     }
 }
