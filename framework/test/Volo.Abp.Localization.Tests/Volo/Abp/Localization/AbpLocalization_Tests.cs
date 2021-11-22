0042960dd9971975baf168fdaa08f0a7b8b24f53
maliming <malimings@gmail.com>
2021-11-22 14:40:11 +0800

    Revert "update localization test"
    This reverts commit 27a87d7ef9f46754fc9aa3a290985603328b7ffe.

--- framework/test/Volo.Abp.Localization.Tests/Volo/Abp/Localization/AbpLocalization_Tests.cs
+++ framework/test/Volo.Abp.Localization.Tests/Volo/Abp/Localization/AbpLocalization_Tests.cs
@@ -89,12 +89,7 @@ public class AbpLocalization_Tests : AbpIntegratedTest<AbpLocalization_Tests.Tes
 
         using (CultureHelper.Use("tr"))
         {
-            _localizer["SeeYou"].Value.ShouldBe("Görüşürüz");
-        }
-        
-        using (CultureHelper.Use("pl"))
-        {
-            _localizer["SeeYou"].Value.ShouldBe("See you"); //Not defined in pl, getting from default lang
+            _localizer["SeeYou"].Value.ShouldBe("See you"); //Not defined in tr, getting from default lang
         }
 
         using (CultureHelper.Use("it"))
