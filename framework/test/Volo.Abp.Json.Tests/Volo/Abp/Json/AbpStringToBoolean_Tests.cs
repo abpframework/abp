efde7c542d3dbb43570a17c5d3ffe531f9de3fb4
maliming <malimings@gmail.com>
2021-11-29 17:33:34 +0800

    Add some unit tests.

---
+++ framework/test/Volo.Abp.Json.Tests/Volo/Abp/Json/AbpStringToBoolean_Tests.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using Shouldly;
+using Volo.Abp.Json.SystemTextJson.JsonConverters;
+using Xunit;
+
+namespace Volo.Abp.Json
+{
+    public class AbpStringToBoolean_Tests
+    {
+        [Fact]
+        public void Test_Read()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                Converters =
+                {
+                    new AbpStringToBooleanConverter()
+                }
+            };
+
+            var testClass = JsonSerializer.Deserialize<TestClass>("{\"Enabled\": \"TrUe\"}", options);
+            testClass.ShouldNotBeNull();
+            testClass.Enabled.ShouldBe(true);
+            
+            testClass = JsonSerializer.Deserialize<TestClass>("{\"Enabled\": true}", options);
+            testClass.ShouldNotBeNull();
+            testClass.Enabled.ShouldBe(true);
+        }
+        
+        [Fact]
+        public void Test_Write()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                Converters =
+                {
+                    new AbpStringToEnumFactory()
+                }
+            };
+
+            var testClassJson = JsonSerializer.Serialize(new TestClass()
+            {
+                Enabled = true
+            });
+            
+            testClassJson.ShouldBe("{\"Enabled\":true}");
+        }
+
+        class TestClass
+        {
+            public bool Enabled { get; set; }
+        }
+    }
+}
