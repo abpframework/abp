6d2d2643f339fb5407f92b90acd4079b374d26a6
maliming <malimings@gmail.com>
2021-11-29 17:33:34 +0800

    Add some unit tests.

---
+++ framework/test/Volo.Abp.Json.Tests/Volo/Abp/Json/AbpStringToEnum_Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.Json;
+using Shouldly;
+using Volo.Abp.Json.SystemTextJson.JsonConverters;
+using Xunit;
+
+namespace Volo.Abp.Json
+{
+    public class AbpStringToEnum_Tests
+    {
+        [Fact]
+        public void Test_Read()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                Converters =
+                {
+                    new AbpStringToEnumFactory()
+                }
+            };
+
+            var testClass = JsonSerializer.Deserialize<TestClass>("{\"Day\": \"Monday\"}", options);
+            testClass.ShouldNotBeNull();
+            testClass.Day.ShouldBe(DayOfWeek.Monday);
+            
+            testClass = JsonSerializer.Deserialize<TestClass>("{\"Day\": 1}", options);
+            testClass.ShouldNotBeNull();
+            testClass.Day.ShouldBe(DayOfWeek.Monday);
+        }
+        
+        [Fact]
+        public void Test_Write()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                Converters =
+                {
+                    new AbpStringToEnumFactory()
+                }
+            };
+
+            var testClassJson = JsonSerializer.Serialize(new TestClass()
+            {
+                Day = DayOfWeek.Monday
+            });
+            
+            testClassJson.ShouldBe("{\"Day\":1}");
+        }
+
+        class TestClass
+        {
+            public DayOfWeek Day { get; set; }
+        }
+    }
+}
