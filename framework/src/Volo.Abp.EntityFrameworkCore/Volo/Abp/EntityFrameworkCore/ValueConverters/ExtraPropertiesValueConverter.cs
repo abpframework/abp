7453381958df56c3a3aa764420cc778e0322c6a0
maliming <malimings@gmail.com>
2021-11-30 14:14:38 +0800

    Reuse more JsonSerializerOptions instances.

--- framework/src/Volo.Abp.EntityFrameworkCore/Volo/Abp/EntityFrameworkCore/ValueConverters/ExtraPropertiesValueConverter.cs
+++ framework/src/Volo.Abp.EntityFrameworkCore/Volo/Abp/EntityFrameworkCore/ValueConverters/ExtraPropertiesValueConverter.cs
@@ -41,17 +41,22 @@ namespace Volo.Abp.EntityFrameworkCore.ValueConverters
             return JsonSerializer.Serialize(copyDictionary);
         }
 
+        private static readonly JsonSerializerOptions DeserializeOptions = new JsonSerializerOptions()
+        {
+            Converters =
+            {
+                new ObjectToInferredTypesConverter()
+            }
+        };
+        
         private static ExtraPropertyDictionary DeserializeObject(string extraPropertiesAsJson, Type entityType)
         {
             if (extraPropertiesAsJson.IsNullOrEmpty() || extraPropertiesAsJson == "{}")
             {
                 return new ExtraPropertyDictionary();
             }
-
-            var deserializeOptions = new JsonSerializerOptions();
-            deserializeOptions.Converters.Add(new ObjectToInferredTypesConverter());
-
-            var dictionary = JsonSerializer.Deserialize<ExtraPropertyDictionary>(extraPropertiesAsJson, deserializeOptions) ??
+            
+            var dictionary = JsonSerializer.Deserialize<ExtraPropertyDictionary>(extraPropertiesAsJson, DeserializeOptions) ??
                              new ExtraPropertyDictionary();
 
             if (entityType != null)
