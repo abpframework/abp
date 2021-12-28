45ec807326054fa0a6ee24afca391882f72c8b92
liangshiwei <liangshiw@outlook.com>
2021-12-28 12:00:46 +0800

    Update ExtensionPropertyComponentBase.cs

--- framework/src/Volo.Abp.BlazoriseUI/Components/ObjectExtending/ExtensionPropertyComponentBase.cs
+++ framework/src/Volo.Abp.BlazoriseUI/Components/ObjectExtending/ExtensionPropertyComponentBase.cs
@@ -33,47 +33,51 @@ public abstract class ExtensionPropertyComponentBase<TEntity, TResourceType> : O
     {
         e.Status = ValidationStatus.Success;
 
-        var result = ExtensibleObjectValidator.GetValidationErrors(Entity, PropertyInfo.Name, e.Value);
-        if (!result.Any())
-        {
-            return;
-        }
-
-        if (LH == null)
-        {
-            e.MemberNames = result.First().MemberNames;
-            e.Status = ValidationStatus.Error;
-            e.ErrorText = result.First().ErrorMessage;
-            return;
-        }
-
-
-        var formattedValidationAttributes = PropertyInfo.GetValidationAttributes();
-        foreach (var validationAttribute in formattedValidationAttributes )
-        {
-            if (validationAttribute.ErrorMessageResourceName == null)
-            {
-                ValidationAttributeHelper.SetDefaultErrorMessage(validationAttribute);
-            }
-        }
-
-        var formattedResults = new List<ValidationResult>();
+        var validationAttributes = PropertyInfo.GetValidationAttributes();
         var validationContext = new ValidationContext(Entity)
         {
             DisplayName = PropertyInfo.Name,
             MemberName = PropertyInfo.Name
         };
 
-        Validator.TryValidateValue(e.Value, validationContext, formattedResults, formattedValidationAttributes);
+        foreach (var validationAttribute in validationAttributes)
+        {
+            var result = validationAttribute.GetValidationResult(e.Value, validationContext);
+            if (result == ValidationResult.Success || result == null)
+            {
+                continue;
+            }
 
-        var errorMessage = result.First().ErrorMessage;
-        var errorMessageString = ValidationAttributeHelper.RevertErrorMessagePlaceholders(formattedResults.First().ErrorMessage);
-        var errorMessageArguments = ValidationMessageLocalizerAttributeFinder.FindAll(errorMessage, errorMessageString)
-            ?.OrderBy(x => x.Index)
-            ?.Select(x => x.Argument);
+            var errorMessage = result.ErrorMessage;
+            if (LH != null)
+            {
+                var formattedErrorMessage = GetDefaultErrorMessage(validationAttribute);
+                var errorMessageString = ValidationAttributeHelper.RevertErrorMessagePlaceholders(formattedErrorMessage);
+                var errorMessageArguments = ValidationMessageLocalizerAttributeFinder.FindAll(errorMessage, errorMessageString)
+                    ?.OrderBy(x => x.Index)
+                    ?.Select(x => x.Argument);
 
-        e.MemberNames = result.First().MemberNames;
-        e.Status = ValidationStatus.Error;
-        e.ErrorText = LH.Localize(errorMessageString, errorMessageArguments);
+                errorMessage = LH.Localize(errorMessageString, errorMessageArguments);
+            }
+
+            e.MemberNames = result.MemberNames;
+            e.Status = ValidationStatus.Error;
+            e.ErrorText = errorMessage;
+            break;
+        }
+    }
+
+    private static string GetDefaultErrorMessage(ValidationAttribute validationAttribute)
+    {
+        if (validationAttribute is StringLengthAttribute stringLengthAttribute && stringLengthAttribute.MinimumLength != 0)
+        {
+            var nullable = ValidationAttributeHelper.ValidationAttributeCustomErrorMessageSetProperty.GetValue((object) validationAttribute) as bool?;
+            var flag = true;
+            if (!(nullable.GetValueOrDefault() == flag & nullable.HasValue))
+            {
+                return ValidationAttributeHelper.SetErrorMessagePlaceholders("The field {0} must be a string with a minimum length of {2} and a maximum length of {1}.");
+            }
+        }
+        return ValidationAttributeHelper.SetErrorMessagePlaceholders(ValidationAttributeHelper.ValidationAttributeErrorMessageStringProperty.GetValue((object) validationAttribute) as string);
     }
 }
