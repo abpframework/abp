fea2d4308ce6ce6304c8b6f8fe30a097455f4b1f
maliming <malimings@gmail.com>
2022-01-06 13:11:25 +0800

    Update bootstrap.enable.popovers.everywhere.js

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Web.Host/wwwroot/libs/bootstrap/js/bootstrap.enable.popovers.everywhere.js
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Web.Host/wwwroot/libs/bootstrap/js/bootstrap.enable.popovers.everywhere.js
@@ -1,5 +1,5 @@
 (function () {
-    [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]').map(function (popoverTriggerEl) {
+    [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]')).map(function (popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl)
     })
 })();
