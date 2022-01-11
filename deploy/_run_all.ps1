7f77ae54f0bacf64b0c17f5bd2b67a174ed18122
Alper Ebicoglu <alper.ebicoglu@volosoft.com>
2022-01-11 18:36:37 +0300

    Create _run_all.ps1

---
+++ deploy/_run_all.ps1
@@ -0,0 +1,7 @@
+./1-fetch-and-build.ps1
+./2-nuget-pack.ps1
+./3-nuget-push.ps1
+./4-npm-publish-mvc.ps1
+./5-npm-publish-angular.ps1
+./6-git-commit.ps1
+echo "Create a new release on GitHub manually and run the step 8..."
