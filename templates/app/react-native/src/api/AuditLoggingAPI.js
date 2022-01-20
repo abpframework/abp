01ed0ad9aeacd43ee4cd55eeb111f36a5b8d1861
mehmet-erim <bilgi@mehmeterim.com>
2022-01-20 11:14:50 +0300

    remove an unused api

--- templates/app/react-native/src/api/AuditLoggingAPI.js
+++
@@ -1,7 +0,0 @@
-import api from './API';
-
-export function getErrorRateStatistics(params = {}) {
-  return api
-    .get('/api/audit-logging/audit-logs/statistics/error-rate', { params })
-    .then(({ data }) => data);
-}
