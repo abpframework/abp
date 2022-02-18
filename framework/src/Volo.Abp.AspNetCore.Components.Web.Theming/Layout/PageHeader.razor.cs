c2d8e12fe44a6873f12a2ee0cda3acd862b68504
enisn <info@enisnecipoglu.com>
2022-02-18 13:50:04 +0300

    Updating PageLayout to notify when one of its members changed

--- framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/Layout/PageHeader.razor.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/Layout/PageHeader.razor.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Volo.Abp.AspNetCore.Components.Web.Theming.PageToolbars;
 using Volo.Abp.BlazoriseUI;
+using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace Volo.Abp.AspNetCore.Components.Web.Theming.Layout;
 
@@ -30,8 +32,8 @@ public partial class PageHeader : ComponentBase
 
     [Parameter] // TODO: Consider removing this property in future and use only PageLayout.
     public List<BreadcrumbItem> BreadcrumbItems {
-        get => PageLayout.BreadcrumbItems;
-        set => PageLayout.BreadcrumbItems = value;
+        get => PageLayout.BreadcrumbItems.ToList();
+        set => PageLayout.BreadcrumbItems = new ObservableCollection<BreadcrumbItem>(value);
     }
 
     [Parameter]
