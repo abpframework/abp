354af0bc48e6e45b74d259ded16d8a221b10238d
Engincan VESKE <enginveske@gmail.com>
2022-02-23 09:04:55 +0300

    Add support to hide ABP endpoints on Swagger UI

---
+++ framework/src/Volo.Abp.Swashbuckle/Microsoft/Extensions/DependencyInjection/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using Volo.Abp.Swashbuckle;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class SwaggerGenOptionsExtensions
+{
+    public static void HideAbpEndpoints(this SwaggerGenOptions swaggerGenOptions)
+    {
+        swaggerGenOptions.DocumentFilter<AbpSwashbuckleDocumentFilter>();
+    }
+}
