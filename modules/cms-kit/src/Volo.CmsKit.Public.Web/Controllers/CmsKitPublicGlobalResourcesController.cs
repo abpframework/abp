ef7f03a2d41398d64ec57a7822705c850ef331d2
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2022-02-24 16:49:00 +0300

    CmsKit global resources Layout hook implementation

---
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Controllers/CmsKitPublicGlobalResourcesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.CmsKit.Public.GlobalResources;
+
+namespace Volo.CmsKit.Public.Web.Controllers;
+
+[Route("global-resources")]
+public class CmsKitPublicGlobalResourcesController: AbpController
+{
+    private readonly IGlobalResourcePublicAppService _globalResourcePublicAppService;
+
+    public CmsKitPublicGlobalResourcesController(IGlobalResourcePublicAppService globalResourcePublicAppService)
+    {
+        _globalResourcePublicAppService = globalResourcePublicAppService;
+    }
+    
+    [HttpGet]
+    [Route("style")]
+    public async Task<IActionResult> GetGlobalStyleAsync(string entityType, string entityId)
+    {
+        var style = await _globalResourcePublicAppService.GetGlobalStyleAsync();
+        
+        var contents = Encoding.ASCII.GetBytes(style.Value);
+        
+        return new FileContentResult(contents, "text/css");
+    }
+    
+    [HttpGet]
+    [Route("script")]
+    public async Task<IActionResult> GetGlobalScriptAsync(string entityType, string entityId)
+    {
+        var script = await _globalResourcePublicAppService.GetGlobalScriptAsync();
+        
+        var contents = Encoding.ASCII.GetBytes(script.Value);
+        
+        return new FileContentResult(contents, "application/javascript");
+    }
+}
