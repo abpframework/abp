d6597d3db17cbb46b583d5332155da23a66fba5c
liangshiwei <liangshiw@outlook.com>
2022-03-01 11:03:16 +0800

    Update datatables-extensions.js

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/datatables/datatables-extensions.js
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/datatables/datatables-extensions.js
@@ -366,7 +366,7 @@ var abp = abp || {};
                 }
 
                 //Text filter
-                if(settings.oInit.searching || settings.oInit.searching === undefined){
+                if(settings.oInit.searching !== false){
                     if (requestData.search && requestData.search.value !== "") {
                         input.filter = requestData.search.value;
                     } else {
