e4dfad0a9e3806a11d91c8cb8f42647f6663cf87
liangshiwei <liangshiw@outlook.com>
2022-03-14 15:17:43 +0800

    Fix Cross-site scripting problem

--- npm/packs/jquery-form/abp.resourcemapping.js
+++ npm/packs/jquery-form/abp.resourcemapping.js
@@ -1,6 +1,5 @@
 module.exports = {
     mappings: {
-        "@node_modules/jquery-form/dist/jquery.form.min.js": "@libs/jquery-form/",
-        "@node_modules/jquery-form/dist/jquery.form.min.js.map": "@libs/jquery-form/"
+        "@node_modules/@abp/jquery-form/src/*.*": "@libs/jquery-form/"
     }
 }
