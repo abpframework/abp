b870fbe4769add75cab464791b546bad18f61af0
Enis Necipoglu <enis.necipoglu@gmail.com>
2022-03-14 08:54:31 +0300

    Add code-mirror mapping for test app

--- modules/cms-kit/host/Volo.CmsKit.Web.Unified/abp.resourcemapping.js
+++ modules/cms-kit/host/Volo.CmsKit.Web.Unified/abp.resourcemapping.js
@@ -7,6 +7,9 @@ module.exports = {
         "@libs"
     ],
     mappings: {
-
+        "@node_modules/codemirror/lib/*.*": "@libs/codemirror/",
+        "@node_modules/codemirror/mode/**/*.*": "@libs/codemirror/mode/",
+        "@node_modules/codemirror/theme/**/*.*": "@libs/codemirror/theme/",
+        "@node_modules/codemirror/addon/**/*.*": "@libs/codemirror/addon/"
     }
 }
