68daf21693525f867ebd9171e35b8f19b6f7f107
selman koc <64414348+skoc10@users.noreply.github.com>
2022-03-18 10:35:42 +0300

    Add files via upload

---
+++ nupkg/unit_test.ps1
@@ -0,0 +1,7 @@
+. ".\common.ps1"
+
+# Unit test for all solutions
+foreach($solution in $solutions) {
+    $solutionFolder = Join-Path $rootFolder $solution
+    Set-Location $solutionFolder
+    dotnet test --no-build
