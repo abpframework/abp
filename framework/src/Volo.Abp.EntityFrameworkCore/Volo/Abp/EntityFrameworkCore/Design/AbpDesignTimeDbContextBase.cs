0891d82d48a57df22e5b70ad2d54236815c76cba
maliming <malimings@gmail.com>
2022-04-18 14:47:05 +0800

    Add `AbpDesignTimeDbContextBase`.

---
+++ framework/src/Volo.Abp.EntityFrameworkCore/Volo/Abp/EntityFrameworkCore/Design/AbpDesignTimeDbContextBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+using Volo.Abp.Threading;
+
+namespace Volo.Abp.EntityFrameworkCore.Design;
+
+public abstract class AbpDesignTimeDbContextBase<TModule, TContext> : IDesignTimeDbContextFactory<TContext>
+    where TModule : AbpModule
+    where TContext : DbContext
+{
+    public virtual TContext CreateDbContext(string[] args)
+    {
+        return AsyncHelper.RunSync(() => CreateDbContextAsync(args));
+    }
+
+    protected virtual async Task<TContext> CreateDbContextAsync(string[] args)
+    {
+        var application = await AbpApplicationFactory.CreateAsync<TModule>();
+        application.Services.ReplaceConfiguration(BuildConfiguration());
+        await application.InitializeAsync();
+        return application.ServiceProvider.GetRequiredService<TContext>();
+    }
+
+    protected abstract IConfigurationRoot BuildConfiguration();
+}
