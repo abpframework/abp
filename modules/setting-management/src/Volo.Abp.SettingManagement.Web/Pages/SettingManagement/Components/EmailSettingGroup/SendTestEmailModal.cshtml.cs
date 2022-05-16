c736c57d3528fc21babab05e927ee1a90f411781
liangshiwei <liangshiw@outlook.com>
2022-05-16 17:15:04 +0800

    Sending test email in the setting page with MVC and Blazor UI

---
+++ modules/setting-management/src/Volo.Abp.SettingManagement.Web/Pages/SettingManagement/Components/EmailSettingGroup/SendTestEmailModal.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+using Volo.Abp.SettingManagement.Localization;
+
+namespace Volo.Abp.SettingManagement.Web.Pages.SettingManagement.Components.EmailSettingGroup;
+
+[Authorize(SettingManagementPermissions.EmailingTest)]
+public class SendTestEmailModal : AbpPageModel
+{
+    [BindProperty]
+    public SendTestEmailViewModel Input { get; set; }
+    
+    protected IEmailSettingsAppService EmailSettingsAppService { get; }
+    
+    public SendTestEmailModal(IEmailSettingsAppService emailSettingsAppService)
+    {
+        LocalizationResourceType = typeof(AbpSettingManagementResource);
+        EmailSettingsAppService = emailSettingsAppService;
+    }
+
+    public async Task OnGetAsync()
+    {
+        var emailSettings = await EmailSettingsAppService.GetAsync();
+        Input = new SendTestEmailViewModel 
+        {
+            SenderEmailAddress = emailSettings.DefaultFromAddress,
+            TargetEmailAddress = CurrentUser.Email,
+            Subject = L["TestEmailSubject", new Random().Next(1000, 9999)],
+            Body = L["TestEmailBody"]
+        };
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        ValidateModel();
+        
+        await EmailSettingsAppService.SendTestEmailAsync(ObjectMapper.Map<SendTestEmailViewModel, SendTestEmailInput>(Input));
+        
+        return NoContent();
+    }
+    
+    public class SendTestEmailViewModel
+    {
+        [Required]
+        [Display(Name = "SenderEmailAddress")]
+        public string SenderEmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "TargetEmailAddress")]
+        public string TargetEmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+    
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+    }
+}
