6057f2a12d272c6eec13588eb525256139e02337
malik masis <malik.masis@gmail.com>
2022-06-06 12:04:16 +0300

    Converted widget name to the actual vc

--- modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/Pages/ContentParser.cs
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/Pages/ContentParser.cs
@@ -1,19 +1,31 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
 
 namespace Volo.CmsKit.Public.Web.Pages.CmsKit.Shared.Components.Pages;
 
 public class ContentParser : IContentParser, ITransientDependency
 {
+    private readonly CmsKitContentWidgetOptions _options;
+    public ContentParser(IOptions<CmsKitContentWidgetOptions> options)
+    {
+        _options = options.Value;
+    }
+
     public async Task<List<ContentFragment>> ParseAsync(string content)
     {
+        var name = _options.WidgetConfigs.FirstOrDefault(p => p.Key == "Poll").Value?.Name;
         return new List<ContentFragment> {
             new MarkdownContentFragment {
                 Content = "**ABP Framework** is completely open source and developed in a community-driven manner."
             },
-            new WidgetContentFragment("CmsPoll") { Properties = { { "widgetName", "poll-right" } } },
-            new WidgetContentFragment("CmsPollByName") { Properties = { { "name", "poll-name" } } },
+            new WidgetContentFragment(name) {
+                Properties = { 
+                    { "name", "poll-name" }
+                } 
+            },
             new MarkdownContentFragment {
                 Content = "Thanks _for_ *your* feedback."
             }
