6773ab41b97d83d318dc4dfaee7ec53a6a39e976
liangshiwei <liangshiw@outlook.com>
2022-06-09 18:27:26 +0800

    Add MAUI template

---
+++ templates/maui/src/MyCompanyName.MyProjectName/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Volo.Abp;
+
+namespace MyCompanyName.MyProjectName;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddApplication<MyProjectNameModule>(options =>
+        {
+            options.UseAutofac();
+            options.Services.ReplaceConfiguration(builder.Configuration);
+        });
+
+        var app = builder.Build();
+
+        app.Services.GetRequiredService<IAbpApplicationWithExternalServiceProvider>().Initialize(app.Services);
+
+        return app;
+    }
+}
---
+++ templates/maui/src/MyCompanyName.MyProjectName/MyProjectNameModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace MyCompanyName.MyProjectName;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class MyProjectNameModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddTransient<MainPage>();
+    }
+}
