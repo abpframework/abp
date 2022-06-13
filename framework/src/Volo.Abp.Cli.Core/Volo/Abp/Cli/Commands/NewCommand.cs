258025d71951bf33e53e63d8345acf0169f3dadd
Engincan VESKE <enginveske@gmail.com>
2022-06-13 15:08:28 +0300

    Add `--theme` option to usage-info for CLI

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/NewCommand.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/Commands/NewCommand.cs
@@ -109,6 +109,7 @@ public class NewCommand : ProjectCreationCommandBase, IConsoleCommand, ITransien
         sb.AppendLine("-csf|--create-solution-folder               (default: true)");
         sb.AppendLine("-cs|--connection-string <connection-string> (your database connection string)");
         sb.AppendLine("--dbms <database-management-system>         (your database management system)");
+        sb.AppendLine("--theme <theme-name>                        (if supported by the template. default: leptonx-lite)");
         sb.AppendLine("--tiered                                    (if supported by the template)");
         sb.AppendLine("--no-ui                                     (if supported by the template)");
         sb.AppendLine("--no-random-port                            (Use template's default ports)");
@@ -132,6 +133,7 @@ public class NewCommand : ProjectCreationCommandBase, IConsoleCommand, ITransien
         sb.AppendLine("  abp new Acme.BookStore -csf false");
         sb.AppendLine("  abp new Acme.BookStore --local-framework-ref --abp-path \"D:\\github\\abp\"");
         sb.AppendLine("  abp new Acme.BookStore --dbms mysql");
+        sb.AppendLine("  abp new Acme.BookStore --theme basic");
         sb.AppendLine("  abp new Acme.BookStore --connection-string \"Server=myServerName\\myInstanceName;Database=myDatabase;User Id=myUsername;Password=myPassword\"");
         sb.AppendLine("");
         sb.AppendLine("See the documentation for more info: https://docs.abp.io/en/abp/latest/CLI");
