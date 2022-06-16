3de1f919d8b4a0daafc5f6fa69bfa76311cb9aa1
malik masis <malik.masis@gmail.com>
2022-06-16 15:49:56 +0300

    Removed duplication

---
+++ modules/cms-kit/src/Volo.CmsKit.Common.Web/Contents/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Volo.CmsKit.Web.Contents;
+
+public static class Helper
+{
+    public static dynamic ConvertToDynamicObject(this Dictionary<string, object> dict)
+    {
+        var eo = new ExpandoObject();
+        var eoColl = (ICollection<KeyValuePair<string, object>>)eo;
+
+        foreach (var kvp in dict)
+        {
+            eoColl.Add(kvp);
+        }
+
+        dynamic eoDynamic = eo;
+
+        return eoDynamic;
+    }
+}
+
