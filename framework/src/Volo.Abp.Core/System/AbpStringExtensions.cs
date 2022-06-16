30786a0e53dfd47eba8271c145c7334155c4ace9
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-06-16 14:21:50 +0300

    Implement ContractAnnotation for IsNullOrEmpty & IsNullOrWhiteSpace extension methods.

--- framework/src/Volo.Abp.Core/System/AbpStringExtensions.cs
+++ framework/src/Volo.Abp.Core/System/AbpStringExtensions.cs
@@ -46,6 +46,7 @@ public static class AbpStringExtensions
     /// <summary>
     /// Indicates whether this string is null or an System.String.Empty string.
     /// </summary>
+    [ContractAnnotation("str:null => true")]
     public static bool IsNullOrEmpty(this string str)
     {
         return string.IsNullOrEmpty(str);
@@ -54,6 +55,7 @@ public static class AbpStringExtensions
     /// <summary>
     /// indicates whether this string is null, empty, or consists only of white-space characters.
     /// </summary>
+    [ContractAnnotation("str:null => true")]
     public static bool IsNullOrWhiteSpace(this string str)
     {
         return string.IsNullOrWhiteSpace(str);
