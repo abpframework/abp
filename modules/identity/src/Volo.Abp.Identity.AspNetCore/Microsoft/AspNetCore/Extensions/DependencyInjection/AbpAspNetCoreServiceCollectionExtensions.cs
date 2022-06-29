481d031c3eb33a635a381d4ab12254353e710bb7
maliming <malimings@gmail.com>
2022-06-29 16:20:27 +0800

    Update AbpAspNetCoreServiceCollectionExtensions.cs

--- modules/identity/src/Volo.Abp.Identity.AspNetCore/Microsoft/AspNetCore/Extensions/DependencyInjection/AbpAspNetCoreServiceCollectionExtensions.cs
+++ modules/identity/src/Volo.Abp.Identity.AspNetCore/Microsoft/AspNetCore/Extensions/DependencyInjection/AbpAspNetCoreServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ namespace Microsoft.AspNetCore.Extensions.DependencyInjection;
 
 public static class AbpAspNetCoreServiceCollectionExtensions
 {
-    public static IServiceCollection ForwardIdentityAuthenticationForBearer(this IServiceCollection services, string targetAuthenticationScheme = "Bearer")
+    public static IServiceCollection ForwardIdentityAuthenticationForBearer(this IServiceCollection services, string jwtBearerScheme = "Bearer")
     {
         services.ConfigureApplicationCookie(options =>
         {
@@ -14,7 +14,7 @@ public static class AbpAspNetCoreServiceCollectionExtensions
                 string authorization = ctx.Request.Headers.Authorization;
                 if (!authorization.IsNullOrWhiteSpace() && authorization.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
                 {
-                    return targetAuthenticationScheme;
+                    return jwtBearerScheme;
                 }
 
                 return null;
