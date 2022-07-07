c95658683fa5c399fbf505d38b970c184f84f75b
Engincan VESKE <enginveske@gmail.com>
2022-07-07 14:28:52 +0300

    Create dynamic components for blazor layout

---
+++ framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/AbpDynamicComponentOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Volo.Abp.AspNetCore.Components.Web.Theming;
+
+public class AbpDynamicComponentOptions
+{
+    /// <summary>
+    /// Used to define components that renders in the layout
+    /// </summary>
+    [NotNull]
+    public Dictionary<Type, IDictionary<string,object>?> Components { get; set; }
+
+    public AbpDynamicComponentOptions()
+    {
+        Components = new Dictionary<Type, IDictionary<string, object>?>();
+    }
+}
---
+++ framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/Components/AbpDynamicComponents.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+
+namespace Volo.Abp.AspNetCore.Components.Web.Theming.Components;
+
+public partial class AbpDynamicComponents : ComponentBase
+{
+    [Inject]
+    protected IOptions<AbpDynamicComponentOptions> AbpDynamicComponentOptions { get; set; }
+}
