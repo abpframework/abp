8b361da7bb8babb53799f1ae42932d8afbb9e607
malik masis <malik.masis@gmail.com>
2022-07-14 10:12:26 +0300

    Added some example widgets for doc

---
+++ modules/cms-kit/host/Volo.CmsKit.Web.Unified/ViewComponents/DecisionCommentDateViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace Volo.CmsKit.ViewComponents;
+
+[Widget(
+    AutoInitialize = true
+)]
+
+[ViewComponent(Name = "DecisionCommentDate")]
+public class DecisionCommentDateViewComponent : AbpViewComponent
+{
+    public DecisionCommentDateViewComponent()
+    {
+    }
+
+    public virtual async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("~/ViewComponents/DecisionCommentDate.cshtml");
+    }
+}
