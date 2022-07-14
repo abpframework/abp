24ec31bfe20fdd1aaaaa50795a4b26fa26e4a4bd
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2022-07-14 11:12:58 +0300

    Fix issue #13006

--- npm/ng-packs/packages/theme-shared/src/lib/constants/validation.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/constants/validation.ts
@@ -2,10 +2,10 @@ export const DEFAULT_VALIDATION_BLUEPRINTS = {
   creditCard: 'AbpValidation::ThisFieldIsNotAValidCreditCardNumber.',
   email: 'AbpValidation::ThisFieldIsNotAValidEmailAddress.',
   invalid: 'AbpValidation::ThisFieldIsNotValid.',
-  max: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+  max: 'AbpValidation::ThisFieldMustBeLessOrEqual{0}[{{ max }}]',
   maxlength:
     'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMaximumLengthOf{0}[{{ requiredLength }}]',
-  min: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+  min: 'AbpValidation::ThisFieldMustBeGreaterThanOrEqual{0}[{{ min }}]',
   minlength:
     'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMinimumLengthOf{0}[{{ requiredLength }}]',
   ngbDate: 'AbpValidation::ThisFieldIsNotValid.',
