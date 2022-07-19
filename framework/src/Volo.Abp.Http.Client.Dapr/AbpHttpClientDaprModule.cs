e8ab404420ffbd2e183d24a41e735441858b663b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-07-19 13:29:17 +0300

    Added Volo.Abp.Http.Client.Dapr package.

---
+++ framework/src/Volo.Abp.Http.Client.Dapr/AbpHttpClientDaprModule.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Dapr;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.Http.Client.Dapr;
+
+[DependsOn(
+    typeof(AbpHttpClientModule),
+    typeof(AbpDaprModule)
+    )]
+public class AbpHttpClientDaprModule : AbpModule
+{
+}
