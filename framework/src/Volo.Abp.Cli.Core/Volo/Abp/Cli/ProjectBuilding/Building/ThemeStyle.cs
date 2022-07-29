10b84d71ce2c172a79f00aae91e5d35530a92e8b
Engincan VESKE <enginveske@gmail.com>
2022-07-29 16:44:24 +0300

    Update ThemeStyle.cs

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectBuilding/Building/ThemeStyle.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectBuilding/Building/ThemeStyle.cs
@@ -1,9 +1,8 @@
 namespace Volo.Abp.Cli.ProjectBuilding.Building;
 public enum ThemeStyle : byte
 {
-    NotSpecified = 0,
-    System = 1,
-    Dim = 2,
-    Light = 3,
-    Dark = 4
+    System = 0,
+    Dim = 1,
+    Light = 2,
+    Dark = 3
 }
