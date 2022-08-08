4da3ad0b6fbc7763a9b838081fc3fa0c6f37ffbf
malik masis <malik.masis@gmail.com>
2022-08-08 14:31:23 +0300

    Added a new column to Page named `IsHomePage`

--- modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Pages/PageManager.cs
+++ modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Pages/PageManager.cs
@@ -1,9 +1,5 @@
-﻿using JetBrains.Annotations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Volo.Abp;
 using Volo.Abp.Domain.Services;
 
