000f32e6b45d5588d2a6c69b7595702710b2300f
maliming <malimings@gmail.com>
2022-08-08 14:21:57 +0800

    Use abp JsonSerializerOptions in `AbpDaprClientFactory`.

--- framework/src/Volo.Abp.Dapr/Volo/Abp/Dapr/AbpDaprClientFactory.cs
+++ framework/src/Volo.Abp.Dapr/Volo/Abp/Dapr/AbpDaprClientFactory.cs
@@ -1,25 +1,44 @@
-﻿using Dapr.Client;
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
+using Dapr.Client;
 using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Json.SystemTextJson;
 
 namespace Volo.Abp.Dapr;
 
 public class AbpDaprClientFactory : ITransientDependency
 {
     protected AbpDaprOptions Options { get; }
+    protected AbpSystemTextJsonSerializerOptions SystemTextJsonSerializerOptions { get; }
 
-    public AbpDaprClientFactory(IOptions<AbpDaprOptions> options)
+    public AbpDaprClientFactory(
+        IOptions<AbpDaprOptions> options,
+        IOptions<AbpSystemTextJsonSerializerOptions> systemTextJsonSerializerOptions)
     {
         Options = options.Value;
+        SystemTextJsonSerializerOptions = systemTextJsonSerializerOptions.Value;
     }
 
-    public virtual Task<DaprClient> CreateAsync()
+    public virtual async Task<DaprClient> CreateAsync()
     {
-        var daprClient = new DaprClientBuilder()
+        var builder = new DaprClientBuilder()
             .UseHttpEndpoint(Options.HttpEndpoint)
-            //.UseJsonSerializationOptions()//TODO: Use abp JsonSerializerOptions
-            .Build();
+            .UseJsonSerializationOptions(await CreateJsonSerializerOptions());
 
-        return Task.FromResult(daprClient);
+        if (!Options.GrpcEndpoint.IsNullOrWhiteSpace())
+        {
+            builder.UseGrpcEndpoint(Options.GrpcEndpoint);
+        }
+
+        return builder.Build();
+    }
+
+    private readonly static ConcurrentDictionary<string, JsonSerializerOptions> JsonSerializerOptionsCache = new ConcurrentDictionary<string, JsonSerializerOptions>();
+
+    protected virtual Task<JsonSerializerOptions> CreateJsonSerializerOptions()
+    {
+        return Task.FromResult(JsonSerializerOptionsCache.GetOrAdd(nameof(AbpDaprClientFactory),
+            _ => new JsonSerializerOptions(SystemTextJsonSerializerOptions.JsonSerializerOptions)));
     }
 }
