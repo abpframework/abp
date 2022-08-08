d6455db2bde22e2f0bd6e60db3fdc7ceae6cc018
maliming <malimings@gmail.com>
2022-08-08 14:03:06 +0800

    Add `AbpDaprClientFactory`.

---
+++ framework/src/Volo.Abp.Dapr/Volo/Abp/Dapr/AbpDaprClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using Dapr.Client;
+using Microsoft.Extensions.Options;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.Dapr;
+
+public class AbpDaprClientFactory : ITransientDependency
+{
+    protected AbpDaprOptions Options { get; }
+
+    public AbpDaprClientFactory(IOptions<AbpDaprOptions> options)
+    {
+        Options = options.Value;
+    }
+
+    public virtual Task<DaprClient> CreateAsync()
+    {
+        var daprClient = new DaprClientBuilder()
+            .UseHttpEndpoint(Options.HttpEndpoint)
+            //.UseJsonSerializationOptions()//TODO: Use abp JsonSerializerOptions
+            .Build();
+
+        return Task.FromResult(daprClient);
+    }
+}
