bccd1155906b0a0c34a711e3f27165516c9432fe
liangshiwei <liangshiw@outlook.com>
2022-08-09 18:04:45 +0800

    Implement the Dapr http client

---
+++ framework/src/Volo.Abp.Http.Client.Dapr/Volo/Abp/Http/Client/Dapr/AbpDaprClientOptions.cs
@@ -0,0 +1,24 @@
+﻿using Dapr.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Volo.Abp.Http.Client.Dapr;
+
+public class AbpDaprClientOptions
+{
+    public Action<string, IHttpClientBuilder, string> DaprHttpClientBuilderAction { get; set; }
+
+    public AbpDaprClientOptions()
+    {
+        DaprHttpClientBuilderAction = DefaultDaprHttpClientBuilder;
+    }
+    
+    private void DefaultDaprHttpClientBuilder(string serviceName, IHttpClientBuilder clientBuilder, string daprEndpoint)
+    {
+        Check.NotNull(daprEndpoint, nameof(daprEndpoint));
+        
+        clientBuilder.AddHttpMessageHandler(() => new InvocationHandler
+        {
+            DaprEndpoint = daprEndpoint
+        });
+    }
+}
--- framework/src/Volo.Abp.Http.Client.Dapr/Volo/Abp/Http/Client/Dapr/AbpHttpClientDaprModule.cs
+++ framework/src/Volo.Abp.Http.Client.Dapr/Volo/Abp/Http/Client/Dapr/AbpHttpClientDaprModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.Dapr;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Dapr;
 using Volo.Abp.Modularity;
 
 namespace Volo.Abp.Http.Client.Dapr;
@@ -6,7 +7,20 @@ namespace Volo.Abp.Http.Client.Dapr;
 [DependsOn(
     typeof(AbpHttpClientModule),
     typeof(AbpDaprModule)
-    )]
+)]
 public class AbpHttpClientDaprModule : AbpModule
-{
+{ 
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var daprOptions = context.Services.ExecutePreConfiguredActions<AbpDaprOptions>();
+        var daprClientOptions = context.Services.ExecutePreConfiguredActions<AbpDaprClientOptions>();
+
+        PreConfigure<AbpHttpClientBuilderOptions>(options =>
+        {
+            options.ProxyClientBuildActions.Add((_, clientBuilder) =>
+            {
+                daprClientOptions.DaprHttpClientBuilderAction(_, clientBuilder, daprOptions.HttpEndpoint);
+            });
+        });
+    }
 }
