556667e7f0af6a027e934e1bbddb21b10b971d05
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-08-11 16:20:13 +0300

    implemented mongo repos for permission entities.

---
+++ modules/permission-management/src/Volo.Abp.PermissionManagement.MongoDB/Volo/Abp/PermissionManagement/MongoDb/MongoPermissionDefinitionRecordRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Domain.Repositories.MongoDB;
+using Volo.Abp.MongoDB;
+
+namespace Volo.Abp.PermissionManagement.MongoDB;
+
+public class MongoPermissionDefinitionRecordRepository :
+    MongoDbRepository<IPermissionManagementMongoDbContext, PermissionDefinitionRecord, Guid>,
+    IPermissionDefinitionRecordRepository
+{
+    public MongoPermissionDefinitionRecordRepository(
+        IMongoDbContextProvider<IPermissionManagementMongoDbContext> dbContextProvider)
+        : base(dbContextProvider)
+    {
+    }
+}
