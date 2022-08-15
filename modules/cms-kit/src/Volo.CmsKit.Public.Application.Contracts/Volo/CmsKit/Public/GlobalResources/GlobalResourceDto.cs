24696bb4c00ebc0449029f3f6fa0f5d2ecaf3ae5
malik masis <malik.masis@gmail.com>
2022-08-15 14:01:20 +0300

    Used `LastModificationTime` instead of GUID

--- modules/cms-kit/src/Volo.CmsKit.Public.Application.Contracts/Volo/CmsKit/Public/GlobalResources/GlobalResourceDto.cs
+++ modules/cms-kit/src/Volo.CmsKit.Public.Application.Contracts/Volo/CmsKit/Public/GlobalResources/GlobalResourceDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Volo.CmsKit.Public.GlobalResources;
+﻿using System;
 
+namespace Volo.CmsKit.Public.GlobalResources;
+
+[Serializable]
 public class GlobalResourceDto
 {
     public string Name { get; set; }
-    
+
     public string Value { get; set; }
+
+    public DateTime? LastModificationTime { get; set; }
 }
