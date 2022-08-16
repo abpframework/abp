1751216a2ea1a8fb5aa307e0cacb79bd3e67751f
malik masis <malik.masis@gmail.com>
2022-08-16 17:34:01 +0300

    Added validation to `EntityTagSetDto` for Tags

--- modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Tags/EntityTagSetDto.cs
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Application.Contracts/Volo/CmsKit/Admin/Tags/EntityTagSetDto.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Volo.CmsKit.Tags;
 
 namespace Volo.CmsKit.Admin.Tags;
 
 [Serializable]
-public class EntityTagSetDto
+public class EntityTagSetDto : IValidatableObject
 {
     public string EntityId { get; set; }
     public string EntityType { get; set; }
     public List<string> Tags { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        foreach (var tag in Tags)
+        {
+            if (tag.Length > TagConsts.MaxNameLength)
+            {
+                yield return new ValidationResult(
+                    $"{nameof(tag)} length must be equal to or lower than {TagConsts.MaxNameLength}",
+                    new[] { nameof(Tags) }
+                );
+            }
+        }
+    }
 }
