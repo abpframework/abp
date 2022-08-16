6235870bfc3f7d58003c5c0af1b955d4231bb6db
malik masis <malik.masis@gmail.com>
2022-08-16 10:52:01 +0300

    Formatted the line

--- modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/GlobalResources/Style/GlobalStyleViewComponent.cs
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Pages/CmsKit/Shared/Components/GlobalResources/Style/GlobalStyleViewComponent.cs
@@ -21,8 +21,8 @@ public class GlobalStyleViewComponent : AbpViewComponent
     public async Task<IViewComponentResult> InvokeAsync()
     {
         var lastModificationTime = (await GlobalResourcePublicAppService.GetGlobalStyleAsync())?.LastModificationTime;
-        var lastModificationTimeTimestamp = (long)(lastModificationTime.HasValue ? lastModificationTime.Value .Subtract(DateTime.UnixEpoch).TotalSeconds: 0);
-        
+        var lastModificationTimeTimestamp = (long)(lastModificationTime.HasValue ? lastModificationTime.Value.Subtract(DateTime.UnixEpoch).TotalSeconds : 0);
+
         return View("~/Pages/CmsKit/Shared/Components/GlobalResources/Style/Default.cshtml",
             new GlobalStyleModel()
             {
