7897fa0dfb4c6ad641900af7d50c69b6f4a7ebf2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-08-18 08:46:13 +0300

    Implemented AuthenticatedSimpleStateCheckerSerializerContributor.

---
+++ framework/src/Volo.Abp.Authorization/Volo/Abp/Authorization/Permissions/AuthenticatedSimpleStateCheckerSerializerContributor.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Nodes;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.SimpleStateChecking;
+
+namespace Volo.Abp.Authorization.Permissions;
+
+public class AuthenticatedSimpleStateCheckerSerializerContributor : 
+    ISimpleStateCheckerSerializerContributor,
+    ISingletonDependency
+{
+    public string SerializeToJson<TState>(ISimpleStateChecker<TState> checker) 
+        where TState : IHasSimpleStateCheckers<TState>
+    {
+        if (checker is not RequireAuthenticatedSimpleStateChecker<TState> requireAuthenticatedSimpleStateChecker)
+        {
+            return null;
+        }
+
+        var jsonObject = new JsonObject {
+            ["T"] = "A"
+        };
+
+        return jsonObject.ToJsonString();
+    }
+
+    public ISimpleStateChecker<TState> Deserialize<TState>(JsonObject jsonObject)
+        where TState : IHasSimpleStateCheckers<TState>
+    {
+        if (jsonObject["T"]?.ToString() != "A")
+        {
+            return null;
+        }
+        
+        return new RequireAuthenticatedSimpleStateChecker<TState>();
+    }
+}
