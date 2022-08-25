cfa60f3a5f2db0ddd8a944761c3d23ac12d6bc61
maliming <malimings@gmail.com>
2022-08-25 20:09:11 +0800

    Remove `Newtonsoft` from `feature-management`.

--- modules/feature-management/src/Volo.Abp.FeatureManagement.Application.Contracts/Volo/Abp/FeatureManagement/AbpFeatureManagementApplicationContractsModule.cs
+++ modules/feature-management/src/Volo.Abp.FeatureManagement.Application.Contracts/Volo/Abp/FeatureManagement/AbpFeatureManagementApplicationContractsModule.cs
@@ -4,7 +4,6 @@ using Volo.Abp.Application;
 using Volo.Abp.Authorization;
 using Volo.Abp.FeatureManagement.JsonConverters;
 using Volo.Abp.Json;
-using Volo.Abp.Json.Newtonsoft;
 using Volo.Abp.Json.SystemTextJson;
 using Volo.Abp.Modularity;
 using Volo.Abp.VirtualFileSystem;
@@ -30,11 +29,6 @@ public class AbpFeatureManagementApplicationContractsModule : AbpModule
         Configure<AbpFeatureManagementApplicationContractsOptions>(options =>
         {
             contractsOptionsActions.Configure(options);
-        });
-
-        Configure<AbpNewtonsoftJsonSerializerOptions>(options =>
-        {
-            options.Converters.Add<NewtonsoftStringValueTypeJsonConverter>();
         });
 
         Configure<AbpSystemTextJsonSerializerOptions>(options =>
