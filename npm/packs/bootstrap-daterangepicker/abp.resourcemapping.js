83f0dcd370ed8aac9eafc4910756baac072047e7
Salih <salihozkara00@gmail.com>
2022-09-02 15:40:57 +0300

    Delete moment.min.js from bootstrap-daterangepicker resourcemap

--- npm/packs/bootstrap-daterangepicker/abp.resourcemapping.js
+++ npm/packs/bootstrap-daterangepicker/abp.resourcemapping.js
@@ -1,6 +1,5 @@
 module.exports = {
     mappings: {
-        "@node_modules/bootstrap-daterangepicker/moment.min.js": "@libs/bootstrap-daterangepicker/",
         "@node_modules/bootstrap-daterangepicker/daterangepicker.js": "@libs/bootstrap-daterangepicker/",
         "@node_modules/bootstrap-daterangepicker/daterangepicker.css": "@libs/bootstrap-daterangepicker/",
     }
