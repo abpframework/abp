65d012d8fd0c06156e480664176ad8693e81b5c4
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2022-09-05 11:52:19 +0300

    Adjust Blogging module for studio

---
+++ modules/blogging/src/Volo.Blogging.Installer/Volo/Blogging/BloggingInstallerModule.cs
@@ -0,0 +1,20 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.Studio;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Volo.Abp.Identity;
+
+[DependsOn(
+    typeof(AbpStudioModuleInstallerModule),
+    typeof(AbpVirtualFileSystemModule)
+    )]
+public class BloggingInstallerModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<BloggingInstallerModule>();
+        });
+    }
+}
