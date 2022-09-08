fbc32f5f891f42a881266e58ecfa5d6f39c169e4
maliming <malimings@gmail.com>
2022-09-08 14:28:05 +0800

    Add `DataSeederExtensions_Tests`.

--- framework/src/Volo.Abp.Data/Volo/Abp/Data/DataSeeder.cs
+++ framework/src/Volo.Abp.Data/Volo/Abp/Data/DataSeeder.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
@@ -25,12 +27,17 @@ public class DataSeeder : IDataSeeder, ITransientDependency
     {
         using (var scope = ServiceScopeFactory.CreateScope())
         {
-            if (context.Properties.ContainsKey(nameof(DataSeederExtensions.SeedInSeparateUowAsync)))
+            if (context.Properties.ContainsKey(DataSeederExtensions.SeedInSeparateUow))
             {
                 var manager = scope.ServiceProvider.GetRequiredService<IUnitOfWorkManager>();
                 foreach (var contributorType in Options.Contributors)
                 {
-                    using (var uow = manager.Begin(requiresNew: true, isTransactional: true))
+                    var options = context.Properties.TryGetValue(DataSeederExtensions.SeedInSeparateUowOptions, out var uowOptions)
+                        ? (AbpUnitOfWorkOptions) uowOptions
+                        : new AbpUnitOfWorkOptions();
+                    var requiresNew = context.Properties.TryGetValue(DataSeederExtensions.SeedInSeparateUowRequiresNew, out var obj) && (bool) obj;
+
+                    using (var uow = manager.Begin(options, requiresNew))
                     {
                         var contributor = (IDataSeedContributor)scope.ServiceProvider.GetRequiredService(contributorType);
                         await contributor.SeedAsync(context);
--- framework/src/Volo.Abp.Data/Volo/Abp/Data/DataSeederExtensions.cs
+++ framework/src/Volo.Abp.Data/Volo/Abp/Data/DataSeederExtensions.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Volo.Abp.Uow;
 
 namespace Volo.Abp.Data;
 
 public static class DataSeederExtensions
 {
+    public const string SeedInSeparateUow = "__SeedInSeparateUow";
+    public const string SeedInSeparateUowOptions = "__SeedInSeparateUowOptions";
+    public const string SeedInSeparateUowRequiresNew = "__SeedInSeparateUowRequiresNew";
+
     public static Task SeedAsync(this IDataSeeder seeder, Guid? tenantId = null)
     {
         return seeder.SeedAsync(new DataSeedContext(tenantId));
     }
 
-    public static Task SeedInSeparateUowAsync(this IDataSeeder seeder, Guid? tenantId = null)
+    public static Task SeedInSeparateUowAsync(this IDataSeeder seeder, Guid? tenantId = null, AbpUnitOfWorkOptions options = null, bool requiresNew = false)
     {
-        return seeder.SeedAsync(new DataSeedContext(tenantId).WithProperty(nameof(DataSeederExtensions.SeedInSeparateUowAsync), true));
+        var context = new DataSeedContext(tenantId);
+        context.WithProperty(SeedInSeparateUow, true);
+        context.WithProperty(SeedInSeparateUowOptions, options);
+        context.WithProperty(SeedInSeparateUowRequiresNew, requiresNew);
+        return seeder.SeedAsync(context);
     }
 }
