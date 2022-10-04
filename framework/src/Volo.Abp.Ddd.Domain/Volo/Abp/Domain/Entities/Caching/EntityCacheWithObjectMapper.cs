3fa985e923cf9302cb63584db717356317dbf3bf
maliming <malimings@gmail.com>
2022-10-04 15:51:36 +0800

    Fix build errors.
    https://github.com/abpframework/abp/pull/14055

--- framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Entities/Caching/EntityCacheWithObjectMapper.cs
+++ framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Entities/Caching/EntityCacheWithObjectMapper.cs
@@ -2,23 +2,23 @@
 using Volo.Abp.Caching;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.ObjectMapping;
+using Volo.Abp.Uow;
 
 namespace Volo.Abp.Domain.Entities.Caching;
 
 public class EntityCacheWithObjectMapper<TEntity, TEntityCacheItem, TKey> :
-    EntityCacheBase<TEntity, TEntityCacheItem, TKey> 
+    EntityCacheBase<TEntity, TEntityCacheItem, TKey>
     where TEntity : Entity<TKey>
     where TEntityCacheItem : class
 {
     protected IObjectMapper ObjectMapper { get; }
 
     public EntityCacheWithObjectMapper(
-        IReadOnlyRepository<TEntity, TKey> repository, 
+        IReadOnlyRepository<TEntity, TKey> repository,
         IDistributedCache<TEntityCacheItem, TKey> cache,
+        IUnitOfWorkManager unitOfWorkManager,
         IObjectMapper objectMapper)
-        : base(
-            repository,
-            cache)
+        : base(repository, cache, unitOfWorkManager)
     {
         ObjectMapper = objectMapper;
     }
@@ -29,7 +29,7 @@ public class EntityCacheWithObjectMapper<TEntity, TEntityCacheItem, TKey> :
         {
             return null;
         }
-        
+
         if (typeof(TEntity) == typeof(TEntityCacheItem))
         {
             return entity.As<TEntityCacheItem>();
--- framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Entities/Caching/EntityCacheWithObjectMapperContext.cs
+++ framework/src/Volo.Abp.Ddd.Domain/Volo/Abp/Domain/Entities/Caching/EntityCacheWithObjectMapperContext.cs
@@ -1,22 +1,22 @@
 ﻿using Volo.Abp.Caching;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.ObjectMapping;
+using Volo.Abp.Uow;
 
 namespace Volo.Abp.Domain.Entities.Caching;
 
 public class EntityCacheWithObjectMapperContext<TObjectMapperContext, TEntity, TEntityCacheItem, TKey> :
-    EntityCacheWithObjectMapper<TEntity, TEntityCacheItem, TKey> 
+    EntityCacheWithObjectMapper<TEntity, TEntityCacheItem, TKey>
     where TEntity : Entity<TKey>
     where TEntityCacheItem : class
 {
     public EntityCacheWithObjectMapperContext(
         IReadOnlyRepository<TEntity, TKey> repository,
         IDistributedCache<TEntityCacheItem, TKey> cache,
-        IObjectMapper<TObjectMapperContext> objectMapper) // Intentionally injected with TContext 
-        : base(
-            repository,
-            cache,
-            objectMapper)
+        IUnitOfWorkManager unitOfWorkManager,
+        IObjectMapper objectMapper)// Intentionally injected with TContext
+        : base(repository, cache, unitOfWorkManager, objectMapper)
     {
+
     }
 }
