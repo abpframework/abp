a5fffc08ab1f45aef93f283d3da5069c2c276f49
Jakub Baranowski <jakub@curvestone.io>
2022-10-28 13:46:52 +0200

    XML description of SentrySpanAttribute

--- framework/src/Volo.Abp.Instrumentation.Sentry/Volo/Abp/Instrumentation/Sentry/SentrySpanAttribute.cs
+++ framework/src/Volo.Abp.Instrumentation.Sentry/Volo/Abp/Instrumentation/Sentry/SentrySpanAttribute.cs
@@ -2,12 +2,30 @@ using JetBrains.Annotations;
 
 namespace Volo.Abp.Instrumentation.Sentry.Volo.Abp.Instrumentation.Sentry;
 
+/// <summary>
+/// Used to indicate that declaring method (or all methods of the class) should get child Sentry span.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Interface)]
 public class SentrySpanAttribute : Attribute
 {
-    public bool IsDisabled { get; set; }
+    /// <summary>
+    /// Sentry span operation name. 
+    /// Uses ClassName.MethodName if not supplied.
+    /// </summary>
     public string? Operation { get; set; }
+    
+    /// <summary>
+    /// Sentry span description.
+    /// Sentry uses SpanId if not supplied.
+    /// Default: null
+    /// </summary>
     public string? Description { get; set; }
+    
+    /// <summary>
+    /// Used to prevent starting a Sentry span for the method.
+    /// Default: false.
+    /// </summary>
+    public bool IsDisabled { get; set; }
 
 
     public SentrySpanAttribute()
