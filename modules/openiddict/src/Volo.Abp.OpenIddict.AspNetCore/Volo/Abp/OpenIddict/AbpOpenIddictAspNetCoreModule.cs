a83221f5dd0d7304c997903437951cc58f2120c6
maliming <malimings@gmail.com>
2022-11-06 17:37:41 +0800

    Remove `AttachAbpCustomChallengeErrors`.

--- modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore/Volo/Abp/OpenIddict/AbpOpenIddictAspNetCoreModule.cs
+++ modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore/Volo/Abp/OpenIddict/AbpOpenIddictAspNetCoreModule.cs
@@ -5,7 +5,6 @@ using OpenIddict.Server;
 using Volo.Abp.AspNetCore.MultiTenancy;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
 using Volo.Abp.Modularity;
-using Volo.Abp.OpenIddict.Handlers;
 using Volo.Abp.OpenIddict.WildcardDomains;
 using Volo.Abp.Security.Claims;
 
@@ -130,7 +129,6 @@ public class AbpOpenIddictAspNetCoreModule : AbpModule
                     builder.AddEventHandler(AbpValidatePostLogoutRedirectUriParameter.Descriptor);
                 }
 
-                builder.AddEventHandler(AttachAbpCustomChallengeErrors.Descriptor);
                 builder.AddEventHandler(RemoveClaimsFromClientCredentialsGrantType.Descriptor);
 
                 services.ExecutePreConfiguredActions(builder);
