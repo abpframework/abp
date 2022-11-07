496d56580c1b6c253752591fd7933439689ff4fd
malik masis <malik.masis@gmail.com>
2022-11-07 15:12:36 +0300

    Create UserPasswordChangeRequestedEto.cs

---
+++ modules/users/src/Volo.Abp.Users.Abstractions/Volo/Abp/Users/UserPasswordChangeRequestedEto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Volo.Abp.EventBus;
+using Volo.Abp.MultiTenancy;
+
+namespace Volo.Abp.Users;
+
+[EventName("Volo.Abp.Users.UserPasswordChangeRequested")]
+public class UserPasswordChangeRequestedEto : IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Password { get; set; }
+}
