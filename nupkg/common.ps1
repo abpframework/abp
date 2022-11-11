8a1be7bb07181944624a21aaad560f788a0b1645
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2022-11-11 14:02:56 +0300

    Update common.ps1

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -245,7 +245,7 @@ $projects = (
     "modules/account/src/Volo.Abp.Account.Web.OpenIddict",
     "modules/account/src/Volo.Abp.Account.Blazor",
     "modules/account/src/Volo.Abp.Account.Installer",
-    "studio/source-codes/Volo.Abp.Account.SourceCode",
+    "source-code/Volo.Abp.Account.SourceCode",
         
     # modules/audit-logging
     "modules/audit-logging/src/Volo.Abp.AuditLogging.Domain",
@@ -253,7 +253,7 @@ $projects = (
     "modules/audit-logging/src/Volo.Abp.AuditLogging.EntityFrameworkCore",
     "modules/audit-logging/src/Volo.Abp.AuditLogging.MongoDB",
     # "modules/audit-logging/src/Volo.Abp.AuditLogging.Installer",
-    # "studio/source-codes/Volo.Abp.AuditLogging.SourceCode",
+    # "source-code/Volo.Abp.AuditLogging.SourceCode",
 
     # modules/background-jobs
     "modules/background-jobs/src/Volo.Abp.BackgroundJobs.Domain",
@@ -261,7 +261,7 @@ $projects = (
     "modules/background-jobs/src/Volo.Abp.BackgroundJobs.EntityFrameworkCore",
     "modules/background-jobs/src/Volo.Abp.BackgroundJobs.MongoDB",
     "modules/background-jobs/src/Volo.Abp.BackgroundJobs.Installer",
-    "studio/source-codes/Volo.Abp.BackgroundJobs.SourceCode",
+    "source-code/Volo.Abp.BackgroundJobs.SourceCode",
 
     # modules/basic-theme
     "modules/basic-theme/src/Volo.Abp.AspNetCore.Components.Server.BasicTheme",
@@ -269,7 +269,7 @@ $projects = (
     "modules/basic-theme/src/Volo.Abp.AspNetCore.Components.WebAssembly.BasicTheme",
     "modules/basic-theme/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic",
     "modules/basic-theme/src/Volo.Abp.BasicTheme.Installer",
-    "studio/source-codes/Volo.Abp.BasicTheme.SourceCode",
+    "source-code/Volo.Abp.BasicTheme.SourceCode",
 
     # modules/blogging
     "modules/blogging/src/Volo.Blogging.Application.Contracts.Shared",
@@ -288,7 +288,7 @@ $projects = (
     "modules/blogging/src/Volo.Blogging.Admin.HttpApi.Client",
     "modules/blogging/src/Volo.Blogging.Admin.Web",
     "modules/blogging/src/Volo.Blogging.Installer",
-    "studio/source-codes/Volo.Blogging.SourceCode",
+    "source-code/Volo.Blogging.SourceCode",
 
     # modules/client-simulation
     "modules/client-simulation/src/Volo.ClientSimulation",
@@ -310,7 +310,7 @@ $projects = (
     "modules/docs/src/Volo.Docs.MongoDB",
     "modules/docs/src/Volo.Docs.Web",
     "modules/docs/src/Volo.Docs.Installer",
-    "studio/source-codes/Volo.Docs.SourceCode",
+    "source-code/Volo.Docs.SourceCode",
 
     # modules/feature-management
     "modules/feature-management/src/Volo.Abp.FeatureManagement.Application.Contracts",
@@ -326,7 +326,7 @@ $projects = (
     "modules/feature-management/src/Volo.Abp.FeatureManagement.Blazor.Server",
     "modules/feature-management/src/Volo.Abp.FeatureManagement.Blazor.WebAssembly",
     "modules/feature-management/src/Volo.Abp.FeatureManagement.Installer",
-    "studio/source-codes/Volo.Abp.FeatureManagement.SourceCode",
+    "source-code/Volo.Abp.FeatureManagement.SourceCode",
 
     # modules/identity
     "modules/identity/src/Volo.Abp.Identity.Application.Contracts",
@@ -344,7 +344,7 @@ $projects = (
     "modules/identity/src/Volo.Abp.Identity.Blazor.WebAssembly",
     "modules/identity/src/Volo.Abp.PermissionManagement.Domain.Identity",
     "modules/identity/src/Volo.Abp.Identity.Installer",
-    "studio/source-codes/Volo.Abp.Identity.SourceCode",
+    "source-code/Volo.Abp.Identity.SourceCode",
     
     # modules/identityserver
     "modules/identityserver/src/Volo.Abp.IdentityServer.Domain",
@@ -353,7 +353,7 @@ $projects = (
     "modules/identityserver/src/Volo.Abp.IdentityServer.MongoDB",
     "modules/identityserver/src/Volo.Abp.PermissionManagement.Domain.IdentityServer",
     "modules/identityserver/src/Volo.Abp.IdentityServer.Installer",
-    "studio/source-codes/Volo.Abp.IdentityServer.SourceCode",
+    "source-code/Volo.Abp.IdentityServer.SourceCode",
 
     # modules/openiddict
     "modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore",
@@ -363,7 +363,7 @@ $projects = (
     "modules/openiddict/src/Volo.Abp.OpenIddict.MongoDB",
     "modules/openiddict/src/Volo.Abp.PermissionManagement.Domain.OpenIddict",
     "modules/openiddict/src/Volo.Abp.OpenIddict.Installer",
-    "studio/source-codes/Volo.Abp.OpenIddict.SourceCode",
+    "source-code/Volo.Abp.OpenIddict.SourceCode",
 
     # modules/permission-management
     "modules/permission-management/src/Volo.Abp.PermissionManagement.Application.Contracts",
@@ -379,7 +379,7 @@ $projects = (
     "modules/permission-management/src/Volo.Abp.PermissionManagement.Blazor.Server",
     "modules/permission-management/src/Volo.Abp.PermissionManagement.Blazor.WebAssembly",
     "modules/permission-management/src/Volo.Abp.PermissionManagement.Installer",
-    "studio/source-codes/Volo.Abp.PermissionManagement.SourceCode",
+    "source-code/Volo.Abp.PermissionManagement.SourceCode",
 
     # modules/setting-management
     "modules/setting-management/src/Volo.Abp.SettingManagement.Application.Contracts",
@@ -395,7 +395,7 @@ $projects = (
     "modules/setting-management/src/Volo.Abp.SettingManagement.MongoDB",
     "modules/setting-management/src/Volo.Abp.SettingManagement.Web",
     "modules/setting-management/src/Volo.Abp.SettingManagement.Installer",
-    "studio/source-codes/Volo.Abp.SettingManagement.SourceCode",
+    "source-code/Volo.Abp.SettingManagement.SourceCode",
 
     # modules/tenant-management
     "modules/tenant-management/src/Volo.Abp.TenantManagement.Application.Contracts",
@@ -411,7 +411,7 @@ $projects = (
     "modules/tenant-management/src/Volo.Abp.TenantManagement.MongoDB",
     "modules/tenant-management/src/Volo.Abp.TenantManagement.Web",
     "modules/tenant-management/src/Volo.Abp.TenantManagement.Installer",
-    "studio/source-codes/Volo.Abp.TenantManagement.SourceCode",
+    "source-code/Volo.Abp.TenantManagement.SourceCode",
 
     # modules/users
     "modules/users/src/Volo.Abp.Users.Abstractions",
@@ -420,12 +420,12 @@ $projects = (
     "modules/users/src/Volo.Abp.Users.EntityFrameworkCore",
     "modules/users/src/Volo.Abp.Users.MongoDB",
     "modules/users/src/Volo.Abp.Users.Installer",
-    "studio/source-codes/Volo.Abp.Users.SourceCode",
+    "source-code/Volo.Abp.Users.SourceCode",
 
     # modules/virtual-file-explorer
     "modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Web",
     "modules/virtual-file-explorer/src/Volo.Abp.VirtualFileExplorer.Installer",
-    "studio/source-codes/Volo.Abp.VirtualFileExplorer.SourceCode",
+    "source-code/Volo.Abp.VirtualFileExplorer.SourceCode",
 	
     # modules/blob-storing-database
     "modules/blob-storing-database/src/Volo.Abp.BlobStoring.Database.Domain",
@@ -433,7 +433,7 @@ $projects = (
     "modules/blob-storing-database/src/Volo.Abp.BlobStoring.Database.EntityFrameworkCore",
     "modules/blob-storing-database/src/Volo.Abp.BlobStoring.Database.MongoDB",
     "modules/blob-storing-database/src/Volo.Abp.BlobStoring.Database.Installer",
-    "studio/source-codes/Volo.Abp.BlobStoring.Database.SourceCode",
+    "source-code/Volo.Abp.BlobStoring.Database.SourceCode",
 	
     # abp/cms-kit	
     "modules/cms-kit/src/Volo.CmsKit.Admin.Application",
@@ -461,5 +461,5 @@ $projects = (
     "modules/cms-kit/src/Volo.CmsKit.Public.Web",
     "modules/cms-kit/src/Volo.CmsKit.Web",
     "modules/cms-kit/src/Volo.CmsKit.Installer",
-    "studio/source-codes/Volo.CmsKit.SourceCode"
+    "source-code/Volo.CmsKit.SourceCode"
 )
