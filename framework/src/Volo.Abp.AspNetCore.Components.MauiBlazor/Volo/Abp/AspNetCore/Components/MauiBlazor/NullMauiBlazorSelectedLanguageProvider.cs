bc1d4de16ad587042a9f1e7693a0208d9b655e9e
liangshiwei <shiwei.liang@volosoft.com>
2022-11-24 19:33:03 +0800

    Add IMauiBlazorSelectedLanguageProvider

---
+++ framework/src/Volo.Abp.AspNetCore.Components.MauiBlazor/Volo/Abp/AspNetCore/Components/MauiBlazor/NullMauiBlazorSelectedLanguageProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.AspNetCore.Components.MauiBlazor;
+
+public class NullMauiBlazorSelectedLanguageProvider : IMauiBlazorSelectedLanguageProvider, ITransientDependency
+{
+    public Task<string> GetSelectedLanguageAsync()
+    {
+        return Task.FromResult((string)null);
+    }
+}
