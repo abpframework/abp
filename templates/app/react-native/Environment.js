190f56ddff1c66f26d747e53e4e3843043da8d1f
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2022-12-08 16:03:15 +0300

    Update Environment.js

--- templates/app/react-native/Environment.js
+++ templates/app/react-native/Environment.js
@@ -1,5 +1,5 @@
 
-const yourIP =  '192.168.1.64'//'Your Local IP Address etc 192.168.1.64'; // See the docs https://docs.abp.io/en/abp/latest/Getting-Started-React-Native?Tiered=No
+const yourIP = 'Your Local IP Address etc 192.168.1.64'; // See the docs https://docs.abp.io/en/abp/latest/Getting-Started-React-Native?Tiered=No
 const port  = 44305;
 const apiUrl = `http://${yourIP}:${port}`;
 const ENV = {
