ed14c5c61ca2858872ec00bf16f53017717de6c6
malik masis <malik.masis@gmail.com>
2022-12-08 23:02:16 +0300

    Inherited CaptchaException from UserFriendlyException

--- modules/cms-kit/src/Volo.CmsKit.Public.Application/Volo/Security/VoloCaptcha/CaptchaException.cs
+++ modules/cms-kit/src/Volo.CmsKit.Public.Application/Volo/Security/VoloCaptcha/CaptchaException.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using System.Runtime.Serialization;
+using Volo.Abp;
 
 namespace Volo.CmsKit.Public.Application.Security.VoloCaptcha;
 
-public class CaptchaException : Exception
+public class CaptchaException : UserFriendlyException
 {
     public CaptchaException(string message) : base(message)
+    {
+    }
+
+    public CaptchaException(SerializationInfo serializationInfo, StreamingContext context) : base(serializationInfo, context)
     {
     }
 }
