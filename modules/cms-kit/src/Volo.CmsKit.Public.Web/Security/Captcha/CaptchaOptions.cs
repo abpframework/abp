fc6dae3c9e3a42dc28fac9639872cf6b2bd90d8b
malik masis <malik.masis@gmail.com>
2022-12-09 14:45:09 +0300

    Moved captcha from the app layer to web

---
+++ modules/cms-kit/src/Volo.CmsKit.Public.Web/Security/Captcha/CaptchaOptions.cs
@@ -0,0 +1,66 @@
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+
+namespace Volo.CmsKit.Public.Web.Security.Captcha;
+
+public class CaptchaOptions
+{
+    /// <summary>
+    /// Default fonts are  "Arial", "Verdana", "Times New Roman" in Windows. These fonts must exist in the target OS.
+    /// </summary>
+    public string[] FontFamilies { get; set; } = new string[] { "Arial", "Verdana", "Times New Roman" };
+
+    public Color[] TextColor { get; set; } = new Color[]
+    {
+        Color.Blue, Color.Black, Color.Black, Color.Brown, Color.Gray, Color.Green
+    };
+    public Color[] DrawLinesColor { get; set; } = new Color[]
+    {
+        Color.Blue, Color.Black, Color.Black, Color.Brown, Color.Gray, Color.Green
+    };
+
+    public float MinLineThickness { get; set; } = 0.7f;
+
+    public float MaxLineThickness { get; set; } = 2.0f;
+
+    public ushort Width { get; set; } = 180;
+
+    public ushort Height { get; set; } = 70;
+
+    public ushort NoiseRate { get; set; } = 500;
+
+    public Color[] NoiseRateColor { get; set; } = new Color[] { Color.Gray };
+
+    public byte FontSize { get; set; } = 32;
+
+    public FontStyle FontStyle { get; set; } = FontStyle.Regular;
+
+    public EncoderTypes EncoderType { get; set; } = EncoderTypes.Png;
+
+    public IImageEncoder Encoder => RandomTextGenerator.GetEncoder(EncoderType);
+
+    public byte DrawLines { get; set; } = 2;
+
+    public byte MaxRotationDegrees { get; set; } = 4;
+
+    public int Number1MinValue { get; set; } = 1;
+
+    public int Number1MaxValue { get; set; } = 99;
+
+    public int Number2MinValue { get; set; } = 1;
+
+    public int Number2MaxValue { get; set; } = 99;
+
+    public CaptchaOptions()
+    {
+
+    }
+    public CaptchaOptions(int number1MinValue, int number1MaxValue, int number2MinValue, int number2MaxValue)
+    {
+        Number1MinValue = number1MinValue;
+        Number1MaxValue = number1MaxValue;
+        Number2MinValue = number2MinValue;
+        Number1MaxValue = number2MaxValue;
+    }
+}
