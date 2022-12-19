016017c050c94add500fb9477366a05c2ad4e5b8
Onur Pıçakcı <onurpicakci@hotmail.com>
2022-12-19 15:40:46 +0300

    password show/hide edited

--- modules/account/src/Volo.Abp.Account.Web/Pages/Account/Components/ProfileManagementGroup/Password/Default.js
+++ modules/account/src/Volo.Abp.Account.Web/Pages/Account/Components/ProfileManagementGroup/Password/Default.js
@@ -1,25 +1,29 @@
 (function ($) {
-    $("#PasswordVisibilityButton").click(function (e) {
+    $(".password-visibility-button").click(function (e) {
         let button = $(this);
         let passwordInput = button.parent().find("input");
         if (!passwordInput) {
             return;
         }
+
         if (passwordInput.attr("type") === "password") {
             passwordInput.attr("type", "text");
         }
         else {
             passwordInput.attr("type", "password");
         }
+
         let icon = button.find("i");
         if (icon) {
             icon.toggleClass("fa-eye-slash").toggleClass("fa-eye");
         }
     });
+
     $(function () {
         var l = abp.localization.getResource("AbpAccount");
         $('#ChangePasswordForm').submit(function (e) {
             e.preventDefault();
+
             if (!$('#ChangePasswordForm').valid()) {
                 return false;
             }
@@ -37,12 +41,12 @@
             if (input.currentPassword && input.currentPassword == '') {
                 return;
             }
-            
+
             if (input.currentPassword == input.newPassword) {
                 abp.message.error(l('NewPasswordSameAsOld'));
                 return;
             }
-            
+
             volo.abp.account.profile.changePassword(input).then(function (result) {
                 abp.message.success(l('PasswordChanged'));
                 abp.event.trigger('passwordChanged');
