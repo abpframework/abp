f82e222170aff86de0e063b4e0428d70b60e08fe
malik masis <malik.masis@gmail.com>
2022-12-20 11:40:01 +0300

    Added IHasEntityVersion to Tenant

--- modules/tenant-management/src/Volo.Abp.TenantManagement.Domain.Shared/Volo/Abp/TenantManagement/TenantEto.cs
+++ modules/tenant-management/src/Volo.Abp.TenantManagement.Domain.Shared/Volo/Abp/TenantManagement/TenantEto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using Volo.Abp.Auditing;
 
 namespace Volo.Abp.TenantManagement;
 
 [Serializable]
-public class TenantEto
+public class TenantEto : IHasEntityVersion
 {
     public Guid Id { get; set; }
 
     public string Name { get; set; }
+
+    public int EntityVersion { get; set; }
 }
