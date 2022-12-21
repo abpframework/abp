7aae9bb4478fdc9e30dfcb8615a5ec5004ead6e4
Fatih KILIÇ <fsk202@gmail.com>
2022-12-21 22:17:23 +0300

    edit password.component and change password type in props

--- npm/ng-packs/packages/theme-shared/src/lib/components/password/password.component.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/components/password/password.component.ts
@@ -1,13 +1,21 @@
-import {Component, Injector, Input} from '@angular/core';
+import {Component, forwardRef, Injector, Input} from '@angular/core';
 import {AbstractNgModelComponent} from "@abp/ng.core";
+import {NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'abp-password',
   templateUrl: `password.component.html`,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordComponent),
+      multi: true,
+    },
+  ]
 })
-export class PasswordComponent extends AbstractNgModelComponent{
+export class PasswordComponent extends AbstractNgModelComponent {
   @Input() inputId!: string;
-
+  @Input() formControlName!: string;
   fieldTextType: boolean;
 
   constructor(
