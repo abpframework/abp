6c74679b524fb5473f588532c7ce4bcba594592c
Fatih KILIÇ <fsk202@gmail.com>
2022-12-23 16:20:47 +0300

    import relative

--- npm/ng-packs/packages/theme-shared/src/lib/theme-shared.module.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/theme-shared.module.ts
@@ -1,7 +1,7 @@
-import {CoreModule, noop} from '@abp/ng.core';
-import {DatePipe} from '@angular/common';
-import {APP_INITIALIZER, ModuleWithProviders, NgModule} from '@angular/core';
-import {NgbDateParserFormatter, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
+import { CoreModule, noop } from '@abp/ng.core';
+import { DatePipe } from '@angular/common';
+import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
+import { NgbDateParserFormatter, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import {
   defaultMapErrorsFn,
   NgxValidateCoreModule,
@@ -9,33 +9,33 @@ import {
   VALIDATION_MAP_ERRORS_FN,
   VALIDATION_VALIDATE_ON_SUBMIT,
 } from '@ngx-validate/core';
-import {NgxDatatableModule} from '@swimlane/ngx-datatable';
-import {BreadcrumbItemsComponent} from './components/breadcrumb-items/breadcrumb-items.component';
-import {BreadcrumbComponent} from './components/breadcrumb/breadcrumb.component';
-import {ButtonComponent} from './components/button/button.component';
-import {ConfirmationComponent} from './components/confirmation/confirmation.component';
-import {HttpErrorWrapperComponent} from './components/http-error-wrapper/http-error-wrapper.component';
-import {LoaderBarComponent} from './components/loader-bar/loader-bar.component';
-import {LoadingComponent} from './components/loading/loading.component';
-import {ModalCloseDirective} from './components/modal/modal-close.directive';
-import {ModalComponent} from './components/modal/modal.component';
-import {ToastContainerComponent} from './components/toast-container/toast-container.component';
-import {ToastComponent} from './components/toast/toast.component';
-import {DEFAULT_VALIDATION_BLUEPRINTS} from './constants/validation';
-import {EllipsisModule} from './directives/ellipsis.directive';
-import {LoadingDirective} from './directives/loading.directive';
-import {NgxDatatableDefaultDirective} from './directives/ngx-datatable-default.directive';
-import {NgxDatatableListDirective} from './directives/ngx-datatable-list.directive';
-import {DocumentDirHandlerService} from './handlers/document-dir.handler';
-import {ErrorHandler} from './handlers/error.handler';
-import {RootParams} from './models/common';
-import {NG_BOOTSTRAP_CONFIG_PROVIDERS} from './providers';
-import {THEME_SHARED_ROUTE_PROVIDERS} from './providers/route.provider';
-import {THEME_SHARED_APPEND_CONTENT} from './tokens/append-content.token';
-import {HTTP_ERROR_CONFIG, httpErrorConfigFactory} from './tokens/http-error.token';
-import {DateParserFormatter} from './utils/date-parser-formatter';
-import {CONFIRMATION_ICONS, DEFAULT_CONFIRMATION_ICONS} from './tokens/confirmation-icons.token';
-import {PasswordComponent} from "./components";
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { BreadcrumbItemsComponent } from './components/breadcrumb-items/breadcrumb-items.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { HttpErrorWrapperComponent } from './components/http-error-wrapper/http-error-wrapper.component';
+import { LoaderBarComponent } from './components/loader-bar/loader-bar.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { ModalCloseDirective } from './components/modal/modal-close.directive';
+import { ModalComponent } from './components/modal/modal.component';
+import { ToastContainerComponent } from './components/toast-container/toast-container.component';
+import { ToastComponent } from './components/toast/toast.component';
+import { DEFAULT_VALIDATION_BLUEPRINTS } from './constants/validation';
+import { EllipsisModule } from './directives/ellipsis.directive';
+import { LoadingDirective } from './directives/loading.directive';
+import { NgxDatatableDefaultDirective } from './directives/ngx-datatable-default.directive';
+import { NgxDatatableListDirective } from './directives/ngx-datatable-list.directive';
+import { DocumentDirHandlerService } from './handlers/document-dir.handler';
+import { ErrorHandler } from './handlers/error.handler';
+import { RootParams } from './models/common';
+import { NG_BOOTSTRAP_CONFIG_PROVIDERS } from './providers';
+import { THEME_SHARED_ROUTE_PROVIDERS } from './providers/route.provider';
+import { THEME_SHARED_APPEND_CONTENT } from './tokens/append-content.token';
+import { HTTP_ERROR_CONFIG, httpErrorConfigFactory } from './tokens/http-error.token';
+import { DateParserFormatter } from './utils/date-parser-formatter';
+import { CONFIRMATION_ICONS, DEFAULT_CONFIRMATION_ICONS } from './tokens/confirmation-icons.token';
+import { PasswordComponent } from './components/password/password.component';
 
 const declarationsWithExports = [
   BreadcrumbComponent,
@@ -63,7 +63,7 @@ const declarationsWithExports = [
     EllipsisModule,
   ],
   declarations: [...declarationsWithExports, HttpErrorWrapperComponent],
-  exports: [NgxDatatableModule, EllipsisModule , NgxValidateCoreModule, ...declarationsWithExports],
+  exports: [NgxDatatableModule, EllipsisModule, NgxValidateCoreModule, ...declarationsWithExports],
   providers: [DatePipe],
 })
 export class BaseThemeSharedModule {}
