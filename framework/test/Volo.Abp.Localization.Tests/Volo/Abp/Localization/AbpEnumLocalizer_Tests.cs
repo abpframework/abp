b9e531ed0c8343c66aaf9eb773929cf38e7779c5
maliming <malimings@gmail.com>
2022-12-30 18:51:34 +0800

    Add `AbpEnumLocalizerExtensions`.

--- framework/test/Volo.Abp.Localization.Tests/Volo/Abp/Localization/AbpEnumLocalizer_Tests.cs
+++ framework/test/Volo.Abp.Localization.Tests/Volo/Abp/Localization/AbpEnumLocalizer_Tests.cs
@@ -20,21 +20,21 @@ public class AbpEnumLocalizer_Tests : AbpIntegratedTest<AbpLocalizationTestModul
     {
         using (CultureHelper.Use("en"))
         {
-            _enumLocalizer.GetString(typeof(BookType), BookType.Undefined).ShouldBe("Undefined");
-            _enumLocalizer.GetString(typeof(BookType), BookType.Adventure).ShouldBe("Adventure");
-            _enumLocalizer.GetString(typeof(BookType), 0).ShouldBe("Undefined with value 0");
-            _enumLocalizer.GetString(typeof(BookType), 1).ShouldBe("Adventure with value 1");
-            _enumLocalizer.GetString(typeof(BookType), BookType.Biography).ShouldBe("Biography");
+            _enumLocalizer.GetString<BookType>(BookType.Undefined).ShouldBe("Undefined");
+            _enumLocalizer.GetString<BookType>(BookType.Adventure).ShouldBe("Adventure");
+            _enumLocalizer.GetString<BookType>(0).ShouldBe("Undefined with value 0");
+            _enumLocalizer.GetString<BookType>(1).ShouldBe("Adventure with value 1");
+            _enumLocalizer.GetString<BookType>(BookType.Biography).ShouldBe("Biography");
 
             var specifyLocalizer = new[]
             {
                 GetRequiredService<IStringLocalizerFactory>().Create<LocalizationTestValidationResource>()
             };
-            _enumLocalizer.GetString(typeof(BookType), BookType.Undefined, specifyLocalizer).ShouldBe("Undefined from ValidationResource");
-            _enumLocalizer.GetString(typeof(BookType), BookType.Adventure, specifyLocalizer).ShouldBe("Adventure from ValidationResource");
-            _enumLocalizer.GetString(typeof(BookType), 0, specifyLocalizer).ShouldBe("Undefined with value 0 from ValidationResource");
-            _enumLocalizer.GetString(typeof(BookType), 1, specifyLocalizer).ShouldBe("Adventure with value 1 from ValidationResource");
-            _enumLocalizer.GetString(typeof(BookType), BookType.Biography, specifyLocalizer).ShouldBe("Biography from ValidationResource");
+            _enumLocalizer.GetString<BookType>(BookType.Undefined, specifyLocalizer).ShouldBe("Undefined from ValidationResource");
+            _enumLocalizer.GetString<BookType>(BookType.Adventure, specifyLocalizer).ShouldBe("Adventure from ValidationResource");
+            _enumLocalizer.GetString<BookType>(0, specifyLocalizer).ShouldBe("Undefined with value 0 from ValidationResource");
+            _enumLocalizer.GetString<BookType>(1, specifyLocalizer).ShouldBe("Adventure with value 1 from ValidationResource");
+            _enumLocalizer.GetString<BookType>(BookType.Biography, specifyLocalizer).ShouldBe("Biography from ValidationResource");
         }
     }
 }
