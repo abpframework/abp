ccea7ae26afb541c3fcd350cc88c7d7c5cec0223
ozermzn <ozer.ramazan@outlook.com.tr>
2022-12-31 03:22:16 +0300

    When the login button is clicked, the button will be disabled and the loading icon will appear in front of it and then redirect to the login page.

--- npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
+++ npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  isLog: boolean = false;
   get hasLoggedIn(): boolean {
     return this.authService.isAuthenticated;
   }
@@ -14,6 +16,10 @@ export class HomeComponent {
   constructor(private authService: AuthService) {}
 
   login() {
-    this.authService.navigateToLogin();
+    this.isLog = true;
+    setTimeout(data => {
+      this.authService.navigateToLogin();
+      this.isLog = false;
+    }, 1000);
   }
 }
