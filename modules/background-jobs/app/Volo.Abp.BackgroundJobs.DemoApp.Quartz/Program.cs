1c8ea86c3525dc061d3ddb8cae5f80e21256b5ba
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-01-02 13:38:13 +0300

    Background job cancellation sample added

--- modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp.Quartz/Program.cs
+++ modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp.Quartz/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Volo.Abp.BackgroundJobs.DemoApp.Shared.Jobs;
+using Volo.Abp.Threading;
 
 namespace Volo.Abp.BackgroundJobs.DemoApp.Quartz;
 
@@ -13,11 +18,25 @@ class Program
         {
             application.Initialize();
 
+            CancelableBackgroundJob(application.ServiceProvider);
             Console.WriteLine("Started: " + typeof(Program).Namespace);
             Console.WriteLine("Press ENTER to stop the application..!");
             Console.ReadLine();
 
             application.Shutdown();
         }
+    }
+    
+    private static void CancelableBackgroundJob(IServiceProvider serviceProvider)
+    {
+        AsyncHelper.RunSync(async () =>
+        {
+            var backgroundJobManager = serviceProvider.GetRequiredService<IBackgroundJobManager>();
+            var jobId = await backgroundJobManager.EnqueueAsync(new LongRunningJobArgs { Value = "test-1" });
+            await backgroundJobManager.EnqueueAsync(new LongRunningJobArgs { Value = "test-2" });
+            Thread.Sleep(1000);
+            var scheduler = serviceProvider.GetRequiredService<IScheduler>();
+            await scheduler.Interrupt(new JobKey(jobId.Split('.')[1],jobId.Split('.')[0]));
+        });
     }
 }
---
+++ modules/background-jobs/app/Volo.Abp.BackgroundJobs.DemoApp.Shared/Jobs/LongRunningJob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using Volo.Abp.DependencyInjection;
+
+namespace Volo.Abp.BackgroundJobs.DemoApp.Shared.Jobs
+{
+    public class LongRunningJob : BackgroundJob<LongRunningJobArgs>, ITransientDependency
+    {
+        public override void Execute(LongRunningJobArgs args, CancellationToken cancellationToken = default)
+        {
+            lock (Console.Out)
+            {
+                var oldColor = Console.ForegroundColor;
+                try
+                {
+                    Console.WriteLine($"Long running {args.Value} start: {DateTime.Now}");
+
+                    for (var i = 1; i <= 10; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"{args.Value} step-{i} done: {DateTime.Now}");
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Long running {args.Value} completed: {DateTime.Now}");
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Long running {args.Value} cancelled!!!");
+                }
+                finally
+                {
+                    Console.ForegroundColor = oldColor;
+                }
+            }
+        }
+    }
+}
