f8a3d9a4ee1faa279c1944213c2c12e647f5a6f4
ozermzn <ozer.ramazan@outlook.com.tr>
2023-01-03 00:14:47 +0300

    * abp-loading-directive added to button with other styles.

--- npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
+++ npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
@@ -1,24 +1,32 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
-import { timeInterval } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  //for disabled button and abp loading spinner
   isLog: boolean = false;
   get hasLoggedIn(): boolean {
     return this.authService.isAuthenticated;
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+    // this.router.events.subscribe(event => {
+    //   if (event instanceof NavigationStart) {
+    //     console.log(event);
+    //   }
+    // });
+  }
 
   login() {
+    //when clicked button spinner and disabled will be active
     this.isLog = true;
-    setTimeout(data => {
+    setTimeout(a => {
       this.authService.navigateToLogin();
+      //all functions is over here
       this.isLog = false;
     }, 1000);
   }
--- npm/ng-packs/apps/dev-app/src/app/home/home.module.ts
+++ npm/ng-packs/apps/dev-app/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoadingDirective, ThemeSharedModule } from '@abp/ng.theme.shared';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, HomeRoutingModule],
+  imports: [SharedModule, HomeRoutingModule, ThemeSharedModule],
 })
 export class HomeModule {}
