ef22c298de99f8b3a26e00ce740f8b060aacc111
ozermzn <ozer.ramazan@outlook.com.tr>
2023-01-05 20:01:16 +0300

    updated login button for loading design.

--- npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
+++ npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
@@ -1,33 +1,21 @@
-import { AuthService } from '@abp/ng.core';
+import { AuthService, RouterEvents } from '@abp/ng.core';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Event, NavigationCancel, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  //for disabled button and abp loading spinner
-  isLog: boolean = false;
+  loading: boolean = false;
   get hasLoggedIn(): boolean {
     return this.authService.isAuthenticated;
   }
 
-  constructor(private authService: AuthService, private router: Router) {
-    // this.router.events.subscribe(event => {
-    //   if (event instanceof NavigationStart) {
-    //     console.log(event);
-    //   }
-    // });
-  }
-
+  constructor(private authService: AuthService) {}
   login() {
-    //when clicked button spinner and disabled will be active
-    this.isLog = true;
-    setTimeout(a => {
-      this.authService.navigateToLogin();
-      //all functions is over here
-      this.isLog = false;
-    }, 1000);
+    this.loading = true;
+    this.authService.navigateToLogin();
   }
 }
