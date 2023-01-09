ae44c91c83a84fad5d6e45039b8f78513adbf1d8
Qingxiao Ren <rqx110@163.com>
2023-01-09 16:02:34 +0800

    using AuthService instead of OAuthService

--- templates/app/angular/src/app/home/home.component.ts
+++ templates/app/angular/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +8,10 @@ import { OAuthService } from 'angular-oauth2-oidc';
 })
 export class HomeComponent {
   get hasLoggedIn(): boolean {
-    return this.oAuthService.hasValidAccessToken();
+    return this.authService.isAuthenticated;
   }
 
-  constructor(private oAuthService: OAuthService, private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
   login() {
     this.authService.navigateToLogin();
