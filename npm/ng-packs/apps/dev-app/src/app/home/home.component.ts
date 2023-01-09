261171c7e56381d956bb81df48a1c75c23b4369b
masumulu28 <masum.ulu@hotmail.com>
2023-01-09 22:12:46 +0300

    Loading indicator added for login button on the home page

--- npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
+++ npm/ng-packs/apps/dev-app/src/app/home/home.component.ts
@@ -1,19 +1,29 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  isNavigating: boolean = false;
+
   get hasLoggedIn(): boolean {
     return this.authService.isAuthenticated;
+  }
+
+  get iconClass(): string {
+    return `fa ${this.isNavigating ? 'fa-spinner fa-pulse abp-loading' : 'fa-sign-in'}`;
+  }
+
+  get title(): string {
+    return `AbpAccount::${this.isNavigating ? 'Redirecting' : 'Login'}`;
   }
 
   constructor(private authService: AuthService) {}
 
   login() {
+    this.isNavigating = true;
     this.authService.navigateToLogin();
   }
 }
