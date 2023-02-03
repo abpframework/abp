366ad6da92b302a8eccec9dae5cae6d96365a2e9
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2023-02-03 17:42:59 +0300

    add visible$ property to nav items

--- npm/ng-packs/packages/theme-shared/src/lib/models/nav-item.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/models/nav-item.ts
@@ -1,4 +1,5 @@
 import { Type } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 export class NavItem {
   id?: string | number;
@@ -8,8 +9,13 @@ export class NavItem {
   order?: number;
   requiredPolicy?: string;
   visible?: () => boolean;
+  visible$?: Observable<boolean>;
   constructor(props: Partial<NavItem>) {
-    props = { ...props, visible: props.visible || (() => true) };
+    props = {
+      ...props,
+      visible: props.visible || (() => true),
+      visible$: props.visible$ || of(true),
+    };
     Object.assign(this, props);
   }
 }
