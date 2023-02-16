a8284c0bf1dbfaacb74fab682cabf6917e52921c
Salih <salihozkara00@gmail.com>
2023-02-16 19:17:41 +0300

    Encode URL before saving it into database

--- modules/blogging/src/Volo.Blogging.Web/Pages/Blogs/Posts/New.cshtml.cs
+++ modules/blogging/src/Volo.Blogging.Web/Pages/Blogs/Posts/New.cshtml.cs
@@ -66,6 +66,9 @@ namespace Volo.Blogging.Pages.Blog.Posts
                 Post.Description = Post.Content.Truncate(PostConsts.MaxSeoFriendlyDescriptionLength);
             }
 
+            // Url encode the url
+            Post.Url = WebUtility.UrlEncode(Post.Url);
+            
             var postWithDetailsDto = await _postAppService.CreateAsync(ObjectMapper.Map<CreatePostViewModel, CreatePostDto>(Post));
 
             //TODO: Try Url.Page(...)
