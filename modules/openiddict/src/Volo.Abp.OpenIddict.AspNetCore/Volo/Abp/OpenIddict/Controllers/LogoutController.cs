52c046b4eb00f913d3a8516ddbb5592101347110
maliming <malimings@gmail.com>
2023-03-03 12:15:03 +0800

    Remove `AuthenticationProperties` from `LogoutController`.

--- modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore/Volo/Abp/OpenIddict/Controllers/LogoutController.cs
+++ modules/openiddict/src/Volo.Abp.OpenIddict.AspNetCore/Volo/Abp/OpenIddict/Controllers/LogoutController.cs
@@ -20,8 +20,6 @@ public class LogoutController : AbpOpenIdDictControllerBase
         // Returning a SignOutResult will ask OpenIddict to redirect the user agent
         // to the post_logout_redirect_uri specified by the client application or to
         // the RedirectUri specified in the authentication properties if none was set.
-        return SignOut(
-            authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
-            properties: new AuthenticationProperties {RedirectUri = "/"});
+        return SignOut(authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
     }
 }
