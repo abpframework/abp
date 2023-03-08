f0120d121fc8c374eaeff8b3141d94cfbd60ea04
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2023-03-08 18:12:51 +0300

    implement primitive tests

--- npm/ng-packs/packages/core/src/lib/tests/local-storage.service.spec.ts
+++ npm/ng-packs/packages/core/src/lib/tests/local-storage.service.spec.ts
@@ -13,4 +13,40 @@ describe('LocalStorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be called getItem', () => {
+    const spy = jest.spyOn(service, 'getItem');
+    service.getItem('test');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should be called setItem', () => {
+    const spy = jest.spyOn(service, 'setItem');
+    service.setItem('test', 'value');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should be called removeItem', () => {
+    const spy = jest.spyOn(service, 'removeItem');
+    service.removeItem('test');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should be called clear', () => {
+    const spy = jest.spyOn(service, 'clear');
+    service.clear();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should be called key', () => {
+    const spy = jest.spyOn(service, 'key');
+    service.key(0);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should be called length', () => {
+    const spy = jest.spyOn(service, 'length', 'get');
+    service.length;
+    expect(spy).toHaveBeenCalled();
+  });
 });
