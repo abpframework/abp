883506e5202615d7e0af0f0319a7820c1403da2a
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-03-15 15:40:20 +0300

    Separate Ldap ocalizations to `Volo.Abp.Ldap.Abstractions` assembly

--- framework/src/Volo.Abp.Ldap/Volo/Abp/Ldap/AbpLdapModule.cs
+++ framework/src/Volo.Abp.Ldap/Volo/Abp/Ldap/AbpLdapModule.cs
@@ -1,33 +1,12 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
-using Volo.Abp.Ldap.Localization;
-using Volo.Abp.Localization;
-using Volo.Abp.Modularity;
+﻿using Volo.Abp.Modularity;
 using Volo.Abp.Settings;
-using Volo.Abp.VirtualFileSystem;
 
 namespace Volo.Abp.Ldap;
 
 [DependsOn(
-    typeof(AbpSettingsModule),
-    typeof(AbpVirtualFileSystemModule),
-    typeof(AbpLocalizationModule))]
+    typeof(AbpLdapAbstractionsModule),
+    typeof(AbpSettingsModule))]
 public class AbpLdapModule : AbpModule
 {
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
-        Configure<AbpVirtualFileSystemOptions>(options =>
-        {
-            options.FileSets.AddEmbedded<AbpLdapModule>();
-        });
-
-        Configure<AbpLocalizationOptions>(options =>
-        {
-            options.Resources
-                .Add<LdapResource>("en")
-                .AddVirtualJson("/Volo/Abp/Ldap/Localization");
-        });
-    }
+   
 }
