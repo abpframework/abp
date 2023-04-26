c51a48aa144c0a8e1dfbeade224937538df69f8e
maliming <malimings@gmail.com>
2023-04-26 17:42:27 +0800

    Update test-all.ps1

--- build/test-all.ps1
+++ build/test-all.ps1
@@ -7,6 +7,8 @@ $full = $args[0]
 foreach ($solutionPath in $solutionPaths) {    
     $solutionAbsPath = (Join-Path $rootFolder $solutionPath)
     Set-Location $solutionAbsPath
+
+    export NUGET_PACKAGES=/home/.nuget/packages/
     sudo dotnet test --no-build --no-restore --collect:"XPlat Code Coverage"
     if (-Not $?) {
         Write-Host ("Test failed for the solution: " + $solutionPath)
