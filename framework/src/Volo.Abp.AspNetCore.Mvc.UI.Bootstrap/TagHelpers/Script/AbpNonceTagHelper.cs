868ce85c9d564ccd4e81dd6e0370b3a5f691ecc0
Salih <salihozkara00@gmail.com>
2023-05-04 17:48:00 +0300

    Replaced NonceHtmlHelper with AbpNonceTagHelper

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Script/AbpNonceTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Script;
+
+
+[HtmlTargetElement("script", Attributes = "abp-nonce")]
+public class AbpNonceTagHelper : AbpTagHelper
+{
+    [HtmlAttributeNotBound]
+    [ViewContext]
+    public ViewContext ViewContext { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll("abp-nonce");
+        if (ViewContext.HttpContext.Items.TryGetValue(AbpAspNetCoreConsts.ScriptNonceKey, out var nonce) && nonce is string nonceString && !string.IsNullOrEmpty(nonceString))
+        {
+            output.Attributes.Add("nonce", nonceString);
+        }
+    }
+}
