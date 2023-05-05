bc37a62d375183e8bd732c19a6b0228ffa69e4cc
Salih <salihozkara00@gmail.com>
2023-05-05 14:12:48 +0300

    Add AbpSecurityHeaderNonceHelper

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bundling/Volo/Abp/AspNetCore/Mvc/UI/Bundling/TagHelpers/AbpNonceScriptTagHelper.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bundling/Volo/Abp/AspNetCore/Mvc/UI/Bundling/TagHelpers/AbpNonceScriptTagHelper.cs
@@ -1,20 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers;
 
-namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Script;
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bundling.TagHelpers;
 
-
-[HtmlTargetElement("script", Attributes = "abp-nonce")]
-public class AbpNonceTagHelper : AbpTagHelper
+[HtmlTargetElement("script")]
+public class AbpNonceScriptTagHelper : AbpTagHelper
 {
     [HtmlAttributeNotBound]
     [ViewContext]
     public ViewContext ViewContext { get; set; }
-
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
-        output.Attributes.RemoveAll("abp-nonce");
         if (ViewContext.HttpContext.Items.TryGetValue(AbpAspNetCoreConsts.ScriptNonceKey, out var nonce) && nonce is string nonceString && !string.IsNullOrEmpty(nonceString))
         {
             output.Attributes.Add("nonce", nonceString);
