2c3653aff354c8ad85c56dc7f06824361bf5b8d4
Alper Ebiçoğlu <9526587+ebicoglu@users.noreply.github.com>
2023-05-15 14:09:55 +0300

    Update UserConsts.cs

--- modules/blogging/src/Volo.Blogging.Domain.Shared/Volo/Blogging/Users/UserConsts.cs
+++ modules/blogging/src/Volo.Blogging.Domain.Shared/Volo/Blogging/Users/UserConsts.cs
@@ -10,13 +10,13 @@ public class UserConsts
 
     public const int MaxWebSiteLength = 256;
 
-    public const int MaxTwitterLength = 64;
+    public const int MaxTwitterLength = 128;
 
-    public const int MaxGithubLength = 64;
+    public const int MaxGithubLength = 256;
 
-    public const int MaxLinkedinLength = 64;
+    public const int MaxLinkedinLength = 256;
 
-    public const int MaxCompanyLength = 128;
+    public const int MaxCompanyLength = 256;
 
-    public const int MaxJobTitleLength = 32;
+    public const int MaxJobTitleLength = 128;
 }
