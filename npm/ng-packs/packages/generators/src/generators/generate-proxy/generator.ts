656fade54868525259ab5cceb889a26006e02e56
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2023-05-26 16:36:25 +0300

    Create generator project

---
+++ npm/ng-packs/packages/generators/src/generators/generate-proxy/generator.ts
@@ -0,0 +1,17 @@
+import { addProjectConfiguration, formatFiles, generateFiles, Tree } from '@nx/devkit';
+import * as path from 'path';
+import { GenerateProxyGeneratorSchema } from './schema';
+
+export async function generateProxyGenerator(tree: Tree, options: GenerateProxyGeneratorSchema) {
+  const projectRoot = `libs/${options.name}`;
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {},
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  await formatFiles(tree);
+}
+
+export default generateProxyGenerator;
---
+++ npm/ng-packs/packages/generators/src/generators/generate-proxy/schema.d.ts
@@ -0,0 +1,3 @@
+export interface GenerateProxyGeneratorSchema {
+  name: string;
+}
