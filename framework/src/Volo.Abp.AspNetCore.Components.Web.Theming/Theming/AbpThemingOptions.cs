e007b8d68d15dc532181c41083d60a8d1081b5fb
liangshiwei <deawback@hotmail.com>
2023-06-14 16:20:54 +0800

    Add Theme system to Blazor UI

---
+++ framework/src/Volo.Abp.AspNetCore.Components.Web.Theming/Theming/AbpThemingOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Volo.Abp.AspNetCore.Components.Web.Theming.Theming;
+
+public class 
+{
+    public ThemeDictionary Themes { get; }
+
+    public string DefaultThemeName { get; set; }
+
+    public AbpThemingOptions()
+    {
+        Themes = new ThemeDictionary();
+    }
+}
