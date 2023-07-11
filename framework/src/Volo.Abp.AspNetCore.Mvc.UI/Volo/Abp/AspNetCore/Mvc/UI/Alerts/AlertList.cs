841f4f17a9da3101fb47a1a9eede21903af61a48
liangshiwei <deawback@hotmail.com>
2023-07-11 16:39:10 +0800

    Enable nullable annotations for Volo.Abp.AspNetCore.Mvc.UI

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI/Volo/Abp/AspNetCore/Mvc/UI/Alerts/AlertList.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI/Volo/Abp/AspNetCore/Mvc/UI/Alerts/AlertList.cs
@@ -4,27 +4,27 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Alerts;
 
 public class AlertList : List<AlertMessage>
 {
-    public void Add(AlertType type, string text, string title = null, bool dismissible = true)
+    public void Add(AlertType type, string text, string? title = null, bool dismissible = true)
     {
         Add(new AlertMessage(type, text, title, dismissible));
     }
 
-    public void Info(string text, string title = null, bool dismissible = true)
+    public void Info(string text, string? title = null, bool dismissible = true)
     {
         Add(new AlertMessage(AlertType.Info, text, title, dismissible));
     }
 
-    public void Warning(string text, string title = null, bool dismissible = true)
+    public void Warning(string text, string? title = null, bool dismissible = true)
     {
         Add(new AlertMessage(AlertType.Warning, text, title, dismissible));
     }
 
-    public void Danger(string text, string title = null, bool dismissible = true)
+    public void Danger(string text, string? title = null, bool dismissible = true)
     {
         Add(new AlertMessage(AlertType.Danger, text, title, dismissible));
     }
 
-    public void Success(string text, string title = null, bool dismissible = true)
+    public void Success(string text, string? title = null, bool dismissible = true)
     {
         Add(new AlertMessage(AlertType.Success, text, title, dismissible));
     }
