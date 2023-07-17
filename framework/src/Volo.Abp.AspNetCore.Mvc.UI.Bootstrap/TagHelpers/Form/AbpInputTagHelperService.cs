0ffb6f382918c74c498b7624995b63c434ea2d39
masoodkhoshgard <masoodkhoshgard@gmail.com>
2023-07-17 18:31:38 +0330

    Improved display for Floating Labels

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/AbpInputTagHelperService.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bootstrap/TagHelpers/Form/AbpInputTagHelperService.cs
@@ -227,10 +227,11 @@ public class AbpInputTagHelperService : AbpTagHelperService<AbpInputTagHelper>
 
         var attribute = TagHelper.AspFor.ModelExplorer.GetAttribute<Placeholder>();
 
-        if (attribute != null)
+        if (attribute != null || TagHelper.FloatingLabel)
         {
-            var placeholderLocalized = _tagHelperLocalizer.GetLocalizedText(attribute.Value, TagHelper.AspFor.ModelExplorer);
-
+            var placeholderLocalized = _tagHelperLocalizer.GetLocalizedText(attribute?.Value ?? string.Empty, TagHelper.AspFor.ModelExplorer);
+            if (string.IsNullOrWhiteSpace(placeholderLocalized))
+                placeholderLocalized = TagHelper.AspFor.Name;
             inputTagHelperOutput.Attributes.Add("placeholder", placeholderLocalized);
         }
     }
