3916e929ff88d4b02fa81001b354379ba092a4ef
BlueXT <bluextchannel@gmail.com>
2023-07-27 21:42:54 +0300

    Added AbpMapsterModule

---
+++ framework/src/Volo.Abp.Mapster/Volo/Abp/Mapster/AbpMapsterModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+using Volo.Abp.ObjectMapping;
+
+namespace Volo.Abp.Mapster;
+
+[DependsOn(
+    typeof(AbpObjectMappingModule)
+)]
+public class AbpMapsterModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddMapsterObjectMapper();
+    }
+}
