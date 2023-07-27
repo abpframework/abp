ab1a9f6dfb284177f05b22b4e4a2616da63d4681
BlueXT <bluextchannel@gmail.com>
2023-07-27 21:32:43 +0300

    Added MapsterObjectMapper and MapsterAutoObjectMappingProvider

---
+++ framework/src/Volo.Abp.Mapster/Volo/Abp/Mapster/MapsterAutoObjectMappingProvider.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using Volo.Abp.ObjectMapping;
+
+namespace Volo.Abp.Mapster;
+
+public class MapsterAutoObjectMappingProvider : IAutoObjectMappingProvider
+{
+    public TDestination Map<TSource, TDestination>(object source)
+    {
+        return source.Adapt<TDestination>();
+    }
+
+    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+    {
+        return source.Adapt(destination);
+    }
+}
+
+public class MapsterAutoObjectMappingProvider<TContext> : MapsterAutoObjectMappingProvider,
+    IAutoObjectMappingProvider<TContext>
+{
+}
