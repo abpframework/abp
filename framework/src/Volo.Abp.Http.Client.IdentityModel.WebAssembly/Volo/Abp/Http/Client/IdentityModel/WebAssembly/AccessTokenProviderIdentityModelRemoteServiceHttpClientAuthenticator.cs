14b103668b267e96160cac8a7cd831f743a35efa
maliming <malimings@gmail.com>
2023-07-31 16:38:00 +0800

    Add `IAbpAccessTokenProvider`.

--- framework/src/Volo.Abp.Http.Client.IdentityModel.WebAssembly/Volo/Abp/Http/Client/IdentityModel/WebAssembly/AccessTokenProviderIdentityModelRemoteServiceHttpClientAuthenticator.cs
+++ framework/src/Volo.Abp.Http.Client.IdentityModel.WebAssembly/Volo/Abp/Http/Client/IdentityModel/WebAssembly/AccessTokenProviderIdentityModelRemoteServiceHttpClientAuthenticator.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using IdentityModel.Client;
-using JetBrains.Annotations;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Http.Client.Authentication;
 using Volo.Abp.IdentityModel;
@@ -14,22 +10,21 @@ namespace Volo.Abp.Http.Client.IdentityModel.WebAssembly;
 public class AccessTokenProviderIdentityModelRemoteServiceHttpClientAuthenticator
     : IdentityModelRemoteServiceHttpClientAuthenticator
 {
-    [CanBeNull]
-    protected IAccessTokenProvider AccessTokenProvider { get; }
+    protected IAbpAccessTokenProvider AccessTokenProvider { get; }
 
     public AccessTokenProviderIdentityModelRemoteServiceHttpClientAuthenticator(
         IIdentityModelAuthenticationService identityModelAuthenticationService,
-        IServiceProvider serviceProvider)
+        IAbpAccessTokenProvider accessTokenProvider)
         : base(identityModelAuthenticationService)
     {
-        AccessTokenProvider = serviceProvider.GetService<IAccessTokenProvider>();
+        AccessTokenProvider = accessTokenProvider;
     }
 
-    public override async Task Authenticate(RemoteServiceHttpClientAuthenticateContext context)
+    public async override Task Authenticate(RemoteServiceHttpClientAuthenticateContext context)
     {
         if (context.RemoteService.GetUseCurrentAccessToken() != false)
         {
-            var accessToken = await GetAccessTokenFromAccessTokenProviderOrNullAsync();
+            var accessToken = await AccessTokenProvider.GetTokenAsync();
             if (accessToken != null)
             {
                 context.Request.SetBearerToken(accessToken);
@@ -38,22 +33,5 @@ public class AccessTokenProviderIdentityModelRemoteServiceHttpClientAuthenticato
         }
 
         await base.Authenticate(context);
-    }
-
-    protected virtual async Task<string> GetAccessTokenFromAccessTokenProviderOrNullAsync()
-    {
-        if (AccessTokenProvider == null)
-        {
-            return null;
-        }
-
-        var result = await AccessTokenProvider.RequestAccessToken();
-        if (result.Status != AccessTokenResultStatus.Success)
-        {
-            return null;
-        }
-
-        result.TryGetToken(out var token);
-        return token.Value;
     }
 }
