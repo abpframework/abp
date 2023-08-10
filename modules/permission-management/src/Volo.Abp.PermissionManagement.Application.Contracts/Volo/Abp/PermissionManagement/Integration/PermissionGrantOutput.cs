4d33f3592e3ed3e08ffabe43c4ee9b40c6f8c97e
maliming <malimings@gmail.com>
2023-08-10 16:21:09 +0800

    Add `UserId` to `PermissionGrantOutput `.

--- modules/permission-management/src/Volo.Abp.PermissionManagement.Application.Contracts/Volo/Abp/PermissionManagement/Integration/PermissionGrantOutput.cs
+++ modules/permission-management/src/Volo.Abp.PermissionManagement.Application.Contracts/Volo/Abp/PermissionManagement/Integration/PermissionGrantOutput.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace Volo.Abp.PermissionManagement.Integration;
 
 public class PermissionGrantOutput
 {
+    public Guid UserId { get; set; }
+
     public Dictionary<string, bool> Permissions { get; set; }
 }
