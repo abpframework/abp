09006788afb900528f5aa4dd9698462e079b65a8
liangshiwei <deawback@hotmail.com>
2023-08-10 18:06:31 +0800

    Remove duplicate proxy classes

--- modules/identity/src/Volo.Abp.Identity.HttpApi.Client/ClientProxies/Volo/Abp/Identity/IdentityUserLookupClientProxy.cs
+++
@@ -1,7 +0,0 @@
-// This file is part of IdentityUserLookupClientProxy, you can customize it here
-// ReSharper disable once CheckNamespace
-namespace Volo.Abp.Identity;
-
-public partial class IdentityUserLookupClientProxy
-{
-}
