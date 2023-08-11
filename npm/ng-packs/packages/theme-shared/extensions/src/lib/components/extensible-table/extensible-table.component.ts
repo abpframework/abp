5f0fc43ff857b42ad2d7e90702d19f8376094165
Sinan997 <sinanozturk997@gmail.com>
2023-08-11 13:51:19 +0300

    fix

--- npm/ng-packs/packages/theme-shared/extensions/src/lib/components/extensible-table/extensible-table.component.ts
+++ npm/ng-packs/packages/theme-shared/extensions/src/lib/components/extensible-table/extensible-table.component.ts
@@ -150,7 +150,11 @@ export class ExtensibleTableComponent<R = any> implements OnChanges {
 
   ngOnChanges({ data }: SimpleChanges) {
     if (!data?.currentValue) return;
-    this.list.totalCount = this.recordsTotal
+    
+    if (data.currentValue.length < 1) {
+      this.list.totalCount = this.recordsTotal
+    }
+
     this.data = data.currentValue.map((record: any, index: number) => {
       this.propList.forEach(prop => {
         const propData = { getInjected: this.getInjected, record, index } as any;
