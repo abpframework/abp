e8fb08cf38a8244aac906e26c32fc189fd0b2d4a
maliming <malimings@gmail.com>
2023-08-14 14:15:30 +0800

    Regenerate proxy files for all modules.
    Resolve #17349

--- modules/docs/src/Volo.Docs.Admin.HttpApi.Client/ClientProxies/DocumentsAdminClientProxy.cs
+++
@@ -1,7 +0,0 @@
-// This file is part of DocumentsAdminClientProxy, you can customize it here
-// ReSharper disable once CheckNamespace
-namespace Volo.Docs.Admin.ClientProxies;
-
-public partial class DocumentsAdminClientProxy
-{
-}
