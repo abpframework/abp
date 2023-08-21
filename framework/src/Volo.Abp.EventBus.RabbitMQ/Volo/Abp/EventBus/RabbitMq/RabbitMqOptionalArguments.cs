ec326cb42e1bf20f6c7a176985d319d5108efbbd
Miguel Ryan <htve@outlook.com>
2023-08-21 12:00:04 +0800

    Reme reflections for performance reasons in RabbitMqOptionalArguments

--- framework/src/Volo.Abp.EventBus.RabbitMQ/Volo/Abp/EventBus/RabbitMq/RabbitMqOptionalArguments.cs
+++ framework/src/Volo.Abp.EventBus.RabbitMQ/Volo/Abp/EventBus/RabbitMq/RabbitMqOptionalArguments.cs
@@ -1,8 +1,6 @@
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 namespace Volo.Abp.EventBus.RabbitMq;
 
@@ -12,21 +10,18 @@ public class OptionalArguments
     ///     Set the queue in classic or quorum or stream
     ///     (Sets the "x-queue-type" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-queue-type")]
     public string QueueType { get; set; }
 
     /// <summary>
     ///     How long a queue can be unused for before it is automatically deleted (milliseconds).
     ///     (Sets the "x-expires" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-expires")]
     public int? Expires { get; set; }
 
     /// <summary>
     ///     How long a message published to a queue can live before it is discarded (milliseconds).
     ///     (Sets the "x-message-ttl" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-message-ttl")]
     public int? MessageTtl { get; set; }
 
     /// <summary>
@@ -35,7 +30,6 @@ public class OptionalArguments
     ///     drop-head and reject-publish.
     ///     (Sets the "x-overflow" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-overflow")]
     public string Overflow { get; set; }
 
     /// <summary>
@@ -43,14 +37,12 @@ public class OptionalArguments
     ///     consumer in case the active one is cancelled or dies.
     ///     (Sets the "x-single-active-consumer" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-single-active-consumer")]
     public bool? SingleActiveConsumer { get; set; }
 
     /// <summary>
     ///     Optional name of an exchange to which messages will be republished if they are rejected or expire.
     ///     (Sets the "x-dead-letter-exchange" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-dead-letter-exchange")]
     public string DeadLetterExchange { get; set; }
 
     /// <summary>
@@ -58,21 +50,18 @@ public class OptionalArguments
     ///     routing key will be used.
     ///     (Sets the "x-dead-letter-routing-key" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-dead-letter-routing-key")]
     public string DeadLetterRoutingKey { get; set; }
 
     /// <summary>
     ///     How many (ready) messages a queue can contain before it starts to drop them from its head.
     ///     (Sets the "x-max-length" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-max-length")]
     public int? MaxLength { get; set; }
 
     /// <summary>
     ///     Total body size for ready messages a queue can contain before it starts to drop them from its head.
     ///     (Sets the "x-max-length-bytes" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-max-length-bytes")]
     public int? MaxLengthBytes { get; set; }
 
     /// <summary>
@@ -80,7 +69,6 @@ public class OptionalArguments
     ///     priorities.
     ///     (Sets the "x-max-priority" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-max-priority")]
     public int? MaxPriority { get; set; }
 
     /// <summary>
@@ -90,7 +78,6 @@ public class OptionalArguments
     ///     per-queue store for messages that were previously embedded.
     ///     (Sets the "x-queue-version" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-queue-version")]
     public int? QueueVersion { get; set; }
 
     /// <summary>
@@ -98,7 +85,6 @@ public class OptionalArguments
     ///     a cluster of nodes.
     ///     (Sets the "x-queue-master-locator" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-queue-master-locator")]
     public string QueueMasterLocator { get; set; }
 
     /// <summary>
@@ -106,14 +92,12 @@ public class OptionalArguments
     ///     this many times it will be dropped or dead-lettered, depending on the queue configuration.
     ///     (Sets the "x-delivery-limit" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-delivery-limit")]
     public int? DeliveryLimit { get; set; }
 
     /// <summary>
     ///     Set the queue initial cluster size.
     ///     (Sets the "x-quorum-initial-group-size" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-quorum-initial-group-size")]
     public int? QuorumInitialGroupSize { get; set; }
 
     /// <summary>
@@ -122,7 +106,6 @@ public class OptionalArguments
     ///     strategy will fall back to at-most-once.
     ///     (Sets the "x-dead-letter-strategy" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-dead-letter-strategy")]
     public string DeadLetterStrategy { get; set; }
 
     /// <summary>
@@ -130,7 +113,6 @@ public class OptionalArguments
     ///     client-local (default) and balanced.
     ///     (Sets the "x-queue-leader-locator" argument.)
     /// </summary>
-    [ConfigurationKeyName("x-queue-leader-locator")]
     public string QueueLeaderLocator { get; set; }
 
     /// <summary>
@@ -139,38 +121,54 @@ public class OptionalArguments
     ///     E.g. "1h" configures the stream to only keep the last 1 hour of received messages.
     ///     (Sets the x-max-age argument.)
     /// </summary>
-    [ConfigurationKeyName("x-max-age")]
     public string MaxAge { get; set; }
 
     /// <summary>
     ///     Total segment size for stream segments on disk.
     ///     (Sets the x-stream-max-segment-size-bytes argument.)
     /// </summary>
-    [ConfigurationKeyName("x-stream-max-segment-size-bytes")]
     public int? StreamMaxSegmentSizeInBytes { get; set; }
 
     /// <summary>
     ///     Set the queue initial cluster size.
     ///     (Sets the x-initial-cluster-size argument.)
     /// </summary>
-    [ConfigurationKeyName("x-initial-cluster-size")]
     public int? InitialClusterSize { get; set; }
 
     /// <summary>
     ///     If messages to this exchange cannot otherwise be routed, send them to the alternate exchange named here.
     ///     (Sets the "alternate-exchange" argument.)
     /// </summary>
-    [ConfigurationKeyName("alternate-exchange")]
     public string AlternateExchange { get; set; }
 
     public static implicit operator Dictionary<string, object>(OptionalArguments args)
     {
         if (args is null) return null;
 
-        return args.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
-            .Where(x => x.GetCustomAttributes(typeof(ConfigurationKeyNameAttribute), false).Length==1)
-            .Select(x=>new { Key=x.CustomAttributes.Select(a => a.ConstructorArguments[0].Value?.ToString()).First(), Value=x.GetValue(args, null)})
-            .Where(x => x.Value != null && !x.Value.ToString().IsNullOrWhiteSpace())
+        var items = new Dictionary<string, object>
+        {
+            {"x-queue-type", args.QueueType},
+            {"x-expires", args.Expires},
+            {"x-message-ttl", args.MessageTtl},
+            {"x-overflow", args.Overflow},
+            {"x-single-active-consumer", args.SingleActiveConsumer},
+            {"x-dead-letter-exchange", args.DeadLetterExchange},
+            {"x-dead-letter-routing-key", args.DeadLetterRoutingKey},
+            {"x-max-length", args.MaxLength},
+            {"x-max-length-bytes", args.MaxLengthBytes},
+            {"x-max-priority", args.MaxPriority},
+            {"x-queue-version", args.QueueVersion},
+            {"x-queue-master-locator", args.QueueMasterLocator},
+            {"x-delivery-limit", args.DeliveryLimit},
+            {"x-quorum-initial-group-size", args.QuorumInitialGroupSize},
+            {"x-dead-letter-strategy", args.DeadLetterStrategy},
+            {"x-queue-leader-locator", args.QueueLeaderLocator},
+            {"x-max-age", args.MaxAge},
+            {"x-stream-max-segment-size-bytes", args.StreamMaxSegmentSizeInBytes},
+            {"x-initial-cluster-size", args.InitialClusterSize},
+            {"alternate-exchange", args.AlternateExchange}
+        };
+        return items.Where(x => x.Value != null && !x.Value.ToString().IsNullOrWhiteSpace())
             .ToDictionary(x => x.Key, x => x.Value);
     }
 }
