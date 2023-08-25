c0771843e74725df541fc27aae276fe7fad92afc
liangshiwei <deawback@hotmail.com>
2023-08-25 11:00:01 +0800

    Set CurrentTenant Id

--- modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/IdentityUserDelegationManager.cs
+++ modules/identity/src/Volo.Abp.Identity.Domain/Volo/Abp/Identity/IdentityUserDelegationManager.cs
@@ -43,7 +43,8 @@ public class IdentityUserDelegationManager : DomainService
                 sourceUserId,
                 targetUserId,
                 startTime,
-                endTime
+                endTime,
+                CurrentTenant.Id
             ),
             cancellationToken: cancellationToken
         );
