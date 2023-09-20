dd8c3894bf7fcba2196af6d0a8570f284535251f
maliming <malimings@gmail.com>
2023-09-20 11:34:50 +0800

    Update CheckRedisPreRequirements.cs

--- framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectBuilding/Templates/CheckRedisPreRequirements.cs
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectBuilding/Templates/CheckRedisPreRequirements.cs
@@ -9,7 +9,7 @@ public class CheckRedisPreRequirements : ProjectBuildPipelineStep
     public override void Execute(ProjectBuildContext context)
     {
         var modules = context.Files.Where(f => f.Name.EndsWith("Module.cs", StringComparison.OrdinalIgnoreCase));
-        if (modules.Any(module => !module.Content.Contains("Redis:Configuration")))
+        if (modules.Any(module => module.Content.Contains("Redis:Configuration")))
         {
             context.BuildArgs.ExtraProperties["PreRequirements:Redis"] = "true";
         }
