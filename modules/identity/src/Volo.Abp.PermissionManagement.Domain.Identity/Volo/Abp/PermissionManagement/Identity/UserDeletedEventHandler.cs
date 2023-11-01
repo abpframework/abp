bcf65ab18d4b0a26b922dbad28ebaf24f5da53ba
maliming <malimings@gmail.com>
2023-11-01 11:09:44 +0800

    Add `UserDeletedEventHandler`.

---
+++ modules/identity/src/Volo.Abp.PermissionManagement.Domain.Identity/Volo/Abp/PermissionManagement/Identity/UserDeletedEventHandler.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events.Distributed;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.Users;
+
+namespace Volo.Abp.PermissionManagement.Identity;
+
+public class UserDeletedEventHandler :
+    IDistributedEventHandler<EntityDeletedEto<UserEto>>,
+    ITransientDependency
+{
+    protected IPermissionManager PermissionManager { get; }
+
+    public UserDeletedEventHandler(IPermissionManager permissionManager)
+    {
+        PermissionManager = permissionManager;
+    }
+
+    public async Task HandleEventAsync(EntityDeletedEto<UserEto> eventData)
+    {
+        await PermissionManager.DeleteAsync(UserPermissionValueProvider.ProviderName, eventData.Entity.Name);
+    }
+}
