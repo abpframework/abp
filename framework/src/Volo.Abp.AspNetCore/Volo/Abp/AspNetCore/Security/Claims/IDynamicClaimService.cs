7f760d073badfb3c0d05e995b029c10cc14feb43
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-11-02 18:17:13 +0300

    Created draft services

---
+++ framework/src/Volo.Abp.AspNetCore/Volo/Abp/AspNetCore/Security/Claims/IDynamicClaimService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.AspNetCore.Security.Claims;
+
+public interface IDynamicClaimService
+{
+    Task<Claim[]> GetClaimsAsync();
+}
