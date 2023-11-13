dbfd015eccc20d69e758dfbe16b07327717a5072
Salih <salihozkara00@gmail.com>
2023-11-13 15:41:14 +0300

    Update BundleFile.cs

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bundling.Abstractions/Volo/Abp/AspNetCore/Mvc/UI/Bundling/BundleFile.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Bundling.Abstractions/Volo/Abp/AspNetCore/Mvc/UI/Bundling/BundleFile.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Volo.Abp.AspNetCore.Mvc.UI.Bundling;
 
-public class BundleFile
+public class BundleFile : IEquatable<BundleFile>, IComparable<BundleFile>
 {
-    public string FileName { get; set; }
+    public string FileName { get; }
 
-    public bool IsExternalFile { get; set; }
+    public bool IsExternalFile { get; }
 
     public BundleFile(string fileName)
     {
@@ -27,5 +27,60 @@ public class BundleFile
     public static implicit operator BundleFile(string fileName)
     {
         return new BundleFile(fileName);
+    }
+
+    public bool Equals(BundleFile? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return FileName == other.FileName;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((BundleFile)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return FileName.GetHashCode();
+    }
+
+    public int CompareTo(BundleFile? other)
+    {
+        if (ReferenceEquals(this, other))
+        {
+            return 0;
+        }
+
+        if (ReferenceEquals(null, other))
+        {
+            return 1;
+        }
+
+        return string.Compare(FileName, other.FileName, StringComparison.Ordinal);
     }
 }
