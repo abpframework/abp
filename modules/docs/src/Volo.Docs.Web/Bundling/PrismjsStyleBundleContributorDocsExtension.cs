9294a70c73bc01d3b821abcd9b93bf7d667ec52f
Salih <salihozkara00@gmail.com>
2023-11-13 12:19:41 +0300

    Fix replaceOne error

--- modules/docs/src/Volo.Docs.Web/Bundling/PrismjsStyleBundleContributorDocsExtension.cs
+++ modules/docs/src/Volo.Docs.Web/Bundling/PrismjsStyleBundleContributorDocsExtension.cs
@@ -7,10 +7,19 @@ namespace Volo.Docs.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
-            context.Files.ReplaceOne("/libs/prismjs/themes/prism.css","/libs/prismjs/themes/prism-okaidia.css");
+            ReplaceOne(context.Files, "/libs/prismjs/themes/prism.css","/libs/prismjs/themes/prism-okaidia.css");
             context.Files.AddIfNotContains("/libs/prismjs/plugins/line-highlight/prism-line-highlight.css");
             context.Files.AddIfNotContains("/libs/prismjs/plugins/toolbar/prism-toolbar.css");
             context.Files.AddIfNotContains("/libs/prismjs/plugins/diff-highlight/prism-diff-highlight.css");
+        }
+        
+        private static void ReplaceOne(List<BundleFile> files, string oldFile, string newFile)
+        {
+            var index = files.FindIndex(x => x.FileName == oldFile);
+            if (index >= 0)
+            {
+                files[index] = new BundleFile(newFile);
+            }
         }
     }
 }
