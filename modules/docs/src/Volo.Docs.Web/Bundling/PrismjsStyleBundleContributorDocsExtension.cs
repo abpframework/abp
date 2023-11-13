8ab5aacec2dafaacfb336ea655b98ec24edffab3
maliming <malimings@gmail.com>
2023-11-13 21:45:04 +0800

    Update PrismjsStyleBundleContributorDocsExtension.cs

--- modules/docs/src/Volo.Docs.Web/Bundling/PrismjsStyleBundleContributorDocsExtension.cs
+++ modules/docs/src/Volo.Docs.Web/Bundling/PrismjsStyleBundleContributorDocsExtension.cs
@@ -8,11 +8,7 @@ namespace Volo.Docs.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
-            var prismCss = context.Files.FirstOrDefault(x => x.FileName == "/libs/prismjs/themes/prism.css");
-            if (prismCss != null)
-            {
-                prismCss.FileName = "/libs/prismjs/themes/prism-okaidia.css";
-            }
+            context.Files.ReplaceOne(x => x.FileName == "/libs/prismjs/themes/prism.css", "/libs/prismjs/themes/prism-okaidia.css");
             context.Files.AddIfNotContains("/libs/prismjs/plugins/line-highlight/prism-line-highlight.css");
             context.Files.AddIfNotContains("/libs/prismjs/plugins/toolbar/prism-toolbar.css");
             context.Files.AddIfNotContains("/libs/prismjs/plugins/diff-highlight/prism-diff-highlight.css");
