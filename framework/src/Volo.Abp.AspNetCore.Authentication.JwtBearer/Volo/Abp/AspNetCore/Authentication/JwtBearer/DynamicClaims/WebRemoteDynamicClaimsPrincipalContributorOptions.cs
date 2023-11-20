29eac02f0ac7629dc2cabed875c64e92dd0b43b6
maliming <malimings@gmail.com>
2023-11-20 11:08:36 +0800

    Add `IsEnabled ` to `WebRemoteDynamicClaimsPrincipalContributorOptions`.

--- framework/src/Volo.Abp.AspNetCore.Authentication.JwtBearer/Volo/Abp/AspNetCore/Authentication/JwtBearer/DynamicClaims/WebRemoteDynamicClaimsPrincipalContributorOptions.cs
+++ framework/src/Volo.Abp.AspNetCore.Authentication.JwtBearer/Volo/Abp/AspNetCore/Authentication/JwtBearer/DynamicClaims/WebRemoteDynamicClaimsPrincipalContributorOptions.cs
@@ -4,10 +4,13 @@ namespace Volo.Abp.AspNetCore.Authentication.JwtBearer.DynamicClaims;
 
 public class WebRemoteDynamicClaimsPrincipalContributorOptions
 {
+    public bool IsEnabled { get; set; }
+
     public string AuthenticationScheme { get; set; }
 
     public WebRemoteDynamicClaimsPrincipalContributorOptions()
     {
+        IsEnabled = false;
         AuthenticationScheme = JwtBearerDefaults.AuthenticationScheme;
     }
 }
