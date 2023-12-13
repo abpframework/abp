d68d983a09cf60afa1e7fc046e5cf0a788edc021
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2023-12-13 11:30:16 +0300

    demo config for theme-basic on ng-packs

--- npm/ng-packs/apps/dev-app/src/app/app.module.ts
+++ npm/ng-packs/apps/dev-app/src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@abp/ng.core';
 import { registerLocale } from '@abp/ng.core/locale';
 import { InternetConnectionStatusComponent, ThemeSharedModule } from '@abp/ng.theme.shared';
-import { ThemeLeptonXModule } from '@abp/ng.theme.lepton-x';
-import { SideMenuLayoutModule } from '@abp/ng.theme.lepton-x/layouts';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
 import { AbpOAuthModule } from '@abp/ng.oauth';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
 import { FeatureManagementModule } from '@abp/ng.feature-management';
 import { AccountConfigModule } from '@abp/ng.account/config';
-import { AccountLayoutModule } from '@abp/ng.theme.lepton-x/account';
-import { environment } from '../environments/environment';
+ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_ROUTE_PROVIDER } from './route.provider';
+import {ThemeBasicModule} from "@abp/ng.theme.basic";
 
 @NgModule({
   imports: [
@@ -36,10 +34,8 @@ import { APP_ROUTE_PROVIDER } from './route.provider';
     TenantManagementConfigModule.forRoot(),
     FeatureManagementModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
-    ThemeLeptonXModule.forRoot(),
-    SideMenuLayoutModule.forRoot(),
-    AccountLayoutModule.forRoot(),
-    InternetConnectionStatusComponent
+    InternetConnectionStatusComponent,
+    ThemeBasicModule.forRoot()
   ],
   providers: [APP_ROUTE_PROVIDER],
   declarations: [AppComponent],
