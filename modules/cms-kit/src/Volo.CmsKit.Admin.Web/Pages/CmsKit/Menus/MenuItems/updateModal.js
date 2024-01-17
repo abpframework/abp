3a7998f2bd231f6f11b61ae5e07c8f77543d43e7
Enis Necipoglu <iletisim@enisnecipoglu.com>
2024-01-17 11:43:28 +0300

    Update logic in MenuItem UpdateModal

--- modules/cms-kit/src/Volo.CmsKit.Admin.Web/Pages/CmsKit/Menus/MenuItems/updateModal.js
+++ modules/cms-kit/src/Volo.CmsKit.Admin.Web/Pages/CmsKit/Menus/MenuItems/updateModal.js
@@ -7,9 +7,6 @@ $(function () {
             var $pageId = $('#ViewModel_PageId');
             var $url = $('#ViewModel_Url');
             var $displayName = $('#ViewModel_DisplayName');
-            var $pageIdClearButton = $('#url-tab');
-
-            initSelectPageId();
 
             $pageId.on('change', function (params) {
                 $url.prop('disabled', $pageId.val());
@@ -22,22 +19,6 @@ $(function () {
             })
 
             $pageId.trigger('change');
-
-            $pageIdClearButton.click(function () {
-                $pageId.val('');
-                $pageId.trigger('change');
-            });
-
-            function initSelectPageId() {
-                $pageId.data('autocompleteApiUrl', '/api/cms-kit-admin/menu-items/lookup/pages');
-                $pageId.data('autocompleteDisplayProperty', 'title');
-                $pageId.data('autocompleteValueProperty', 'id');
-                $pageId.data('autocompleteItemsProperty', 'items');
-                $pageId.data('autocompleteFilterParamName', 'filter');
-                $pageId.data('autocompleteParentSelector', '#menu-update-modal');
-
-                abp.dom.initializers.initializeAutocompleteSelects($pageId);
-            }
         };
 
         return {
