0dfc313b05c91f9b006524d90290f77d82c00d81
Alex Soh <alex.soh@bertelsmann.de>
2024-03-03 04:57:34 +0800

    Remove more obsolete exception as stated in #19190

--- modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Ratings/EntityCantHaveRatingException.cs
+++ modules/cms-kit/src/Volo.CmsKit.Domain/Volo/CmsKit/Ratings/EntityCantHaveRatingException.cs
@@ -1,15 +1,10 @@
 ﻿using JetBrains.Annotations;
-using System.Runtime.Serialization;
 using Volo.Abp;
 
 namespace Volo.CmsKit.Ratings;
 
 public class EntityCantHaveRatingException : BusinessException
 {
-    public EntityCantHaveRatingException(SerializationInfo serializationInfo, StreamingContext context) : base(serializationInfo, context)
-    {
-    }
-
     public EntityCantHaveRatingException([NotNull] string entityType)
     {
         Code = CmsKitErrorCodes.Ratings.EntityCantHaveRating;
