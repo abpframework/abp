4323e0bdc2038689d284eae4a3db877891443653
Engincan VESKE <43685404+EngincanV@users.noreply.github.com>
2024-03-06 09:45:02 +0300

    remove flag-icon completely.

--- modules/setting-management/app/Volo.Abp.SettingManagement.DemoApp/DemoAppModule.cs
+++ modules/setting-management/app/Volo.Abp.SettingManagement.DemoApp/DemoAppModule.cs
@@ -93,9 +93,9 @@ public class DemoAppModule : AbpModule
             options.Languages.Add(new LanguageInfo("fi", "fi", "Finnish"));
             options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
             options.Languages.Add(new LanguageInfo("ro-RO", "ro-RO", "Română"));
-            options.Languages.Add(new LanguageInfo("hi", "hi", "Hindi", "in"));
-            options.Languages.Add(new LanguageInfo("is", "is", "Icelandic", "is"));
-            options.Languages.Add(new LanguageInfo("it", "it", "Italiano", "it"));
+            options.Languages.Add(new LanguageInfo("hi", "hi", "Hindi"));
+            options.Languages.Add(new LanguageInfo("is", "is", "Icelandic"));
+            options.Languages.Add(new LanguageInfo("it", "it", "Italiano"));
             options.Languages.Add(new LanguageInfo("el", "el", "Ελληνικά"));
         });
 
