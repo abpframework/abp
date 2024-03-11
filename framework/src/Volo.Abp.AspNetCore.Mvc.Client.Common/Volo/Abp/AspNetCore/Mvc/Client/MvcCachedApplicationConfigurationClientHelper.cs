929fc246dbe532b27dcd1ad2a672cf1bbefdaba1
maliming <malimings@gmail.com>
2024-03-11 16:21:34 +0800

    Clear the `ApplicationConfigurationDto`  cache when getting dynamic claims fails.

--- framework/src/Volo.Abp.AspNetCore.Mvc.Client.Common/Volo/Abp/AspNetCore/Mvc/Client/MvcCachedApplicationConfigurationClientHelper.cs
+++ framework/src/Volo.Abp.AspNetCore.Mvc.Client.Common/Volo/Abp/AspNetCore/Mvc/Client/MvcCachedApplicationConfigurationClientHelper.cs
@@ -3,7 +3,7 @@ using Volo.Abp.Users;
 
 namespace Volo.Abp.AspNetCore.Mvc.Client;
 
-internal static class MvcCachedApplicationConfigurationClientHelper
+public static class MvcCachedApplicationConfigurationClientHelper
 {
     public static string CreateCacheKey(ICurrentUser currentUser)
     {
