c49e400c40a9bf2a57a16a96f976921f81dfff91
Enis Necipoglu <info@enisnecipoglu.com>
2024-03-12 15:00:44 +0300

    Update Index.razor.cs

--- templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor.WebApp.Client/Pages/Index.razor.cs
+++ templates/app/aspnet-core/src/MyCompanyName.MyProjectName.Blazor.WebApp.Client/Pages/Index.razor.cs
@@ -4,19 +4,4 @@ namespace MyCompanyName.MyProjectName.Blazor.WebApp.Client.Pages;
 
 public partial class Index
 {
-    protected override void OnInitialized()
-    {
-        Alerts.Warning(
-                "We will have a service interruption between 02:00 AM and 04:00 AM at October 23, 2023!",
-                "Service Interruption");
-    }
-
-    //   protected override void OnAfterRender(bool firstRender)
-    // {
-    //     if (firstRender)
-    //     {
-            
-    //     }
-    //     base.OnAfterRender(firstRender);
-    // }
 }
