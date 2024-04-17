8a5762fbb7a29c51a86a1c4ba1beab698f7465e5
liangshiwei <deawback@hotmail.com>
2024-04-17 15:44:54 +0800

    Add `AbpAuthenticationSessionState` to logout from other tabs

---
+++ framework/src/Volo.Abp.AspNetCore.Components.Web/wwwroot/libs/abp/js/authentication-session-state-listener.js
@@ -0,0 +1,27 @@
+(function () {
+    
+    var sessionKey = 'authentication-session-id';
+    
+    window.addEventListener('storage', function (event) {
+        
+        console.log(event);
+        if (event.key !== sessionKey) {
+            return;
+        }
+
+        var previousSessionId = event.oldValue
+        var sessionId = event.newValue;
+        
+        if(previousSessionId === sessionId) {
+            return;
+        }
+        
+        if(previousSessionId || !sessionId) {
+            abp.utils.removeOidcUser();
+            window.location.reload();
+            return;
+        }
+
+        location.assign('/')
+    });
+}());
