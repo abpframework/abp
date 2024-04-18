f9eb84cbc01cbacc1b0c9c0d252395225c49b3c1
liangshiwei <deawback@hotmail.com>
2024-04-18 15:45:53 +0800

    Update authentication-state-listener.js

--- framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/authentication-state/authentication-state-listener.js
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared/wwwroot/libs/abp/aspnetcore-mvc-ui-theme-shared/authentication-state/authentication-state-listener.js
@@ -1,30 +1,30 @@
 (function () {
-    
+
     const stateKey = 'authentication-state-id';
 
     window.addEventListener('load', function () {
         if (!abp || !abp.currentUser) {
             return;
         }
-        
-        if (!abp.currentUser.isAuthenticated){
+
+        if (!abp.currentUser.isAuthenticated) {
             localStorage.removeItem(stateKey);
-        }else{
+        } else {
             localStorage.setItem(stateKey, abp.currentUser.id);
         }
 
         window.addEventListener('storage', function (event) {
-            
+
             if (event.key !== stateKey || event.oldValue === event.newValue) {
                 return;
             }
-            
-            if(event.oldValue || !event.newValue) {
+
+            if (event.oldValue || !event.newValue) {
                 window.location.reload();
-            }else{
+            } else {
                 location.assign('/')
             }
         });
     });
-    
+
 }());
