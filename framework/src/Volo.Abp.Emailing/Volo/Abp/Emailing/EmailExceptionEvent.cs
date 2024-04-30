762864c3a35a58e757dcce7c9e2e3ba8be004bbe
honurbu <h.onur4377@gmail.com>
2024-04-30 13:04:24 +0300

    Update EmailExceptionEvent.cs

--- framework/src/Volo.Abp.Emailing/Volo/Abp/Emailing/EmailExceptionEvent.cs
+++ framework/src/Volo.Abp.Emailing/Volo/Abp/Emailing/EmailExceptionEvent.cs
@@ -8,9 +8,8 @@ namespace Volo.Abp.Emailing;
 public class EmailExceptionEvent
 {
     public string? Subject { get; set; }
-    public MailAddress? From { get; set; }
-    public MailAddressCollection? To { get; set; }
-    public MailAddress? Sender { get; set; }
+    public string? From { get; set; }
+    public string? To { get; set; }
     public string? MailFailDescription { get; set; }
     public string? Body { get; set; }
     public DateTime Date { get; set; }
