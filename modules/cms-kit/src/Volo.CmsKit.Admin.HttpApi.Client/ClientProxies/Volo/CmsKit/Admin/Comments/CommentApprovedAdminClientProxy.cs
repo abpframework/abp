88936a6fd0ae32ae96128e8f9f4f57105dda598a
Emre <101802979+EmreKendirli@users.noreply.github.com>
2024-05-28 11:07:03 +0300

    HttpApi.Client rebuilt

--- modules/cms-kit/src/Volo.CmsKit.Admin.HttpApi.Client/ClientProxies/Volo/CmsKit/Admin/Comments/CommentApprovedAdminClientProxy.cs
+++
@@ -1,7 +0,0 @@
-// This file is part of CommentApprovedAdminClientProxy, you can customize it here
-// ReSharper disable once CheckNamespace
-namespace Volo.CmsKit.Admin.Comments;
-
-public partial class CommentApprovedAdminClientProxy
-{
-}
