7efe0a4a628a19fa08fbf304538784064dade56f
machonky <martin@macrobert.com>
2024-06-12 11:52:11 +0800

    BackgroundWorkerBase - Whitespace change

--- framework/src/Volo.Abp.BackgroundWorkers/Volo/Abp/BackgroundWorkers/BackgroundWorkerBase.cs
+++ framework/src/Volo.Abp.BackgroundWorkers/Volo/Abp/BackgroundWorkers/BackgroundWorkerBase.cs
@@ -42,7 +42,7 @@ public abstract class BackgroundWorkerBase : IBackgroundWorker
     {
         Logger.LogDebug("Stopped background worker: " + ToString());
         StoppingTokenSource.Cancel();
-        StoppingTokenSource.Dispose();        
+        StoppingTokenSource.Dispose();
         return Task.CompletedTask;
     }
 
