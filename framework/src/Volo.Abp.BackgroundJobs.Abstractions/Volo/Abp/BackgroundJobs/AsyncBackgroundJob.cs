ced1ded293883a79309306c8130b365cc20e46e1
machonky <martin@macrobert.com>
2024-07-20 15:29:09 +0800

    BackgroundJobs - Enhance AsyncBackgroundJob with extra services

--- framework/src/Volo.Abp.BackgroundJobs.Abstractions/Volo/Abp/BackgroundJobs/AsyncBackgroundJob.cs
+++ framework/src/Volo.Abp.BackgroundJobs.Abstractions/Volo/Abp/BackgroundJobs/AsyncBackgroundJob.cs
@@ -7,13 +7,58 @@ namespace Volo.Abp.BackgroundJobs;
 
 public abstract class AsyncBackgroundJob<TArgs> : IAsyncBackgroundJob<TArgs>
 {
-    //TODO: Add UOW, Localization, CancellationTokenProvider and other useful properties..?
+    public IAbpLazyServiceProvider LazyServiceProvider { get; set; } = default!;
 
-    public ILogger<AsyncBackgroundJob<TArgs>> Logger { get; set; }
+    protected IGuidGenerator GuidGenerator => LazyServiceProvider.GetRequiredService<IGuidGenerator>();
+    protected ICurrentTenant CurrentTenant => LazyServiceProvider.GetRequiredService<ICurrentTenant>();
+    protected IClock Clock => LazyServiceProvider.GetRequiredService<IClock>();
+    protected IUnitOfWorkManager UnitOfWorkManager => LazyServiceProvider.GetRequiredService<IUnitOfWorkManager>();
+    protected IUnitOfWork? CurrentUnitOfWork => UnitOfWorkManager?.Current;
+    protected ILoggerFactory LoggerFactory => LazyServiceProvider.GetRequiredService<ILoggerFactory>();
+    protected ILogger Logger => LazyServiceProvider.GetService<ILogger>(provider => LoggerFactory?.CreateLogger(GetType().FullName!) ?? NullLogger.Instance);
+    protected ICancellationTokenProvider CancellationTokenProvider => LazyServiceProvider.GetRequiredService<ICancellationTokenProvider>();
 
-    protected AsyncBackgroundJob()
+    protected IStringLocalizerFactory StringLocalizerFactory => LazyServiceProvider.LazyGetRequiredService<IStringLocalizerFactory>();
+
+    protected IStringLocalizer L
     {
-        Logger = NullLogger<AsyncBackgroundJob<TArgs>>.Instance;
+        get
+        {
+            if (_localizer == null)
+            {
+                _localizer = CreateLocalizer();
+            }
+
+            return _localizer;
+        }
+    }
+    private IStringLocalizer? _localizer;
+
+    protected Type? LocalizationResource
+    {
+        get => _localizationResource;
+        set
+        {
+            _localizationResource = value;
+            _localizer = null;
+        }
+    }
+    private Type? _localizationResource = typeof(DefaultResource);
+
+    protected virtual IStringLocalizer CreateLocalizer()
+    {
+        if (LocalizationResource != null)
+        {
+            return StringLocalizerFactory.Create(LocalizationResource);
+        }
+
+        var localizer = StringLocalizerFactory.CreateDefaultOrNull();
+        if (localizer == null)
+        {
+            throw new AbpException($"Set {nameof(LocalizationResource)} or define the default localization resource type (by configuring the {nameof(AbpLocalizationOptions)}.{nameof(AbpLocalizationOptions.DefaultResourceType)}) to be able to use the {nameof(L)} object!");
+        }
+
+        return localizer;
     }
 
     public abstract Task ExecuteAsync(TArgs args);
--- framework/test/Volo.Abp.BackgroundJobs.Tests/Volo/Abp/BackgroundJobs/MyAsyncJob.cs
+++ framework/test/Volo.Abp.BackgroundJobs.Tests/Volo/Abp/BackgroundJobs/MyAsyncJob.cs
@@ -14,28 +14,17 @@ public class MyAsyncJob : AsyncBackgroundJob<MyAsyncJobArgs>, ISingletonDependen
 
     public Guid? TenantId { get; set; }
 
-    private readonly ICurrentTenant _currentTenant;
-    private readonly ICancellationTokenProvider _cancellationTokenProvider;
-
     public bool Canceled { get; set; }
-
-    public MyAsyncJob(
-        ICurrentTenant currentTenant,
-        ICancellationTokenProvider cancellationTokenProvider)
-    {
-        _currentTenant = currentTenant;
-        _cancellationTokenProvider = cancellationTokenProvider;
-    }
 
     public override Task ExecuteAsync(MyAsyncJobArgs args)
     {
-        if (_cancellationTokenProvider.Token.IsCancellationRequested)
+        if (CancellationTokenProvider.Token.IsCancellationRequested)
         {
             Canceled = true;
         }
 
         ExecutedValues.Add(args.Value);
-        TenantId = _currentTenant.Id;
+        TenantId = CurrentTenant.Id;
         return Task.CompletedTask;
     }
 }
