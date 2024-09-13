a584787522361ab47dda9d681d65fb882da4d220
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2024-09-13 10:49:36 +0300

    add: spinner component for theme-shared

---
+++ npm/ng-packs/packages/theme-shared/src/lib/components/spinner/spinner.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  exportAs: 'spinner',
+  selector: 'abp-spinner',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="d-flex justify-content-center align-items-center m-4">
+      <div class="spinner-border" role="status" id="loading">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  `,
+})
+export class SpinnerComponent {}
