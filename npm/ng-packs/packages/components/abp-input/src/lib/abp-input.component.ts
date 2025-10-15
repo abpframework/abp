ec2ced0ff92e6c7bd1855f72eec64e8f514b2f21
erdemcaygor <erdem.caygor@gmail.com>
2025-10-15 15:56:55 +0300

    reactive form implementation

--- npm/ng-packs/packages/components/abp-input/src/lib/abp-input.component.ts
+++ npm/ng-packs/packages/components/abp-input/src/lib/abp-input.component.ts
@@ -1,8 +1,78 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  forwardRef,
+  inject,
+  OnInit,
+  input
+} from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormBuilder,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { LocalizationPipe } from '@abp/ng.core';
+
+const ABP_INPUT_CONTROL_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => AbpInputComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'abp-input',
-  template: `<input class="abp-input" />`
+  templateUrl: './abp-input.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ReactiveFormsModule, LocalizationPipe],
+  exportAs: 'abpInput',
+  host: {
+    class: 'abp-input'
+  },
+  providers: [
+    ABP_INPUT_CONTROL_VALUE_ACCESSOR
+  ]
 })
-export class AbpInputComponent {}
+export class AbpInputComponent implements OnInit, ControlValueAccessor {
+  label = input.required<string>();
+  type = input<'text' | 'number' | 'password'>('text');
+  readonly formBuilder = inject(FormBuilder);
+  readonly changeDetectorRef = inject(ChangeDetectorRef);
+  readonly destroyRef = inject(DestroyRef);
+  abpInputFormGroup: FormGroup;
+
+  ngOnInit() {
+    this.abpInputFormGroup = this.formBuilder.group({
+      value: ['']
+    })
+
+    this.value.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(val => {
+      this.onChange(val);
+    })
+  }
+
+  writeValue(value: any[]): void {
+    this.value.setValue(value);
+    this.changeDetectorRef.markForCheck();
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  get value(): AbstractControl<any> {
+    return this.abpInputFormGroup.get('value');
+  }
+
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+}
