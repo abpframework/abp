de87bf873621497619fa2c571a4b3b6bff60ebe0
zh-wq <zh-wq@outlook.com>
2025-11-21 18:09:56 +0800

    fix(Settings): resolve deadlock in SettingValueProviderManager by eager initialization

--- framework/src/Volo.Abp.Settings/Volo/Abp/Settings/SettingValueProviderManager.cs
+++ framework/src/Volo.Abp.Settings/Volo/Abp/Settings/SettingValueProviderManager.cs
@@ -9,32 +9,31 @@ namespace Volo.Abp.Settings;
 
 public class SettingValueProviderManager : ISettingValueProviderManager, ISingletonDependency
 {
-    public List<ISettingValueProvider> Providers => _lazyProviders.Value;
+    public List<ISettingValueProvider> Providers { get; }
 
     protected AbpSettingOptions Options { get; }
+
     protected IServiceProvider ServiceProvider { get; }
-    private readonly Lazy<List<ISettingValueProvider>> _lazyProviders;
 
     public SettingValueProviderManager(
         IServiceProvider serviceProvider,
         IOptions<AbpSettingOptions> options)
     {
-
         Options = options.Value;
         ServiceProvider = serviceProvider;
 
-        _lazyProviders = new Lazy<List<ISettingValueProvider>>(GetProviders, true);
+        Providers = GetProviders();
     }
-    
+
     protected virtual List<ISettingValueProvider> GetProviders()
     {
         var providers = Options
             .ValueProviders
             .Select(type => (ServiceProvider.GetRequiredService(type) as ISettingValueProvider)!)
             .ToList();
-        
+
         var multipleProviders = providers.GroupBy(p => p.Name).FirstOrDefault(x => x.Count() > 1);
-        if(multipleProviders != null)
+        if (multipleProviders != null)
         {
             throw new AbpException($"Duplicate setting value provider name detected: {multipleProviders.Key}. Providers:{Environment.NewLine}{multipleProviders.Select(p => p.GetType().FullName!).JoinAsString(Environment.NewLine)}");
         }
