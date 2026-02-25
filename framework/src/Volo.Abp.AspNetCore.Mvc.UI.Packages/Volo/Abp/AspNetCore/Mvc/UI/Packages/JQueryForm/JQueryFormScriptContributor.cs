e7faf0e09d992dde3f493023db38133a1ac1989b
SALİH ÖZKARA <salihozkara00@gmail.com>
2026-02-25 21:12:16 +0300

    Create JQueryFormScriptContributor.cs

---
+++ framework/src/Volo.Abp.AspNetCore.Mvc.UI.Packages/Volo/Abp/AspNetCore/Mvc/UI/Packages/JQueryForm/JQueryFormScriptContributor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.Packages.JQuery;
+using Volo.Abp.Modularity;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Packages.JQueryForm;
+
+[Obsolete("This class is deprecated.")]
+public class JQueryFormScriptContributor : BundleContributor
+{
+    public override void ConfigureBundle(BundleConfigurationContext context)
+    {
+        
+    }
+}
