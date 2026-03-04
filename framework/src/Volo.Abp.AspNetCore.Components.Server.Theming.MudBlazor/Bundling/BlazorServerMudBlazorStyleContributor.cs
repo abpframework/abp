5183038962836e96cc92969c1c0f937b7271d348
maliming <malimings@gmail.com>
2026-03-04 17:31:03 +0800

    Add FontAwesome webfont files for MudBlazor theming

--- framework/src/Volo.Abp.AspNetCore.Components.Server.Theming.MudBlazor/Bundling/BlazorServerMudBlazorStyleContributor.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.Server.Theming.MudBlazor/Bundling/BlazorServerMudBlazorStyleContributor.cs
@@ -7,8 +7,10 @@ public class BlazorServerMudBlazorStyleContributor : BundleContributor
 {
     public override void ConfigureBundle(BundleConfigurationContext context)
     {
+        context.Files.AddIfNotContains("/_content/Volo.Abp.AspNetCore.Components.Server.Theming.MudBlazor/libs/fontawesome/css/all.css");
         context.Files.AddIfNotContains("/_content/MudBlazor/MudBlazor.min.css");
         context.Files.AddIfNotContains("/_content/Volo.Abp.AspNetCore.Components.Web/libs/abp/css/abp.css");
         context.Files.AddIfNotContains("/_content/Volo.Abp.MudBlazorUI/volo.abp.mudblazorui.css");
+        context.Files.AddIfNotContains("/_content/Volo.Abp.AspNetCore.Components.Server.Theming.MudBlazor/libs/flag-icon/css/flag-icon.css");
     }
 }
