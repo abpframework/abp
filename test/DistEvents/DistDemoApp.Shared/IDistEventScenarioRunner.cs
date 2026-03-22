3658bf3a49497e7f31540eaadc5df502cc6f3f8c
maliming <malimings@gmail.com>
2026-03-22 21:28:30 +0800

    Remove `DistDemoApp`.

--- test/DistEvents/DistDemoApp.Shared/IDistEventScenarioRunner.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DistDemoApp;
-
-public interface IDistEventScenarioRunner
-{
-    Task RunAsync(DistEventScenarioProfile profile);
-}
--- test/DistEvents/DistDemoApp.Shared/ProviderScenarioEvent.cs
+++
@@ -1,9 +0,0 @@
-using Volo.Abp.EventBus;
-
-namespace DistDemoApp;
-
-[EventName("DistDemoApp.ProviderScenarioEvent")]
-public class ProviderScenarioEvent
-{
-    public int Value { get; set; }
-}
