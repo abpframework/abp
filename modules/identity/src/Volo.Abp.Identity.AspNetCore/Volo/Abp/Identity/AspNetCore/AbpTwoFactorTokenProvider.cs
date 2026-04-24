14027a9075b1cb04418bed9e11eb24f89037bf8c
maliming <malimings@gmail.com>
2026-04-24 12:33:17 +0800

    address copilot review: treat expiration boundary as inclusive

--- modules/identity/src/Volo.Abp.Identity.AspNetCore/Volo/Abp/Identity/AspNetCore/AbpTwoFactorTokenProvider.cs
+++ modules/identity/src/Volo.Abp.Identity.AspNetCore/Volo/Abp/Identity/AspNetCore/AbpTwoFactorTokenProvider.cs
@@ -93,7 +93,7 @@ public abstract class AbpTwoFactorTokenProvider : IUserTwoFactorTokenProvider<Id
             return false;
         }
 
-        if (ToUnixSeconds(Clock.Now) > expiresAtUnixSeconds)
+        if (ToUnixSeconds(Clock.Now) >= expiresAtUnixSeconds)
         {
             await TryRemoveStoredTokenAsync(manager, user, tokenName);
             return false;
