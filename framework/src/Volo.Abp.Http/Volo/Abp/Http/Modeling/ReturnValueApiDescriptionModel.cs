89772d24335ddc754d1e4b4f81c0c185015aa755
maliming <malimings@gmail.com>
2026-06-16 14:24:04 +0800

    Add ContentTypes and IsRemoteStream to ReturnValueApiDescriptionModel
    Refs abpframework/abp#23732

--- framework/src/Volo.Abp.Http/Volo/Abp/Http/Modeling/ReturnValueApiDescriptionModel.cs
+++ framework/src/Volo.Abp.Http/Volo/Abp/Http/Modeling/ReturnValueApiDescriptionModel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Volo.Abp.Content;
 using Volo.Abp.Reflection;
 using Volo.Abp.Threading;
 
@@ -13,19 +16,50 @@ public class ReturnValueApiDescriptionModel
 
     public string? Summary { get; set; }
 
+    public IList<string>? ContentTypes { get; set; }
+
+    public bool IsRemoteStream { get; set; }
+
     public ReturnValueApiDescriptionModel()
     {
 
     }
 
-    public static ReturnValueApiDescriptionModel Create(Type type)
+    public static ReturnValueApiDescriptionModel Create(Type type, IList<string>? contentTypes = null)
     {
         var unwrappedType = AsyncHelper.UnwrapTask(type);
 
         return new ReturnValueApiDescriptionModel
         {
             Type = TypeHelper.GetFullNameHandlingNullableAndGenerics(unwrappedType),
-            TypeSimple = ApiTypeNameHelper.GetSimpleTypeName(unwrappedType)
+            TypeSimple = ApiTypeNameHelper.GetSimpleTypeName(unwrappedType),
+            ContentTypes = contentTypes,
+            IsRemoteStream = IsRemoteStreamType(unwrappedType)
         };
+    }
+
+    private static bool IsRemoteStreamType(Type type)
+    {
+        if (typeof(IRemoteStreamContent).IsAssignableFrom(type))
+        {
+            return true;
+        }
+
+        if (type.IsArray && type.GetElementType() is { } elementType &&
+            typeof(IRemoteStreamContent).IsAssignableFrom(elementType))
+        {
+            return true;
+        }
+
+        if (typeof(IEnumerable).IsAssignableFrom(type) && type.IsGenericType)
+        {
+            var genericArg = type.GetGenericArguments()[0];
+            if (typeof(IRemoteStreamContent).IsAssignableFrom(genericArg))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
