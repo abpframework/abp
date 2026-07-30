11b78a9e7c830cad6b7a3b01bcbabfc20619787e
maliming <malimings@gmail.com>
2026-07-30 10:05:54 +0800

    Add MySQL type mapping plugin for Guid[] query parameters

--- framework/src/Volo.Abp.EntityFrameworkCore.MySQL/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextMySQLExtensions.cs
+++ framework/src/Volo.Abp.EntityFrameworkCore.MySQL/Volo/Abp/EntityFrameworkCore/AbpDbContextConfigurationContextMySQLExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using System;
 using Volo.Abp.EntityFrameworkCore.DependencyInjection;
+using Volo.Abp.EntityFrameworkCore.MySQL;
 
 namespace Volo.Abp.EntityFrameworkCore;
 
@@ -11,21 +13,21 @@ public static class AbpDbContextConfigurationContextMySQLExtensions
         [NotNull] this AbpDbContextConfigurationContext context,
         Action<MySql.EntityFrameworkCore.Infrastructure.MySQLDbContextOptionsBuilder>? mySQLOptionsAction = null)
     {
-        if (context.ExistingConnection != null)
-        {
-            return context.DbContextOptions.UseMySQL(context.ExistingConnection, optionsBuilder =>
+        var dbContextOptionsBuilder = context.ExistingConnection != null
+            ? context.DbContextOptions.UseMySQL(context.ExistingConnection, optionsBuilder =>
+            {
+                optionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                mySQLOptionsAction?.Invoke(optionsBuilder);
+            })
+            : context.DbContextOptions.UseMySQL(context.ConnectionString, optionsBuilder =>
             {
                 optionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
                 mySQLOptionsAction?.Invoke(optionsBuilder);
             });
-        }
-        else
-        {
-            return context.DbContextOptions.UseMySQL(context.ConnectionString, optionsBuilder =>
-            {
-                optionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
-                mySQLOptionsAction?.Invoke(optionsBuilder);
-            });
-        }
+
+        ((IDbContextOptionsBuilderInfrastructure)dbContextOptionsBuilder)
+            .AddOrUpdateExtension(new AbpMySQLDbContextOptionsExtension());
+
+        return dbContextOptionsBuilder;
     }
 }
